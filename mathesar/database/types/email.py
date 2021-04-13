0455a4975478b19e3354fde17b76ac3a2e1b63e6
Brent Moran <brentmoran@gmail.com>
2021-04-13 20:44:59 +0200

    clean up constant names

--- mathesar/database/types/email.py
+++ mathesar/database/types/email.py
@@ -7,21 +7,16 @@ from mathesar.database.types import constants as c
 
 preparer = create_engine("postgresql://").dialect.identifier_preparer
 
-EMAIL_SPEC_BASE = "email"
-QUOTED_TYPE_NAME = ".".join(
-    [preparer.quote_schema(c.TYPE_SCHEMA), preparer.quote(EMAIL_SPEC_BASE)]
+EMAIL = "email"
+EMAIL_DOMAIN_NAME = EMAIL + "_domain_name"
+EMAIL_LOCAL_PART = EMAIL + "_local_part"
+
+QUALIFIED_EMAIL = ".".join([preparer.quote_schema(c.TYPE_SCHEMA), EMAIL])
+QUALIFIED_EMAIL_DOMAIN_NAME = ".".join(
+    [preparer.quote_schema(c.TYPE_SCHEMA), EMAIL_DOMAIN_NAME]
 )
-EMAIL_DOMAIN_NAME = ".".join(
-    [
-        preparer.quote_schema(c.TYPE_SCHEMA),
-        preparer.quote(EMAIL_SPEC_BASE + "_domain_name")
-    ]
-)
-EMAIL_LOCAL_PART = ".".join(
-    [
-        preparer.quote_schema(c.TYPE_SCHEMA),
-        preparer.quote(EMAIL_SPEC_BASE + "_local_part")
-    ]
+QUALIFIED_EMAIL_LOCAL_PART = ".".join(
+    [preparer.quote_schema(c.TYPE_SCHEMA), EMAIL_LOCAL_PART]
 )
 
 EMAIL_REGEX_STR = (
@@ -31,41 +26,41 @@ EMAIL_REGEX_STR = (
 
 class Email(UserDefinedType):
     def get_col_spec(self, **kw):
-        return QUOTED_TYPE_NAME
+        return QUALIFIED_EMAIL
 
 
 class email_domain_name(GenericFunction):
     type = Text
-    name = quoted_name(EMAIL_DOMAIN_NAME, False)
-    identifier = "email_domain_name"
+    name = quoted_name(QUALIFIED_EMAIL_DOMAIN_NAME, False)
+    identifier = EMAIL_DOMAIN_NAME
 
 
 class email_local_part(GenericFunction):
     type = Text
-    name = quoted_name(EMAIL_LOCAL_PART, False)
-    identifier = "email_local_part"
+    name = quoted_name(QUALIFIED_EMAIL_LOCAL_PART, False)
+    identifier = EMAIL_LOCAL_PART
 
 
 def create_email_type(engine):
     drop_domain_query = f"""
-    DROP DOMAIN IF EXISTS {QUOTED_TYPE_NAME};
+    DROP DOMAIN IF EXISTS {QUALIFIED_EMAIL};
     """
     create_domain_query = f"""
-    CREATE DOMAIN {QUOTED_TYPE_NAME} AS text CHECK (value ~ {EMAIL_REGEX_STR});
+    CREATE DOMAIN {QUALIFIED_EMAIL} AS text CHECK (value ~ {EMAIL_REGEX_STR});
     """
     create_email_domain_name_query = f"""
-    CREATE OR REPLACE FUNCTION {EMAIL_DOMAIN_NAME}({QUOTED_TYPE_NAME})
+    CREATE OR REPLACE FUNCTION {QUALIFIED_EMAIL_DOMAIN_NAME}({QUALIFIED_EMAIL})
     RETURNS text AS $$
         SELECT split_part($1, '@', 2);
     $$
-    LANGUAGE SQL RETURNS NULL ON NULL INPUT;
+    LANGUAGE SQL IMMUTABLE RETURNS NULL ON NULL INPUT;
     """
     create_email_local_part_query = f"""
-    CREATE OR REPLACE FUNCTION {EMAIL_LOCAL_PART}({QUOTED_TYPE_NAME})
+    CREATE OR REPLACE FUNCTION {QUALIFIED_EMAIL_LOCAL_PART}({QUALIFIED_EMAIL})
     RETURNS text AS $$
         SELECT split_part($1, '@', 1);
     $$
-    LANGUAGE SQL RETURNS NULL ON NULL INPUT;
+    LANGUAGE SQL IMMUTABLE RETURNS NULL ON NULL INPUT;
     """
     with engine.connect() as conn:
         conn.execute(text(drop_domain_query))
--- mathesar/database/types/types.py
+++ mathesar/database/types/types.py
@@ -1,5 +1,5 @@
 from mathesar.database.types import email
 
 CUSTOM_TYPE_DICT = {
-    email.QUOTED_TYPE_NAME: email.Email,
+    email.QUALIFIED_EMAIL: email.Email,
 }
