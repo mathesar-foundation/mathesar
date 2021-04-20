5ca0c816bee08a2aeb66c33f9b1a247129bbc726
Brent Moran <brentmoran@gmail.com>
2021-04-20 21:50:18 +0800

    fix flake8 errors

--- mathesar_db/types/base.py
+++ mathesar_db/types/base.py
@@ -6,5 +6,6 @@ SCHEMA = f"{constants.MATHESAR_PREFIX}types"
 # PostgreSQL, we want to use the postgres dialect preparer to set this up.
 preparer = create_engine("postgresql://").dialect.identifier_preparer
 
+
 def get_qualified_name(name):
     return ".".join([preparer.quote_schema(SCHEMA), name])
--- mathesar_db/types/install.py
+++ mathesar_db/types/install.py
@@ -1,5 +1,6 @@
 from mathesar_db.types import base
 from mathesar_db.schemas import create_schema
 
+
 def create_type_schema(engine):
     create_schema(base.SCHEMA, engine)
