0e937a8c416ba59051efdbc1c4550ceb851ed7f5
Brent Moran <brentmoran@gmail.com>
2021-04-21 23:12:44 +0800

    split DBs, add migration to create Mathesar DB

--- db/types/install.py
+++ db/types/install.py
@@ -3,7 +3,7 @@ from db.schemas import create_schema
 
 
 def install_mathesar_on_database(engine):
-    create_schema(engine)
+    create_type_schema(engine)
     email.create_email_type(engine)
 
 
