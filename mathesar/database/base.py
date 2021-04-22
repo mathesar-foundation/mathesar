b4785f3c98d09ecf132f20e0925fc2c298d1472a
Brent Moran <brentmoran@gmail.com>
2021-04-22 20:20:27 +0800

    use new default tables DB for mathesar engines

--- mathesar/database/base.py
+++ mathesar/database/base.py
@@ -1,12 +1,18 @@
 from django.conf import settings
 from db import engine
 
+default_user = settings.DATABASES["mathesar_tables"]["USER"]
+default_password = settings.DATABASES["mathesar_tables"]["PASSWORD"]
+default_host = settings.DATABASES["mathesar_tables"]["HOST"]
+default_tables_db = settings.DATABASES["mathesar_tables"]["NAME"]
 
-def create_mathesar_engine():
+
+def create_mathesar_engine(
+        user=default_user,
+        password=default_password,
+        host=default_host,
+        database=default_tables_db
+):
     return engine.create_future_engine_with_custom_types(
-        settings.DATABASES["default"]["USER"],
-        settings.DATABASES["default"]["PASSWORD"],
-        settings.DATABASES["default"]["HOST"],
-        settings.DATABASES["default"]["NAME"],
-        future=True,
+        user, password, host, database,
     )
