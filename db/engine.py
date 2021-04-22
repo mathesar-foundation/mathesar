45e843c40c02934682ba8746b5d47519f7a93ba0
Brent Moran <brentmoran@gmail.com>
2021-04-23 00:56:28 +0800

    clean up previously extracted function

--- db/engine.py
+++ db/engine.py
@@ -17,13 +17,9 @@ def create_future_engine_with_custom_types(
 def create_future_engine(
         username, password, hostname, database, *args, **kwargs
 ):
-    conn_str = get_postgres_conn_string(username, password, hostname, database)
+    conn_str = f"postgresql://{username}:{password}@{hostname}/{database}"
     kwargs.update(future=True)
     return create_engine(conn_str, *args, **kwargs)
-
-
-def get_postgres_conn_string(username, password, hostname, database):
-    return f"postgresql://{username}:{password}@{hostname}/{database}"
 
 
 def _add_custom_types_to_engine(engine):
