029ce9e84649aa1a55070b994b41832a2d673b21
Brent Moran <brentmoran@gmail.com>
2021-04-23 23:06:49 +0800

    remove non-default DB key assumption from installation

--- install.py
+++ install.py
@@ -6,12 +6,17 @@ from db import install
 
 
 def main():
-    if DATABASES["mathesar_tables"]["HOST"] == "db":
+    for mathesar_tables_key in [key for key in DATABASES if key != "default"]:
+        install_on_db_with_key(mathesar_tables_key)
+
+
+def install_on_db_with_key(mathesar_tables_key):
+    if DATABASES[mathesar_tables_key]["HOST"] == "db":
         # if we're going to install on the docker-created Postgres, we'll
         # create the DB
         print("Creating Mathesar DB on docker-created PostgreSQL instance")
         install.create_mathesar_database(
-            DATABASES["mathesar_tables"]["NAME"],
+            DATABASES[mathesar_tables_key]["NAME"],
             DATABASES["default"]["USER"],
             DATABASES["default"]["PASSWORD"],
             DATABASES["default"]["HOST"],
@@ -22,11 +27,11 @@ def main():
     else:
         # if we're installing anywhere else, we require the DB to exist in
         # advance.
-        username = DATABASES["mathesar_tables"]["USER"]
-        password = DATABASES["mathesar_tables"]["PASSWORD"]
-        host = DATABASES["mathesar_tables"]["HOST"]
-        db_name = DATABASES["mathesar_tables"]["NAME"]
-        port = DATABASES["mathesar_tables"]["PORT"]
+        username = DATABASES[mathesar_tables_key]["USER"]
+        password = DATABASES[mathesar_tables_key]["PASSWORD"]
+        host = DATABASES[mathesar_tables_key]["HOST"]
+        db_name = DATABASES[mathesar_tables_key]["NAME"]
+        port = DATABASES[mathesar_tables_key]["PORT"]
         print("Installing Mathesar DB on preexisting PostgreSQL instance...")
         confirmation = input(
             f"Mathesar will be installed on DB {db_name} at host {host}."
@@ -35,14 +40,14 @@ def main():
         if confirmation.lower() in ["y", "yes"]:
             print("Installing...")
             install.install_mathesar_on_preexisting_database(
-                DATABASES["mathesar_tables"]["USER"],
-                DATABASES["mathesar_tables"]["PASSWORD"],
-                DATABASES["mathesar_tables"]["HOST"],
-                DATABASES["mathesar_tables"]["NAME"],
-                DATABASES["mathesar_tables"]["PORT"],
+                DATABASES[mathesar_tables_key]["USER"],
+                DATABASES[mathesar_tables_key]["PASSWORD"],
+                DATABASES[mathesar_tables_key]["HOST"],
+                DATABASES[mathesar_tables_key]["NAME"],
+                DATABASES[mathesar_tables_key]["PORT"],
             )
         else:
-            print("Canceled.")
+            print("Skipping DB with key {mathesar_tables_key}.")
 
 
 if __name__ == "__main__":
