93f196270338f750d5b245b05315be28320ad22e
Kriti Godey <kriti@kritigodey.com>
2021-04-26 15:49:13 -0400

    Implemented record API via nested viewset; still read-only/non-paginated

--- mathesar/urls.py
+++ mathesar/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework_nested import routers
 
 from mathesar.views import api, frontend
 
@@ -7,6 +7,9 @@ from mathesar.views import api, frontend
 router = routers.DefaultRouter()
 router.register(r'tables', api.TableViewSet)
 router.register(r'schemas', api.SchemaViewSet)
+
+records_router = routers.NestedSimpleRouter(router, r'tables', lookup='table')
+records_router.register(r'records', api.RecordViewSet, basename='table-records')
 
 urlpatterns = [
     path('', frontend.index, name="index"),
@@ -16,4 +19,5 @@ urlpatterns = [
         name='frontend-table-detail',
     ),
     path('api/v0/', include(router.urls)),
+    path('api/v0/', include(records_router.urls)),
 ]
--- mathesar/views/api.py
+++ mathesar/views/api.py
@@ -1,20 +1,22 @@
 from rest_framework import viewsets
-from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from mathesar.models import Table, Schema
 from mathesar.serializers import TableSerializer, SchemaSerializer, RecordSerializer
 
 
+class RecordViewSet(viewsets.GenericViewSet):
+    queryset = Table.objects.all().order_by('-created_at')
+
+    def list(self, request, table_pk=None):
+        table = self.queryset.get(id=table_pk)
+        serializer = RecordSerializer(table.sa_records, many=True)
+        return Response(serializer.data)
+
+
 class TableViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Table.objects.all().order_by('-created_at')
     serializer_class = TableSerializer
-
-    @action(detail=True)
-    def records(self, request, pk=None):
-        table = self.get_object()
-        serializer = RecordSerializer(table.sa_records, many=True)
-        return Response(serializer.data)
 
 
 class SchemaViewSet(viewsets.ReadOnlyModelViewSet):
