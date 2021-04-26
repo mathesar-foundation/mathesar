17c6f8db070c1b3620b0442fb7514fe49eb060e7
Kriti Godey <kriti@kritigodey.com>
2021-04-26 17:18:20 -0400

    Removed unused imports.

--- mathesar/pagination.py
+++ mathesar/pagination.py
@@ -1,5 +1,4 @@
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.response import Response
 
 
 class DefaultLimitOffsetPagination(LimitOffsetPagination):
--- mathesar/views/api.py
+++ mathesar/views/api.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
 
 from mathesar.models import Table, Schema
 from mathesar.pagination import DefaultLimitOffsetPagination, TableLimitOffsetPagination
