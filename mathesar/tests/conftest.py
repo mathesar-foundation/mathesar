cea36f202b1feebe9e9b7bfde30c3c7e706ce071
Kriti Godey <kriti@kritigodey.com>
2021-04-28 13:37:24 -0400

    Created separate test settings file that includes the test DB.

--- mathesar/tests/conftest.py
+++ mathesar/tests/conftest.py
@@ -3,25 +3,8 @@ This inherits the fixtures in the root conftest.py
 """
 
 import pytest
-from config.settings import DATABASES
-
-
-TEST_DB = 'mathesar_db_test_database'
 
 
 @pytest.fixture(autouse=True)
 def enable_db_access_for_all_tests(db):
     pass
-
-
-@pytest.fixture(autouse=True)
-def add_test_db_to_databases(settings):
-    databases = DATABASES
-    databases[TEST_DB] = {
-        'USER': DATABASES['default']['USER'],
-        'PASSWORD': DATABASES['default']['PASSWORD'],
-        'HOST': DATABASES['default']['HOST'],
-        'PORT': DATABASES['default']['PORT'],
-        'NAME': TEST_DB
-    }
-    settings.DATABASES = databases
