164154d7e6cb6829574852d8f1d756cf11e703f1
Kriti Godey <kriti@kritigodey.com>
2021-04-29 15:06:55 -0400

    Added tests for record API pagination

--- mathesar/tests/views/api/test_record_api.py
+++ mathesar/tests/views/api/test_record_api.py
@@ -58,3 +58,45 @@ def test_record_list(create_table, client):
     assert len(response_data['results']) == 25
     for column_name in table.sa_column_names:
         assert column_name in record_data
+
+
+def test_record_list_pagination_limit(create_table, client):
+    table_name = 'Fairfax County Record List Pagination Limit'
+    create_table(table_name)
+    table = Table.objects.get(name=table_name)
+
+    response = client.get(f'/api/v0/tables/{table.id}/records/?limit=5')
+    response_data = response.json()
+    record_data = response_data['results'][0]
+
+    assert response.status_code == 200
+    assert response_data['count'] == 25
+    assert len(response_data['results']) == 5
+    for column_name in table.sa_column_names:
+        assert column_name in record_data
+
+
+def test_record_list_pagination_offset(create_table, client):
+    table_name = 'Fairfax County Record List Pagination Offset'
+    create_table(table_name)
+    table = Table.objects.get(name=table_name)
+
+    response_1 = client.get(f'/api/v0/tables/{table.id}/records/?limit=5&offset=5')
+    response_1_data = response_1.json()
+    record_1_data = response_1_data['results'][0]
+    response_2 = client.get(f'/api/v0/tables/{table.id}/records/?limit=5&offset=10')
+    response_2_data = response_2.json()
+    record_2_data = response_2_data['results'][0]
+
+    assert response_1.status_code == 200
+    assert response_2.status_code == 200
+    assert response_1_data['count'] == 25
+    assert response_2_data['count'] == 25
+    assert len(response_1_data['results']) == 5
+    assert len(response_2_data['results']) == 5
+
+    assert record_1_data['mathesar_id'] != record_2_data['mathesar_id']
+    assert record_1_data['X'] != record_2_data['X']
+    assert record_1_data['Y'] != record_2_data['Y']
+    assert record_1_data['OBJECTID'] != record_2_data['OBJECTID']
+    assert record_1_data['DESCRIPTION'] != record_2_data['DESCRIPTION']
