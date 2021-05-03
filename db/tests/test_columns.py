19d5009f0499221a5608860ccc4db0a0b552bc7b
Brent Moran <brentmoran@gmail.com>
2021-05-03 23:40:29 +0800

    parameterize to check both init methods

--- db/tests/test_columns.py
+++ db/tests/test_columns.py
@@ -1,91 +1,124 @@
+import pytest
 from sqlalchemy import String, Integer, Column
 from db import columns
 
 
-def test_MC_inits_with_name():
+def init_column(*args, **kwargs):
+    return columns.MathesarColumn(*args, **kwargs)
+
+
+def from_column_column(*args, **kwargs):
+    """
+    This creates a condition to check that the MathesarColumn.from_column
+    class method returns the same value of the original __init__ for a given
+    input.
+    """
+    col = columns.MathesarColumn(*args, **kwargs)
+    return columns.MathesarColumn.from_column(col)
+
+
+column_builder_list = [init_column, from_column_column]
+
+
+@pytest.mark.parametrize("column_builder", column_builder_list)
+def test_MC_inits_with_name(column_builder):
     name = "mycol"
-    c = columns.MathesarColumn(name, String)
-    assert c.name == name
+    col = column_builder(name, String)
+    assert col.name == name
 
 
-def test_MC_inits_with_sa_type():
+@pytest.mark.parametrize("column_builder", column_builder_list)
+def test_MC_inits_with_sa_type(column_builder):
     sa_type = String
-    c = columns.MathesarColumn("anycol", sa_type)
-    actual_cls = c.type.__class__
+    col = column_builder("anycol", sa_type)
+    actual_cls = col.type.__class__
     assert actual_cls == sa_type
 
 
-def test_MC_inits_default_not_pk():
-    c = columns.MathesarColumn("anycol", String)
-    assert not c.primary_key
+@pytest.mark.parametrize("column_builder", column_builder_list)
+def test_MC_inits_default_not_pk(column_builder):
+    col = column_builder("anycol", String)
+    assert not col.primary_key
 
 
-def test_MC_inits_with_pk_true():
-    c = columns.MathesarColumn("anycol", String, primary_key=True)
-    assert c.primary_key
+@pytest.mark.parametrize("column_builder", column_builder_list)
+def test_MC_inits_with_pk_true(column_builder):
+    col = column_builder("anycol", String, primary_key=True)
+    assert col.primary_key
 
 
-def test_MC_inits_default_nullable():
-    c = columns.MathesarColumn("a_col", String)
-    assert c.nullable
+@pytest.mark.parametrize("column_builder", column_builder_list)
+def test_MC_inits_default_nullable(column_builder):
+    col = column_builder("a_col", String)
+    assert col.nullable
 
 
-def test_MC_inits_with_nullable_false():
-    c = columns.MathesarColumn("a_col", String, nullable=False)
-    assert not c.nullable
+@pytest.mark.parametrize("column_builder", column_builder_list)
+def test_MC_inits_with_nullable_false(column_builder):
+    col = column_builder("a_col", String, nullable=False)
+    assert not col.nullable
 
 
 def test_MC_is_default_when_true():
     for default_col in columns.DEFAULT_COLUMNS:
         dc_definition = columns.DEFAULT_COLUMNS[default_col]
-        c = columns.MathesarColumn(
+        col = columns.MathesarColumn(
             default_col,
             dc_definition["type"],
             primary_key=dc_definition.get("primary_key", False),
             nullable=dc_definition.get("nullable", True),
         )
-        assert c.is_default
+        assert col.is_default
 
 
 def test_MC_is_default_when_false_for_name():
     for default_col in columns.DEFAULT_COLUMNS:
         dc_definition = columns.DEFAULT_COLUMNS[default_col]
-        c = columns.MathesarColumn(
+        col = columns.MathesarColumn(
             "definitely_not_a_default",
             dc_definition["type"],
             primary_key=dc_definition.get("primary_key", False),
             nullable=dc_definition.get("nullable", True),
         )
-        assert not c.is_default
+        assert not col.is_default
 
 
 def test_MC_is_default_when_false_for_type():
     for default_col in columns.DEFAULT_COLUMNS:
         dc_definition = columns.DEFAULT_COLUMNS[default_col]
         changed_type = Integer if dc_definition["type"] == String else String
-        c = columns.MathesarColumn(
+        col = columns.MathesarColumn(
             default_col,
             changed_type,
             primary_key=dc_definition.get("primary_key", False),
             nullable=dc_definition.get("nullable", True),
         )
-        assert not c.is_default
+        assert not col.is_default
 
 
 def test_MC_is_default_when_false_for_pk():
     for default_col in columns.DEFAULT_COLUMNS:
         dc_definition = columns.DEFAULT_COLUMNS[default_col]
         not_pk = not dc_definition.get("primary_key", False),
-        c = columns.MathesarColumn(
+        col = columns.MathesarColumn(
             default_col,
             dc_definition["type"],
             primary_key=not_pk,
             nullable=dc_definition.get("nullable", True),
         )
-        assert not c.is_default
+        assert not col.is_default
 
 
-def test_MC_from_column():
-    orig_col = Column("testcol", String)
-    new_col = columns.MathesarColumn.from_column(orig_col)
-    assert new_col.name == orig_col.name and new_col.type == orig_col.type
+# def test_MC_from_column():
+#     orig_col = Column("testcol", String)
+#     new_col = columns.MathesarColumn.from_column(orig_col)
+#     assert new_col.name == orig_col.name and new_col.type == orig_col.type
+#
+#
+# def test_self_globals():
+#     print("\n\n\n\n\n ")
+#     global_dict = globals().copy()
+#     for g in global_dict:
+#         if g[:4] == "test":
+#             print(g, global_dict[g])
+#     print("\n\n\n\n\n")
