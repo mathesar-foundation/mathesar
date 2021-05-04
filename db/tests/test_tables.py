802ab40bafa4d47d4c8c888b2ae4acc7177a5fe9
Brent Moran <brentmoran@gmail.com>
2021-05-04 23:09:42 +0800

    add rich test data and fixture

--- db/tests/test_tables.py
+++ db/tests/test_tables.py
@@ -1,6 +1,12 @@
+import os
 import pytest
 from sqlalchemy import text
 from db import tables
+
+FILE_DIR = os.path.abspath(os.path.dirname(__file__))
+RESOURCES = os.path.join(FILE_DIR, "resources")
+ROSTER_SQL = os.path.join(RESOURCES, "roster_create.sql")
+ROSTER_NAME = "Roster"
 
 
 @pytest.fixture
@@ -11,6 +17,15 @@ def engine_with_schema(engine):
     yield engine, app_schema
     with engine.begin() as conn:
         conn.execute(text(f"DROP SCHEMA {app_schema} CASCADE;"))
+
+
+@pytest.fixture
+def engine_with_roster(engine_with_schema):
+    engine, schema = engine_with_schema
+    with engine.begin() as conn, open(ROSTER_SQL) as f:
+        conn.execute(text(f"SET search_path={schema}"))
+        conn.execute(text(f.read()))
+    return engine, schema
 
 
 def test_table_creation_doesnt_reuse_defaults(engine_with_schema):
