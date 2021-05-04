2939db0cc8b64afe630be8a0a14243e80ad680e4
Brent Moran <brentmoran@gmail.com>
2021-05-05 01:07:15 +0800

    add basic tests for column extraction

--- db/tests/test_tables.py
+++ db/tests/test_tables.py
@@ -1,7 +1,7 @@
 import os
 import pytest
-from sqlalchemy import text
-from db import tables
+from sqlalchemy import text, MetaData
+from db import tables, constants
 
 FILE_DIR = os.path.abspath(os.path.dirname(__file__))
 RESOURCES = os.path.join(FILE_DIR, "resources")
@@ -39,3 +39,91 @@ def test_table_creation_doesnt_reuse_defaults(engine_with_schema):
             for c1, c2 in zip(t1.columns, t2.columns)
         ]
     )
+
+
+def test_extract_columns_from_table_creates_tables(engine_with_roster):
+    engine, schema = engine_with_roster
+    teachers = "Teachers"
+    roster_no_teachers = "Roster without Teachers"
+    tables.extract_columns_from_table(
+        "Roster",
+        ["Teacher", "Teacher Email"],
+        teachers,
+        roster_no_teachers,
+        schema,
+        engine,
+    )
+    metadata = MetaData(bind=engine, schema=schema)
+    metadata.reflect()
+    t_dict = metadata.tables
+    assert (
+        f"{schema}.{teachers}" in t_dict
+        and f"{schema}.{roster_no_teachers}" in t_dict
+    )
+
+
+def test_extract_columns_from_table_sets_up_one_fkey(engine_with_roster):
+    engine, schema = engine_with_roster
+    teachers_name = "Teachers"
+    roster_no_teachers_name = "Roster without Teachers"
+    tables.extract_columns_from_table(
+        "Roster",
+        ["Teacher", "Teacher Email"],
+        teachers_name,
+        roster_no_teachers_name,
+        schema,
+        engine,
+    )
+    metadata = MetaData(bind=engine, schema=schema)
+    metadata.reflect()
+    roster_no_teachers = metadata.tables[f"{schema}.{roster_no_teachers_name}"]
+    fkeys = list(roster_no_teachers.foreign_keys)
+    assert len(fkeys) == 1
+
+
+def test_extract_columns_from_table_sets_correct_reference(engine_with_roster):
+    engine, schema = engine_with_roster
+    teachers_name = "Teachers"
+    roster_no_teachers_name = "Roster without Teachers"
+    tables.extract_columns_from_table(
+        "Roster",
+        ["Teacher", "Teacher Email"],
+        teachers_name,
+        roster_no_teachers_name,
+        schema,
+        engine,
+    )
+    metadata = MetaData(bind=engine, schema=schema)
+    metadata.reflect()
+    teachers = metadata.tables[f"{schema}.{teachers_name}"]
+    roster_no_teachers = metadata.tables[f"{schema}.{roster_no_teachers_name}"]
+    fkeys = list(roster_no_teachers.foreign_keys)
+    assert fkeys[0].references(teachers)
+    expect_referenced_column = teachers.columns[constants.ID]
+    actual_referenced_column = fkeys[0].column
+    assert expect_referenced_column == actual_referenced_column
+
+
+def test_extract_columns_from_table_sets_correct_fkey(engine_with_roster):
+    engine, schema = engine_with_roster
+    teachers_name = "Teachers"
+    roster_no_teachers_name = "Roster without Teachers"
+    tables.extract_columns_from_table(
+        "Roster",
+        ["Teacher", "Teacher Email"],
+        teachers_name,
+        roster_no_teachers_name,
+        schema,
+        engine,
+    )
+    metadata = MetaData(bind=engine, schema=schema)
+    metadata.reflect()
+    teachers = metadata.tables[f"{schema}.{teachers_name}"]
+    roster_no_teachers = metadata.tables[f"{schema}.{roster_no_teachers_name}"]
+    fkeys = list(roster_no_teachers.foreign_keys)
+    assert fkeys[0].references(teachers)
+    expect_fkey_column = roster_no_teachers.columns[
+        f"{teachers_name}_{constants.ID}"
+    ]
+    actual_fkey_column = fkeys[0].parent
+    assert expect_fkey_column == actual_fkey_column
