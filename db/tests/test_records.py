ec27e9f62a00d1661d1b424071151b3a211289f4
Brent Moran <brentmoran@gmail.com>
2021-05-24 22:11:05 +0800

    add tests for new get_record functionality

--- db/tests/test_records.py
+++ db/tests/test_records.py
@@ -30,3 +30,53 @@ def test_get_records_gets_limited_offset_records(roster_table_obj):
     base_records = records.get_records(roster, engine, limit=10)
     offset_records = records.get_records(roster, engine, limit=10, offset=5)
     assert len(offset_records) == 10 and offset_records[0] == base_records[5]
+
+
+def test_get_records_gets_ordered_records_str_col_name(roster_table_obj):
+    roster, engine = roster_table_obj
+    record_list = records.get_records(roster, engine, order_by=["Teacher"])
+    assert record_list[0][4] == "Amber Hudson"
+
+
+def test_get_records_gets_ordered_records_num_col(roster_table_obj):
+    roster, engine = roster_table_obj
+    record_list = records.get_records(roster, engine, order_by=["Grade"])
+    assert record_list[0][7] == 25
+
+
+def test_get_records_gets_ordered_records_str_col_obj(roster_table_obj):
+    roster, engine = roster_table_obj
+    teacher_col = roster.columns["Teacher"]
+    record_list = records.get_records(roster, engine, order_by=[teacher_col])
+    assert record_list[0][4] == "Amber Hudson"
+
+
+def test_get_records_gets_ordered_records_num_col_obj(roster_table_obj):
+    roster, engine = roster_table_obj
+    grade_col = roster.columns["Grade"]
+    record_list = records.get_records(roster, engine, order_by=[grade_col])
+    assert record_list[0][7] == 25
+
+
+def test_get_records_ordered_col_set(roster_table_obj):
+    roster, engine = roster_table_obj
+    record_list = records.get_records(
+        roster, engine, order_by=["Student Name", "Grade"]
+    )
+    assert record_list[0][2] == "Alejandro Lam" and record_list[0][7] == 40
+
+
+def test_get_records_ordered_col_set_different_col_order(roster_table_obj):
+    roster, engine = roster_table_obj
+    record_list = records.get_records(
+        roster, engine, order_by=["Grade", "Student Name"]
+    )
+    assert record_list[0][7] == 25 and record_list[0][2] == "Amy Gamble"
+
+
+def test_get_records_orders_before_limiting(roster_table_obj):
+    roster, engine = roster_table_obj
+    record_list = records.get_records(
+        roster, engine, limit=1, order_by=["Grade", "Student Name"]
+    )
+    assert record_list[0][7] == 25 and record_list[0][2] == "Amy Gamble"
