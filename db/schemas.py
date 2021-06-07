21e7c8555858ab2a6a6af9419ab1af22c3580a6f
Brent Moran <brentmoran@gmail.com>
2021-06-07 22:06:43 +0800

    add logic to get all schemas with their OIDs

--- db/schemas.py
+++ db/schemas.py
@@ -1,21 +1,40 @@
 from sqlalchemy.schema import CreateSchema
-from sqlalchemy import inspect
+from sqlalchemy import inspect, MetaData, select, and_, not_, Table
 
 from db import types
 
 TYPES_SCHEMA = types.base.SCHEMA
 
+EXCLUDED_SCHEMATA = [TYPES_SCHEMA, "information_schema"]
+
 
 def get_mathesar_schemas(engine):
-    return [
-        schema
-        for schema in get_all_schemas(engine)
-        if schema not in [TYPES_SCHEMA, "information_schema"]
-    ]
+    return [schema for schema, _ in get_mathesar_schemas_with_oids(engine)]
+
+
+def get_mathesar_schemas_with_oids(engine):
+    metadata = MetaData()
+    pg_namespace = Table("pg_namespace", metadata, autoload_with=engine)
+    sel = (
+        select(pg_namespace.c.nspname.label('schema'), pg_namespace.c.oid)
+        .where(
+            and_(
+                *[pg_namespace.c.nspname != schema for schema in EXCLUDED_SCHEMATA],
+                not_(pg_namespace.c.nspname.like("pg_%"))
+            )
+        )
+    )
+    print(sel)
+    with engine.begin() as conn:
+        result = conn.execute(sel).fetchall()
+    return result
+
 
 
 def get_all_schemas(engine):
     inspector = inspect(engine)
+    # We don't need to exclude system schemas (i.e., starting with "pg_")
+    # since Inspector.get_schema_names already excludes them.
     return inspector.get_schema_names()
 
 
--- db/tests/test_schemas.py
+++ db/tests/test_schemas.py
@@ -1,27 +1,11 @@
+from unittest.mock import call, patch
 from sqlalchemy import create_engine
 from db import schemas
 from db import types
 
 
-def test_get_mathesar_schemas_gets_public(monkeypatch):
+def test_get_mathesar_schemas():
     engine = create_engine("postgresql://")
-    monkeypatch.setattr(
-        schemas,
-        "get_all_schemas",
-        lambda x: ["public"]
-    )
-    actual_schemas = schemas.get_mathesar_schemas(engine)
-    expect_schemas = ["public"]
-    assert actual_schemas == expect_schemas
-
-
-def test_get_mathesar_schemas_avoids_utility_schemata(monkeypatch):
-    engine = create_engine("postgresql://")
-    monkeypatch.setattr(
-        schemas,
-        "get_all_schemas",
-        lambda x: ["public", "information_schema", types.base.SCHEMA]
-    )
-    actual_schemas = schemas.get_mathesar_schemas(engine)
-    expect_schemas = ["public"]
-    assert actual_schemas == expect_schemas
+    with patch.object(schemas, "get_mathesar_schemas_with_oids") as mock_schemas:
+        schemas.get_mathesar_schemas(engine)
+    mock_schemas.assert_called_once_with(engine)
