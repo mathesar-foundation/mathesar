c5e2c30ce3403a5ce575299156ab1fffc0e1b16b
Brent Moran <brentmoran@gmail.com>
2021-06-08 14:14:57 +0800

    add schema reflection functions

--- db/schemas.py
+++ db/schemas.py
@@ -1,11 +1,40 @@
+import logging
 from sqlalchemy.schema import CreateSchema
-from sqlalchemy import inspect, MetaData, select, and_, not_, Table
+from sqlalchemy import inspect, MetaData, select, and_, not_, or_, Table
 
 from db import types
+
+logger = logging.getLogger(__name__)
 
 TYPES_SCHEMA = types.base.SCHEMA
 
 EXCLUDED_SCHEMATA = [TYPES_SCHEMA, "information_schema"]
+
+
+def get_schema_name_from_oid(oid, engine):
+    return reflect_schema(engine, oid=oid)["name"]
+
+
+def get_schema_oid_from_name(name, engine):
+    return reflect_schema(engine, name=name)["oid"]
+
+
+def reflect_schema(engine, name=None, oid=None):
+    # If we have both arguments, the behavior is undefined.
+    try:
+        assert name is None or oid is None
+    except Exception as e:
+        logger.error("ERROR:  Only one of 'name' or 'oid' can be given!")
+        raise e
+    metadata = MetaData()
+    pg_namespace = Table("pg_namespace", metadata, autoload_with=engine)
+    sel = (
+        select(pg_namespace.c.oid, pg_namespace.c.nspname.label("name"))
+        .where(or_(pg_namespace.c.nspname == name, pg_namespace.c.oid == oid))
+    )
+    with engine.begin() as conn:
+        schema_info = conn.execute(sel).fetchone()
+    return schema_info
 
 
 def get_mathesar_schemas(engine):
