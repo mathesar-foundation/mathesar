65c9b8ca9f124867c859b0cc944d73bf110b80bc
pavish <pavishkumar.r@gmail.com>
2021-06-08 20:22:56 +0530

    Handle redirection when database is not specified/incorrect in url

--- mathesar/views/frontend.py
+++ mathesar/views/frontend.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from mathesar.forms.forms import UploadFileForm
 from mathesar.imports.csv import legacy_create_table_from_csv
@@ -8,12 +8,9 @@ from mathesar.serializers import SchemaSerializer
 from mathesar.database.utils import get_non_default_database_keys
 
 
-def get_common_data(request):
+def get_schemas(request):
     schema_serializer = SchemaSerializer(Schema.objects.all(), many=True, context={'request': request})
-    return {
-        "schemas": schema_serializer.data,
-        "databases": get_non_default_database_keys(),
-    }
+    return schema_serializer.data
 
 
 def index(request, **kwargs):
@@ -29,10 +26,17 @@ def index(request, **kwargs):
                 csv_file=request.FILES["file"]
             )
             return JsonResponse({"pk": table.id}, status=200)
+    requested_db = kwargs.get('dbname')
+    database_list = get_non_default_database_keys()
+    if (requested_db is None or requested_db not in database_list) and len(database_list) > 0:
+        return redirect('index', dbname=database_list[0])
     return render(
         request,
         "mathesar/index.html",
         {
-            "common_data": get_common_data(request),
+            "common_data": {
+                "schemas": get_schemas(request),
+                "databases": database_list
+            }
         }
     )
