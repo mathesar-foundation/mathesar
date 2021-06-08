a066009357d1200f399062b5910e5bc07aa8879c
Brent Moran <brentmoran@gmail.com>
2021-06-08 11:33:59 +0800

    add more tests for schema reflection

--- db/tests/test_schemas.py
+++ db/tests/test_schemas.py
@@ -1,5 +1,5 @@
 from unittest.mock import patch
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select, Table, MetaData
 from db import schemas
 from db import types
 
@@ -32,4 +32,16 @@ def test_get_mathesar_schemas_with_oids_avoids_information_schema(engine_with_sc
 def test_get_mathesar_schemas_with_oids_avoids_types_schema(engine_with_schema):
     engine, schema = engine_with_schema
     actual_schemas = schemas.get_mathesar_schemas_with_oids(engine)
-    assert all([schema != types.base.schema for schema, oid in actual_schemas])
+    assert all([schema != types.base.SCHEMA for schema, oid in actual_schemas])
+
+
+def test_get_mathesar_schemas_with_oids_gets_correct_oid(engine_with_schema):
+    engine, schema = engine_with_schema
+    metadata = MetaData()
+    pg_namespace = Table("pg_namespace", metadata, autoload_with=engine)
+    sel = select(pg_namespace.c.oid).where(pg_namespace.c.nspname == schema)
+    with engine.begin() as conn:
+        expect_oid = conn.execute(sel).fetchone()[0]
+    actual_schemata = schemas.get_mathesar_schemas_with_oids(engine)
+    actual_oid = [oid for schm, oid in actual_schemata if schm == schema][0]
+    assert actual_oid == expect_oid
