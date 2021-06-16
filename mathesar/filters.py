a43c6b1d905fb240397a6921e0b2103f6e20f059
Eamon Ito-Fisher <eamon@itofisher.com>
2021-06-15 17:33:52 -0700

    Add additional table filters

--- mathesar/filters.py
+++ mathesar/filters.py
@@ -1,5 +1,6 @@
 from django_property_filter import (
     PropertyFilterSet, PropertyBaseInFilter, PropertyCharFilter,
+    PropertyDateTimeFromToRangeFilter, PropertyBooleanFilter
 )
 
 from mathesar.models import Schema, Table
@@ -20,7 +21,11 @@ class SchemaFilter(PropertyFilterSet):
 
 class TableFilter(PropertyFilterSet):
     name = CharInFilter(field_name='name', lookup_expr='in')
+    schema = CharInFilter(field_name='schema__name', lookup_expr='in')
+    created = PropertyDateTimeFromToRangeFilter(field_name='created_at')
+    updated = PropertyDateTimeFromToRangeFilter(field_name='updated_at')
+    import_verified = PropertyBooleanFilter(field_name='import_verified')
 
     class Meta:
         model = Table
-        fields = ['name']
+        fields = ['name', 'schema', 'created_at', 'updated_at', 'import_verified']
