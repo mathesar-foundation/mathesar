3014ad519303edd5341ea3aeef89c0c39d6e722b
pavish <pavishkumar.r@gmail.com>
2021-06-22 20:42:17 +0530

    Button component

--- mathesar_ui/src/components/index.ts
+++ mathesar_ui/src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as Button } from './button/Button.svelte';
 export { default as TabContainer } from './tabs/TabContainer.svelte';
 export { default as Tree } from './tree/Tree.svelte';
 export { default as Icon } from './icon/Icon.svelte';
