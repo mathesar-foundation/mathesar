5379459d489baf3e31b19f814f0154b4e4bdbeb4
Eamon Ito-Fisher <eamon@itofisher.com>
2021-06-30 14:17:27 -0700

    FILTERSORT -> FILTER_SORT

--- db/tests/conftest.py
+++ db/tests/conftest.py
@@ -8,7 +8,7 @@ APP_SCHEMA = "test_schema"
 FILE_DIR = os.path.abspath(os.path.dirname(__file__))
 RESOURCES = os.path.join(FILE_DIR, "resources")
 ROSTER_SQL = os.path.join(RESOURCES, "roster_create.sql")
-FILTERSORT_SQL = os.path.join(RESOURCES, "filter_sort_create.sql")
+FILTER_SORT_SQL = os.path.join(RESOURCES, "filter_sort_create.sql")
 
 
 @pytest.fixture
@@ -33,7 +33,7 @@ def engine_with_roster(engine_with_schema):
 @pytest.fixture
 def engine_with_filter_sort(engine_with_schema):
     engine, schema = engine_with_schema
-    with engine.begin() as conn, open(FILTERSORT_SQL) as f:
+    with engine.begin() as conn, open(FILTER_SORT_SQL) as f:
         conn.execute(text(f"SET search_path={schema}"))
         conn.execute(text(f.read()))
     return engine, schema
