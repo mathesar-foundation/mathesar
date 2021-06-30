a8d91554b37ca64b6a414432a780a3d49cf5348b
Brent Moran <brentmoran@gmail.com>
2021-06-30 16:28:01 +0800

    add test for column dropping function

--- db/tests/test_columns.py
+++ db/tests/test_columns.py
@@ -384,6 +384,26 @@ def test_change_column_nullable_changes_raises_with_null_data(engine_with_schema
         assert type(e.orig) == NotNullViolation
 
 
+def test_drop_column_correct_column(engine_with_schema):
+    engine, schema = engine_with_schema
+    table_name = "atable"
+    target_column_name = "thecolumntodrop"
+    nontarget_column_name = "notthecolumntodrop"
+    table = Table(
+        table_name,
+        MetaData(bind=engine, schema=schema),
+        Column(target_column_name, Integer),
+        Column(nontarget_column_name, String),
+    )
+    table.create()
+    table_oid = tables.get_oid_from_table(table_name, schema, engine)
+    columns.drop_column(engine, table_oid, 0)
+    altered_table = tables.reflect_table_from_oid(table_oid, engine)
+    assert len(altered_table.columns) == 1
+    assert nontarget_column_name in altered_table.columns
+    assert target_column_name not in altered_table.columns
+
+
 def get_mathesar_column_init_args():
     init_code = columns.MathesarColumn.__init__.__code__
     return init_code.co_varnames[1:init_code.co_argcount]
