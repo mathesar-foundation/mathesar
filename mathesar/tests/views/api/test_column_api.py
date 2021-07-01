13e42afbed898dd993a7b858964215780149c2ce
Brent Moran <brentmoran@gmail.com>
2021-07-01 18:42:47 +0800

    initial API-level columns API test, fixture

---
+++ mathesar/tests/views/api/test_column_api.py
@@ -0,0 +1,45 @@
+from django.conf import settings
+from django.core.cache import cache
+import pytest
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Table as SATable
+from sqlalchemy.schema import CreateSchema, CreateTable
+
+from db.tables import get_oid_from_table, create_mathesar_table
+from mathesar.models import Schema, Table
+
+
+@pytest.fixture
+def column_test_table(patent_schema):
+    engine = patent_schema._sa_engine
+    column_list_in = [
+        Column("mycolumn0", Integer, primary_key=True),
+        Column("mycolumn1", Integer, nullable=False),
+        Column("mycolumn2", Integer),
+        Column("mycolumn3", String),
+    ]
+    db_table = SATable(
+        "anewtable",
+        MetaData(bind=engine),
+        *column_list_in,
+        schema=patent_schema.name
+    )
+    db_table.create()
+    db_table_oid = get_oid_from_table(db_table.name, db_table.schema, engine)
+    table = Table.objects.create(oid=db_table_oid, schema=patent_schema)
+    return table
+
+
+
+def test_column_list(column_test_table, client):
+    cache.clear()
+    response = client.get(f"/api/v0/tables/{column_test_table.id}/columns/")
+    response_data = response.json()
+    assert response_data['count'] == len(column_test_table.sa_columns)
+    expect_results = [
+        {'name': 'mycolumn0', 'type': 'INTEGER', 'nullable': False, 'primary_key': True},
+        {'name': 'mycolumn1', 'type': 'INTEGER', 'nullable': False, 'primary_key': False},
+        {'name': 'mycolumn2', 'type': 'INTEGER', 'nullable': True, 'primary_key': False},
+        {'name': 'mycolumn3', 'type': 'VARCHAR', 'nullable': True, 'primary_key': False}
+    ]
+    assert response_data['results'] == expect_results
