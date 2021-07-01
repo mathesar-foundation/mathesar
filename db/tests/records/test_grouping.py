9fd6d1ea44988d340e8a074bf5e160c5dc2ae6ac
Eamon Ito-Fisher <eamon@itofisher.com>
2021-06-30 20:27:49 -0700

    Add test for checking count values

--- db/tests/records/test_grouping.py
+++ db/tests/records/test_grouping.py
@@ -1,3 +1,9 @@
+import itertools
+from collections import Counter
+
+import pytest
+from sqlalchemy import select
+
 from db import records
 
 
@@ -32,6 +38,7 @@ def test_get_group_counts_limit_ordering(filter_sort_table_obj):
     group_by = [filter_sort.c.numeric]
     counts = records.get_group_counts(filter_sort, engine, limit=limit,
                                       order_by=order_by, group_by=group_by)
+    assert len(counts) == 50
     for i in range(1, 100):
         if i > 50:
             assert i in counts
@@ -53,3 +60,32 @@ def test_get_group_counts_limit_offset_ordering(filter_sort_table_obj):
             assert i in counts
         else:
             assert i not in counts
+
+
+count_values_test_list = itertools.chain(*[
+    itertools.combinations([
+        "Student Name",
+        "Student Email",
+        "Teacher Email",
+        "Subject",
+        "Grade"
+    ], i) for i in range(1, 5)
+])
+
+
+@pytest.mark.parametrize("group_by", count_values_test_list)
+def test_get_group_counts_count_values(roster_table_obj, group_by):
+    roster, engine = roster_table_obj
+    print(group_by)
+    counts = records.get_group_counts(roster, engine, group_by=group_by)
+
+    cols = [roster.c[f] for f in group_by]
+    with engine.begin() as conn:
+        all_records = conn.execute(select(*cols)).fetchall()
+    manual_count = Counter(all_records)
+
+    for key, value in counts.items():
+        # The counter uses tuples as keys
+        if type(key) is not tuple:
+            key = (key,)
+        assert manual_count[key] == value
