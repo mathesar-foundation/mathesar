e5771490586821e1cf2cf9866931fb87cea7c061
Brent Moran <brentmoran@gmail.com>
2021-07-02 21:49:37 +0800

    merge both nested routers into table_router

--- mathesar/urls.py
+++ mathesar/urls.py
@@ -10,17 +10,14 @@ router.register(r'schemas', api.SchemaViewSet, basename='schema')
 router.register(r'database_keys', api.DatabaseKeyViewSet, basename='database-key')
 router.register(r'data_files', api.DataFileViewSet)
 
-records_router = routers.NestedSimpleRouter(router, r'tables', lookup='table')
-records_router.register(r'records', api.RecordViewSet, basename='table-record')
-
-columns_router = routers.NestedSimpleRouter(router, r'tables', lookup='table')
-columns_router.register(r'columns', api.ColumnViewSet, basename='table-column')
+table_router = routers.NestedSimpleRouter(router, r'tables', lookup='table')
+table_router.register(r'records', api.RecordViewSet, basename='table-record')
+table_router.register(r'columns', api.ColumnViewSet, basename='table-column')
 
 urlpatterns = [
     path('', frontend.index, name="index"),
     path('api/v0/', include(router.urls)),
-    path('api/v0/', include(records_router.urls)),
-    path('api/v0/', include(columns_router.urls)),
+    path('api/v0/', include(table_router.urls)),
     # TODO: Handle known urls like /favicon.ico etc.,
     # Currenty, this catches all
     path('<dbname>', frontend.index, name="index"),
