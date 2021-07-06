8a34dbfdb3bd989fb2f1bd7c7db3775cc802431f
pavish <pavishkumar.r@gmail.com>
2021-07-06 13:22:58 +0530

    FileUpload component

---
+++ mathesar_ui/src/components/file-upload/FileUpload.svelte
@@ -0,0 +1,131 @@
+<script lang="ts" context="module">
+  let id = 0;
+
+  function getId() {
+    id += 1;
+    return id;
+  }
+</script>
+
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import {
+    faFile,
+    faFileUpload,
+  } from '@fortawesome/free-solid-svg-icons';
+  import { Icon, Progress, Button } from '@mathesar-components';
+  import type {
+    FileUpload,
+    FileUploadProgress,
+    FileUploadAddDetail,
+  } from './FileUpload.d';
+
+  const dispatch = createEventDispatcher();
+  const componentId = `file-import-${getId()}`;
+
+  export let fileProgress: Record<string, FileUploadProgress> = {};
+  export let multiple = false;
+  export let fileUploads: FileUpload[] = [];
+
+  let fileId = 0;
+  let state = 'idle';
+
+  export function updateState(
+    fileIdentifier: string,
+    progress: FileUploadProgress,
+  ) : void {
+    fileProgress = {
+      ...fileProgress,
+      [fileIdentifier]: progress,
+    };
+  }
+
+  function processFiles(event: Event, files: FileList) {
+    const newUploads: FileUpload[] = [];
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const file of files) {
+      newUploads.push({
+        fileId: `${componentId}-${fileId}`,
+        file,
+      });
+      fileId += 1;
+    }
+    fileUploads = [...fileUploads, ...newUploads];
+
+    const fileUploadAddEvent: FileUploadAddDetail = {
+      added: newUploads,
+      all: fileUploads,
+      originalEvent: event,
+    };
+
+    dispatch('add', fileUploadAddEvent);
+  }
+
+  function onChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    if (target.files) {
+      processFiles(event, target.files);
+    }
+  }
+
+  function checkAndOpen(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      (event.target as HTMLElement).click();
+    }
+  }
+</script>
+
+<div class="file-upload" class:inprogress={fileUploads.length > 0}>
+  {#if fileUploads.length > 0}
+    <div class="files">
+      {#each fileUploads as upload (upload.fileId)}
+        <div class="file">
+          <Icon data={faFile} size="3.5em"/>
+          <div class="file-info">
+            <div class="name">{upload.file.name}</div>
+            <Progress percentage={Math.round(fileProgress[upload.fileId]?.progress || 0)} />
+            <div class="upload-info">
+              <span>
+                Uploaded {Math.round(fileProgress[upload.fileId]?.progress || 0)}%
+              </span>
+              <span>{upload.file.size}Kb</span>
+            </div>
+            <div class="actions">
+              <Button>Cancel Upload</Button>
+            </div>
+          </div>
+        </div>
+      {/each}
+    </div>
+  {/if}
+
+  <input type='file' id={componentId}
+          {multiple}
+          style="display: none;"
+          on:change={onChange}/>
+
+  {#if multiple || fileUploads.length === 0}
+    <label tabindex="0" for={componentId} class="file-upload-trigger {state}"
+      on:keydown={checkAndOpen}
+      on:drop|preventDefault|stopPropagation={(e) => processFiles(e, e.dataTransfer.files)} 
+      on:dragenter|preventDefault|stopPropagation={() => { state = 'in'; }}
+      on:dragover|preventDefault|stopPropagation={() => { state = 'in'; }}
+      on:dragleave|preventDefault|stopPropagation={() => { state = 'out'; }}
+      on:dragend|preventDefault|stopPropagation={() => { state = 'out'; }}>      
+      <slot>
+        <div class="message">
+          <Icon size="60px" data={faFileUpload}/>
+          <div class="text">
+            <div>Drag a file here</div>
+            <div>Or click to browse a file from your computer</div>
+          </div>
+        </div>
+      </slot>
+    </label>
+  {/if}
+</div>
+
+<style global lang="scss">
+  @import "FileUpload.scss";
+</style>
