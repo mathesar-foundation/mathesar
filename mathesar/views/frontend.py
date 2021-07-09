d813b67ba753ffa222ac87f2d4fea8b2eea76000
Eamon Ito-Fisher <eamon@itofisher.com>
2021-07-08 17:15:09 -0700

    Remove deprecated code in frontend.py

--- mathesar/views/frontend.py
+++ mathesar/views/frontend.py
@@ -18,18 +18,6 @@ def get_schemas(request):
 
 
 def index(request, **kwargs):
-    # TODO: Remove this path once frontend switches to using the API
-    # See https://github.com/centerofci/mathesar/issues/150
-    if request.method == "POST":
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            table = legacy_create_table_from_csv(
-                name=form.cleaned_data["table_name"],
-                schema=form.cleaned_data["schema_name"],
-                database_key=form.cleaned_data["database_key"],
-                csv_file=request.FILES["file"]
-            )
-            return JsonResponse({"pk": table.id}, status=200)
     requested_db = kwargs.get('dbname')
     database_list = get_non_default_database_keys()
     if (requested_db is None or requested_db not in database_list) and len(database_list) > 0:
