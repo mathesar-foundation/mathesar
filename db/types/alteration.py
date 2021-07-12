929d51468cce0672e9d44971fcbb25be6c722e83
Brent Moran <brentmoran@gmail.com>
2021-07-12 17:37:42 +0800

    refactor, add function to get valid target_type for given type

--- db/types/alteration.py
+++ db/types/alteration.py
@@ -83,180 +83,51 @@ def install_all_casts(engine):
 
 
 def create_boolean_casts(engine):
-    """
-    Using the `create_cast_functions` python function, we create a
-    PL/pgSQL function to cast types corresponding to each of:
-
-    boolean -> boolean:  Identity. No remarks
-    text -> boolean:     We only cast 't', 'f', 'true', or 'false' all
-                         others raise a custom exception.
-    numeric -> boolean:  We only cast 1 -> true, 0 -> false (this is not
-                         default behavior for PostgreSQL). Others raise a
-                         custom exception.
-    """
-    not_bool_exception_str = f"RAISE EXCEPTION '% is not a {BOOLEAN}', $1;"
-    type_body_map = {
-        BOOLEAN: """
-        BEGIN
-          RETURN $1;
-        END;
-        """,
-        TEXT: f"""
-        DECLARE
-        istrue {BOOLEAN};
-        BEGIN
-          SELECT lower($1)='t' OR lower($1)='true' OR $1='1' INTO istrue;
-          IF istrue OR lower($1)='f' OR lower($1)='false' OR $1='0' THEN
-            RETURN istrue;
-          END IF;
-          {not_bool_exception_str}
-        END;
-        """,
-        NUMERIC: f"""
-        BEGIN
-          IF $1<>0 AND $1<>1 THEN
-            {not_bool_exception_str}
-          END IF;
-          RETURN $1<>0;
-        END;
-        """,
-    }
+    type_body_map = _get_boolean_type_body_map()
     create_cast_functions(BOOLEAN, type_body_map, engine)
 
 
-def create_numeric_casts(engine):
-    """
-    Using the `create_cast_functions` python function, we create a
-    PL/pgSQL function to cast types corresponding to each of:
-
-    numeric -> numeric:  Identity. No remarks
-    boolean -> numeric:  We cast TRUE -> 1, FALSE -> 0
-    text -> numeric:     We use the default PostgreSQL behavior.
-    """
-    type_body_map = {
-        NUMERIC: """
-        BEGIN
-          RETURN $1;
-        END;
-        """,
-        BOOLEAN: f"""
-        BEGIN
-          IF $1 THEN
-            RETURN 1::{NUMERIC};
-          END IF;
-          RETURN 0;
-        END;
-        """,
-        TEXT: f"""
-        BEGIN
-          RETURN $1::{NUMERIC};
-        END;
-        """,
-    }
-    create_cast_functions(NUMERIC, type_body_map, engine)
-
-
 def create_email_casts(engine):
-    """
-    Using the `create_cast_functions` python function, we create a
-    PL/pgSQL function to cast types corresponding to each of:
-
-    email -> email:  Identity. No remarks
-    text -> email:   We use the default PostgreSQL behavior (this will
-                     just check that the TEXT object satisfies the email
-                     DOMAIN).
-    """
-    type_body_map = {
-        email.QUALIFIED_EMAIL: """
-        BEGIN
-          RETURN $1;
-        END;
-        """,
-        TEXT: f"""
-        BEGIN
-          RETURN $1::{email.QUALIFIED_EMAIL};
-        END;
-        """,
-    }
+    type_body_map = _get_email_type_body_map()
     create_cast_functions(email.QUALIFIED_EMAIL, type_body_map, engine)
 
 
 def create_interval_casts(engine):
-    """
-    Using the `create_cast_functions` python function, we create a
-    PL/pgSQL function to cast types corresponding to each of:
-
-    interval -> interval:  Identity. No remarks
-    text -> interval:      We first check that the text *cannot* be cast
-                           to a numeric, and then try to cast the text to an
-                           interval.
-    """
-    type_body_map = {
-        INTERVAL: """
-        BEGIN
-          RETURN $1;
-        END;
-        """,
-        # We need to check that a string isn't a valid number before
-        # casting to intervals (since a number is more likely)
-        TEXT: f"""
-        BEGIN
-          PERFORM $1::{NUMERIC};
-          RAISE EXCEPTION '% is a {NUMERIC}', $1;
-          EXCEPTION
-            WHEN sqlstate '22P02' THEN
-              RETURN $1::{INTERVAL};
-        END;
-        """,
-    }
+    type_body_map = _get_interval_type_body_map()
     create_cast_functions(INTERVAL, type_body_map, engine)
 
 
-def create_varchar_casts(engine):
-    """
-    Using the `create_cast_functions` python function, we create a
-    PL/pgSQL function to cast types corresponding to each of:
+def create_numeric_casts(engine):
+    type_body_map = _get_numeric_type_body_map()
+    create_cast_functions(NUMERIC, type_body_map, engine)
 
-    varchar -> varchar:   Identity. No remarks
-    boolean -> varchar:   We use the default PostgreSQL cast behavior.
-    email -> varchar:     We use the default PostgreSQL cast behavior.
-    interval -> varchar:  We use the default PostgreSQL cast behavior.
-    numeric -> varchar:   We use the default PostgreSQL cast behavior.
-    text -> varchar:      We use the default PostgreSQL cast behavior.
-    """
-    type_body_map = {
-        VARCHAR: """
-        BEGIN
-          RETURN $1;
-        END;
-        """,
-        BOOLEAN: f"""
-        BEGIN
-          RETURN $1::{VARCHAR};
-        END;
-        """,
-        email.QUALIFIED_EMAIL: f"""
-        BEGIN
-          RETURN $1::{VARCHAR};
-        END;
-        """,
-        INTERVAL: f"""
-        BEGIN
-          RETURN $1::{VARCHAR};
-        END;
-        """,
-        NUMERIC: f"""
-        BEGIN
-          RETURN $1::{VARCHAR};
-        END;
-        """,
-        TEXT: f"""
-        BEGIN
-          RETURN $1::{VARCHAR};
-        END;
-        """,
-    }
+
+def create_varchar_casts(engine):
+    type_body_map = _get_varchar_type_body_map()
     create_cast_functions(VARCHAR, type_body_map, engine)
+
+
+def get_defined_source_type_target_type_cast_map(engine):
+    full_cast_map = {}
+    for k, v in get_defined_source_target_cast_tuples():
+        full_cast_map.setdefault(k, []).append(v)
+
+    return full_cast_map
+
+
+def get_defined_source_target_cast_tuples():
+    type_body_map_map = {
+        BOOLEAN: _get_boolean_type_body_map,
+        EMAIL: _get_email_type_body_map,
+        INTERVAL: _get_interval_type_body_map,
+        NUMERIC: _get_numeric_type_body_map,
+        VARCHAR: _get_varchar_type_body_map,
+    }
+    return {
+        (source_type, target_type)
+        for target_type in type_body_map_map
+        for source_type in type_body_map_map[target_type]()
+    }
 
 
 def create_cast_functions(target_type, type_body_map, engine):
@@ -297,3 +168,174 @@ def get_cast_function_name(target_type):
     unqualified_type_name = target_type.split('.')[-1].lower()
     bare_function_name = f"cast_to_{unqualified_type_name}"
     return f"{base.get_qualified_name(bare_function_name)}"
+
+
+def _get_boolean_type_body_map():
+    """
+    Get SQL strings that create various functions for casting different
+    types to booleans.
+
+    boolean -> boolean:  Identity. No remarks
+    text -> boolean:     We only cast 't', 'f', 'true', or 'false' all
+                         others raise a custom exception.
+    numeric -> boolean:  We only cast 1 -> true, 0 -> false (this is not
+                         default behavior for PostgreSQL). Others raise a
+                         custom exception.
+    """
+    not_bool_exception_str = f"RAISE EXCEPTION '% is not a {BOOLEAN}', $1;"
+    return {
+        BOOLEAN: """
+        BEGIN
+          RETURN $1;
+        END;
+        """,
+        TEXT: f"""
+        DECLARE
+        istrue {BOOLEAN};
+        BEGIN
+          SELECT lower($1)='t' OR lower($1)='true' OR $1='1' INTO istrue;
+          IF istrue OR lower($1)='f' OR lower($1)='false' OR $1='0' THEN
+            RETURN istrue;
+          END IF;
+          {not_bool_exception_str}
+        END;
+        """,
+        NUMERIC: f"""
+        BEGIN
+          IF $1<>0 AND $1<>1 THEN
+            {not_bool_exception_str} END IF;
+          RETURN $1<>0;
+        END;
+        """,
+    }
+
+
+def _get_email_type_body_map():
+    """
+    Get SQL strings that create various functions for casting different
+    types to email.
+
+    email -> email:  Identity. No remarks
+    text -> email:   We use the default PostgreSQL behavior (this will
+                     just check that the TEXT object satisfies the email
+                     DOMAIN).
+    """
+    return {
+        email.QUALIFIED_EMAIL: """
+        BEGIN
+          RETURN $1;
+        END;
+        """,
+        TEXT: f"""
+        BEGIN
+          RETURN $1::{email.QUALIFIED_EMAIL};
+        END;
+        """,
+    }
+
+
+def _get_interval_type_body_map():
+    """
+    Get SQL strings that create various functions for casting different
+    types to interval.
+
+    interval -> interval:  Identity. No remarks
+    text -> interval:      We first check that the text *cannot* be cast
+                           to a numeric, and then try to cast the text to an
+                           interval.
+    """
+    return {
+        INTERVAL: """
+        BEGIN
+          RETURN $1;
+        END;
+        """,
+        # We need to check that a string isn't a valid number before
+        # casting to intervals (since a number is more likely)
+        TEXT: f"""
+        BEGIN
+          PERFORM $1::{NUMERIC};
+          RAISE EXCEPTION '% is a {NUMERIC}', $1;
+          EXCEPTION
+            WHEN sqlstate '22P02' THEN
+              RETURN $1::{INTERVAL};
+        END;
+        """,
+    }
+
+
+def _get_numeric_type_body_map():
+    """
+    Get SQL strings that create various functions for casting different
+    types to numerics.
+
+    numeric -> numeric:  Identity. No remarks
+    boolean -> numeric:  We cast TRUE -> 1, FALSE -> 0
+    text -> numeric:     We use the default PostgreSQL behavior.
+    """
+    return {
+        NUMERIC: """
+        BEGIN
+          RETURN $1;
+        END;
+        """,
+        BOOLEAN: f"""
+        BEGIN
+          IF $1 THEN
+            RETURN 1::{NUMERIC};
+          END IF;
+          RETURN 0;
+        END;
+        """,
+        TEXT: f"""
+        BEGIN
+          RETURN $1::{NUMERIC};
+        END;
+        """,
+    }
+
+
+def _get_varchar_type_body_map():
+    """
+    Get SQL strings that create various functions for casting different
+    types to varchar.
+
+    varchar -> varchar:   Identity. No remarks
+    boolean -> varchar:   We use the default PostgreSQL cast behavior.
+    email -> varchar:     We use the default PostgreSQL cast behavior.
+    interval -> varchar:  We use the default PostgreSQL cast behavior.
+    numeric -> varchar:   We use the default PostgreSQL cast behavior.
+    text -> varchar:      We use the default PostgreSQL cast behavior.
+    """
+    return {
+        VARCHAR: """
+        BEGIN
+          RETURN $1;
+        END;
+        """,
+        BOOLEAN: f"""
+        BEGIN
+          RETURN $1::{VARCHAR};
+        END;
+        """,
+        email.QUALIFIED_EMAIL: f"""
+        BEGIN
+          RETURN $1::{VARCHAR};
+        END;
+        """,
+        INTERVAL: f"""
+        BEGIN
+          RETURN $1::{VARCHAR};
+        END;
+        """,
+        NUMERIC: f"""
+        BEGIN
+          RETURN $1::{VARCHAR};
+        END;
+        """,
+        TEXT: f"""
+        BEGIN
+          RETURN $1::{VARCHAR};
+        END;
+        """,
+    }
