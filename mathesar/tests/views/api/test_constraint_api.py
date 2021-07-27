5f33170cb4c8bbe5b1c8d9d9940efb3b9e49042b
Kriti Godey <kriti@kritigodey.com>
2021-07-27 14:13:51 -0400

    Fixed table tests and added one more constraint test.

--- mathesar/tests/views/api/test_constraint_api.py
+++ mathesar/tests/views/api/test_constraint_api.py
@@ -12,3 +12,22 @@ def test_default_constraint_list(create_table, client):
     assert constraint_data["id"] == "NASA Constraint List_pkey"
     assert constraint_data["name"] == "NASA Constraint List_pkey"
     assert constraint_data["type"] == "primary"
+
+
+def test_multiple_constraint_list(create_table, client):
+    table_name = 'NASA Constraint List 2'
+    table = create_table(table_name)
+    table.add_constraint({'type': 'unique', 'columns': ['Case Number']})
+
+    response = client.get(f'/api/v0/tables/{table.id}/constraints/')
+    response_data = response.json()
+    constraints_data = response_data['results']
+
+    assert response.status_code == 200
+    assert response_data['count'] == 2
+    assert set(["unique", "primary"]) == set([constraint_data["type"] for constraint_data in constraints_data])
+    for constraint_data in constraints_data:
+        if constraint_data["type"] == "unique":
+            assert constraint_data["columns"] == ["Case Number"]
+            assert constraint_data["id"] == "NASA Constraint List 2_Case Number_key"
+            assert constraint_data["name"] == "NASA Constraint List 2_Case Number_key"
--- mathesar/tests/views/api/test_table_api.py
+++ mathesar/tests/views/api/test_table_api.py
@@ -46,7 +46,9 @@ def check_table_response(response_table, table, table_name):
     assert response_table['records_url'].startswith('http')
     assert response_table['columns_url'].startswith('http')
     assert response_table['constraints_url'].startswith('http')
-    assert '/api/v0/tables/' in response_table['records']
+    assert '/api/v0/tables/' in response_table['records_url']
+    assert '/api/v0/tables/' in response_table['columns_url']
+    assert '/api/v0/tables/' in response_table['constraints_url']
     assert response_table['records_url'].endswith('/records/')
     assert response_table['columns_url'].endswith('/columns/')
     assert response_table['constraints_url'].endswith('/constraints/')
