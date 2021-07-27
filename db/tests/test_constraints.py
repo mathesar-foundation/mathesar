2dfbfe6866fdb54ee22d59e86185f9eb29cbf1d4
Kriti Godey <kriti@kritigodey.com>
2021-07-27 11:48:10 -0400

    Added more test cases

--- db/tests/test_constraints.py
+++ db/tests/test_constraints.py
@@ -1,4 +1,6 @@
+import pytest
 from sqlalchemy import String, Integer, Column, Table, MetaData, PrimaryKeyConstraint, UniqueConstraint
+from sqlalchemy.exc import ProgrammingError
 
 from db import constraints, tables
 
@@ -93,3 +95,49 @@ def test_drop_unique_constraint(engine_with_schema):
     constraints.drop_constraint(table_name, schema, engine, unique_constraint.name)
     new_altered_table = tables.reflect_table_from_oid(table_oid, engine)
     _assert_only_primary_key_present(new_altered_table)
+
+
+def test_create_unique_constraint_with_custom_name(engine_with_schema):
+    engine, schema = engine_with_schema
+    table_name = "orders_4"
+    unique_column_name = 'product_name'
+    constraint_name = 'unique_product_name'
+    table = Table(
+        table_name,
+        MetaData(bind=engine, schema=schema),
+        Column('order_id', Integer, primary_key=True),
+        Column(unique_column_name, String),
+    )
+    table.create()
+    table_oid = tables.get_oid_from_table(table_name, schema, engine)
+    constraints.create_unique_constraint(table.name, schema, engine, [unique_column_name], constraint_name)
+
+    altered_table = tables.reflect_table_from_oid(table_oid, engine)
+    _assert_primary_key_and_unique_present(altered_table)
+
+    unique_constraint = _get_first_unique_constraint(altered_table)
+    assert unique_constraint.name == constraint_name
+    assert len(list(unique_constraint.columns)) == 1
+    assert list(unique_constraint.columns)[0].name == unique_column_name
+
+
+def test_create_unique_constraint_with_duplicate_name(engine_with_schema):
+    engine, schema = engine_with_schema
+    table_name = "orders_4"
+    unique_column_names = ['product_name', 'customer_name']
+    constraint_name = 'unique_product_name'
+    table = Table(
+        table_name,
+        MetaData(bind=engine, schema=schema),
+        Column('order_id', Integer, primary_key=True),
+        Column(unique_column_names[0], String),
+        Column(unique_column_names[1], String),
+    )
+    table.create()
+    table_oid = tables.get_oid_from_table(table_name, schema, engine)
+    constraints.create_unique_constraint(table.name, schema, engine, [unique_column_names[0]], constraint_name)
+
+    altered_table = tables.reflect_table_from_oid(table_oid, engine)
+    _assert_primary_key_and_unique_present(altered_table)
+    with pytest.raises(ProgrammingError):
+        constraints.create_unique_constraint(table.name, schema, engine, [unique_column_names[1]], constraint_name)
