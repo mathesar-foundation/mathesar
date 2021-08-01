f50d5b7d36758a3e1cb22212a178116a3022fcb1
Dhruv Bhanushali <dhruv_b@live.com>
2021-08-01 17:58:47 +0530

    Create a Modal component

---
+++ mathesar_ui/src/components/modal/Modal.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Portal from 'svelte-portal/src/Portal.svelte';
+
+  export let overlayTarget = 'body';
+
+  // Additional classes
+  let classes = '';
+  export { classes as class };
+
+  // Inline styles
+  export let style = '';
+</script>
+
+<div class="modal-wrapper">
+  <div class={['modal', classes].join(' ')} {style}>
+    <slot/>
+  </div>
+</div>
+
+<Portal target={overlayTarget}>
+  <div class="overlay"></div>
+</Portal>
+
+<style global lang="scss">
+  @import "Modal.scss";
+</style>
