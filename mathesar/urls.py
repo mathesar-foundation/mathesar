f3ba8a333e3b3b6fdd96cc4937e22b623ec86706
pavish <pavishkumar.r@gmail.com>
2021-08-05 01:48:01 +0530

    Allow schema id in url, handle pre-rendering based on routes

--- mathesar/urls.py
+++ mathesar/urls.py
@@ -16,10 +16,12 @@ table_router.register(r'records', api.RecordViewSet, basename='table-record')
 table_router.register(r'columns', api.ColumnViewSet, basename='table-column')
 
 urlpatterns = [
-    path('', frontend.index, name="index"),
     path('api/v0/', include(router.urls)),
     path('api/v0/', include(table_router.urls)),
-    # TODO: Handle known urls like /favicon.ico etc.,
-    # Currently, this catches all
-    re_path(r'(?P<dbname>\w+)/.*$', frontend.index, name="index"),
+
+    # Specifying each route individually to facilitate redirection and data pre-rendering based on route
+    path('', frontend.index, name="index"),
+    path('<dbname>/', frontend.index, name="index"),
+    path('<dbname>/schemas/', frontend.schemas, name="schemas"),
+    path('<dbname>/<int:schema>/', frontend.index, name="index"),
 ]
--- mathesar/views/frontend.py
+++ mathesar/views/frontend.py
@@ -12,6 +12,7 @@ def get_schemas(request, database):
     )
     return schema_serializer.data
 
+
 def get_databases(request):
     database_serializer = DatabaseSerializer(
         Database.objects.all(),
@@ -20,22 +21,83 @@ def get_databases(request):
     )
     return database_serializer.data
 
-def index(request, **kwargs):
+
+def get_render_info(request, **kwargs):
     requested_db = kwargs.get('dbname')
+    requested_schema = kwargs.get('schema')
+    is_db_updated = False
+    is_schema_updated = False
+
     database_list = get_databases(request)
 
-    # If no db is requested or if the requested db is not present, redirect to first available db
-    if (requested_db is None or next((x for x in database_list if x.get('name') == requested_db), None) is None) and len(database_list) > 0:
-        return redirect('index', dbname=database_list[0].get('name'))
+    # This block check if db is none or invalid, and if true, redirects to the first db.
+    # If db is invalid, and no dbs are configured, sets requested_db to None
+    #   - Mathesar wont start in this case, but if we plan to show a specific error page, or
+    #     allow initial start of Mathesar without a db in the future, this will be essential.
+    if (requested_db is None or next((x for x in database_list if x.get('name') == requested_db), None) is None):
+        if len(database_list) > 0:
+            requested_db = database_list[0].get('name')
+            is_db_updated = True
+        elif requested_db is not None:
+            requested_db = None
+            is_db_updated = True
 
-    # Send database list and schemas for selected db to the template
+    schema_list = get_schemas(request, requested_db)
+
+    # Performs the same checks as above, but for schemas
+    if (requested_schema is None or next((x for x in schema_list if x.get('id') == requested_schema), None) is None):
+        if len(schema_list) > 0:
+            requested_schema = schema_list[0].get('id')
+            is_schema_updated = True
+        elif requested_schema is not None:
+            requested_schema = None
+            is_schema_updated = True
+
+    return {
+        'is_db_updated': is_db_updated,
+        'is_schema_updated': is_schema_updated,
+        'common_data': {
+            'selected_db': requested_db,
+            'selected_schema': requested_schema,
+            'schemas': schema_list,
+            'databases': database_list
+        }
+    }
+
+
+def index(request, **kwargs):
+    render_info = get_render_info(request, **kwargs)
+    common_data = render_info.get('common_data')
+    selected_db = common_data.get('selected_db')
+    selected_schema = common_data.get('selected_schema')
+
+    if render_info.get('is_db_updated') or render_info.get('is_schema_updated'):
+        return redirect('index', dbname=selected_db, schema=selected_schema)
+
+    if selected_db is not None and selected_schema is None:
+        return redirect('schemas', dbname=selected_db)
+
     return render(
         request,
         "mathesar/index.html",
         {
-            "common_data": {
-                "schemas": get_schemas(request, requested_db),
-                "databases": database_list
-            }
+            "common_data": common_data
+        }
+    )
+
+
+def schemas(request, **kwargs):
+    render_info = get_render_info(request, **kwargs)
+    common_data = render_info.get('common_data')
+    selected_db = common_data.get('selected_db')
+
+    if render_info.get('is_db_updated'):
+        return redirect('schemas', dbname=selected_db)
+
+    return render(
+        request,
+        "mathesar/index.html",
+        {
+            "common_data": common_data
         }
     )
