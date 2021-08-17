6abda5468721ce010d4baa8b6615da065dba8233
pavish <pavishkumar.r@gmail.com>
2021-08-18 00:00:29 +0530

    Adding footer slot, size, onClose dispatch to Modal

--- mathesar_ui/src/components/modal/Modal.svelte
+++ mathesar_ui/src/components/modal/Modal.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import type { Size } from '@mathesar-components/types';
   import { portal } from '@mathesar-components';
+
+  const dispatch = createEventDispatcher();
 
   // Additional classes
   let classes = '';
@@ -7,6 +11,9 @@
 
   // Inline styles
   export let style = '';
+
+  // Size
+  export let size: Size = 'medium';
 
   // Boolean to open/close modal
   export let isOpen = true;
@@ -19,14 +26,30 @@
       isOpen = false;
     }
   }
+
+  function dispatchEventOnClose(_isOpen: boolean) {
+    if (!_isOpen) {
+      dispatch('close');
+    }
+  }
+
+  $: dispatchEventOnClose(isOpen);
 </script>
 
 <svelte:window on:keydown={handleKeydown}/>
 
 {#if isOpen}
   <div class="modal-wrapper" use:portal>
-    <div class={['modal', classes].join(' ')} {style}>
-      <slot/>
+    <div class={['modal', `modal-size-${size}`, classes].join(' ')} {style}>
+      <div class="body">
+        <slot/>
+      </div>
+
+      {#if $$slots.footer}
+        <div class="footer">
+          <slot name="footer"/>
+        </div>
+      {/if}
     </div>
   </div>
 {/if}
