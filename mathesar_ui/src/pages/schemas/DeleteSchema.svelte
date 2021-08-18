d51d93c318fab0cb50b89dd81aee9bb3efce87cd
Pavish Kumar <pavishkumar.r@gmail.com>
2021-08-18 21:11:51 +0530

    Update warning text in delete schema modal
    Co-authored-by: Kriti Godey <kriti@kritigodey.com>

--- mathesar_ui/src/pages/schemas/DeleteSchema.svelte
+++ mathesar_ui/src/pages/schemas/DeleteSchema.svelte
@@ -38,7 +38,7 @@
 
   {#if schema?.has_dependencies}
     <div class="help-text">
-      All objects in this schema will be deleted permanently, including tables and views.
+      All objects in this schema will be deleted permanently, including (but not limited to) tables and views. Some of these objects may not be visible in the Mathesar UI.
     </div>
   {/if}
 
