d4e81b878c96896f232d537ce1aea519f8ecc5a2
Asharon Baltazar <asharonbaltazar@outlook.com>
2021-08-20 11:53:18 -0400

    Add functionBeforeClose prop

--- mathesar_ui/src/components/dropdown/Dropdown.svelte
+++ mathesar_ui/src/components/dropdown/Dropdown.svelte
@@ -1,36 +1,40 @@
 <script lang="ts">
-  import {
-    faAngleDown,
-  } from '@fortawesome/free-solid-svg-icons';
+  import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
   import {
     portal,
     popper,
     Button,
     Icon,
     clickOffBounds,
-  } from '@mathesar-components';
-  import type {
-    Appearance,
-  } from '@mathesar-components/types';
-  import type { Placement } from '@popperjs/core/lib/enums';
+  } from "@mathesar-components";
+  import type { Appearance } from "@mathesar-components/types";
+  import type { Placement } from "@popperjs/core/lib/enums";
 
-  export let triggerClass = '';
-  export let triggerAppearance : Appearance = 'default';
-  export let contentClass = '';
+  export let triggerClass = "";
+  export let triggerAppearance: Appearance = "default";
+  export let contentClass = "";
   export let isOpen = false;
   export let closeOnInnerClick = false;
-  export let ariaLabel:string = null;
+  export let functionBeforeClose = null;
+  export let ariaLabel: string = null;
   export let ariaControls: string = null;
-  export let placement: Placement = 'bottom-start';
+  export let placement: Placement = "bottom-start";
 
   let trigger: HTMLElement;
-  $: tgClasses = ['dropdown', 'trigger', triggerClass].join(' ');
+  $: tgClasses = ["dropdown", "trigger", triggerClass].join(" ");
+
+  function checkAndRunFunctionBeforeClose() {
+    if (functionBeforeClose) {
+      functionBeforeClose();
+    }
+  }
 
   function toggle() {
     isOpen = !isOpen;
   }
 
   function close() {
+    checkAndRunFunctionBeforeClose();
     isOpen = false;
   }
 
@@ -41,27 +45,35 @@
   }
 </script>
 
-<Button bind:element={trigger} appearance={triggerAppearance} class={tgClasses} on:click={toggle}
-        aria-controls={ariaControls} aria-haspopup="listbox" aria-label={ariaLabel}>
+<Button
+  bind:element={trigger}
+  appearance={triggerAppearance}
+  class={tgClasses}
+  on:click={toggle}
+  aria-controls={ariaControls}
+  aria-haspopup="listbox"
+  aria-label={ariaLabel}
+>
   <span class="label">
-    <slot name="trigger"></slot>
+    <slot name="trigger" />
   </span>
   <span class="arrow">
-    <Icon data={faAngleDown}/>
+    <Icon data={faAngleDown} />
   </span>
 </Button>
 
 {#if isOpen}
-  <div class={['dropdown content', contentClass].join(' ')}
-        use:portal use:popper={{ reference: trigger, options: { placement } }}
-        use:clickOffBounds={{
-          callback: close,
-          references: [
-            trigger,
-          ],
-        }}
-        on:click={checkAndCloseOnInnerClick}>
-    <slot name="content"></slot>
+  <div
+    class={["dropdown content", contentClass].join(" ")}
+    use:portal
+    use:popper={{ reference: trigger, options: { placement } }}
+    use:clickOffBounds={{
+      callback: close,
+      references: [trigger],
+    }}
+    on:click={checkAndCloseOnInnerClick}
+  >
+    <slot name="content" />
   </div>
 {/if}
 
