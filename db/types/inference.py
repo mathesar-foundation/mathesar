c715e2c335e8b3494d623869aef935733fa1e66e
Kriti Godey <kriti@kritigodey.com>
2021-08-27 08:45:36 -0400

    Moved connection starting position to avoid hanging.

--- db/types/inference.py
+++ db/types/inference.py
@@ -68,25 +68,25 @@ def infer_column_type(
 
     logger.debug(f"column_type_str: {column_type_str}")
     for type_str in type_inference_dag.get(column_type_str, []):
-        with engine.begin() as conn:
-            try:
+        try:
+            with engine.begin() as conn:
                 alter_column_type(table, column_name, engine, conn, type_str)
-                logger.info(f"Column {column_name} altered to type {type_str}")
-                column_type = infer_column_type(
-                    schema,
-                    table_name,
-                    column_name,
-                    engine,
-                    depth=depth + 1,
-                    type_inference_dag=type_inference_dag,
-                )
-                break
-            # It's expected we catch this error when the test to see whether
-            # a type is appropriate for a column fails.
-            except DatabaseError:
-                logger.info(
-                    f"Cannot alter column {column_name} to type {type_str}"
-                )
+            logger.info(f"Column {column_name} altered to type {type_str}")
+            column_type = infer_column_type(
+                schema,
+                table_name,
+                column_name,
+                engine,
+                depth=depth + 1,
+                type_inference_dag=type_inference_dag,
+            )
+            break
+        # It's expected we catch this error when the test to see whether
+        # a type is appropriate for a column fails.
+        except DatabaseError:
+            logger.info(
+                f"Cannot alter column {column_name} to type {type_str}"
+            )
     return column_type
 
 
