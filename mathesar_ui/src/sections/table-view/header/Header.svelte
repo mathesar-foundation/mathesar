61e260747d312ad42848d7ec799e8a885ad56d25
pavish <pavishkumar.r@gmail.com>
2021-09-08 19:26:32 +0530

    Split headercell as separate component, use new stores

---
+++ mathesar_ui/src/sections/table-view/header/Header.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { onMount, getContext } from 'svelte';
+  import {
+    GROUP_MARGIN_LEFT,
+    ROW_CONTROL_COLUMN_WIDTH,
+  } from '@mathesar/stores/table-data/meta';
+
+  import type { TabularDataStore, TabularData } from '@mathesar/stores/table-data/store';
+  import HeaderCell from './HeaderCell.svelte';
+
+  const tabularData = getContext<TabularDataStore>('tabularData');
+  $: ({ columns, records, meta } = $tabularData as TabularData);
+  $: ({ horizontalScrollOffset } = meta as TabularData['meta']);
+
+  $: paddingLeft = $records.groupData ? GROUP_MARGIN_LEFT : 0;
+
+  let headerRef: HTMLElement;
+
+  function onHScrollOffsetChange(_hscrollOffset: number) {
+    if (headerRef) {
+      headerRef.scrollLeft = _hscrollOffset;
+    }
+  }
+
+  $: onHScrollOffsetChange($horizontalScrollOffset);
+
+  function onHeaderScroll(scrollLeft: number) {
+    if ($horizontalScrollOffset !== scrollLeft) {
+      $horizontalScrollOffset = scrollLeft;
+    }
+  }
+
+  onMount(() => {
+    onHScrollOffsetChange($horizontalScrollOffset);
+
+    const scrollListener = (event: Event) => {
+      const { scrollLeft } = event.target as HTMLElement;
+      onHeaderScroll(scrollLeft);
+    };
+
+    headerRef.addEventListener('scroll', scrollListener);
+
+    return () => {
+      headerRef.removeEventListener('scroll', scrollListener);
+    };
+  });
+</script>
+
+<div bind:this={headerRef} class="header">
+  <div class="cell row-control" style="width:{ROW_CONTROL_COLUMN_WIDTH + paddingLeft}px;">
+  </div>
+
+  {#each $columns.data as column (column.name)}
+    <HeaderCell {column} {meta} {paddingLeft}/>
+  {/each}
+</div>
---
+++ mathesar_ui/src/sections/table-view/header/HeaderCell.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+  import {
+    faSortAmountDown,
+    faSortAmountDownAlt,
+    faThList,
+  } from '@fortawesome/free-solid-svg-icons';
+  import { Dropdown, Icon } from '@mathesar-components';
+  import type { TableColumn } from '@mathesar/stores/table-data/columns';
+  import type {
+    Meta,
+    ColumnPositionMap,
+    SortOption,
+    GroupOption,
+  } from '@mathesar/stores/table-data/meta';
+
+  export let column: TableColumn;
+  export let meta: Meta;
+  export let paddingLeft: number;
+
+  $: ({
+    columnPositionMap, sort, group,
+  } = meta);
+  $: position = ($columnPositionMap as ColumnPositionMap).get(column.name);
+  $: sortDirection = ($sort as SortOption)?.get(column.name);
+  $: hasGrouping = ($group as GroupOption)?.has(column.name);
+
+  function handleSort(order: 'asc' | 'desc') {
+    if (sortDirection === order) {
+      meta.removeSort(column.name);
+    } else {
+      meta.addUpdateSort(column.name, order);
+    }
+  }
+
+  function toggleGroup() {
+    if (hasGrouping) {
+      meta.removeGroup(column.name);
+    } else {
+      meta.addGroup(column.name);
+    }
+  }
+</script>
+
+<div class="cell" style="width:{position.width}px;
+      left:{position.left + paddingLeft}px;">
+  <span class="type">
+    {#if column.type === 'INTEGER'}
+      #
+    {:else if column.type === 'VARCHAR'}
+      T
+    {:else}
+      i
+    {/if}
+  </span>
+  <span class="name">{column.name}</span>
+
+  <Dropdown closeOnInnerClick={true}
+            triggerClass="opts" triggerAppearance="plain"
+            contentClass="table-opts-content">
+    <svelte:fragment slot="content">
+      <ul>
+        <li on:click={() => handleSort('asc')}>
+          <Icon class="opt" data={faSortAmountDownAlt}/>
+          <span>
+            {#if sortDirection === 'asc'}
+              Remove asc sort
+            {:else}
+              Sort Ascending
+            {/if}
+          </span>
+        </li>
+        <li on:click={() => handleSort('desc')}>
+          <Icon class="opt" data={faSortAmountDown}/>
+          <span>
+            {#if sortDirection === 'desc'}
+              Remove desc sort
+            {:else}
+              Sort Descending
+            {/if}
+          </span>
+        </li>
+        <li on:click={toggleGroup}>
+          <Icon class="opt" data={faThList}/>
+          <span>
+            {#if hasGrouping}
+              Remove grouping
+            {:else}
+              Group by column
+            {/if}
+          </span>
+        </li>
+      </ul>
+    </svelte:fragment>
+  </Dropdown>
+</div>
