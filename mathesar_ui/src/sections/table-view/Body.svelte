ae00a83b9f44108a58f350cb5dac83c095e4e044
pavish <pavishkumar.r@gmail.com>
2021-09-17 01:10:52 +0530

    Handle scroll based on active cell movement

--- mathesar_ui/src/sections/table-view/Body.svelte
+++ mathesar_ui/src/sections/table-view/Body.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { getContext } from 'svelte';
+  import { getContext, tick } from 'svelte';
   import type {
     TabularDataStore,
     TabularData,
@@ -30,31 +30,53 @@
     return `__index_${index}`;
   }
 
-  function handleKeyDown(event: KeyboardEvent) {
-    if (bodyRef.contains(event.target as HTMLElement)) {
-      (display as Display).handleKeyEventsOnActiveCell(event.key);
-    } else {
-      (display as Display).resetActiveCell();
+  // TODO: Create a common utility action to handle active element based scroll
+  function handleScroll() {
+    const activeCell: HTMLElement = bodyRef.querySelector('.cell.is-active');
+    const activeRow = activeCell?.parentElement;
+    const container = bodyRef.querySelector('.virtual-list.outerElement');
+    if (container && activeRow) {
+      // Vertical scroll
+      if (activeRow.offsetTop + activeRow.clientHeight + 40
+        > (container.scrollTop + container.clientHeight)) {
+        const offsetValue: number = container.getBoundingClientRect().bottom
+          - activeRow.getBoundingClientRect().bottom - 40;
+        container.scrollTop -= offsetValue;
+      } else if (activeRow.offsetTop - 30 < container.scrollTop) {
+        container.scrollTop = activeRow.offsetTop - 30;
+      }
+
+      // Horizontal scroll
+      if (activeCell.offsetLeft + activeRow.clientWidth + 30
+        > (container.scrollLeft + container.clientWidth)) {
+        const offsetValue: number = container.getBoundingClientRect().right
+          - activeCell.getBoundingClientRect().right - 30;
+        container.scrollLeft -= offsetValue;
+      } else if (activeCell.offsetLeft - 30 < container.scrollLeft) {
+        container.scrollLeft = activeCell.offsetLeft - 30;
+      }
     }
   }
 
-  function handleTab(event: KeyboardEvent) {
-    if (event.key === 'Tab') {
-      (display as Display).handleKeyEventsOnActiveCell('ArrowRight');
-      event.stopPropagation();
-    }
-  }
-
-  function handleClick(event: MouseEvent) {
+  function checkAndResetActiveCell(event: Event) {
     if (!bodyRef.contains(event.target as HTMLElement)) {
       (display as Display).resetActiveCell();
     }
   }
+
+  async function handleKeyDownWithinBody(event: KeyboardEvent) {
+    (display as Display).handleKeyEventsOnActiveCell(event.key);
+    event.stopPropagation();
+    await tick();
+    handleScroll();
+  }
 </script>
 
-<svelte:window on:keydown={handleKeyDown} on:mousedown={handleClick}/>
+<svelte:window
+  on:keydown={checkAndResetActiveCell}
+  on:mousedown={checkAndResetActiveCell}/>
 
-<div bind:this={bodyRef} class="body" tabindex="-1" on:keydown={handleTab}>
+<div bind:this={bodyRef} class="body" tabindex="-1" on:keydown={handleKeyDownWithinBody}>
   <Resizer let:height>
     {#key id}
       <VirtualList
