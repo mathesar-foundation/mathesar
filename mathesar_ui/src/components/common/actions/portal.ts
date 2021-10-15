b68d09f5a5a0765ecf8914f9e6932cfee10cf968
Sean Colsen <colsen.sean@gmail.com>
2021-10-15 15:59:36 -0400

    Refactor portal action to accept isEnabled prop
    As far as I can tell, no components were utilizing the `target`
    parameter, so this refactor can be limited to this file only.

--- mathesar_ui/src/components/common/actions/portal.ts
+++ mathesar_ui/src/components/common/actions/portal.ts
@@ -1,9 +1,21 @@
 import type { Action } from './types.d';
 
-export default function portal(node: Element, target?: HTMLElement) : Action {
-  const targetElement = target ?? document.querySelector('body');
+interface PortalProps {
+  target?: HTMLElement,
+  /**
+   * Set this to `false` to disable the portal action. Defaults to `true`.
+   */
+  isEnabled?: boolean,
+}
+
+export default function portal(node: Element, props: PortalProps) : Action {
+  const targetElement = props.target ?? document.querySelector('body');
+  const isEnabled = props.isEnabled ?? true;
 
   function update(newTarget: HTMLElement) {
+    if (!isEnabled) {
+      return;
+    }
     if (newTarget && newTarget instanceof HTMLElement) {
       newTarget.appendChild(node);
     }
