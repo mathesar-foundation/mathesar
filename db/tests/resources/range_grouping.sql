b5e9ea5697f2cd5f0297f29d7b1374f337344a83
Brent Moran <brentmoran@gmail.com>
2021-11-02 00:38:31 +0800

    update test SQL to conform with current logic

--- db/tests/resources/range_grouping.sql
+++ db/tests/resources/range_grouping.sql
@@ -1,9 +1,9 @@
 WITH cume_dist_cte AS (
-  SELECT *, cume_dist() OVER (ORDER BY "Center") AS cume_dist
+  SELECT "Center", "Patent Expiration Date", cume_dist() OVER (ORDER BY "Center", "Patent Expiration Date") AS cume_dist
   FROM patents
 ),
 ranges AS (
-  SELECT *, CASE
+  SELECT "Center", "Patent Expiration Date", CASE
     WHEN cume_dist > 0.00 AND cume_dist <= 0.25 THEN 1
     WHEN cume_dist > 0.25 AND cume_dist <= 0.50 THEN 2
     WHEN cume_dist > 0.50 AND cume_dist <= 0.75 THEN 3
@@ -11,9 +11,12 @@ ranges AS (
   END as range
   FROM cume_dist_cte
 )
-SELECT
+SELECT DISTINCT
   range,
-  MIN("Center"),
-  MAX("Center"),
-  COUNT(1)
-FROM ranges GROUP BY range ORDER BY range;
+  first_value(ROW("Center", "Patent Expiration Date")) OVER w AS min_row,
+  last_value(ROW("Center", "Patent Expiration Date")) OVER w AS max_row,
+  COUNT(1) OVER w
+FROM ranges
+WINDOW w AS (
+  PARTITION BY range ORDER BY "Center", "Patent Expiration Date" RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+);
