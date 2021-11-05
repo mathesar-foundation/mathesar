e04017cd92a4397a73df0c3226faf96e3169035f
Sean Colsen <colsen.sean@gmail.com>
2021-11-05 11:59:22 -0400

    Add size prop to Spinner component

--- mathesar_ui/src/component-library/spinner/Spinner.svelte
+++ mathesar_ui/src/component-library/spinner/Spinner.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { faSpinner } from '@fortawesome/free-solid-svg-icons';
   import { Icon } from '@mathesar-component-library';
+
+  export let size: string | undefined = undefined;
 </script>
 
-<Icon data={faSpinner} spin={true}/>
+<Icon data={faSpinner} spin={true} {size} />
