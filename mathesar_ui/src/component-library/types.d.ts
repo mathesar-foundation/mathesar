59ca92bfc91c28d0d219579f55f08ecc8eb2e0fe
Gabriel <gabriel.sotomayor24@gmail.com>
2021-11-11 10:27:06 -0600

    Add radio type to index.ts file

--- mathesar_ui/src/component-library/types.d.ts
+++ mathesar_ui/src/component-library/types.d.ts
@@ -3,6 +3,7 @@ export type Size = 'small' | 'medium' | 'large';
 
 export type { Tab } from './tabs/TabContainer.d';
 export type { TreeItem } from './tree/Tree.d';
+export type { RadioGroupOption } from '/.radio-group/RadioGroup.d';
 export * from './icon/Icon.d';
 export * from './select/Select.d';
 export * from './file-upload/FileUpload.d';
