5025f8713a708cbf8c24948170e2f9602e3ef59d
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2021-11-23 13:44:13 +0200

    Revert to using mixins
    I was wrong. Apparently I just got the order of mixin application wrong.

--- db/filters/base.py
+++ db/filters/base.py
@@ -67,99 +67,85 @@ class Predicate:
         return getSAIdFromPredicateType(self.type)
 
 @frozen_dataclass
-class LeafPredicate(Predicate):
+class Leaf(Predicate):
     superType: PredicateSuperType = fauxStatic(PredicateSuperType.LEAF)
     type: LeafPredicateType
     field: str 
 
 @frozen_dataclass
-class LeafPredicateSingleParameter(LeafPredicate):
+class SingleParameter:
     parameterType: ParameterType = fauxStatic(ParameterType.SINGLE)
     parameter: Any
 
 @frozen_dataclass
-class LeafPredicateMultiParameter(LeafPredicate):
+class MultiParameter:
     parameterType: ParameterType = fauxStatic(ParameterType.MULTI)
     parameters: List[Any]
 
 @frozen_dataclass
-class LeafPredicateNoParameter(LeafPredicate):
+class NoParameter:
     parameterType: ParameterType = fauxStatic(ParameterType.NONE)
 
 @frozen_dataclass
-class BranchPredicate(Predicate):
+class Branch(Predicate):
     superType: PredicateSuperType = fauxStatic(PredicateSuperType.BRANCH)
     type: BranchPredicateType
 
 @frozen_dataclass
-class BranchPredicateSingleParameter(BranchPredicate):
-    parameterType: ParameterType = fauxStatic(ParameterType.SINGLE)
-    parameter: Predicate
-
-@frozen_dataclass
-class BranchPredicateMultiParameter(BranchPredicate):
-    parameterType: ParameterType = fauxStatic(ParameterType.MULTI)
-    parameters: List[Predicate]
-
-@frozen_dataclass
-class BranchPredicateNoParameter(BranchPredicate):
-    parameterType: ParameterType = fauxStatic(ParameterType.NONE)
-
-@frozen_dataclass
-class Equal(LeafPredicateSingleParameter):
+class Equal(SingleParameter, Leaf, Predicate):
     type: LeafPredicateType = fauxStatic(LeafPredicateType.EQUAL)
 
 @frozen_dataclass
-class Greater(LeafPredicateSingleParameter):
+class Greater(SingleParameter, Leaf, Predicate):
     type: LeafPredicateType = fauxStatic(LeafPredicateType.GREATER)
 
 @frozen_dataclass
-class GreaterOrEqual(LeafPredicateSingleParameter):
+class GreaterOrEqual(SingleParameter, Leaf, Predicate):
     type: LeafPredicateType = fauxStatic(LeafPredicateType.GREATER_OR_EQUAL)
 
 @frozen_dataclass
-class Lesser(LeafPredicateSingleParameter):
+class Lesser(SingleParameter, Leaf, Predicate):
     type: LeafPredicateType = fauxStatic(LeafPredicateType.LESSER)
 
 @frozen_dataclass
-class LesserOrEqual(LeafPredicateSingleParameter):
+class LesserOrEqual(SingleParameter, Leaf, Predicate):
     type: LeafPredicateType = fauxStatic(LeafPredicateType.LESSER_OR_EQUAL)
 
 @frozen_dataclass
-class Empty(LeafPredicateNoParameter):
+class Empty(NoParameter, Leaf, Predicate):
     type: LeafPredicateType = fauxStatic(LeafPredicateType.EMPTY)
 
 @frozen_dataclass
-class In(LeafPredicateMultiParameter):
+class In(MultiParameter, Leaf, Predicate):
     type: LeafPredicateType = fauxStatic(LeafPredicateType.IN)
 
 @frozen_dataclass
-class Not(BranchPredicateSingleParameter):
+class Not(SingleParameter, Branch, Predicate):
     type: BranchPredicateType = fauxStatic(BranchPredicateType.NOT)
 
 @frozen_dataclass
-class And(BranchPredicateMultiParameter):
+class And(MultiParameter, Branch, Predicate):
     type: BranchPredicateType = fauxStatic(BranchPredicateType.AND)
 
 @frozen_dataclass
-class Or(BranchPredicateMultiParameter):
+class Or(MultiParameter, Branch, Predicate):
     type: BranchPredicateType = fauxStatic(BranchPredicateType.OR)
 
 def getSAFilterSpecFromPredicate(pred: Predicate) -> dict:
-    if isinstance(pred, LeafPredicate):
-        if isinstance(pred, LeafPredicateSingleParameter):
+    if isinstance(pred, Leaf):
+        if isinstance(pred, SingleParameter):
             return {'field': pred.field, 'op': pred.saId(), 'value': pred.parameter}
-        elif isinstance(pred, LeafPredicateMultiParameter):
+        elif isinstance(pred, MultiParameter):
             return {'field': pred.field, 'op': pred.saId(), 'value': pred.parameters}
-        elif isinstance(pred, LeafPredicateNoParameter):
+        elif isinstance(pred, NoParameter):
             return {'field': pred.field, 'op': pred.saId()}
         else:
             raise Exception("This should never happen.")
-    elif isinstance(pred, BranchPredicate):
-        if isinstance(pred, BranchPredicateSingleParameter):
+    elif isinstance(pred, Branch):
+        if isinstance(pred, SingleParameter):
             subject = getSAFilterSpecFromPredicate(pred.parameter)
             return {pred.saId(): [subject]}
-        elif isinstance(pred, BranchPredicateMultiParameter):
+        elif isinstance(pred, MultiParameter):
             subjects = [ getSAFilterSpecFromPredicate(subject) for subject in pred.parameters ]
             return {pred.saId(): subjects}
         else:
