88efd0bf2f113339bf4fd68fa24b6812ca9e4de2
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2021-12-13 18:06:10 +0200

    Simplify deserialization

--- db/filters/operations/deserialize.py
+++ db/filters/operations/deserialize.py
@@ -14,15 +14,7 @@ def get_predicate_from_MA_filter_spec(spec: dict) -> Predicate:
         predicate_subclass = get_predicate_subclass_by_type_str(predicate_type_str)
         predicate_body = spec[predicate_type_str]
         if issubclass(predicate_subclass, Leaf):
-            columnName = predicate_body['column']
-            if issubclass(predicate_subclass, SingleParameter):
-                return predicate_subclass(column=columnName, parameter=predicate_body['parameter'])
-            elif issubclass(predicate_subclass, MultiParameter):
-                return predicate_subclass(column=columnName, parameters=predicate_body['parameters'])
-            elif issubclass(predicate_subclass, NoParameter):
-                return predicate_subclass(column=columnName)
-            else:
-                raise Exception("This should never happen.")
+            return predicate_subclass(*predicate_body)
         elif issubclass(predicate_subclass, Branch):
             if issubclass(predicate_subclass, SingleParameter):
                 parameter_predicate = get_predicate_from_MA_filter_spec(predicate_body)
