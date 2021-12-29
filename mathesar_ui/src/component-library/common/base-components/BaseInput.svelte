e3053506744adf0d69de78494c385606a2155d66
pavish <pavishkumar.r@gmail.com>
2021-12-30 00:36:19 +0530

    Create BaseInput headless component

---
+++ mathesar_ui/src/component-library/common/base-components/BaseInput.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { getLabelControllerFromContainingLabel } from '@mathesar-component-library-dir/label/LabelController';
+  import { getGloballyUniqueId } from '@mathesar-component-library-dir/common/utils/domUtils';
+
+  export let id = getGloballyUniqueId();
+  export let labelController = getLabelControllerFromContainingLabel();
+  export let disabled = false;
+
+  $: labelController?.disabled.set(disabled);
+  $: labelController?.inputId.set(id);
+</script>
