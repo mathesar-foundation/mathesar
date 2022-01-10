1a48f0362552c01a4f742bfa10e29373c40efe61
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2022-01-10 18:32:46 +0200

    Minor reformat

--- db/filters/operations/apply.py
+++ db/filters/operations/apply.py
@@ -28,7 +28,10 @@ def _ma_expression_to_sa_expression(expression):
     """An MA expression is considered to be either an Expression subclass or a literal."""
     if isinstance(expression, Expression):
         raw_parameters = expression.parameters
-        parameters = [_ma_expression_to_sa_expression(raw_parameter) for raw_parameter in raw_parameters]
+        parameters = [
+            _ma_expression_to_sa_expression(raw_parameter)
+            for raw_parameter in raw_parameters
+        ]
         expression_subclass = type(expression)
         return expression_subclass.to_sa_expression(*parameters)
     else:
