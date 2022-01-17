446fdf7df068faba7842560ddca8f8f17ae9b4ee
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2022-01-17 13:12:08 +0200

    Make make_hint private

--- db/functions/hints.py
+++ db/functions/hints.py
@@ -1,37 +1,37 @@
 from frozendict import frozendict
 
-def make_hint(id, **rest):
+def _make_hint(id, **rest):
     return frozendict({"id": id, **rest})
 
 def parameter_count(count):
-    return make_hint("parameter_count", count=count)
+    return _make_hint("parameter_count", count=count)
 
 
 def parameter(index, *hints):
-    return make_hint("parameter", index=index, hints=hints)
+    return _make_hint("parameter", index=index, hints=hints)
 
 
 def all_parameters(*hints):
-    return make_hint("all_parameters", hints=hints)
+    return _make_hint("all_parameters", hints=hints)
 
 
 def returns(*hints):
-    return make_hint("returns", hints=hints)
+    return _make_hint("returns", hints=hints)
 
 
-boolean = make_hint("boolean")
+boolean = _make_hint("boolean")
 
 
-comparable = make_hint("comparable")
+comparable = _make_hint("comparable")
 
 
-column = make_hint("column")
+column = _make_hint("column")
 
 
-array = make_hint("array")
+array = _make_hint("array")
 
 
-string_like = make_hint("string_like")
+string_like = _make_hint("string_like")
 
 
-uri = make_hint("uri")
+uri = _make_hint("uri")
