62cc86f1e14209c42ebcb7a9ca35b3342b3562bc
silentninja <mmukesh95@hotmail.com>
2022-01-20 00:53:36 +0530

    Change `ExceptionBody` to `ErrorBody` as it represents errors which are already caught. Add base Exception and Error classes which can be extended upon if needed

--- mathesar/api/exceptions/exceptions.py
+++ mathesar/api/exceptions/exceptions.py
@@ -1,7 +1,45 @@
 from collections import namedtuple
 
+from django.utils.encoding import force_str
+from rest_framework import status
+from rest_framework.exceptions import APIException as DRFApiException
 
-ExceptionBody = namedtuple('ExceptionBody',
-                           ['code', 'message', 'field', 'details'],
-                           defaults=[None, None]
-                           )
+from mathesar.api.exceptions.error_codes import ErrorCodes
+
+ErrorBody = namedtuple(
+        'ErrorBody',
+        ['code', 'message', 'field', 'details'],
+        defaults=[None, None]
+)
+
+
+def get_default_exception_detail(exception, error_code=ErrorCodes.NonClassifiedError.value,
+                                 message=None, field=None, details=None):
+    return ErrorBody(
+            message=force_str(exception) if message is None else message,
+            code=error_code,
+            field=field,
+            details=details
+    )
+
+
+def get_default_api_exception(exc):
+    return APIException(exc, ErrorCodes.NonClassifiedError.value)
+
+
+class APIException(DRFApiException):
+    def __init__(self, exception, error_code=ErrorCodes.NonClassifiedError.value, message=None, field=None,
+                 details=None, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR):
+        exception_detail = get_default_exception_detail(exception, error_code, message, field, details)._asdict()
+        self.detail = [exception_detail]
+        self.status_code = status_code
+
+
+class GenericApiError(APIException):
+    default_code = 'error'
+    """
+    Class which is used to convert list of errors into proper APIException
+    """
+    def __init__(self, error_body_list, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR):
+        self.detail = [error_body._asdict() for error_body in error_body_list]
+        self.status_code = status_code
