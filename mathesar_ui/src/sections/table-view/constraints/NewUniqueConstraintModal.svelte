b340541f663530a535c5a80a317b0f5226b6e14b
Sean Colsen <colsen.sean@gmail.com>
2022-01-20 15:35:10 -0500

    Add UI for adding unique constraints

---
+++ mathesar_ui/src/sections/table-view/constraints/NewUniqueConstraintModal.svelte
@@ -0,0 +1,158 @@
+<script lang="ts">
+  import { getContext } from 'svelte';
+  import type { ModalController } from '@mathesar-component-library';
+  import {
+    CheckboxGroup,
+    LabeledInput,
+    RadioGroup,
+    TextInput,
+  } from '@mathesar-component-library';
+  import { CancelOrProceedButtonPair } from '@mathesar-component-library';
+  import { ControlledModal } from '@mathesar-component-library';
+  import type { TabularDataStore } from '@mathesar/stores/table-data/types';
+  import { tables } from '@mathesar/stores/tables';
+  import FormField from '@mathesar/components/FormField.svelte';
+  import ColumnName from '@mathesar/components/ColumnName.svelte';
+  import { toast } from '@mathesar/stores/toast';
+  import Form from '@mathesar/components/Form.svelte';
+
+  export let controller: ModalController;
+
+  type NamingStrategy = 'auto' | 'manual';
+
+  const tabularData = getContext<TabularDataStore>('tabularData');
+  const namingStrategyOptions = [
+    { value: 'auto', label: 'Automatically' },
+    { value: 'manual', label: 'Manually' },
+  ];
+
+  function getSuggestedName(
+    _tableName: string,
+    columnNames: string[],
+    _existingConstraintNames: string[],
+  ): string | undefined {
+    let ordinal = 0;
+    while (true) {
+      const suffix = ordinal ? `_${ordinal}` : '';
+      const name = `${_tableName}_${columnNames.join('_')}${suffix}`;
+      if (!_existingConstraintNames.includes(name)) {
+        return name;
+      }
+      ordinal += 1;
+    }
+  }
+
+  function getNameValidationErrors(
+    _namingStrategy: NamingStrategy,
+    _constraintName: string | undefined,
+    _existingConstraintNames: string[],
+  ) {
+    if (_namingStrategy === 'auto') {
+      return [];
+    }
+    if (!_constraintName?.trim()) {
+      return ['Name cannot be empty'];
+    }
+    if (_existingConstraintNames.includes(_constraintName?.trim())) {
+      return ['A constraint with that name already exists'];
+    }
+    return [];
+  }
+
+  let constraintColumnNames: string[] = [];
+  let namingStrategy: NamingStrategy = 'auto';
+  let constraintName: string | undefined;
+
+  function init() {
+    constraintColumnNames = [];
+    namingStrategy = 'auto';
+  }
+
+  $: constraintsDataStore = $tabularData.constraintsDataStore;
+  $: existingConstraintNames = $constraintsDataStore.constraints.map(
+    (c) => c.name,
+  );
+  $: tableName = $tables.data.get($tabularData.id)?.name;
+  $: columnsDataStore = $tabularData.columnsDataStore;
+  $: columnsInTable = $columnsDataStore.columns;
+  $: columnsOptions = columnsInTable.map((column) => ({
+    value: column.name,
+    labelComponent: ColumnName,
+    labelComponentProps: { column },
+  }));
+  $: nameValidationErrors = getNameValidationErrors(
+    namingStrategy,
+    constraintName,
+    existingConstraintNames,
+  );
+  $: canProceed =
+    constraintColumnNames.length > 0 && !nameValidationErrors.length;
+
+  function handleNamingStrategyChange() {
+    // Begin with a suggested name as the starting value, but only do it when
+    // the user switches from 'auto' to 'manual'.
+    constraintName =
+      namingStrategy === 'manual'
+        ? getSuggestedName(
+            tableName,
+            constraintColumnNames,
+            existingConstraintNames,
+          )
+        : undefined;
+  }
+
+  async function handleSave() {
+    try {
+      await constraintsDataStore.add({
+        columns: constraintColumnNames,
+        type: 'unique',
+        name: constraintName,
+      });
+      controller.close();
+    } catch (error) {
+      toast.error(`Unable to add constraint. ${error.message as string}`);
+    }
+  }
+</script>
+
+<ControlledModal title="New Unique Constraint" {controller} on:open={init}>
+  <Form>
+    <FormField>
+      <CheckboxGroup
+        label="Columns"
+        options={columnsOptions}
+        bind:values={constraintColumnNames}
+      />
+    </FormField>
+
+    <FormField>
+      <RadioGroup
+        label="Set Constraint Name"
+        options={namingStrategyOptions}
+        bind:value={namingStrategy}
+        isInline
+        on:change={handleNamingStrategyChange}
+      />
+    </FormField>
+
+    {#if namingStrategy === 'manual'}
+      <FormField errors={nameValidationErrors}>
+        <LabeledInput label="Constraint Name" layout="stacked">
+          <span slot="label">Constraint Name</span>
+          <TextInput
+            bind:value={constraintName}
+            hasValidationErrors={nameValidationErrors.length > 0}
+          />
+        </LabeledInput>
+      </FormField>
+    {/if}
+  </Form>
+
+  <CancelOrProceedButtonPair
+    slot="footer"
+    onProceed={handleSave}
+    onCancel={() => controller.close()}
+    proceedButton={{ label: 'Add' }}
+    {canProceed}
+  />
+</ControlledModal>
--- mathesar_ui/src/sections/table-view/constraints/TableConstraints.svelte
+++ mathesar_ui/src/sections/table-view/constraints/TableConstraints.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
   import type { ModalController } from '@mathesar-component-library';
-  import { Icon, ControlledModal, Button } from '@mathesar-component-library';
+  import {
+    Icon,
+    ControlledModal,
+    Dropdown,
+    Button,
+  } from '@mathesar-component-library';
   import { getContext } from 'svelte';
   import type { TabularDataStore } from '@mathesar/stores/table-data/types';
   import type {
@@ -8,32 +13,39 @@
     ConstraintsDataStore,
   } from '@mathesar/stores/table-data/types';
   import { States } from '@mathesar/utils/api';
-  import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+  import {
+    faPlus,
+    faSnowflake,
+    faSpinner,
+  } from '@fortawesome/free-solid-svg-icons';
+  import { modal } from '@mathesar/stores/modal';
+  import NewUniqueConstraintModal from './NewUniqueConstraintModal.svelte';
   import TableConstraint from './TableConstraint.svelte';
 
   export let controller: ModalController;
 
   const tabularData = getContext<TabularDataStore>('tabularData');
+  const newUniqueConstraintModal = modal.spawnModalController();
 
-  $: constraintsDataStore =
-    $tabularData.constraintsDataStore as ConstraintsDataStore;
-  $: state = $constraintsDataStore.state as States;
-  $: errorMsg = $constraintsDataStore.error as string;
-  $: constraints = $constraintsDataStore.constraints as Constraint[];
+  $: constraintsDataStore = $tabularData.constraintsDataStore;
+  $: state = $constraintsDataStore.state;
+  $: errorMsg = $constraintsDataStore.error;
+  $: constraints = $constraintsDataStore.constraints;
   $: isEmpty = constraints.length === 0;
   $: isLoading = state === States.Idle || state === States.Loading;
   // Only show the spinner during the _initial_ loading event. Hide it for
   // subsequent updates so that we can rely on the spinner used on the button
   // for the more specific update.
-  $: shouldShowLoadingSpinner = isEmpty && (isLoading as boolean);
+  $: shouldShowLoadingSpinner = isEmpty && isLoading;
   $: countText = isEmpty ? '' : ` (${constraints.length as number})`;
+  $: title = `Table Constraints${countText as string}`;
 
   function remove(constraint: Constraint) {
     return (constraintsDataStore as ConstraintsDataStore).remove(constraint.id);
   }
 </script>
 
-<ControlledModal {controller} title={`Table Constraints${countText}`}>
+<ControlledModal {controller} {title}>
   <div class="table-constraints">
     {#if shouldShowLoadingSpinner}
       <Icon data={faSpinner} spin={true} />
@@ -50,8 +62,45 @@
       </div>
     {/if}
   </div>
+
+  <div slot="footer" class="footer">
+    <Dropdown closeOnInnerClick={true} ariaLabel="New table">
+      <div slot="trigger" class="trigger">
+        <Icon data={faPlus} />
+        <span class="label">New Constraint</span>
+      </div>
+      <div slot="content" class="content">
+        <Button
+          on:click={() => newUniqueConstraintModal.open()}
+          appearance="plain"
+        >
+          <Icon data={faSnowflake} />
+          <span>Unique</span>
+        </Button>
+      </div>
+    </Dropdown>
+  </div>
+
+  <NewUniqueConstraintModal controller={newUniqueConstraintModal} />
 </ControlledModal>
 
-<style global lang="scss">
-  @import 'TableConstraints.scss';
+<style>
+  .constraints-list {
+    border: solid #ccc 1px;
+    border-radius: 4px;
+  }
+  .footer {
+    text-align: right;
+  }
+  .trigger {
+    display: flex;
+    align-items: center;
+  }
+  .label {
+    margin-left: 4px;
+  }
+  .content > :global(*) {
+    display: flex;
+    width: 100%;
+  }
 </style>
