ea07f72f0c74d80db55102c4fa1e658c54634453
Sean Colsen <colsen.sean@gmail.com>
2022-01-25 13:23:42 -0500

    Fix linting errors

--- mathesar_ui/src/component-library/common/utils/__tests__/CancellablePromise.test.ts
+++ mathesar_ui/src/component-library/common/utils/__tests__/CancellablePromise.test.ts
@@ -13,12 +13,12 @@ test('CancellablePromise cancelled', () => {
   fakePromise.cancel();
   expect(fakePromise.isCancelled).toBeTruthy();
   expect(dummyOnCancel).toHaveBeenCalledTimes(1);
-  expect(fakePromise.then()).toEqual(null);
-  expect(fakePromise.catch()).toEqual(null);
-  expect(fakePromise.finally()).toEqual(null);
+  expect(fakePromise.then((res) => res)).toEqual(null);
+  expect(fakePromise.catch(() => {})).toEqual(null);
+  expect(fakePromise.finally(() => {})).toEqual(null);
 });
 
-test('CancellablePromise not cancelled gets resolved', () => {
+test('CancellablePromise not cancelled gets resolved', async () => {
   const fakePromise = new CancellablePromise((resolve, reject) => {
     try {
       resolve(202);
@@ -27,13 +27,15 @@ test('CancellablePromise not cancelled gets resolved', () => {
     }
   }, dummyOnCancel);
   expect(fakePromise.isCancelled).toBeFalsy();
-  expect(fakePromise.then((res) => res)).resolves.toEqual(202);
+  await expect(fakePromise.then((res) => res)).resolves.toBe(202);
 });
 
-test('CancellablePromise not cancelled gets rejected', () => {
+test('CancellablePromise not cancelled gets rejected', async () => {
   const fakePromise = new CancellablePromise((resolve, reject) => {
     reject('Rejected: error');
   }, dummyOnCancel);
   expect(fakePromise.isCancelled).toBeFalsy();
-  expect(fakePromise.then((res) => res)).rejects.toEqual('Rejected: error');
+  await expect(fakePromise.then((res) => res)).rejects.toEqual(
+    'Rejected: error',
+  );
 });
