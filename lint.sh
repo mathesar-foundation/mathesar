bc1b9c9f01ae588f40a8e503730dc39ddc1f11d8
silentninja <mmukesh95@hotmail.com>
2022-01-25 12:32:56 +0530

    Revert `lint.sh`

--- lint.sh
+++ lint.sh
@@ -21,7 +21,7 @@ endcol="\e[0m"
 indent() { sed 's/^/│  /'; } # Indents piped output with a indent guide lines
 
 # Run switches
-RUN_PYTHON="true"
+RUN_PYTHON="false"
 CHANGED_NON_NODE_FILES=$(git diff --name-only --staged -- . :^mathesar_ui)
 if [[ $CHANGED_NON_NODE_FILES != "" ]]; then
 	RUN_PYTHON="true"
