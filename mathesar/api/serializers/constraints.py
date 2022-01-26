f593cc39ea526cafd673832561ec0a321e33ee7c
silentninja <mmukesh95@hotmail.com>
2022-01-26 20:06:06 +0530

    Add migration for column model method changes Fix `PrimaryKeyRelatedField` queryset to use non reflection queryset manager to prevent runtime evaluation

--- mathesar/api/serializers/constraints.py
+++ mathesar/api/serializers/constraints.py
@@ -6,7 +6,7 @@ from mathesar.models import Constraint, Column
 class ConstraintSerializer(serializers.ModelSerializer):
     name = serializers.CharField(required=False)
     type = serializers.CharField()
-    columns = serializers.PrimaryKeyRelatedField(queryset=Column.objects.all(), many=True)
+    columns = serializers.PrimaryKeyRelatedField(queryset=Column.current_objects.all(), many=True)
 
     class Meta:
         model = Constraint
