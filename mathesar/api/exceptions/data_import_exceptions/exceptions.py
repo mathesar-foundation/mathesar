bcb6893c28a9d84f37913af0f58a16698beaf542
silentninja <mmukesh95@hotmail.com>
2022-01-31 20:53:57 +0530

    Remove unused `message` field from `mathesar.api.exceptions.data_import_exceptions.exceptions.URLInvalidContentTypeAPIException`

--- mathesar/api/exceptions/data_import_exceptions/exceptions.py
+++ mathesar/api/exceptions/data_import_exceptions/exceptions.py
@@ -44,7 +44,6 @@ class URLInvalidContentTypeAPIException(MathesarAPIException):
 
     def __init__(
             self, exception, error_code=ErrorCodes.URLInvalidContentType.value,
-            message=None,
             field=None,
             details=None, status_code=status.HTTP_400_BAD_REQUEST
     ):
