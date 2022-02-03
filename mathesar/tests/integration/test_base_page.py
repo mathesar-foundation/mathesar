93adbee9d760fbe7363abd90c72a4b10a2c8789b
silentninja <mmukesh95@hotmail.com>
2022-02-03 22:59:52 +0530

    Clear cache automatically before running integration test cases

--- mathesar/tests/integration/test_base_page.py
+++ mathesar/tests/integration/test_base_page.py
@@ -1,3 +1,12 @@
+import pytest
+from django.core.cache import cache
+
+
+@pytest.fixture(autouse=True)
+def clear_cache():
+    cache.clear()
+
+
 def test_page_shows_welcome_text(page, live_server):
     page.goto(f"{live_server}")
     page.pause()
