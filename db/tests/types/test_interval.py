1fab711df9dd0b2d7dde1d0bfe043e7e4549a565
Brent Moran <brentmoran@gmail.com>
2022-02-08 21:00:53 +0800

    remaining interval tests

--- db/tests/types/test_interval.py
+++ db/tests/types/test_interval.py
@@ -1,3 +1,4 @@
+from datetime import datetime as dt
 from psycopg2.errors import CheckViolation
 import pytest
 from sqlalchemy import text, Table, Column, MetaData, select, cast
@@ -5,7 +6,7 @@ from sqlalchemy import Interval as SAInterval
 from sqlalchemy.exc import IntegrityError
 from db.engine import _add_custom_types_to_engine
 from db.tests.types import fixtures
-from db.types import interval
+from db.types import interval, datetime
 
 
 # We need to set these variables when the file loads, or pytest can't
@@ -82,60 +83,64 @@ intervals_out_to_ins = {
     # different months have different numbers of days).
     'P1Y1M40DT0H0M0S': ['1 year 1 month 40 days', '13 months 40 days'],
     'P0Y0M0DT0H0M1.234567S': [
-        '1.234567 seconds', '1234.567 milliseconds', '1234.567ms'
-    ]
-
+        '1.234567 seconds', '1234.567 milliseconds', '1234.567ms',
+        '1234567 microseconds'
+    ],
+    # some more exotic time intervals
+    'P73Y2M10DT0H0M0S': ['5 decades 22 years 14 months 1 week 3 days'],
+    'P100Y0M0DT0H0M0S': ['1 century', '1C', '1 centuries', '10 decades'],
+    'P2000Y0M0DT0H0M0S': ['2 millennia', '2 millennium', '20 centuries'],
 }
 
 intervals_self_map = [(key, key) for key in intervals_out_to_ins]
 intervals_exploded = intervals_self_map + [
     (val, key) for key in intervals_out_to_ins for val in intervals_out_to_ins[key]
-    # ('3 decades 2 days ago', 'P-30Y0M-2DT0H0M0S'),
-    # ('3 decades 5 years 2 days ago', 'P-35Y0M-2DT0H0M0S'),
-    # (
-    #     '3 decades 5 years 2 days 100 hours 90 minutes 120 seconds',
-    #     'P35Y0M2DT101H32M0S',
-    # ),
 ]
 
 
 @pytest.mark.parametrize('interval_in,interval_out', intervals_exploded)
 def test_interval_transformations(engine_with_types, interval_in, interval_out):
+    # First we make sure the interval input strings cast properly
     with engine_with_types.begin() as conn:
         res = conn.execute(select(cast(interval_in, interval.Interval))).scalar()
     assert res == interval_out
-#
-#
-# def test_create_email_type_domain_passes_correct_emails(engine_email_type):
-#     engine, _ = engine_email_type
-#     email_addresses_correct = ["alice@example.com", "alice@example"]
-#     for address in email_addresses_correct:
-#         with engine.begin() as conn:
-#             res = conn.execute(
-#                 text(f"SELECT '{address}'::{email.DB_TYPE};")
-#             )
-#             assert res.fetchone()[0] == address
-#
-#
-# def test_create_email_type_domain_accepts_uppercase(engine_email_type):
-#     engine, _ = engine_email_type
-#     email_addresses_correct = ["alice@example.com", "alice@example"]
-#     for address in email_addresses_correct:
-#         with engine.begin() as conn:
-#             res = conn.execute(
-#                 text(f"SELECT '{address}'::{email.DB_TYPE.upper()};")
-#             )
-#             assert res.fetchone()[0] == address
-#
-#
-# def test_create_email_type_domain_checks_broken_emails(engine_email_type):
-#     engine, _ = engine_email_type
-#     address_incorrect = "aliceexample.com"
-#     with pytest.raises(IntegrityError) as e:
-#         with engine.begin() as conn:
-#             conn.execute(
-#                 text(
-#                     f"SELECT '{address_incorrect}'::{email.DB_TYPE};"
-#                 )
-#             )
-#         assert type(e.orig) == CheckViolation
+
+
+def test_interval_insert_select(engine_email_type):
+    # Now we bulk test inserting and selecting
+    engine, app_schema = engine_email_type
+    column_name = 'time_interval'
+    insert_dicts = [{column_name: tup[0]} for tup in intervals_exploded]
+    output_values = [tup[1] for tup in intervals_exploded]
+    with engine.begin() as conn:
+        metadata = MetaData(bind=conn, schema=app_schema)
+        test_table = Table(
+            'test_table',
+            metadata,
+            Column(column_name, interval.Interval),
+        )
+        test_table.create()
+        conn.execute(test_table.insert().values(insert_dicts))
+        res = conn.execute(select(test_table)).fetchall()
+    assert len(res) == len(output_values)
+    assert all([res[i][0] == output_values[i] for i in range(len(res))])
+
+
+def test_interval_datetime_addition(engine_with_types):
+    three_days_interval = cast('3 days 4 hours 30 minutes', interval.Interval)
+    the_date = cast(dt(2020, 1, 1), datetime.TIMESTAMP_WITHOUT_TIME_ZONE)
+    with engine_with_types.begin() as conn:
+        res = conn.execute(
+            select(the_date + three_days_interval)
+        ).scalar()
+    assert res == dt(2020, 1, 4, 4, 30)
+
+
+def test_interval_interval_addition(engine_with_types):
+    three_days_interval = cast('3 days 4 hours 30 minutes', interval.Interval)
+    five_days_interval = cast('5 days', interval.Interval)
+    with engine_with_types.begin() as conn:
+        res = conn.execute(
+            select(three_days_interval + five_days_interval)
+        ).scalar()
+    assert res == 'P0Y0M8DT4H30M0S'
