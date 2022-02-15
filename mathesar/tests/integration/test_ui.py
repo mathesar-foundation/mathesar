1c9886c3ce73c0225afdfb3db3380c59d2d3359b
Sean Colsen <colsen.sean@gmail.com>
2022-02-15 12:22:43 -0500

    Add E2E test for tabs system

--- mathesar/tests/integration/test_ui.py
+++ mathesar/tests/integration/test_ui.py
@@ -1,13 +1,72 @@
-from playwright.sync_api import Page, expect
+import re
+from playwright.sync_api import Page, expect, Locator
+from mathesar.tests.integration.utils.locators import get_table_entry
 
 
-def test_open_tab(page: Page, base_schema_url):
+def create_new_empty_table(page: Page):
+    page.click("[aria-label='New Table']")
+    page.click("button:has-text('Empty Table')")
+
+
+def get_tab(page: Page, tab_text):
+    return page.locator(
+        f".tab-container [role=tablist] [role=presentation]:has-text('{tab_text}')"
+    )
+
+
+def close_tab(tab_locator: Locator):
+    tab_locator.hover()
+    tab_locator.locator("[aria-label=remove]").click()
+
+
+def test_tabs(page: Page, base_schema_url):
     page.goto(base_schema_url)
-    page.pause()
-    raise NotImplementedError()
 
+    # Create Table 0
+    create_new_empty_table(page)
+    table_0_entry = get_table_entry(page, "Table 0")
+    table_0_tab = get_tab(page, "Table 0")
+    close_tab(table_0_tab)
 
-def test_close_tab(page: Page, base_schema_url):
-    page.goto(base_schema_url)
-    page.pause()
-    raise NotImplementedError()
+    # Create Table 1
+    create_new_empty_table(page)
+    table_1_entry = get_table_entry(page, "Table 1")
+    table_1_tab = get_tab(page, "Table 1")
+    close_tab(table_1_tab)
+
+    # No tabs should be open
+    expect(table_0_tab).not_to_be_visible()
+    expect(table_1_tab).not_to_be_visible()
+
+    # Open Table 0
+    table_0_entry.click()
+    expect(table_0_entry.locator(".item")).to_have_class(re.compile("active"))
+    expect(table_0_tab).to_have_class(re.compile("active"))
+
+    # Open Table 1
+    table_1_entry.click()
+    expect(table_0_entry.locator(".item")).not_to_have_class(re.compile("active"))
+    expect(table_1_entry.locator(".item")).to_have_class(re.compile("active"))
+    expect(table_0_tab).not_to_have_class(re.compile("active"))
+    expect(table_1_tab).to_have_class(re.compile("active"))
+
+    # Switch to tab for Table 0
+    table_0_tab.click()
+    expect(table_0_entry.locator(".item")).to_have_class(re.compile("active"))
+    expect(table_1_entry.locator(".item")).not_to_have_class(re.compile("active"))
+    expect(table_0_tab).to_have_class(re.compile("active"))
+    expect(table_1_tab).not_to_have_class(re.compile("active"))
+
+    # Close tab for Table 0
+    close_tab(table_0_tab)
+    expect(table_0_entry.locator(".item")).not_to_have_class(re.compile("active"))
+    expect(table_1_entry.locator(".item")).to_have_class(re.compile("active"))
+    expect(table_0_tab).not_to_be_visible()
+    expect(table_1_tab).to_have_class(re.compile("active"))
+
+    # Close tab for Table 1
+    close_tab(table_1_tab)
+    expect(table_0_entry.locator(".item")).not_to_have_class(re.compile("active"))
+    expect(table_1_entry.locator(".item")).not_to_have_class(re.compile("active"))
+    expect(table_0_tab).not_to_be_visible()
+    expect(table_1_tab).not_to_be_visible()
