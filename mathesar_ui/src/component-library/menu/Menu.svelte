271602823b7eee2d76acbac11183405cd3f8122f
Sean Colsen <colsen.sean@gmail.com>
2022-03-03 17:06:29 -0500

    Improve grid spacing in Menu

--- mathesar_ui/src/component-library/menu/Menu.svelte
+++ mathesar_ui/src/component-library/menu/Menu.svelte
@@ -7,7 +7,16 @@
 
 <style>
   .menu {
+    --spacing-x: 0.4em;
+    --spacing-y: 0.4em;
     display: inline-grid;
-    grid-template: 1fr / auto auto auto;
+    min-width: var(--min-width, 0);
+    grid-template-columns:
+      [spacer-left] var(--spacing-x)
+      [control] auto
+      [icon] auto
+      [label] 1fr
+      [spacer-right] var(--spacing-x);
+    padding: var(--spacing-y) 0;
   }
 </style>
--- mathesar_ui/src/component-library/menu/MenuItem.svelte
+++ mathesar_ui/src/component-library/menu/MenuItem.svelte
@@ -28,6 +28,7 @@
 </script>
 
 <div class="menu-item" on:click={handleClick} class:disabled>
+  <div class="spacer cell" />
   <div class="control cell"><slot name="control" /></div>
   <div class="icon cell">
     {#if icon}
@@ -41,8 +42,28 @@
       <slot />
     {/if}
   </div>
+  <div class="spacer cell" />
 </div>
 
+<!-- 
+Tricky aspects of this CSS:
+
+We want to vertically align all the controls, icons, and labels, even if the
+Menu doesn't consistently have a control or icon for all its MenuItems. We're
+leveraging the power of CSS grid to horizontally position the controls, icons,
+and labels so that they are in vertical alignment across the entire Menu.
+
+This component would be an ideal use-case for sub-grid, but sub-grid is not
+widely supported enough yet. So instead we're faking it by setting `display:
+contents;` on `.menu-item`. We also want a row hover background, but because of
+`display: contents;`, we need to set the hover background, per cell instead of
+per row. Because the hover background is per-cell, we can't use `gap` (or there
+would be gaps in the background). Foregoing `gap` makes it harder to get
+consistent spacing between and around cells, which is why we have the funky
+`padding` stuff and extra grid columns. I don't like the extra grid columns.
+There may be a cleaner way to do this, but I wasn't able to think of one
+quickly.
+-->
 <style>
   .menu-item {
     display: contents;
@@ -57,15 +78,15 @@
     color: #999;
   }
   .control {
-    grid-column: 1;
+    grid-column: 'control';
   }
   .icon {
-    grid-column: 2;
+    grid-column: 'icon';
   }
   .label {
-    grid-column: 3;
+    grid-column: 'label';
   }
   .cell:not(:empty) {
-    padding: 0.1em 0.4em;
+    padding: var(--spacing-y) var(--spacing-x);
   }
 </style>
