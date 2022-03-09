8167fb5c5a90f8b76d13d3fa7eb088e6a30414b8
Sean Colsen <colsen.sean@gmail.com>
2022-03-09 12:04:47 -0500

    Use global CSS for Menu system

--- mathesar_ui/src/component-library/menu/Menu.svelte
+++ mathesar_ui/src/component-library/menu/Menu.svelte
@@ -1,22 +1,3 @@
-<script lang="ts">
-</script>
-
 <div class="menu">
   <slot />
 </div>
-
-<style>
-  .menu {
-    --spacing-x: 0.4em;
-    --spacing-y: 0.4em;
-    display: inline-grid;
-    min-width: var(--min-width, 0);
-    grid-template-columns:
-      [spacer-left] var(--spacing-x)
-      [control] auto
-      [icon] auto
-      [label] 1fr
-      [spacer-right] var(--spacing-x);
-    padding: var(--spacing-y) 0;
-  }
-</style>
--- mathesar_ui/src/component-library/menu/MenuDivider.svelte
+++ mathesar_ui/src/component-library/menu/MenuDivider.svelte
@@ -1,10 +1,1 @@
 <div class="menu-divider" />
-
-<style>
-  .menu-divider {
-    grid-column: 1 / -1;
-    border-top: 0.1em solid #ccc;
-    height: 0;
-    margin: var(--spacing-y, 0.4em) 0;
-  }
-</style>
