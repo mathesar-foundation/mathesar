d1c59f598732ad994ddb56bf9e46857722dd7ce7
silentninja <mmukesh95@hotmail.com>
2022-03-10 18:23:12 +0530

    Add TimeStamp related types to `column_test_dict` which is used for testing default values

--- db/tests/columns/utils.py
+++ db/tests/columns/utils.py
@@ -1,14 +1,15 @@
-import datetime
 import decimal
-from datetime import date
 
 from sqlalchemy import (
     CHAR, FLOAT, SMALLINT, String, Integer, BOOLEAN, TEXT, VARCHAR, select, Table, MetaData, NUMERIC, BIGINT, DECIMAL,
     REAL
 )
-from sqlalchemy.dialects.postgresql import DOUBLE_PRECISION, INTERVAL, MONEY
+from sqlalchemy.dialects.postgresql import DOUBLE_PRECISION, MONEY
 
-from db.types.datetime import DATE
+from db.types.datetime import (
+    DATE, Interval, TIMESTAMP_WITHOUT_TIME_ZONE, TIMESTAMP_WITH_TIME_ZONE,
+    TIME_WITHOUT_TIME_ZONE, TIME_WITH_TIME_ZONE,
+)
 from db.types.email import Email
 from db.types.money import MathesarMoney
 from db.types import multicurrency
@@ -20,41 +21,35 @@ column_test_dict = {
     CHAR: {"start": "a", "set": "b", "expt": "b"},
     DECIMAL: {"start": "111.01111", "set": "111.01112", "expt": decimal.Decimal('111.01112')},
     DOUBLE_PRECISION: {"start": "111.01111", "set": "111.01112", "expt": 111.01112},
-    DATE: {"start": "1999-01-15 AD", "set": "1999-01-18 AD", "expt": date(1999, 1, 18)},
-    INTERVAL: {
+    DATE: {"start": "1999-01-15 AD", "set": "1999-01-18 AD", "expt": "1999-01-18 AD"},
+    Email: {"start": "alice@example.com", "set": "ob@example.com", "expt": "ob@example.com"},
+    Interval: {
         "start": "1 year 2 months 3 days 4 hours 5 minutes 6 seconds",
         "set": "1 year 2 months 3 days 4:05:06",
-        "expt": datetime.timedelta(days=428, seconds=14706)
+        "expt": "P1Y2M3DT4H5M6S"
     },
 
     FLOAT: {"start": "111.01111", "set": "111.01112", "expt": 111.01112},
     Integer: {"start": "0", "set": "5", "expt": 5},
     # Rounds to 2 digits
     MONEY: {"start": "$12,312.23", "set": "$12,312.24", "expt": "$12,312.24"},
-    NUMERIC: {"start": "111.01111", "set": "111.01112", "expt": decimal.Decimal('111.01112')},
-    REAL: {"start": "111.01111", "set": "111.01112", "expt": 111.01112},
-    SMALLINT: {"start": "500", "set": "500", "expt": 500},
-    TEXT: {"start": "default", "set": "test", "expt": "test"},
-    String: {"start": "default", "set": "test", "expt": "test"},
-    VARCHAR: {"start": "default", "set": "test", "expt": "test"},
-}
-custom_type_column_dict = {
-    DATE: {"start": "1999-01-15 AD", "set": "1999-01-18 AD", "expt": "1999-01-18 AD"},
-    Email: {"start": "alice@example.com", "set": "ob@example.com", "expt": "ob@example.com"},
-    INTERVAL: {
-        "start": "1 year 2 months 3 days 4 hours 5 minutes 6 seconds",
-        "set": "1 year 2 months 3 days 4:05:06",
-        "expt": "P1Y2M3DT4H5M6S"
-    },
     MathesarMoney: {"start": "12312.23", "set": "12312.24", "expt": decimal.Decimal("12312.24")},
     multicurrency.MulticurrencyMoney: {
         "start": "(1234.12,XYZ)", "set": "(1234.12,XYZ)",
         "expt": {multicurrency.CURRENCY: 'XYZ', multicurrency.VALUE: 1234.12}
     },
+    NUMERIC: {"start": "111.01111", "set": "111.01112", "expt": decimal.Decimal('111.01112')},
+    REAL: {"start": "111.01111", "set": "111.01112", "expt": 111.01112},
+    SMALLINT: {"start": "500", "set": "500", "expt": 500},
+    TIME_WITH_TIME_ZONE: {"start": "12:30:45.0Z", "set": "12:30:45.0+05:30", "expt": '12:30:45.0+05:30'},
+    TIME_WITHOUT_TIME_ZONE: {"start": "12:30:00.0", "set": "12:30:00.0", "expt": '12:30:00.0'},
+    TIMESTAMP_WITH_TIME_ZONE: {"start": "10000-01-01T00:00:00.0Z AD", "set": "2000-07-30T19:15:03.65Z AD", "expt": '2000-07-30T19:15:03.65Z AD'},
+    TIMESTAMP_WITHOUT_TIME_ZONE: {"start": "10000-01-01T00:00:00.0 AD", "set": "2000-07-30T19:15:03.65 AD", "expt": '2000-07-30T19:15:03.65 AD'},
+    TEXT: {"start": "default", "set": "test", "expt": "test"},
+    String: {"start": "default", "set": "test", "expt": "test"},
     URI: {"start": "https://centerofci.com", "set": "https://centerofci.org", "expt": "https://centerofci.org"},
+    VARCHAR: {"start": "default", "set": "test", "expt": "test"},
 }
-
-column_test_dict_with_engine = tuple(('engine_with_schema', col_name, col_data) for col_name, col_data in column_test_dict.items()) + tuple(('engine_email_type', col_name, col_data) for col_name, col_data in custom_type_column_dict.items())
 
 
 def create_test_table(table_name, cols, insert_data, schema, engine):
