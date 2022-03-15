5bb3fac6c464f162d9d47f6f1b0e22e8e44da115
Sahil <76941329+sahilsaini110@users.noreply.github.com>
2022-03-15 12:48:52 +0530

    Update test_columns.py

--- mathesar/tests/integration/test_columns.py
+++ mathesar/tests/integration/test_columns.py
@@ -9,6 +9,7 @@ def test_add_column(page, go_to_patents_data_table):
     column_header = f".table-content .header .cell .name:has-text('{column_name}')"
     expect(page.locator(column_header)).to_be_visible()
 
+    
 def test_convert_boolean_col_to_text_col(page, base_schema_url):
     page.goto(base_schema_url)
     page.click("[aria-label='New Table']")
@@ -22,3 +23,4 @@ def test_convert_boolean_col_to_text_col(page, base_schema_url):
     page.click("text=T Text")
     page.click("button:has-text('Save')")
     page.locator(".dropdown .container .section .btn:has-text('Text')")
+    
