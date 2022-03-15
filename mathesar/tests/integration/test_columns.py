1ce19b2da02cae6901c33ff0cabd5a593e602593
sahilsaini <sahilsaini110>
2022-03-15 11:37:57 +0530

    Add E2E test for converting a  boolean column to text column fixes #1127

--- mathesar/tests/integration/test_columns.py
+++ mathesar/tests/integration/test_columns.py
@@ -8,3 +8,18 @@ def test_add_column(page, go_to_patents_data_table):
     page.click("button:has-text('Add')")
     column_header = f".table-content .header .cell .name:has-text('{column_name}')"
     expect(page.locator(column_header)).to_be_visible()
+
+
+def test_convert_boolean_col_to_text_col(page, base_schema_url):
+    page.goto(base_schema_url)
+    page.click("[aria-label='New Table']")
+    page.click("button:has-text('Import Data')")
+    page.click("text=Copy and Paste Text")
+    page.fill("textarea", "foo,bar\ntrue,false")
+    page.click("button:has-text('Continue')")
+    page.click("button:has-text('Finish Import')")
+    page.click("button:has-text('foo')")
+    page.click(".dropdown .container .section .btn:has-text('Boolean')")
+    page.click("text=Text")
+    page.click("button:has-text('Save')")
+    page.locator(".dropdown .container .section .btn:has-text('Text')")
