d53279fa484c22ce336bb3821004bfc632a623ba
Ova <centriukas@gmail.com>
2022-03-16 19:01:57 +0200

    fix lint error in test_ui.py, table_actions.py

--- mathesar/tests/integration/utils/table_actions.py
+++ mathesar/tests/integration/utils/table_actions.py
@@ -2,12 +2,12 @@ from mathesar.tests.integration.utils.validators import expect_modal_not_to_be_v
 from playwright.sync_api import Locator, Page
 
 
-def create_empty_table(page):
+def create_empty_table(page: Page):
     page.click("[aria-label='New Table']")
     page.click("button:has-text('Empty Table')")
 
 
-def delete_active_table(page):
+def delete_active_table(page: Page):
     page.click("[aria-label='Table']")
     page.click("div.menu div.menu-item:visible :text('Delete')")
     expect_modal_to_be_visible(page, "Delete Table?")
