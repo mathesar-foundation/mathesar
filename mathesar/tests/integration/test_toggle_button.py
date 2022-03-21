8801ce43f368a519095425a90e4ab506e586ef9f
Cronus1007 <lit2019058@iiitl.ac.in>
2022-03-21 19:33:50 +0530

    basic function defination for toggling buttons

---
+++ mathesar/tests/integration/test_toggle_button.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import expect
+
+from mathesar.tests.integration.utils.locators import get_table_entry, get_tables_list
+
+
+def test_toggle_button(page, base_schema_url):
+    page.goto(base_schema_url)
+    expect(get_tables_list(page)).to_be_empty()
+    page.click("[aria-label='New Table']")
+    page.click("button:has-text('Import Data')")
+    page.click("text=Copy and Paste Text")
+    page.fill("textarea", "foo\ntrue\nfalse\nfalse\ntrue")
+    page.click("button:has-text('Continue')")
+    page.click("button:has-text('Finish Import')")
+    expect(get_table_entry(page, "Table 0")).to_be_visible()
+    page.click("button:has-text('New Record')")
+    page.click("button:has-text('Refresh')")    
+    cell1 = page.goto("role=checkbox")
+    print(cell1)
