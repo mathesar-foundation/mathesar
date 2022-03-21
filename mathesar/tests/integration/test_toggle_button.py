edb6eeb377bcf9640d1df1a19a30e3392200734b
Cronus1007 <lit2019058@iiitl.ac.in>
2022-03-21 22:56:47 +0530

    fully functional tests for toggling the checkbox

--- mathesar/tests/integration/test_toggle_button.py
+++ mathesar/tests/integration/test_toggle_button.py
@@ -1,3 +1,6 @@
+from multiprocessing.context import assert_spawning
+from tkinter.tix import Tree
+from xml.etree import cElementTree
 from playwright.sync_api import expect
 
 from mathesar.tests.integration.utils.locators import get_table_entry, get_tables_list
@@ -15,3 +18,10 @@ def test_toggle_button(page, base_schema_url):
     expect(get_table_entry(page, "Table 0")).to_be_visible()
     page.click("button:has-text('New Record')")
     page.click("button:has-text('Refresh')")    
+    cell1 = page.goto(".s-sG5YPZwKUQz_ div :nth-child(5) input[type=checkbox]")
+    page.check(cell1)
+    assert page.is_checked(cell1) is True
+    page.uncheck(cell1)
+    assert page.is_checked(cell1) is False
+    page.check(cell1)
+    assert page.is_checked(cell1) is True
