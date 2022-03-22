0dc47cd4aad7891a1eb5e43674872c3a44ce8290
Avi Wadhwa <aviwad@gmail.com>
2022-03-21 18:37:40 -0700

    increase timeout for function test_import_from_file

--- mathesar/tests/integration/test_imports.py
+++ mathesar/tests/integration/test_imports.py
@@ -16,10 +16,12 @@ def test_import_from_clipboard(page, base_schema_url):
 
 
 def test_import_from_file(page, base_schema_url):
-    page.goto(base_schema_url)
+    # default timeout (5000ms) is too short for this function
+    page.set_default_timeout(10000)page.goto(base_schema_url)
     page.click("[aria-label='New Table']")
     page.click("button:has-text('Import Data')")
     page.set_input_files(".file-upload input", "/code/mathesar/tests/data/patents.csv")
     page.click("button:has-text('Finish Import')")
+    page.pause()
     # "1393 records" is part of the text shown below the table near the pager
     expect(page.locator("text=1393 records")).to_be_visible()
