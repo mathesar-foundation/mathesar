36cdaa2d81cd743ca3d0a462487127803d376184
Cronus1007 <lit2019058@iiitl.ac.in>
2022-03-22 14:36:49 +0530

    resolved errors in ci/cd pipelines

--- mathesar/tests/integration/test_toggle_button.py
+++ mathesar/tests/integration/test_toggle_button.py
@@ -1,11 +1,12 @@
-from playwright.sync_api import expect
-
+from playwright.sync_api import Playwright, sync_playwright, expect
 from mathesar.tests.integration.utils.locators import get_table_entry, get_tables_list
 
-
-def test_toggle_button(page, base_schema_url):
-    page.goto(base_schema_url)
-    expect(get_tables_list(page)).to_be_empty()
+def run(playwright: Playwright) -> None:
+    browser = playwright.firefox.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("http://0.0.0.0:8000/mathesar_tables/1/")
+    page.goto("http://0.0.0.0:8000/mathesar_tables/1/?t=W1tdLG51bGxd")
     page.click("[aria-label='New Table']")
     page.click("button:has-text('Import Data')")
     page.click("text=Copy and Paste Text")
@@ -15,10 +16,13 @@ def test_toggle_button(page, base_schema_url):
     expect(get_table_entry(page, "Table 0")).to_be_visible()
     page.click("button:has-text('New Record')")
     page.click("button:has-text('Refresh')")
-    cell1 = page.goto(".s-sG5YPZwKUQz_ div :nth-child(5) input[type=checkbox]")
-    page.check(cell1)
-    assert page.is_checked(cell1) is True
-    page.uncheck(cell1)
-    assert page.is_checked(cell1) is False
-    page.check(cell1)
-    assert page.is_checked(cell1) is True
+    cell1 = page.locator("div:nth-child(5) div:nth-child(3) .cell-wrapper input[type=checkbox]")
+    cell1.check()
+    assert page.is_checked("div:nth-child(5) div:nth-child(3) .cell-wrapper input[type=checkbox]") is True
+    cell1.uncheck()
+    assert page.is_checked("div:nth-child(5) div:nth-child(3) .cell-wrapper input[type=checkbox]") is False
+    cell1.check()
+    assert page.is_checked("div:nth-child(5) div:nth-child(3) .cell-wrapper input[type=checkbox]") is True
+
+with sync_playwright() as playwright:
+    run(playwright)
