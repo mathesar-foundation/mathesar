e614e2c7af133287ad8b0b8636b28d947105bd95
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2022-03-24 14:23:51 +0200

    Remove other aliases remnants

--- mathesar/api/serializers/db_types.py
+++ mathesar/api/serializers/db_types.py
@@ -11,11 +11,7 @@ class DBTypeSerializer(serializers.Serializer):
         # TODO solve db type casing holistically
         # https://github.com/centerofci/mathesar/issues/1036
         uppercase_id = db_type.value.upper()
-        uppercased_aliases = [db_type_id.upper() for db_type_id in db_type.aliases]
-        uppercased_alias_of = db_type.alias_of.upper() if db_type.alias_of else None
         return {
             "id": uppercase_id,
-            "aliases": uppercased_aliases,
-            "alias_of": uppercased_alias_of,
             "hints": db_types_hinted.get(db_type, None),
         }
