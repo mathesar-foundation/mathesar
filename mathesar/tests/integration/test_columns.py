b8fcf38f13512943544303b3ae82ea138cd970a2
A1O <centriukas@gmail.com>
2022-04-04 01:20:44 +0300

    e2e test for grouping by a column

--- mathesar/tests/integration/test_columns.py
+++ mathesar/tests/integration/test_columns.py
@@ -30,3 +30,20 @@ def test_convert_text_col_of_num_to_num_col(page, go_to_table_with_numbers_in_te
     page.click("button:has-text('Number')")
     selected_type = ".section .type-list li.selected"
     expect(page.locator(selected_type)).to_contain_text("Number")
+
+
+def test_group_by_column(page, go_to_patents_data_table):
+    # group by the column "Center"
+    page.click(".table-content .header .cell:has-text('Center')")
+    page.click(".dropdown button:has-text('Group by column')")
+    # verify the count of the first listed group and its name
+    expect(page.locator("text=count: 138")).to_be_visible()
+    expect(page.locator("text=Center: undefined")).to_be_visible()
+    expect(page.locator("button:has-text('Group (1)')")).to_be_visible()
+    # ungroup
+    page.click(".table-content .header .cell:has-text('Center')")
+    page.click("button:has-text('Remove grouping')")
+    # verify
+    expect(page.locator("text=count: 138")).not_to_be_visible()
+    expect(page.locator("text=Center: undefined")).not_to_be_visible()
+    expect(page.locator("button:has-text('Group')")).to_be_visible()
