4d088379471854d9fb92f67971da944123de1d19
pavish <pavishkumar.r@gmail.com>
2022-04-12 16:34:52 +0530

    Support auto scrolling to focused item in listbox options

--- mathesar_ui/src/component-library/listbox/ListBoxOptions.svelte
+++ mathesar_ui/src/component-library/listbox/ListBoxOptions.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { getContext } from 'svelte';
+  import { onMount, getContext, tick } from 'svelte';
   import type { ListBoxContext } from './ListBoxTypes';
 
   type Option = $$Generic;
@@ -8,16 +8,53 @@
 
   const { api, state } = getContext<ListBoxContext<Option>>('LISTBOX_CONTEXT');
   const { displayedOptions, value, focusedOptionIndex, staticProps } = state;
+
+  let listboxelement: HTMLUListElement;
+
+  function scrollToFocusedItem(): void {
+    if (listboxelement) {
+      const elementInFocus: HTMLElement | null =
+        listboxelement.querySelector('.in-focus');
+      const container = listboxelement.parentElement as HTMLElement;
+      if (elementInFocus && container) {
+        if (
+          elementInFocus.offsetTop + elementInFocus.clientHeight >
+          container.scrollTop + container.clientHeight
+        ) {
+          const offsetValue: number =
+            container.getBoundingClientRect().bottom -
+            elementInFocus.getBoundingClientRect().bottom;
+          container.scrollTop -= offsetValue;
+        } else if (elementInFocus.offsetTop < container.scrollTop) {
+          container.scrollTop = elementInFocus.offsetTop;
+        }
+      }
+    }
+  }
+
+  onMount(() => {
+    scrollToFocusedItem();
+
+    return focusedOptionIndex.subscribe(() => {
+      void tick().then(() => scrollToFocusedItem());
+    });
+  });
 </script>
 
-<ul tabindex="0" {id} role="listbox" aria-expanded="true">
+<ul
+  bind:this={listboxelement}
+  tabindex="0"
+  {id}
+  role="listbox"
+  aria-expanded="true"
+>
   {#each $displayedOptions as option, index (option)}
     <li
       role="option"
       class:selected={$value.some((opt) =>
         $staticProps.checkEquality(opt, option),
       )}
-      class:hovered={index === $focusedOptionIndex}
+      class:in-focus={index === $focusedOptionIndex}
       on:click={() => api.select(option)}
     >
       <span>{$staticProps.getLabel(option, $staticProps.labelKey)}</span>
