2176e43a46e5e149f2ca4c695cab5ba70037b565
jyuart <yura.palayda@gmail.com>
2022-04-12 22:06:03 +0300

    add integration tests

---
+++ mathesar/tests/integration/test_scroll_state.py
@@ -0,0 +1,36 @@
+import pytest
+from playwright.sync_api import expect
+from mathesar.tests.integration.utils.locators import get_table_entry
+from mathesar.tests.integration.utils.table_actions import create_empty_table, rename_column
+
+def test_scroll_preserving_on_tabs_switching(page, go_to_patents_data_table):
+    # initially, the 30th element is not in the view
+    thirtieth_element = page.locator('span.number:text-is("30")')
+    expect(thirtieth_element).not_to_be_visible()
+    # scroll makes it visible
+    page.locator(".ps__rail-y").click()
+    expect(thirtieth_element).to_be_visible()
+    # creating new table switches to its tab
+    create_empty_table(page)
+    get_table_entry(page, 'patents').click()
+    expect(thirtieth_element).to_be_visible()
+
+def test_scroll_preserving_on_active_table_updating(page, go_to_patents_data_table):
+    thirtieth_element = page.locator('span.number:text-is("30")')
+    expect(thirtieth_element).not_to_be_visible()
+    page.locator(".ps__rail-y").click()
+    expect(thirtieth_element).to_be_visible()
+    rename_column(page, 'Center', 'Center2')
+    expect(thirtieth_element).to_be_visible()
+
+# add Filter when implemented
+@pytest.mark.parametrize("action", ['Sort', 'Group'])
+def test_scroll_resetting_on_sort_group_applying(page, go_to_patents_data_table, action):
+    thirtieth_element = page.locator('span.number:text-is("30")')
+    expect(thirtieth_element).not_to_be_visible()
+    page.locator(".ps__rail-y").click()
+    expect(thirtieth_element).to_be_visible()
+    page.locator(f"button:has-text('{action}')").click()
+    page.locator(f"button:has-text('Add new {action} column')").click()
+    page.locator("td.action >> button:first-child").click()
+    expect(thirtieth_element).not_to_be_visible()
--- mathesar/tests/integration/utils/table_actions.py
+++ mathesar/tests/integration/utils/table_actions.py
@@ -27,3 +27,9 @@ def rename_table(page, new_name):
     page.fill("div.modal input[aria-label='name']", new_name)
     page.click("div.cancel-or-proceed-button-pair span:has-text('Save')")
     expect_modal_not_to_be_visible(page)
+
+def rename_column(page, old_name, new_name):
+    page.click(f"button:has-text('{old_name}')")
+    page.click("button:has-text('Rename')")
+    page.fill("[aria-label='Column name']", new_name)
+    page.keyboard.press("Enter")
