29d792a37166f5bbfd45425207aaa492c9fc7d3f
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2022-04-21 17:56:49 +0300

    some initial changes to mathesar ns

--- mathesar/database/types.py
+++ mathesar/database/types.py
@@ -3,35 +3,239 @@ This file maps "friendly" Mathesar data types to Postgres database types.
 Mathesar data types are shown in the UI.
 """
 from enum import Enum
-
+from collections.abc import Set, Sequence, Collection, Mapping, MutableSequence
+from typing import Optional
+from copy import deepcopy
 from db.types.base import (
-    DatabaseType, PostgresType, MathesarCustomType, get_available_known_db_types,
-    get_qualified_name
+    DatabaseType, PostgresType, MathesarCustomType, get_available_known_db_types
     )
 from db.types.hintsets import db_types_hinted
 
-from copy import deepcopy
 
-from collections.abc import Sequence, Mapping, MutableSequence
-
-from typing import Optional
-
-
-class MathesarType(Enum):
-    BOOLEAN = 'boolean'
-    DATETIME = 'datetime'
-    TIME = 'time'
-    DATE = 'date'
-    DURATION = 'duration'
-    EMAIL = 'email'
-    MONEY = 'money'
-    NUMBER = 'number'
-    TEXT = 'text'
-    URI = 'uri'
+class UIType(Enum):
+    BOOLEAN = (
+        'boolean',
+        'Boolean',
+        {
+            PostgresType.BOOLEAN,
+        },
+    )
+    DATE = (
+        'date',
+        'Date',
+        {
+            PostgresType.DATE,
+        },
+    )
+    TIME = (
+        'time',
+        'Time',
+        {
+            PostgresType.TIME_WITH_TIME_ZONE,
+            PostgresType.TIME_WITHOUT_TIME_ZONE,
+        },
+    )
+    DATETIME = (
+        'datetime',
+        'Date & Time',
+        {
+            PostgresType.TIMESTAMP_WITH_TIME_ZONE,
+            PostgresType.TIMESTAMP_WITHOUT_TIME_ZONE,
+        },
+    )
+    DURATION = (
+        'duration',
+        'Duration',
+        {
+            PostgresType.INTERVAL,
+        },
+    )
+    EMAIL = (
+        'email',
+        'Email',
+        {
+            MathesarCustomType.EMAIL,
+        },
+    )
+    MONEY = (
+        'money',
+        'Money',
+        {
+            PostgresType.MONEY,
+            MathesarCustomType.MATHESAR_MONEY,
+            MathesarCustomType.MULTICURRENCY_MONEY,
+        }
+    )
+    NUMBER = (
+        'number',
+        'Number',
+        {
+            PostgresType.BIGINT,
+            PostgresType.DOUBLE_PRECISION,
+            PostgresType.FLOAT,
+            PostgresType.INTEGER,
+            PostgresType.NUMERIC,
+            PostgresType.REAL,
+            PostgresType.SMALLINT,
+        }
+    )
+    TEXT = (
+        'text',
+        'Text',
+        {
+            PostgresType.CHARACTER,
+            PostgresType.CHARACTER_VARYING,
+            PostgresType.TEXT,
+            PostgresType.NAME,
+            PostgresType.CHAR,
+        },
+    )
+    URI = (
+        'uri',
+        'URI',
+        {
+            MathesarCustomType.URI,
+        }
+    )
     # These are default Postgres types that we don't have specific behavior for yet in the UI.
-    OTHER = 'other'
-    # These are types that we don't know anything about.
-    CUSTOM = 'custom'
+    OTHER = (
+        'other',
+        'Other',
+        {
+            PostgresType._ARRAY,
+            PostgresType.BIT_VARYING,
+            PostgresType.BIT,
+            PostgresType.BYTEA,
+            PostgresType.CIDR,
+            PostgresType.DATERANGE,
+            PostgresType.HSTORE,
+            PostgresType.INET,
+            PostgresType.INT4RANGE,
+            PostgresType.INT8RANGE,
+            PostgresType.JSON,
+            PostgresType.JSONB,
+            PostgresType.MACADDR,
+            PostgresType.NUMRANGE,
+            PostgresType.OID,
+            PostgresType.REGCLASS,
+            PostgresType.TIMESTAMP,
+            PostgresType.TSRANGE,
+            PostgresType.TSTZRANGE,
+            PostgresType.TSVECTOR,
+            PostgresType.UUID,
+        },
+    )
+
+    def __new__(cls, ui_type_id, display_name, db_types: Set[DatabaseType]):
+        """
+        The Enum is adapted to take three initial properties. Enum's value is set to be the first
+        property -- the id.
+        """
+        obj = object.__new__(cls)
+        obj._value_ = ui_type_id
+        obj.id = ui_type_id
+        obj.display_name = display_name
+        obj.db_types = frozenset(db_types)
+        return obj
+
+
+def ma_types_that_satisfy_hintset(ma_types_mapped_to_hintsets, hintset):
+    """
+    Provided a mapping of Mathesar types to their hintsets and a hintset, tries to find Mathesar
+    types whose hintsets satisfy the passed hintset, meaning the Mathesar types whose hintsets are
+    supersets of the passed hintset.
+    """
+    hintset = set(hintset)
+    return tuple(
+        ma_type
+        for ma_type, ma_type_hintset
+        in ma_types_mapped_to_hintsets.items()
+        if set.issubset(hintset, ma_type_hintset)
+    )
+
+
+def get_ma_types_mapped_to_hintsets(engine):
+    """
+    Returns a dict where the keys are Mathesar types and the values their hintsets.
+    A Mathesar type's hintset is defined as the intersection of the hintsets of its associated
+    database types.
+    """
+    ma_type_descriptions = get_types(engine)
+    ma_types_mapped_to_hintsets = {}
+    for ma_type_description in ma_type_descriptions:
+        ma_type = ma_type_description['ma_type']
+        associated_db_types = ma_type_description['db_types']
+        associated_db_type_hintsets = tuple(
+            set(db_types_hinted[associated_db_type])
+            for associated_db_type in associated_db_types
+            if associated_db_type in db_types_hinted
+        )
+        hintsets_intersection = _safe_set_intersection(associated_db_type_hintsets)
+        ma_types_mapped_to_hintsets[ma_type] = tuple(hintsets_intersection)
+    return ma_types_mapped_to_hintsets
+
+
+def _safe_set_intersection(sets):
+    # set.intersection fails if it is not passed anything.
+    if len(sets) > 0:
+        return set.intersection(*sets)
+    else:
+        return set()
+
+
+# TODO notice the awkwardness between _get_actual_type_map and _get_type_map
+def _get_actual_type_map(engine) -> Sequence[Mapping]:
+    installed_types = get_available_known_db_types(engine)
+    type_map = _get_type_map()
+    return type_map
+
+
+# TODO rename to get_ma_types: it's important to easily distinguish MA types referred to here from
+# other types like DB or SA types.
+# TODO consider just having get_types(engine) -> Collection[MathesarType]
+# that implies moving a lot of this logic to the MathesarType enum
+def get_types(engine) -> Collection[Mapping]:
+    """
+    Returns a sequence of dicts describing Mathesar types supported by the database associated with
+    the provided engine. It has the same structure as the output of _get_type_map.
+    """
+    def filter_out_ignored_types(ma_type_desc) -> Mapping:
+        db_types = ma_type_desc["db_types"]
+        filtered_db_types = tuple(
+            db_type
+            for db_type
+            in db_types
+            if (not db_type.is_ignored) and (not _is_ignored_type(db_type))
+        )
+        ma_type_desc["db_types"] = filtered_db_types
+        return ma_type_desc
+    ma_type_descriptions = _get_actual_type_map(engine)
+    ma_type_descriptions = deepcopy(ma_type_descriptions)
+    return tuple(
+        filter_out_ignored_types(ma_type_desc)
+        for ma_type_desc
+        in ma_type_descriptions
+    )
+
+
+def _get_db_types_to_mathesar_types_iterator():
+    """
+    Returns an iterator of (db_type, ma_type) pairs, where the db_type is associated with the
+    ma_type. We use an iterator for its laziness.
+    """
+    return (
+        (db_type, ma_type_desc['ma_type'])
+        for ma_type_desc
+        in _get_type_map()
+        for db_type
+        in ma_type_desc['db_types']
+    )
+
+
+def get_mathesar_type_from_db_type(db_type_to_find: DatabaseType) -> Optional[MathesarType]:
+    for db_type, ma_type in _get_db_types_to_mathesar_types_iterator():
+        if db_type == db_type_to_find:
+            return ma_type
 
 
 def _get_mapped_types(type_map) -> Sequence[DatabaseType]:
@@ -92,21 +296,20 @@ def _get_type_map() -> MutableSequence[dict]:
     }, {
         'ma_type': MathesarType.EMAIL,
         'name': 'Email',
-        'db_types': [get_qualified_name(MathesarCustomType.EMAIL)]
+        'db_types': [MathesarCustomType.EMAIL]
     }, {
         'ma_type': MathesarType.MONEY,
         'name': 'Money',
         'db_types': [
             PostgresType.MONEY,
-            get_qualified_name(MathesarCustomType.MATHESAR_MONEY),
-            get_qualified_name(MathesarCustomType.MULTICURRENCY_MONEY),
+            MathesarCustomType.MATHESAR_MONEY,
+            MathesarCustomType.MULTICURRENCY_MONEY,
         ]
     }, {
         'ma_type': MathesarType.NUMBER,
         'name': 'Number',
         'db_types': [
             PostgresType.BIGINT,
-            PostgresType.DECIMAL,
             PostgresType.DOUBLE_PRECISION,
             PostgresType.FLOAT,
             PostgresType.INTEGER,
@@ -127,7 +330,7 @@ def _get_type_map() -> MutableSequence[dict]:
     }, {
         'ma_type': MathesarType.URI,
         'name': 'URI',
-        'db_types': [get_qualified_name(MathesarCustomType.URI)]
+        'db_types': [MathesarCustomType.URI]
     }]
     type_map.append(_get_other_types(type_map))
     return type_map
@@ -158,103 +361,3 @@ def _is_ignored_type(db_type: DatabaseType):
         PostgresType.NAME,
     ]
     return db_type in IGNORED_TYPES
-
-
-def ma_types_that_satisfy_hintset(ma_types_mapped_to_hintsets, hintset):
-    """
-    Provided a mapping of Mathesar types to their hintsets and a hintset, tries to find Mathesar
-    types whose hintsets satisfy the passed hintset, meaning the Mathesar types whose hintsets are
-    supersets of the passed hintset.
-    """
-    hintset = set(hintset)
-    return tuple(
-        ma_type
-        for ma_type, ma_type_hintset
-        in ma_types_mapped_to_hintsets.items()
-        if set.issubset(hintset, ma_type_hintset)
-    )
-
-
-def get_ma_types_mapped_to_hintsets(engine):
-    """
-    Returns a dict where the keys are Mathesar types and the values their hintsets.
-    A Mathesar type's hintset is defined as the intersection of the hintsets of its associated
-    database types.
-    """
-    ma_type_descriptions = get_types(engine)
-    ma_types_mapped_to_hintsets = {}
-    for ma_type_description in ma_type_descriptions:
-        ma_type = ma_type_description['ma_type']
-        associated_db_types = ma_type_description['db_types']
-        associated_db_type_hintsets = tuple(
-            set(db_types_hinted[associated_db_type])
-            for associated_db_type in associated_db_types
-            if associated_db_type in db_types_hinted
-        )
-        hintsets_intersection = _safe_set_intersection(associated_db_type_hintsets)
-        ma_types_mapped_to_hintsets[ma_type] = tuple(hintsets_intersection)
-    return ma_types_mapped_to_hintsets
-
-
-def _safe_set_intersection(sets):
-    # set.intersection fails if it is not passed anything.
-    if len(sets) > 0:
-        return set.intersection(*sets)
-    else:
-        return set()
-
-
-# TODO notice the awkwardness between _get_actual_type_map and _get_type_map
-def _get_actual_type_map(engine) -> Sequence[Mapping]:
-    installed_types = get_available_known_db_types(engine)
-    type_map = _get_type_map()
-    type_map.append(_get_custom_types(type_map, installed_types))
-    return type_map
-
-
-# TODO rename to get_ma_types: it's important to easily distinguish MA types referred to here from
-# other types like DB or SA types.
-# TODO consider just having get_types(engine) -> Collection[MathesarType]
-# that implies moving a lot of this logic to the MathesarType enum
-def get_types(engine) -> Sequence[Mapping]:
-    """
-    Returns a sequence of dicts describing Mathesar types supported by the database associated with
-    the provided engine. It has the same structure as the output of _get_type_map.
-    """
-    def filter_out_ignored_types(ma_type_desc) -> Mapping:
-        db_types = ma_type_desc["db_types"]
-        filtered_db_types = tuple(
-            db_type
-            for db_type
-            in db_types
-            if (not db_type.is_ignored) and (not _is_ignored_type(db_type))
-        )
-        ma_type_desc["db_types"] = filtered_db_types
-        return ma_type_desc
-    ma_type_descriptions = _get_actual_type_map(engine)
-    ma_type_descriptions = deepcopy(ma_type_descriptions)
-    return tuple(
-        filter_out_ignored_types(ma_type_desc)
-        for ma_type_desc
-        in ma_type_descriptions
-    )
-
-
-def _get_db_types_to_mathesar_types_iterator():
-    """
-    Returns an iterator of (db_type, ma_type) pairs, where the db_type is associated with the
-    ma_type. We use an iterator for its laziness.
-    """
-    return (
-        (db_type, ma_type_desc['ma_type'])
-        for ma_type_desc
-        in _get_type_map()
-        for db_type
-        in ma_type_desc['db_types']
-    )
-
-
-def get_mathesar_type_from_db_type(db_type_to_find: DatabaseType) -> Optional[MathesarType]:
-    for db_type, ma_type in _get_db_types_to_mathesar_types_iterator():
-        if db_type == db_type_to_find:
-            return ma_type
