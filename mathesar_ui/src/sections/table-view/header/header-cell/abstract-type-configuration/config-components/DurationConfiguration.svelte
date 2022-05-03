730760e022a5acbcc8974e9e902f63da44b0d3b2
pavish <pavishkumar.r@gmail.com>
2022-05-04 05:04:56 +0530

    Implement duration configuration in type menu

--- mathesar_ui/src/sections/table-view/header/header-cell/abstract-type-configuration/config-components/DurationConfiguration.svelte
+++ mathesar_ui/src/sections/table-view/header/header-cell/abstract-type-configuration/config-components/DurationConfiguration.svelte
@@ -20,18 +20,83 @@
     ms: 'milliseconds',
   };
   const getLabel = (opt?: DurationUnit) => (opt && labels[opt]) ?? '';
+
+  function calculateFormat(_storeValue: DurationFormValues): string {
+    const range = options.slice(
+      options.indexOf(_storeValue.max),
+      options.indexOf(_storeValue.min) + 1,
+    );
+    if (range[range.length - 1] === 'ms') {
+      return `${range.slice(0, range.length - 1).join(':')}.${
+        range[range.length - 1]
+      }`;
+    }
+    return range.join(':');
+  }
+
+  $: format = calculateFormat($store);
+
+  function onMaxChange(_max?: DurationUnit) {
+    if (!_max) return;
+    let { min } = $store;
+    if (options.indexOf(_max) > options.indexOf($store.min)) {
+      min = _max;
+    }
+    $store = {
+      ...$store,
+      max: _max,
+      min,
+    };
+  }
+
+  function onMinChange(_min?: DurationUnit) {
+    if (!_min) return;
+    let { max } = $store;
+    if (options.indexOf(_min) < options.indexOf($store.max)) {
+      max = _min;
+    }
+    $store = {
+      ...$store,
+      max,
+      min: _min,
+    };
+  }
 </script>
 
 <div class="form-element form-input">
   <LabeledInput label="Max Time Unit" layout="stacked">
-    <Select {options} bind:value={$store.max} {getLabel} />
+    <Select
+      {options}
+      value={$store.max}
+      {getLabel}
+      on:change={(e) => onMaxChange(e.detail)}
+    />
   </LabeledInput>
 </div>
 
 <div class="form-element form-input">
   <LabeledInput label="Min Time Unit" layout="stacked">
-    <Select {options} bind:value={$store.min} {getLabel} />
+    <Select
+      {options}
+      value={$store.min}
+      {getLabel}
+      on:change={(e) => onMinChange(e.detail)}
+    />
   </LabeledInput>
 </div>
 
-<div class="form-element">Format:</div>
+<div class="form-element format">
+  Format: <span>{format}</span>
+</div>
+
+<style lang="scss">
+  .format {
+    margin-top: 1rem;
+    span {
+      padding: 0.3rem 0.5rem;
+      border-radius: 0.2rem;
+      font-weight: 500;
+      background: #efefef;
+    }
+  }
+</style>
