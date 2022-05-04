0319234dc4393aa546f4b427a786b7c52ada81ab
silentninja <mmukesh95@hotmail.com>
2022-05-04 11:24:40 +0400

    Add test data files for foreign key tes cases

--- mathesar/tests/api/test_constraint_api.py
+++ mathesar/tests/api/test_constraint_api.py
@@ -10,6 +10,15 @@ def _verify_primary_and_unique_constraints(response):
     assert set(['unique', 'primary']) == set([constraint_data['type'] for constraint_data in constraints_data])
 
 
+def _verify_foreign_key_constraint(constraint_data, columns, name, referent_table, referent_columns):
+    assert constraint_data['referent']['table'] == referent_table
+    assert constraint_data['columns'] == columns
+    assert constraint_data['referent']['columns'] == referent_columns
+    assert constraint_data['name'] == name
+    assert constraint_data['type'] == 'foreignkey'
+    assert 'id' in constraint_data and type(constraint_data['id']) == int
+
+
 def _verify_unique_constraint(constraint_data, columns, name):
     assert constraint_data['columns'] == columns
     assert constraint_data['name'] == name
@@ -54,6 +63,33 @@ def test_multiple_constraint_list(create_table, client):
     for constraint_data in response_data['results']:
         if constraint_data['type'] == 'unique':
             _verify_unique_constraint(constraint_data, [constraint_column.id], 'NASA Constraint List 1_Case Number_key')
+
+
+def test_existing_foreign_key_constraint_list(create_foreign_key_table, client):
+    table_name = 'Patents'
+    ref_table_name = 'Center'
+    table, ref_table = create_foreign_key_table(table_name, ref_table_name)
+    unique_referent_constraint_column = _get_columns_by_name(ref_table, ['Id'])[0]
+    ref_table.add_constraint('unique', [unique_referent_constraint_column])
+    fk_constraint_column = _get_columns_by_name(table, ['Center'])[0]
+    table.add_constraint(
+        'foreignkey',
+        [fk_constraint_column],
+        referent_table=ref_table,
+        referent_columns=[unique_referent_constraint_column]
+    )
+
+    response = client.get(f'/api/v0/tables/{table.id}/constraints/')
+    response_data = response.json()
+    for constraint_data in response_data['results']:
+        if constraint_data['type'] == 'foreignkey':
+            _verify_foreign_key_constraint(
+                constraint_data,
+                [fk_constraint_column],
+                'Patents_Center_fkey',
+                ref_table.id,
+                [unique_referent_constraint_column]
+            )
 
 
 def test_multiple_column_constraint_list(create_table, client):
