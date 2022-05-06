dd832d8e7a5bff93f7579328de75bdc6ceada701
Sean Colsen <colsen.sean@gmail.com>
2022-05-06 08:53:52 -0400

    Remove unnecessary code

--- mathesar_ui/src/stores/abstract-types/type-configs/uri.ts
+++ mathesar_ui/src/stores/abstract-types/type-configs/uri.ts
@@ -1,18 +1,7 @@
-import type {
-  AbstractTypeConfigForm,
-  AbstractTypeConfiguration,
-} from '../types';
+import type { AbstractTypeConfiguration } from '../types';
 
 const DB_TYPES = {
   MATHESAR_TYPES__URI: 'MATHESAR_TYPES.URI',
-};
-
-const dbForm: AbstractTypeConfigForm = {
-  variables: {},
-  layout: {
-    orientation: 'vertical',
-    elements: [],
-  },
 };
 
 const uriType: AbstractTypeConfiguration = {
@@ -21,14 +10,6 @@ const uriType: AbstractTypeConfiguration = {
   cell: {
     type: 'uri',
   },
-  getDbConfig: () => ({
-    form: dbForm,
-    determineDbTypeAndOptions: () => ({
-      dbType: DB_TYPES.MATHESAR_TYPES__URI,
-      typeOptions: null,
-    }),
-    constructDbFormValuesFromTypeOptions: () => ({}),
-  }),
 };
 
 export default uriType;
