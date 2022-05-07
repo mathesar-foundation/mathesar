d98ed6a54a00d3707eb173185bdca1aa6037d60d
silentninja <mmukesh95@hotmail.com>
2022-05-07 06:35:17 +0400

    Add function to create many to many to the `db` module

--- db/links/operations/create.py
+++ db/links/operations/create.py
@@ -4,7 +4,7 @@ from sqlalchemy import MetaData
 
 from db.columns.base import MathesarColumn
 from db.constraints.utils import naming_convention
-from db.tables.operations.select import reflect_table_from_oid
+from db.tables.operations.select import reflect_table_from_oid, reflect_tables_from_oids
 from db.tables.utils import get_primary_key_column
 
 
@@ -34,3 +34,32 @@ def create_foreign_key_link(engine, schema, column_name, referrer_table_oid, ref
             source_schema=schema,
             referent_schema=schema
         )
+
+
+def create_many_to_many_link(engine, schema, map_table_name, referent_tables_oid):
+    with engine.begin() as conn:
+        referent_tables = reflect_tables_from_oids(referent_tables_oid, engine, conn)
+        metadata = MetaData(bind=engine, schema=schema, naming_convention=naming_convention)
+        opts = {
+            'target_metadata': metadata
+        }
+        ctx = MigrationContext.configure(conn, opts=opts)
+        op = Operations(ctx)
+        op.create_table(map_table_name, schema=schema)
+        for index, referent_table_oid in enumerate(referent_tables):
+            referent_table = referent_tables[referent_table_oid]
+            col_name = f"col_{index}"
+            primary_key_column = get_primary_key_column(referent_table)
+            column = MathesarColumn(
+                col_name, primary_key_column.type
+            )
+            op.add_column(map_table_name, column, schema=schema)
+            op.create_foreign_key(
+                None,
+                map_table_name,
+                referent_table.name,
+                [column.name],
+                [primary_key_column.name],
+                source_schema=schema,
+                referent_schema=schema
+            )
--- db/tables/operations/select.py
+++ db/tables/operations/select.py
@@ -35,6 +35,31 @@ def reflect_table_from_oid(oid, engine, connection_to_use=None):
     return reflect_table(table_name, schema, engine, connection_to_use=connection_to_use)
 
 
+def reflect_tables_from_oids(oids, engine, connection_to_use=None):
+    metadata = MetaData()
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", message="Did not recognize type")
+        pg_class = Table("pg_class", metadata, autoload_with=engine)
+        pg_namespace = Table("pg_namespace", metadata, autoload_with=engine)
+    sel = (
+        select(pg_namespace.c.nspname, pg_class.c.relname, pg_class.c.oid)
+        .select_from(
+            join(
+                pg_class,
+                pg_namespace,
+                pg_class.c.relnamespace == pg_namespace.c.oid
+            )
+        )
+        .where(pg_class.c.oid.in_(oids))
+    )
+    results = execute_statement(engine, sel, connection_to_use).fetchall()
+    tables = {}
+    for (schema, table_name, table_oid) in results:
+        tables[table_oid] = reflect_table(table_name, schema, engine, connection_to_use=connection_to_use)
+    return tables
+
+
 def get_table_oids_from_schema(schema_oid, engine):
     metadata = MetaData()
 
