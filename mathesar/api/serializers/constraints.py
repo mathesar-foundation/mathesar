df3197fb0fc89ab7f7b7b2768cdc876333c66467
Anish Umale <umaleanish120@gmail.com>
2022-05-14 16:41:56 +0530

    Fix 500 when invalid columns are passed

--- mathesar/api/serializers/constraints.py
+++ mathesar/api/serializers/constraints.py
@@ -1,5 +1,6 @@
+from rest_framework import status
 from rest_framework import serializers
-
+from mathesar.api.exceptions.generic_exceptions import base_exceptions as base_api_exceptions
 from mathesar.models import Constraint, Column
 
 
@@ -11,3 +12,12 @@ class ConstraintSerializer(serializers.ModelSerializer):
     class Meta:
         model = Constraint
         fields = ['id', 'name', 'type', 'columns']
+
+    def run_validation(self, data):
+        for col_id in dict(data)['columns']:
+            try:
+                column = Column.current_objects.get(id=col_id)
+            except Column.DoesNotExist:
+                message = "Column does not exist"
+                raise base_api_exceptions.NotFoundAPIException(ValueError, message=message, status_code=status.HTTP_400_BAD_REQUEST)
+        return super(ConstraintSerializer, self).run_validation(data)
