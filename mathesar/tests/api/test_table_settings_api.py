c5ccb89aef2377e9c52fbe0774c1083929533e56
silentninja <mmukesh95@hotmail.com>
2022-05-20 14:22:05 +0400

    Add test cases to check if computed columns are correct

--- mathesar/tests/api/test_table_settings_api.py
+++ mathesar/tests/api/test_table_settings_api.py
@@ -13,10 +13,13 @@ def schema(create_schema, schema_name):
 
 def test_create_table_settings(client, schema, create_table, schema_name):
     table = create_table('Table 2', schema=schema_name)
+    primary_key_column = table.get_column_name_id_bidirectional_map()['id']
+    computed_columns = [primary_key_column]
     response = client.get(
         f"/api/db/v0/tables/{table.id}/settings/"
     )
     response_data = response.json()
-    print(response_data)
+    results = response_data['results']
     assert response.status_code == 200
     assert response_data['count'] == 1
+    assert results[0]['preview_columns']['columns'] == computed_columns
