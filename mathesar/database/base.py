b4dc5922a284b4b952aa3ee83e2f3e20be2c4042
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2022-05-23 18:01:24 +0300

    Minor changes

--- mathesar/database/base.py
+++ mathesar/database/base.py
@@ -5,7 +5,6 @@ from db import engine
 
 def create_mathesar_engine(db_name):
     settings_entry = settings.DATABASES[db_name]
-    _make_sure_settings_entry_is_well_configured(db_name, settings_entry)
     return engine.create_future_engine_with_custom_types(
         username=settings_entry["USER"],
         password=settings_entry["PASSWORD"],
@@ -13,7 +12,3 @@ def create_mathesar_engine(db_name):
         database=settings_entry["NAME"],
         port=settings_entry["PORT"],
     )
-
-
-def _make_sure_settings_entry_is_well_configured(db_name, settings_entry):
-    assert settings_entry["NAME"] == db_name
