627fcb59f905a480881a894e221cedc67a636e4c
Sean Colsen <colsen.sean@gmail.com>
2022-05-27 13:43:28 -0400

    Support max fraction digits in NumberFormatter

--- mathesar_ui/src/component-library/number-input/number-formatter/inference.ts
+++ mathesar_ui/src/component-library/number-input/number-formatter/inference.ts
@@ -1,20 +1,7 @@
-import type { Options } from './options';
-
-function getFractionDigitCount(simplifiedInput: string): number {
+export function fractionDigitCount(simplifiedInput: string): number {
   return (simplifiedInput.split('.')[1] ?? '').length;
 }
 
-function hasTrailingDecimal(simplifiedInput: string): boolean {
+export function hasTrailingDecimal(simplifiedInput: string): boolean {
   return simplifiedInput.endsWith('.');
-}
-
-export function inferOptsFromSimplifiedInput(
-  simplifiedInput: string,
-): Partial<Options> {
-  return {
-    // Need zeros entered after decimal so that the user can continue typing
-    minimumFractionDigits: getFractionDigitCount(simplifiedInput),
-    // Need to retain trailing decimal so that the user can continue typing
-    forceTrailingDecimal: hasTrailingDecimal(simplifiedInput),
-  };
 }
