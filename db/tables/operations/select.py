a23ed7ff7684ae616ab5887eece966643f1866e4
Brent Moran <brentmoran@gmail.com>
2022-06-29 01:00:11 +0800

    globalize some constants, make max_depth a param

--- db/tables/operations/select.py
+++ db/tables/operations/select.py
@@ -7,6 +7,11 @@ from sqlalchemy.dialects.postgresql import JSONB
 
 from db.utils import execute_statement
 
+BASE = 'base'
+DEPTH = 'depth'
+PATH = 'path'
+TARGET = 'target'
+
 
 def reflect_table(name, schema, engine, metadata=None, connection_to_use=None):
     if metadata is None:
@@ -67,16 +72,11 @@ def get_oid_from_table(name, schema, engine):
     return inspector.get_table_oid(name, schema=schema)
 
 
-def get_joinable_tables_query(engine, base_table_name=None, schema=None):
+def get_joinable_tables(base_table_oid, engine, max_depth=3):
     LEFT_REL = 'left_rel'
     RIGHT_REL = 'right_rel'
     LEFT_COL = 'left_col'
     RIGHT_COL = 'right_col'
-
-    BASE = 'base'
-    DEPTH = 'depth'
-    PATH = 'path'
-    TARGET = 'target'
 
     SYMMETRIC_FKEYS = 'symmetric_fkeys'
     SEARCH_FKEY_GRAPH = 'search_fkey_graph'
@@ -160,7 +160,7 @@ def get_joinable_tables_query(engine, base_table_name=None, schema=None):
         ).where(
             and_(
                 symmetric_fkeys.columns[LEFT_REL] == search_fkey_graph.columns[RIGHT_REL],
-                search_fkey_graph.columns[DEPTH] < 3,
+                search_fkey_graph.columns[DEPTH] < max_depth,
                 search_fkey_graph.columns[PATH][-1] != jba(
                     jba(
                         symmetric_fkeys.columns[RIGHT_REL],
@@ -182,11 +182,14 @@ def get_joinable_tables_query(engine, base_table_name=None, schema=None):
         search_fkey_graph.columns[DEPTH].label(DEPTH)
     ).cte(name=OUTPUT_CTE)
 
-    if base_table_name is not None:
+    if base_table_oid is not None:
         final_sel = select(output_cte).where(
-            output_cte.columns[BASE] == get_oid_from_table(base_table_name, schema, engine)
+            output_cte.columns[BASE] == base_table_oid
         )
     else:
         final_sel = select(output_cte)
 
-    return final_sel
+    with engine.begin() as conn:
+        results = conn.execute(final_sel).fetchall()
+
+    return results
