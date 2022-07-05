f4c546d11703810f0d1ec88e1990541f273b4ac8
Yurii Palaida <yura.palayda@gmail.com>
2022-07-05 19:53:09 +0300

    translate recursive part to sqlalchemy statement

--- db/dependents_utils.py
+++ db/dependents_utils.py
@@ -2,7 +2,9 @@ from sqlalchemy import MetaData, Table, any_, case, column, exists, func, select
 from db.tables.operations.select import get_oid_from_table
 
 
-def _get_tables_dependents(foreign_key_dependents, pg_constraint):
+# CTEs for getting dependents of a specific type
+
+def _get_table_dependents(foreign_key_dependents, pg_constraint):
     pg_identify_object = select(
         column("name"),
         column("schema"),
@@ -38,24 +40,16 @@ def _get_tables_dependents(foreign_key_dependents, pg_constraint):
 
 def _get_foreign_key_constraint_dependents(pg_identify_object, base):
     return base.where(pg_identify_object.c.type == 'table constraint')
-    
 
+
+# Full statements
 
 def get_all_dependent_objects(name, schema, engine):
     referenced_object_id = get_oid_from_table(name, schema, engine)
-    
     pg_depend = _get_pg_depend(engine)
     pg_identify_object = _get_pg_identify_object_lateral(pg_depend)
-    pg_constraint = _get_pg_constraint(engine)
-
-    base_stmt = _get_dependent_objects_base_statement(pg_depend, pg_identify_object, referenced_object_id)
-    foreign_key_constraint_dependents = _get_foreign_key_constraint_dependents(pg_identify_object, base_stmt).cte('foreign_key_constraint_dependents')
-    table_dependents = _get_tables_dependents(foreign_key_constraint_dependents, pg_constraint).cte('table_dependents')
-
-    combined = union(select(foreign_key_constraint_dependents), select(table_dependents)).cte("combined")
-    stmt = select(combined)
-
-
+    final = _get_all_dependent_objects_base_statement(pg_depend, pg_identify_object, referenced_object_id).cte()
+    stmt = select(final)
 
     with engine.connect() as conn:
         result = conn.execute(stmt)
@@ -63,17 +57,28 @@ def get_all_dependent_objects(name, schema, engine):
     return result
 
 def get_dependents_hierarchy(name, schema, engine):
-    top_level_dependents = get_all_dependent_objects(name, schema, engine)
+    referenced_object_id = get_oid_from_table(name, schema, engine)
+    pg_depend = _get_pg_depend(engine)
+    pg_identify_object = _get_pg_identify_object_lateral(pg_depend)
+    all = _get_all_dependent_objects_base_statement(pg_depend, pg_identify_object)
+    all_cte = all.cte(recursive=True, name='all')
 
-    select(
-        text('0').label('level'),
-        
-    )
+    topq = select(all_cte).where(all_cte.c.refobjid == referenced_object_id)
+    topq = topq.cte('cte')
 
-    pass
+    bottomq = select(all_cte)
+    bottomq = bottomq.join(topq, all_cte.c.refobjid == topq.c.objid)
+
+    recursive_q = topq.union(bottomq)
+    q = select(recursive_q)
+
+    with engine.connect() as conn:
+        result = conn.execute(q)
+
+    return result
 
 
-def _get_dependent_objects_base_statement(pg_depend, pg_identify_object, referenced_object_id):
+def _get_dependent_objects_by_id_base_statement(pg_depend, pg_identify_object, referenced_object_id):
     return select(
         pg_depend.c.objid,
         func.array_agg(pg_depend.c.objsubid).label('column_number'),
@@ -95,7 +100,33 @@ def _get_dependent_objects_base_statement(pg_depend, pg_identify_object, referen
         pg_identify_object.c.identity,
         pg_depend.c.deptype)
 
+def _get_all_dependent_objects_base_statement(pg_depend, pg_identify_object, referenced_object_id = None):
+    res = select(
+        pg_depend.c.objid,
+        pg_depend.c.refobjid,
+        func.array_agg(pg_depend.c.objsubid).label('column_number'),
+        pg_identify_object.c.name,
+        pg_identify_object.c.schema,
+        pg_identify_object.c.type,
+        pg_identify_object.c.identity,
+        _get_dependency_case(pg_depend).label('dependency_type')) \
+    .select_from(pg_depend) \
+    .join(pg_identify_object, true()) \
+    .where(pg_depend.c.deptype == any_('{a,n}')) \
+    .where(pg_depend.c.objid >= 16384) \
+    .group_by(
+        pg_depend.c.objid,
+        pg_depend.c.refobjid,
+        pg_identify_object.c.type,
+        pg_identify_object.c.name,
+        pg_identify_object.c.schema,
+        pg_identify_object.c.identity,
+        pg_depend.c.deptype)
 
+    return res if referenced_object_id is None else res.where(pg_depend.c.refobjid == referenced_object_id)
+
+
+# Getting helpers
 
 def _get_pg_depend(engine):
     metadata = MetaData()
@@ -125,6 +156,21 @@ def _get_dependency_case(pg_depend):
         (pg_depend.c.deptype == 'a', 'automatic')
     ).label('dependency_type')
 
+
+# Callers
+
+def _get_all_dependent_objects_stmt(name, schema, engine):
+    referenced_object_id = get_oid_from_table(name, schema, engine)
+    
+    pg_depend = _get_pg_depend(engine)
+    pg_identify_object = _get_pg_identify_object_lateral(pg_depend)
+    pg_constraint = _get_pg_constraint(engine)
+
+    base_stmt = _get_dependent_objects_by_id_base_statement(pg_depend, pg_identify_object, referenced_object_id)
+    foreign_key_constraint_dependents = _get_foreign_key_constraint_dependents(pg_identify_object, base_stmt).cte('foreign_key_constraint_dependents')
+    table_dependents = _get_table_dependents(foreign_key_constraint_dependents, pg_constraint).cte('table_dependents')
+
+    return union(select(foreign_key_constraint_dependents), select(table_dependents))
 
 def has_dependencies(name, schema, engine):
     oid = get_oid_from_table(name, schema, engine)
