7070b065853d3238c4cb1b5ae5caf43aa04594af
pavish <pavishkumar.r@gmail.com>
2022-07-08 04:06:26 +0530

    Save status indicator component

---
+++ mathesar_ui/src/components/SaveStatusIndicator.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let status: 'new' | 'processing' | 'success' | 'failure' | undefined;
+</script>
+
+<div
+  class="save-status"
+  class:new={status === 'new'}
+  class:saved={status === 'success'}
+  class:saving={status === 'processing'}
+  class:error={status === 'failure'}
+>
+  {#if status === 'new'}
+    *Unsaved
+  {:else if status === 'success'}
+    All changes saved
+  {:else if status === 'processing'}
+    Saving changes...
+  {:else if status === 'failure'}
+    ! Couldn't save changes
+  {/if}
+</div>
+
+<style lang="scss">
+  .save-status {
+    font-size: 0.9em;
+    color: #888;
+
+    &.error {
+      color: #f47171;
+    }
+  }
+</style>
