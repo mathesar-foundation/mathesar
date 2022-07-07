93058eb7b721250e8accb939d28ddc9d939bccd2
pavish <pavishkumar.r@gmail.com>
2022-07-08 04:08:03 +0530

    Add basic data explorer routes

--- mathesar_ui/src/routes/schema-home/routes/data-explorer/DataExplorer.svelte
+++ mathesar_ui/src/routes/schema-home/routes/data-explorer/DataExplorer.svelte
@@ -1,11 +1,39 @@
 <script lang="ts">
-  import { Route } from 'tinro';
+  import type { TinroRouteMeta } from 'tinro';
+  import EventfulRoute from '@mathesar/components/routing/EventfulRoute.svelte';
+  import QueryBuilder from '@mathesar/systems/query-builder/QueryBuilder.svelte';
+  import QueryManager from '@mathesar/systems/query-builder/QueryManager';
+  import QueryModel from '@mathesar/systems/query-builder/QueryModel';
+
+  const queryManager = new QueryManager(
+    new QueryModel({ name: 'Untitled(0)' }),
+  );
+
+  function newQuery() {
+    // Create new manager
+  }
+
+  function loadQueryById() {
+    // Check query Id
+    // If it doesn't match, fetch the correct one and create new manager for it
+  }
+
+  function newQueryRouteLoaded(meta: TinroRouteMeta) {
+    console.log('NEW query route loaded', meta.params);
+  }
+
+  function savedQueryRouteLoaded(meta: TinroRouteMeta) {
+    console.log('SAVED query route loaded', meta.params);
+  }
 </script>
 
-<div>
-  <h1>Data-explorer</h1>
+<EventfulRoute
+  path="/"
+  on:routeUpdated={(e) => newQueryRouteLoaded(e.detail)}
+/>
+<EventfulRoute
+  path="/:queryId"
+  on:routeUpdated={(e) => savedQueryRouteLoaded(e.detail)}
+/>
 
-  <Route path="/">Base path</Route>
-
-  <Route path="/:queryId">Edit existing query</Route>
-</div>
+<QueryBuilder {queryManager} />
