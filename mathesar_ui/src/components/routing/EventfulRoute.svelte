2b1c2e385d7a0ddb9d8bb12becfa5753dda6cc06
pavish <pavishkumar.r@gmail.com>
2022-07-08 04:07:36 +0530

    Implement EventfulRoute component

---
+++ mathesar_ui/src/components/routing/EventfulRoute.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Route } from 'tinro';
+  import Observer from './Observer.svelte';
+
+  export let path: string;
+  export let firstmatch = false;
+</script>
+
+<Route {path} {firstmatch} let:meta>
+  <Observer {meta} on:routeUpdated />
+  <slot {meta} />
+</Route>
---
+++ mathesar_ui/src/components/routing/Observer.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import type { TinroRouteMeta } from 'tinro';
+
+  const dispatch = createEventDispatcher<{ routeUpdated: TinroRouteMeta }>();
+
+  export let meta: TinroRouteMeta;
+
+  $: dispatch('routeUpdated', $meta);
+</script>
