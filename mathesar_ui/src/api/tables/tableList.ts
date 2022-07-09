c2a41050df11c7cc5d1898a3d7fa7dd74d63a4b3
pavish <pavishkumar.r@gmail.com>
2022-07-10 00:31:48 +0530

    Add api interfaces for joinable tables

--- mathesar_ui/src/api/tables/tableList.ts
+++ mathesar_ui/src/api/tables/tableList.ts
@@ -1,9 +1,14 @@
+import type { PaginatedResponse } from '@mathesar/utils/api';
 import type { Column } from './columns';
 
-type SimpleColumn = Pick<
+type MinimalColumnDetails = Pick<
   Column,
   'id' | 'name' | 'type' | 'type_options' | 'display_options'
 >;
+
+/**
+ * endpoint: /api/db/v0/tables/<table_id>/
+ */
 
 export interface TableEntry {
   id: number;
@@ -11,5 +16,44 @@ export interface TableEntry {
   schema: number;
   import_verified: boolean;
   data_files?: number[];
-  columns: SimpleColumn[];
+  columns: MinimalColumnDetails[];
+}
+
+/**
+ * endpoint: /api/db/v0/tables/
+ */
+
+export type TablesList = PaginatedResponse<TableEntry>;
+
+/**
+ * endpoint: /api/db/v0/tables/<table_id>/joinable_tables/
+ */
+
+type ForeignKeyId = number;
+type IsLinkReversed = boolean;
+
+export interface JoinableTable {
+  target: TableEntry['id']; // baseTableId
+  jp_path: [Column['id'], Column['id']][];
+  fk_path: [ForeignKeyId, IsLinkReversed][];
+  depth: number;
+  multiple_results: boolean;
+}
+
+export interface JoinableTableResult {
+  joinable_tables: JoinableTable[];
+  tables: Record<
+    string, // tableId
+    {
+      name: TableEntry['name'];
+      columns: Column['id'][];
+    }
+  >;
+  columns: Record<
+    string, // columnId
+    {
+      name: Column['name'];
+      type: Column['type'];
+    }
+  >;
 }
