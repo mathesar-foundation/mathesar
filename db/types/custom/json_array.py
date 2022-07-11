feea9fc9f7515adce60dc75d3c618ab27429ccbb
Jinxiao Li <ruclijinxiao@163.com>
2022-07-10 23:10:44 -0700

    use type bind

--- db/types/custom/json_array.py
+++ db/types/custom/json_array.py
@@ -7,10 +7,24 @@ DB_TYPE = MathesarCustomType.MATHESAR_JSON_ARRAY.id
 
 
 class MathesarJsonArray(UserDefinedType):
-
+    impl = postgresql.JSONB
+    cache_ok = True
     def get_col_spec(self, **_):
         return DB_TYPE.upper()
 
+    def get_col_spec(self, **_):
+        return DB_TYPE.upper()
+    
+    def column_expression(self, column):
+        return func.to_char(column)
+
+@compiles(MathesarJsonArray, 'postgresql')
+def _compile_mathesarjsonobject(element, compiler, **kw):
+    unchanged_compiled_string = compiler.visit_VARCHAR(element, **kw)
+    unchanged_id = "JSONB"
+    changed_id = MathesarCustomType.MATHESAR_JSON_ARRAY.id.upper()
+    changed_compiled_string = unchanged_compiled_string.replace(unchanged_id, changed_id)
+    return changed_compiled_string
 
 def install(engine):
     drop_domain_query = f"""
--- db/types/custom/json_object.py
+++ db/types/custom/json_object.py
@@ -1,5 +1,6 @@
 from sqlalchemy import text
 from sqlalchemy.types import UserDefinedType
+from sqlalchemy import func
 
 from db.types.base import MathesarCustomType
 
@@ -7,10 +8,23 @@ DB_TYPE = MathesarCustomType.MATHESAR_JSON_OBJECT.id
 
 
 class MathesarJsonObject(UserDefinedType):
+    impl = postgresql.JSONB
+    cache_ok = True
 
     def get_col_spec(self, **_):
         return DB_TYPE.upper()
+    
+    def column_expression(self, column):
+        return func.to_char(column)
 
+
+@compiles(MathesarJsonObject, 'postgresql')
+def _compile_mathesarjsonobject(element, compiler, **kw):
+    unchanged_compiled_string = compiler.visit_VARCHAR(element, **kw)
+    unchanged_id = "JSONB"
+    changed_id = MathesarCustomType.MATHESAR_JSON_OBJECT.id.upper()
+    changed_compiled_string = unchanged_compiled_string.replace(unchanged_id, changed_id)
+    return changed_compiled_string
 
 def install(engine):
     drop_domain_query = f"""
@@ -19,7 +33,6 @@ def install(engine):
     create_domain_query = f"""
     CREATE DOMAIN {DB_TYPE} AS JSONB CHECK (jsonb_typeof(VALUE) = 'object');
     """
-
     with engine.begin() as conn:
         conn.execute(text(drop_domain_query))
         conn.execute(text(create_domain_query))
