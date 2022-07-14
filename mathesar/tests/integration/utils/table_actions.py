ee3fbdbb805df3085a3240acd72ef1a91f934654
Sean Colsen <colsen.sean@gmail.com>
2022-07-14 14:22:45 -0400

    Increase timeouts on some E2E assertions

--- mathesar/tests/integration/utils/table_actions.py
+++ mathesar/tests/integration/utils/table_actions.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import expect
+from mathesar.tests.integration.conftest import TIMEOUT_HACK
 from mathesar.tests.integration.utils.validators import expect_modal_not_to_be_visible, expect_modal_to_be_visible
 
 
@@ -46,15 +47,15 @@ def get_column_header_locator(page, column_name):
 def open_column_options(page, column_name, column_type):
     page.click(get_column_header_locator(page, column_name))
     type_option = "button.type-switch"
-    expect(page.locator(type_option)).to_contain_text(column_type)
+    expect(page.locator(type_option)).to_contain_text(column_type, **TIMEOUT_HACK)
     page.click(type_option)
-    expect(page.locator(".type-list li.selected")).to_contain_text(column_type)
+    expect(page.locator(".type-list li.selected")).to_contain_text(column_type, **TIMEOUT_HACK)
 
 
 def open_column_options_and_verify_type(page, column_name, column_type, db_type):
     open_column_options(page, column_name, column_type)
     db_type_text = f"Database type {db_type}"
-    expect(page.locator(".type-options-content")).to_contain_text(db_type_text, use_inner_text=True)
+    expect(page.locator(".type-options-content")).to_contain_text(db_type_text, use_inner_text=True, **TIMEOUT_HACK)
 
 
 def get_cell_selector(page, table, row_number, column_name):
