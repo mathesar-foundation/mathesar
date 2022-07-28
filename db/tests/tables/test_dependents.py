66ca38e334f7a42b21f442d064818a7680e59aca
Yurii Palaida <yura.palayda@gmail.com>
2022-07-28 19:50:30 +0300

    add dependents db tests

--- db/tests/tables/test_dependents.py
+++ db/tests/tables/test_dependents.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, ForeignKey, Integer, MetaData, PrimaryKeyConstraint, Table
-from db.dependents_utils import get_dependents_hierarchy
+from db.dependents_utils import get_dependents_graph
 from db.tables.operations.select import get_oid_from_table
 from db.constraints.operations.select import get_constraint_oid_by_name_and_table_oid
 
@@ -19,10 +19,10 @@ def test_table_dependents(engine_with_schema):
     )
     t2.create()
 
-    t1_dependents_graph = get_dependents_hierarchy(t1.name, schema, engine).fetchall()
-
     t1_oid = get_oid_from_table(t1.name, schema, engine)
     t2_oid = get_oid_from_table(t2.name, schema, engine)
+    t1_dependents_graph = get_dependents_graph(t1_oid, engine).fetchall()
+
     t1_dependents = list(filter(lambda x: x.refobjid == t1_oid, t1_dependents_graph))
     t2_dependents = list(filter(lambda x: x.refobjid == t2_oid, t1_dependents_graph))
 
@@ -31,19 +31,46 @@ def test_table_dependents(engine_with_schema):
     assert len(t2_dependents) == 2
     assert all(
         [
-            r.level == 0
+            r.level == 1
             for r in t1_dependents
         ]
     )
     assert all(
         [
-            r.level == 1
+            r.level == 2
             for r in t2_dependents
         ]
     )
 
 
-def test_max_level_graph(engine_with_schema):
+def test_response_format(engine_with_schema):
+    engine, schema = engine_with_schema
+    metadata = MetaData(schema=schema, bind=engine)
+    t1 = Table(
+        't1', metadata,
+        Column('id', Integer, primary_key=True)
+    )
+    t1.create()
+    t2 = Table(
+        't2', metadata,
+        Column('id', Integer, primary_key=True),
+        Column('t1_id', Integer, ForeignKey(t1.c.id))
+    )
+    t2.create()
+
+    t1_oid = get_oid_from_table(t1.name, schema, engine)
+    t1_dependents_graph = get_dependents_graph(t1_oid, engine).fetchall()
+
+    expected_attrs = ['objid', 'refobjid', 'type', 'level', 'identity']
+    assert all(
+        [
+            all([hasattr(dependent, attr) for attr in expected_attrs])
+            for dependent in t1_dependents_graph
+        ]
+    )
+
+
+def test_graph_max_level(engine_with_schema):
     engine, schema = engine_with_schema
     metadata = MetaData(schema=schema, bind=engine)
     t0 = Table(
@@ -58,16 +85,19 @@ def test_max_level_graph(engine_with_schema):
             Column(f't{i}_id', Integer, ForeignKey(f"t{i}.id")))
         t.create()
 
-    dependents = get_dependents_hierarchy(t0.name, schema, engine).fetchall()
+
+    t0_oid = get_oid_from_table(t0.name, schema, engine)
+    dependents = get_dependents_graph(t0_oid, engine).fetchall()
 
     tables_count = len(metadata.tables.keys())
     assert tables_count == 11
 
     dependents_by_level = sorted(dependents, key=lambda x: x.level, reverse=True)
+    assert dependents_by_level[-1].level == 1
     assert dependents_by_level[0].level == 10
 
 
-def test_specific_objects_in_dependents(engine_with_schema):
+def test_specific_object_types(engine_with_schema):
     engine, schema = engine_with_schema
     metadata = MetaData(schema=schema, bind=engine)
     t1_pk_name = "t1_pk_name"
@@ -85,9 +115,9 @@ def test_specific_objects_in_dependents(engine_with_schema):
         Column('t1_id', Integer, ForeignKey(t1.c.id, name=t2_fk_name)))
     t2.create()
 
-    dependents = get_dependents_hierarchy(t1.name, schema, engine).fetchall()
-
     t1_oid = get_oid_from_table(t1.name, schema, engine)
+    dependents = get_dependents_graph(t1_oid, engine).fetchall()
+
     t1_pk_oid = get_constraint_oid_by_name_and_table_oid(t1_pk_name, t1_oid, engine)
     t2_oid = get_oid_from_table(t2.name, schema, engine)
     t2_fk_oid = get_constraint_oid_by_name_and_table_oid(t2_fk_name, t2_oid, engine)
@@ -115,10 +145,10 @@ def test_circular_referene(engine_with_schema):
     t2.create()
     t1.append_column(Column('t2_id', Integer, ForeignKey(t2.c.id)))
 
-    t1_dependents_graph = get_dependents_hierarchy(t1.name, schema, engine).fetchall()
-
     t1_oid = get_oid_from_table(t1.name, schema, engine)
     t2_oid = get_oid_from_table(t2.name, schema, engine)
+    t1_dependents_graph = get_dependents_graph(t1_oid, engine).fetchall()
+
     t2_dependents = list(filter(lambda x: x.refobjid == t2_oid, t1_dependents_graph))
     t2_dependent_oids = [d.objid for d in list(filter(lambda x: x.refobjid == t1_oid, t2_dependents))]
 
