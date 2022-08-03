231cc04169f399fc92875011d0cf387ffb47ad75
abhishak3 <abhishek.pandey.3237@gmail.com>
2022-08-04 00:32:41 +0530

    modified error handling

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,45 +1,29 @@
 #!/usr/bin/env bash
 
-# this script performs the initial setup
-
 URL="http://0.0.0.0:8000/"
 status_code=000
 ENV_EXAMPLE=".env.example"
 ENV_ORIG=".env"
-TIMEOUT_TIME=60
-
-error_check() {
-    if [ $? -ne 0 ]; then
-        echo "FAILED"
-        exit
-    fi
-}
+#TIMEOUT_TIME=60
 
 # instantiate .env file
 cp ${ENV_EXAMPLE} ${ENV_ORIG}
 
 # runs docker compose as daemon process
-docker-compose up -d
-error_check
+docker-compose up -d || echo "FAILED AT: docker-compose up -d"
 
-# loop until http://0.0.0.0:8000/ is not active and
-# timeout does not occurs
-while [[ ${status_code} -ne 302 && ${SECONDS} -lt ${TIMEOUT_TIME} ]];
+#while [[ ${status_code} -ne 302 && ${SECONDS} -lt ${TIMEOUT_TIME} ]];
+while [[ ${status_code} -ne 302 ]];
 do
     status_code=$(curl --write-out %{http_code} --silent --output /dev/null ${URL})
 done
 
-# if timeout occurs
-if [[ ${SECONDS} -ge ${TIMEOUT_TIME} ]]; then
-    echo "TIMEOUT OCCURRED"
-    exit
-fi
+echo "
+DOCKER CONTAINER RUNNING IN THE BACKGROUND...
+Application is now available at http://localhost:8000/
+"
 
-# run migrations
-docker exec mathesar_service python manage.py migrate
-error_check
-
-# run install.py
-docker exec -it mathesar_service python install.py
-error_check
+# run migrations and install.py
+docker exec mathesar_service sh -c "python manage.py migrate && python install.py"\
+    || echo "FAILED AT: docker exec mathesar_service sh -c 'python manage.py migrate && python install.py'"
 
