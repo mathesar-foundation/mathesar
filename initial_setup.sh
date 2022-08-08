3ac4d4a29e0d259c46790c20638cd278dbfbc960
abhishak3 <abhishek.pandey.3237@gmail.com>
2022-08-08 11:49:20 +0530

    changed wrapper function

--- initial_setup.sh
+++ initial_setup.sh
@@ -6,16 +6,25 @@ ENV_EXAMPLE=".env.example"
 ENV_ORIG=".env"
 #TIMEOUT_TIME=60
 
-exit_on_error () {
-    echo "FAILED AT: $@"
-    exit
+function execute {
+    # Echo what the command is before running it
+    echo "$@"
+    # Run the passed command
+    eval $@
+    local eval_status=$?
+    # Enable panicing this function's parent in case of non-zero exit
+    set -e
+    # Do non-zero exit, if eval's return was non-zero
+    [ $eval_status -eq 0 ] || (echo "failed" && exit 1)
+    # Disable panicing this function's parent in case of non-zero exit
+    set +e
 }
 
 # instantiate .env file
 cp ${ENV_EXAMPLE} ${ENV_ORIG}
 
 # runs docker compose as daemon process
-docker-compose up -d || exit_on_error "docker-compose up -d"
+execute docker-compose up -d
 
 #while [[ ${status_code} -ne 302 && ${SECONDS} -lt ${TIMEOUT_TIME} ]];
 while [[ ${status_code} -eq 000 ]];
@@ -29,6 +38,5 @@ Application is now available at http://localhost:8000/
 "
 
 # run migrations and install.py
-docker exec mathesar_service sh -c "python manage.py migrate && python install.py"\
-    || exit_on_error "docker exec mathesar_service sh -c 'python manage.py migrate && python install.py'"
+execute docker exec mathesar_service sh -c "python manage.py migrate && python install.py"
 
