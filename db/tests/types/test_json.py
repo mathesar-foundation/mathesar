d393993399903b513dc038807d4d14c88c12271a
Jinxiao Li <ruclijinxiao@163.com>
2022-08-09 19:23:42 -0400

    fix coding style issues

--- db/tests/types/test_json.py
+++ db/tests/types/test_json.py
@@ -1,16 +1,15 @@
-from psycopg2.errors import CheckViolation
 import pytest
-from sqlalchemy.exc import IntegrityError
 from db.types.custom import json_array
 from db.utils import execute_pg_query
-from db.functions.base import ColumnName, Literal, sa_call_sql_function
+from db.functions.base import ColumnName, Literal
 from db.functions.operations.apply import apply_db_function_as_filter
+
 
 @pytest.mark.parametrize("main_db_function,literal_param,expected_count", [
     (json_array.LengthEquals, 0, 1),
     (json_array.LengthEquals, 3, 11),
     (json_array.LengthEquals, 4, 1),
-    (json_array.LengthEquals,6, 1),
+    (json_array.LengthEquals, 6, 1),
 ])
 def test_json_array_filter_functions(json_table_obj, main_db_function, literal_param, expected_count):
     table, engine = json_table_obj
