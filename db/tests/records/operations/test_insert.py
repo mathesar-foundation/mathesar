abdf2ff4ea51d34eb6a5060e8ed98b4e59e16052
Anish Umale <umaleanish120@gmail.com>
2022-08-15 16:46:14 +0530

    Fix minor lint issues

--- db/tests/records/operations/test_insert.py
+++ db/tests/records/operations/test_insert.py
@@ -7,12 +7,12 @@ def test_insert_from_select_without_mappings(books_table_import_from_obj, books_
     from_table, engine = books_table_import_from_obj
     target_table, _ = books_table_import_target_obj
     records_without_mappings = [
-                                (1, 'Steve Jobs', 'Walter Issacson'),
-                                (2, 'The Idiot', 'Fyodor Dostevsky'),
-                                (3, 'David Copperfield', 'Charles Darwin'),
-                                (4, 'Fyodor Dostoevsky', 'Crime and Punishment'),
-                                (5, 'Cervantes', 'Don Quixote')
-                            ]
+        (1, 'Steve Jobs', 'Walter Issacson'),
+        (2, 'The Idiot', 'Fyodor Dostevsky'),
+        (3, 'David Copperfield', 'Charles Darwin'),
+        (4, 'Fyodor Dostoevsky', 'Crime and Punishment'),
+        (5, 'Cervantes', 'Don Quixote')
+    ]
     res_table, _ = insert_from_select(from_table, target_table, engine, col_mappings=None)
     records = get_records(res_table, engine)
     assert res_table.c['id'] == target_table.c[0]
@@ -26,12 +26,12 @@ def test_insert_from_select_with_mappings(books_table_import_from_obj, books_tab
     from_table, engine = books_table_import_from_obj
     target_table, _ = books_table_import_target_obj
     records_with_mappings = [
-                                (1, 'Steve Jobs', 'Walter Issacson'),
-                                (2, 'The Idiot', 'Fyodor Dostevsky'),
-                                (3, 'David Copperfield', 'Charles Darwin'),
-                                (4, 'Crime and Punishment', 'Fyodor Dostoevsky'),
-                                (5, 'Don Quixote', 'Cervantes')
-                            ]
+        (1, 'Steve Jobs', 'Walter Issacson'),
+        (2, 'The Idiot', 'Fyodor Dostevsky'),
+        (3, 'David Copperfield', 'Charles Darwin'),
+        (4, 'Crime and Punishment', 'Fyodor Dostoevsky'),
+        (5, 'Don Quixote', 'Cervantes')
+    ]
     col_mappings = [['book_title', 'title'], ['author_name', 'author']]
     res_table, _ = insert_from_select(from_table, target_table, engine, col_mappings)
     records = get_records(res_table, engine)
