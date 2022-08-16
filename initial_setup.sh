f069906db8c57400a28957387ee37312a5749369
abhishak3 <abhishek.pandey.3237@gmail.com>
2022-08-16 23:27:47 +0530

    inital_setup container running state

--- initial_setup.sh
+++ initial_setup.sh
@@ -4,11 +4,11 @@ URL="http://0.0.0.0:8000/"
 status_code=000
 ENV_EXAMPLE=".env.example"
 ENV_ORIG=".env"
-#TIMEOUT_TIME=60
+CONTAINER_NAME="mathesar_service"
 
 function execute {
     # Echo what the command is before running it
-    echo "$@"
+    echo "Running: $@"
     # Run the passed command
     eval $@
     local eval_status=$?
@@ -26,17 +26,13 @@ cp ${ENV_EXAMPLE} ${ENV_ORIG}
 # runs docker compose as daemon process
 execute docker-compose up -d
 
-#while [[ ${status_code} -ne 302 && ${SECONDS} -lt ${TIMEOUT_TIME} ]];
-while [[ ${status_code} -eq 000 ]];
+# if container is ready for migrations
+mathesar_service_running=$(docker container inspect -f '{{.State.Running}}' ${CONTAINER_NAME})
+while [[ ${mathesar_service_running} != "true" ]];
 do
-    status_code=$(curl --write-out %{http_code} --silent --output /dev/null ${URL})
+    mathesar_service_running=$(docker container inspect -f '{{.State.Running}}' ${CONTAINER_NAME})
 done
 
-echo "
-DOCKER CONTAINER RUNNING IN THE BACKGROUND...
-Application is now available at http://localhost:8000/
-"
-
 # run migrations and install.py
-execute docker exec mathesar_service sh -c "python manage.py migrate && python install.py"
+execute docker exec mathesar_service sh -c "\"python manage.py migrate && python install.py\""
 
