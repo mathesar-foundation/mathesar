adb654d82cf658bcc6f47e04235273cbe68227ba
Sean Colsen <colsen.sean@gmail.com>
2022-08-16 21:07:02 -0400

    Make row selection logic smarter

--- mathesar_ui/src/systems/record-selector/RecordSelectorResults.svelte
+++ mathesar_ui/src/systems/record-selector/RecordSelectorResults.svelte
@@ -12,54 +12,74 @@
   import CellArranger from './CellArranger.svelte';
   import CellWrapper from './CellWrapper.svelte';
   import NewIndicator from './NewIndicator.svelte';
-  import { getPkValueInRecord } from './recordSelectorUtils';
+  import {
+    findNearestValidSelection,
+    getPkValueInRecord,
+    getValidOffsetSelection,
+  } from './recordSelectorUtils';
+  import type { RecordSelectorSelection } from './recordSelectorTypes';
 
   const tabularData = getTabularDataStoreFromContext();
 
   export let submitPkValue: (v: string | number) => void;
   export let submitNewRecord: (v: Iterable<[number, unknown]>) => void;
 
-  interface RecordSelection {
-    type: 'record';
-    index: number;
-  }
-  interface GhostSelection {
-    type: 'ghost';
-  }
-  type Selection = RecordSelection | GhostSelection;
+  let selection: RecordSelectorSelection = { type: 'record', index: 0 };
 
-  let selection: Selection = { type: 'record', index: 0 };
+  /**
+   * The ghost row will appear and disappear based on whether the user has
+   * entered values into the search fields. Here's the situation that this
+   * variable helps us with:
+   *
+   * 1. The user record selector loads with 10 rows.
+   * 1. The user enters a search term which filters the number of rows to 0.
+   * 1. The ghost row is automatically selected (good).
+   * 1. The user modifies their search term, allowing 5 rows to display.
+   * 1. At this point, we'd like to automatically select the first result row
+   *    (instead of the ghost row) because we know that the user never manually
+   *    selected the ghost row. If we leave the ghost row selected, there's a
+   *    chance the user (if they're not paying close attention) could select the
+   *    ghost, inadvertently creating a new record. We want to make sure that
+   *    when they select the ghost row, they mean it!
+   * 1. Because we also want to support the user case where they've manually
+   *    selected the ghost row and are continuing to build a new record, we need
+   *    this variable.
+   */
+  let userHasManuallySelectedGhostRow = false;
 
   $: ({ display, recordsData, meta, columnsDataStore } = $tabularData);
   $: recordsStore = recordsData.savedRecords;
   $: ({ searchFuzzy } = meta);
   $: records = $recordsStore;
+  $: resultCount = records.length;
   $: rowWidthStore = display.rowWidth;
   $: rowWidth = $rowWidthStore;
   $: rowStyle = `width: ${rowWidth as number}px; height: ${rowHeightPx}px;`;
-  $: showGhostRow = $searchFuzzy.size > 0;
+  $: hasGhostRow = $searchFuzzy.size > 0;
   $: indexIsSelected = (index: number) =>
     selection.type === 'record' && selection.index === index;
   $: ({ columns } = $columnsDataStore);
 
-  function selectPrevious() {
-    if (selection.type === 'ghost') {
-      return;
-    }
-    selection =
-      selection.index === 0
-        ? { type: 'ghost' }
-        : { type: 'record', index: selection.index - 1 };
-  }
+  $: selection = findNearestValidSelection({
+    selection,
+    resultCount,
+    hasGhostRow,
+    userHasManuallySelectedGhostRow,
+  });
 
-  function selectNext() {
-    selection =
-      selection.type === 'ghost'
-        ? { type: 'record', index: 0 }
-        : {
-            type: 'record',
-            index: Math.min(selection.index + 1, records.length - 1),
-          };
+  function moveSelectionByOffset(offset: number) {
+    userHasManuallySelectedGhostRow =
+      userHasManuallySelectedGhostRow ||
+      (selection.type === 'record' && selection.index === 0 && offset < 0);
+    selection = getValidOffsetSelection(
+      {
+        selection,
+        resultCount,
+        hasGhostRow,
+        userHasManuallySelectedGhostRow,
+      },
+      offset,
+    );
   }
 
   function getPkValue(row: Row): string | number {
@@ -90,10 +110,10 @@
     let handled = true;
     switch (e.key) {
       case 'ArrowUp':
-        selectPrevious();
+        moveSelectionByOffset(-1);
         break;
       case 'ArrowDown':
-        selectNext();
+        moveSelectionByOffset(1);
         break;
       case 'Enter':
         submitSelection();
@@ -117,7 +137,7 @@
 </script>
 
 <div class="record-selector-results">
-  {#if showGhostRow}
+  {#if hasGhostRow}
     <div class="row ghost" style={rowStyle} on:click={() => submitGhost()}>
       <div class="new-indicator-wrapper"><NewIndicator /></div>
       <CellArranger {display} let:style let:processedColumn let:columnId>
--- mathesar_ui/src/systems/record-selector/recordSelectorUtils.ts
+++ mathesar_ui/src/systems/record-selector/recordSelectorUtils.ts
@@ -1,5 +1,9 @@
 import type { Column } from '@mathesar/api/tables/columns';
 import type { Result as ApiRecord } from '@mathesar/api/tables/records';
+import type {
+  RecordSelectorSelection,
+  SelectionDetails,
+} from './recordSelectorTypes';
 
 export function getPkValueInRecord(
   record: ApiRecord,
@@ -14,4 +18,76 @@ export function getPkValueInRecord(
     throw new Error('Primary key value is not a string or number.');
   }
   return pkValue;
+}
+
+function getOffsetSelection(
+  selection: RecordSelectorSelection,
+  offset: number,
+): RecordSelectorSelection {
+  const { type } = selection;
+  if (selection.type === 'ghost') {
+    return offset > 0 ? { type: 'record', index: offset - 1 } : selection;
+  }
+  const effectiveIndex = (type === 'record' ? selection.index : -1) + offset;
+  return effectiveIndex < 0
+    ? { type: 'ghost' }
+    : { type: 'record', index: effectiveIndex };
+}
+
+/**
+ * After variables within the results change, the user's selection might no
+ * longer be valid. This function finds the nearest valid selection so that we
+ * can update the selection with the result of this function when things change.
+ */
+export function findNearestValidSelection(
+  d: SelectionDetails,
+): RecordSelectorSelection {
+  if (d.selection.type === 'ghost') {
+    // Sometimes we end up in the ghost row purely by virtue of it being the
+    // only row, even if the user didn't select it. In this case we want to move
+    // back out of the ghost row if some results appear later.
+    const needToMoveOutOfGhostRow =
+      !d.hasGhostRow ||
+      (!d.userHasManuallySelectedGhostRow && d.resultCount > 0);
+    if (needToMoveOutOfGhostRow) {
+      // Note that if `resultCount` is 0 (table is empty) the selection will
+      // technically be invalid, but that's okay.
+      return { type: 'record', index: 0 };
+    }
+    // We have a valid ghost row selection that we should keep.
+    return d.selection;
+  }
+
+  // We have a record selection...
+
+  if (d.hasGhostRow && (d.resultCount === 0 || d.selection.index < 0)) {
+    // We should move to the ghost row.
+    return { type: 'ghost' };
+  }
+  // We should stay in the record selection.
+
+  if (d.selection.index >= d.resultCount) {
+    // We've overflowed off the bottom and need to move to the last row.
+    return { type: 'record', index: d.resultCount - 1 };
+  }
+  if (d.selection.index < 0) {
+    // We've underflowed and need to move to the first row.
+    return { type: 'record', index: 0 };
+  }
+
+  // We have a valid record selection.
+  return d.selection;
+}
+
+/**
+ * Moves the selection, the re-adjusts it as necessary to remain valid.
+ */
+export function getValidOffsetSelection(
+  d: SelectionDetails,
+  offset: number,
+): RecordSelectorSelection {
+  return findNearestValidSelection({
+    ...d,
+    selection: getOffsetSelection(d.selection, offset),
+  });
 }
