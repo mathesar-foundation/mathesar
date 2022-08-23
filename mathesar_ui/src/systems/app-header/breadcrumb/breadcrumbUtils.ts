3cd790c5ceb6d5df5d4e44333a0f00bb06949d3e
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2022-08-23 17:44:22 +0300

    auto format

--- mathesar_ui/src/systems/app-header/breadcrumb/breadcrumbUtils.ts
+++ mathesar_ui/src/systems/app-header/breadcrumb/breadcrumbUtils.ts
@@ -1,7 +1,11 @@
 import { setContext, getContext } from 'svelte';
 import type { Writable } from 'svelte/store';
 import { writable } from 'svelte/store';
-import type { BreadcrumbItem, BreadcrumbSelectorData, BreadcrumbSelectorEntry } from './breadcrumbTypes';
+import type {
+  BreadcrumbItem,
+  BreadcrumbSelectorData,
+  BreadcrumbSelectorEntry,
+} from './breadcrumbTypes';
 
 const contextKey = Symbol('breadcrumb items store');
 
@@ -21,30 +25,23 @@ export function filterBreadcrumbData(
   data: BreadcrumbSelectorData,
   filterString: string,
 ): BreadcrumbSelectorData {
-  let arr = Array.from(data)
+  let arr = Array.from(data);
   arr = arr
-    .map(
-      ([category, entries]) =>
-      [
-        category,
-        Array
-        .from(entries)
-        .filter(
-          (entry) => breadcrumbSelectorEntryMatches(entry, filterString)
-        )
-      ]
-    )
-    .filter(
-      ([_, entries]) => entries.length > 0
-    )
-  return new Map(arr)
+    .map(([category, entries]) => [
+      category,
+      Array.from(entries).filter((entry) =>
+        breadcrumbSelectorEntryMatches(entry, filterString),
+      ),
+    ])
+    .filter(([_, entries]) => entries.length > 0);
+  return new Map(arr);
 }
 
 function breadcrumbSelectorEntryMatches(
   entry: BreadcrumbSelectorEntry,
   filterString: string,
 ): boolean {
-  const mainString = entry.label.toLowerCase()
-  const subString = filterString.toLowerCase()
-  return mainString.includes(subString)
+  const mainString = entry.label.toLowerCase();
+  const subString = filterString.toLowerCase();
+  return mainString.includes(subString);
 }
