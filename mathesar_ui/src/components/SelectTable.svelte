9d35ccafb20de402016440e4f0b4a7b9f3cf6682
pavish <pavishkumar.r@gmail.com>
2022-08-24 11:44:42 +0800

    Improve readability of Select component's auto selection logic

--- mathesar_ui/src/components/SelectTable.svelte
+++ mathesar_ui/src/components/SelectTable.svelte
@@ -8,9 +8,8 @@
   export let tables: TableEntry[];
   export let table: TableEntry | undefined = undefined;
   export let prependBlank = false;
-  export let initialSelectionType: SelectProps<
-    TableEntry | undefined
-  >['initialSelectionType'] = 'first';
+  export let autoSelect: SelectProps<TableEntry | undefined>['autoSelect'] =
+    'first';
 
   $: tableList = prependBlank ? [undefined, ...tables] : tables;
 </script>
@@ -19,7 +18,7 @@
   options={tableList}
   labelKey="name"
   valuesAreEqual={(a, b) => a?.id === b?.id}
-  {initialSelectionType}
+  {autoSelect}
   bind:value={table}
   on:change
 />
