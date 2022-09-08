c4ad30a0652bf136d097e4ac2c3efaed7f11f85a
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2022-09-08 16:26:21 +0300

    Continue implementing metadata cache

--- mathesar/metadata.py
+++ mathesar/metadata.py
@@ -1,10 +1,20 @@
-from sqlalchemy import MetaData
-from db.constraints.utils import naming_convention
+from db.metadata import get_empty_metadata
 
-_metadata_cache = MetaData(naming_convention=naming_convention)
 
 def get_metadata():
     """
     Cached to minimize reflection queries to Postgres.
     """
     return _metadata_cache
+
+
+def clear_metadata():
+    """
+    Clears MetaData cache by replacing it with a an empty instance and returns it.
+    """
+    global _metadata_cache
+    _metadata_cache = get_empty_metadata()
+    return _metadata_cache
+
+
+_metadata_cache = get_empty_metadata()
