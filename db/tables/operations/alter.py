3367ae457b0d7a528fbc53ea4d3dcfcd3be502f4
Brent Moran <brentmoran@gmail.com>
2022-09-08 11:25:53 +0800

    add db layer functions for adding comment to table

--- db/tables/operations/alter.py
+++ db/tables/operations/alter.py
@@ -1,10 +1,12 @@
 from alembic.migration import MigrationContext
 from alembic.operations import Operations
+from sqlalchemy import text
 
 from db.columns.operations.alter import batch_update_columns
 from db.tables.operations.select import reflect_table
+from db.tables.operations.alter import reflect_table
 
-SUPPORTED_TABLE_ALTER_ARGS = {'name', 'columns'}
+SUPPORTED_TABLE_ALTER_ARGS = {'name', 'columns', 'description'}
 
 
 def rename_table(name, schema, engine, rename_to):
@@ -17,9 +19,19 @@ def rename_table(name, schema, engine, rename_to):
         op.rename_table(table.name, rename_to, schema=table.schema)
 
 
+def comment_on_table(name, schema, engine, comment):
+    # Not using the DDLElement since the examples from the docs are
+    # vulnerable to SQL injection attacks.
+    comment_command = text(f'COMMENT ON TABLE "{schema}"."{name}" IS :c')
+    with engine.begin() as conn:
+        conn.execute(comment_command, {'c': comment})
+
+
 def alter_table(table_name, table_oid, schema, engine, update_data):
     if 'name' in update_data and 'columns' in update_data:
         raise ValueError('Only name or columns can be passed in, not both.')
+    if 'description' in update_data:
+        comment_on_table(table_name, schema, engine, update_data['description'])
     if 'name' in update_data:
         rename_table(table_name, schema, engine, update_data['name'])
     if 'columns' in update_data:
--- db/tests/tables/operations/test_alter.py
+++ db/tests/tables/operations/test_alter.py
@@ -1,7 +1,8 @@
 import pytest
+from sqlalchemy import select, func
 from sqlalchemy.exc import NoSuchTableError
 
-from db.tables.operations.alter import rename_table
+from db.tables.operations.alter import comment_on_table, rename_table
 from db.tables.operations.create import create_mathesar_table
 from db.tables.operations.select import get_oid_from_table, reflect_table
 from db.tests.tables import utils as test_utils
@@ -23,6 +24,26 @@ def test_rename_table(engine_with_schema):
 
     with pytest.raises(NoSuchTableError):
         reflect_table(table_name, schema, engine)
+
+
+def test_comment_on_table(engine_with_roster, roster_table_name):
+    engine, schema = engine_with_roster
+    table_oid = get_oid_from_table(roster_table_name, schema, engine)
+    expect_comment = 'my super test comment'
+    comment_on_table(roster_table_name, schema, engine, expect_comment)
+    with engine.begin() as conn:
+        res = conn.execute(select(func.obj_description(table_oid, 'pg_class')))
+    actual_comment = res.fetchone()[0]
+
+    assert actual_comment == expect_comment
+
+    expect_new_comment = 'my new test comment'
+    comment_on_table(roster_table_name, schema, engine, expect_new_comment)
+    with engine.begin() as conn:
+        res = conn.execute(select(func.obj_description(table_oid, 'pg_class')))
+    actual_new_comment = res.fetchone()[0]
+
+    assert actual_new_comment == expect_new_comment
 
 
 def test_rename_table_foreign_key(engine_with_schema):
