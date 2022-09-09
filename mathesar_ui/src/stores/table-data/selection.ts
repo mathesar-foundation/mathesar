1b11a6e6bee273a72e5e7fec7360f4b070623c61
silentninja <mmukesh95@hotmail.com>
2022-09-09 17:38:54 +0530

    Add an empty method for a single column deselection

--- mathesar_ui/src/stores/table-data/selection.ts
+++ mathesar_ui/src/stores/table-data/selection.ts
@@ -169,7 +169,9 @@ export class Selection {
 
     if (isCompleteColumnSelected) {
       // Clear the selection - deselect the column
+      // Replace with deselecting a single column
       this.resetSelection();
+      // this.clearSelection(column);
     } else {
       const cells: Cell[] = [];
       this.allRows.forEach((row) => {
@@ -179,8 +181,8 @@ export class Selection {
       // Clearing the selection
       // since we do not have cmd+click to select
       // disjointed cells
-      this.resetSelection();
       this.selectMultipleCells(cells);
+      this.columnsDataStore.get()
     }
   }
 
@@ -202,5 +204,8 @@ export class Selection {
       this.resetSelection();
       this.selectMultipleCells(cells);
     }
+  }
+
+  private clearSelection(column: Column) {
   }
 }
