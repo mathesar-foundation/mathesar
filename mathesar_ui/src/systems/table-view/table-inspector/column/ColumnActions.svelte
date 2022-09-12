c3624e3773b7a91931121769413a00656042cea8
Sean Colsen <colsen.sean@gmail.com>
2022-09-12 17:29:19 -0400

    Scaffold modal for column extraction

--- mathesar_ui/src/systems/table-view/table-inspector/column/ColumnActions.svelte
+++ mathesar_ui/src/systems/table-view/table-inspector/column/ColumnActions.svelte
@@ -6,13 +6,20 @@
     iconMoveColumnsToExistingLinkedTable,
   } from '@mathesar/icons';
   import { confirmDelete } from '@mathesar/stores/confirmation';
+  import { modal } from '@mathesar/stores/modal';
   import type {
     ColumnsDataStore,
     ProcessedColumn,
   } from '@mathesar/stores/table-data/types';
+  import type { ColumnExtractionTargetType } from './columnExtractionTypes';
+  import ExtractColumnsModal from './ExtractColumnsModal.svelte';
 
   export let columnsDataStore: ColumnsDataStore;
   export let columns: ProcessedColumn[];
+
+  let columnExtractionTargetType: ColumnExtractionTargetType = 'newTable';
+
+  const extractColumns = modal.spawnModalController();
 
   $: column = columns.length === 1 ? columns[0] : undefined;
   $: s = columns.length > 1 ? 's' : '';
@@ -29,27 +36,42 @@
       onProceed: () => columnsDataStore.deleteColumn(c.id),
     });
   }
+
+  function handleMoveColumnsToNewLinkedTable() {
+    columnExtractionTargetType = 'newTable';
+    extractColumns.open();
+  }
+
+  function handleMoveColumnsToExistingLinkedTable() {
+    columnExtractionTargetType = 'existingTable';
+    extractColumns.open();
+  }
 </script>
 
 <div class="actions-container">
   {#if column}
     <Button
-      appearance="ghost"
+      appearance="plain"
       on:click={() => column && handleDeleteColumn(column)}
     >
       <Icon {...iconDelete} />
       <span>Delete Column</span>
     </Button>
   {/if}
-  <Button appearance="plain" on:click={() => {}}>
+  <Button appearance="plain" on:click={handleMoveColumnsToNewLinkedTable}>
     <Icon {...iconMoveColumnsToNewLinkedTable} />
     <span>New linked table from column{s}</span>
   </Button>
-  <Button appearance="plain" on:click={() => {}}>
+  <Button appearance="plain" on:click={handleMoveColumnsToExistingLinkedTable}>
     <Icon {...iconMoveColumnsToExistingLinkedTable} />
     <span>Move column{s} to existing linked table</span>
   </Button>
 </div>
+
+<ExtractColumnsModal
+  controller={extractColumns}
+  targetType={columnExtractionTargetType}
+/>
 
 <style>
   .actions-container {
