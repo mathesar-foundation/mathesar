40ab4a61c8baad4f716a32231af02c2c927384b9
Sean Colsen <colsen.sean@gmail.com>
2022-09-13 14:38:27 -0400

    Add ProcessedColumnName component

--- mathesar_ui/src/components/SelectProcessedColumns.svelte
+++ mathesar_ui/src/components/SelectProcessedColumns.svelte
@@ -2,18 +2,15 @@
   import { MultiSelect } from '@mathesar-component-library';
   import type { ComponentAndProps } from '@mathesar-component-library/types';
   import type { ProcessedColumn } from '@mathesar/stores/table-data/types';
-  import ColumnName from './column/ColumnName.svelte';
+  import ProcessedColumnName from './column/ProcessedColumnName.svelte';
 
   export let availableColumns: ProcessedColumn[];
   export let columns: ProcessedColumn[] = [];
 
-  function getLabel(column: ProcessedColumn): ComponentAndProps {
+  function getLabel(processedColumn: ProcessedColumn): ComponentAndProps {
     return {
-      component: ColumnName,
-      props: {
-        // TODO: how can we get type safety for these props?
-        column: { ...column.column, constraintsType: [column.linkFk?.type] },
-      },
+      component: ProcessedColumnName,
+      props: { processedColumn },
     };
   }
 </script>
