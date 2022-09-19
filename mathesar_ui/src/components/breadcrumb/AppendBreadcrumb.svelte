0166ce33a357b51e2be80021130236594e1c1951
Sean Colsen <colsen.sean@gmail.com>
2022-09-19 12:01:20 -0400

    Fix bug in breadcrumb reactivity

--- mathesar_ui/src/components/breadcrumb/AppendBreadcrumb.svelte
+++ mathesar_ui/src/components/breadcrumb/AppendBreadcrumb.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+  import { onDestroy } from 'svelte';
 
   import type { BreadcrumbItem } from './breadcrumbTypes';
   import { getBreadcrumbItemsFromContext } from './breadcrumbUtils';
@@ -7,10 +7,25 @@
   export let item: BreadcrumbItem;
   const items = getBreadcrumbItemsFromContext();
 
-  onMount(() => {
-    $items = [...$items, item];
-    return () => {
-      $items = $items.slice(0, -1);
-    };
-  });
+  let previousItem: BreadcrumbItem | undefined;
+
+  function popLastItem() {
+    $items = $items.slice(0, -1);
+  }
+
+  function popLastItemIfPrevious() {
+    if (previousItem && $items[$items.length - 1] === previousItem) {
+      popLastItem();
+    }
+  }
+
+  function handleItemChange(i: BreadcrumbItem) {
+    popLastItemIfPrevious();
+    $items = [...$items, i];
+    previousItem = item;
+  }
+
+  $: handleItemChange(item);
+
+  onDestroy(popLastItem);
 </script>
