3895b2d2a800375305dfdcd7e11fd097535369c8
Brent Moran <brentmoran@gmail.com>
2022-09-23 16:46:25 +0800

    add /results/ endpoint with format similar to RPC endpoint

--- mathesar/api/db/viewsets/queries.py
+++ mathesar/api/db/viewsets/queries.py
@@ -50,6 +50,35 @@ class QueryViewSet(CreateModelMixin, UpdateModelMixin, RetrieveModelMixin, ListM
             output_col_desc = query.output_columns_described
             return Response(output_col_desc)
 
+    @action(methods=['get'], detail=True)
+    def results(self, request, pk=None):
+        paginator = TableLimitOffsetPagination()
+        query = self.get_object()
+        if query.not_partial:
+            serializer = RecordListParameterSerializer(data=request.GET)
+            serializer.is_valid(raise_exception=True)
+            records = paginator.paginate_queryset(
+                queryset=self.get_queryset(),
+                request=request,
+                table=query,
+                filters=serializer.validated_data['filter'],
+                order_by=serializer.validated_data['order_by'],
+                grouping=serializer.validated_data['grouping'],
+                search=serializer.validated_data['search_fuzzy'],
+                duplicate_only=serializer.validated_data['duplicate_only'],
+            )
+            paginated_records = paginator.get_paginated_response(records)
+            columns = query.output_columns_simple
+            column_metadata = query.all_columns_description_map
+            return Response(
+                {
+                    "records": paginated_records.data,
+                    "output_columns": columns,
+                    "column_metadata": column_metadata,
+                    "parameters": {k: json.loads(request.GET[k]) for k in request.GET},
+                }
+            )
+
     @action(methods=['post'], detail=False)
     def run(self, request):
         params = request.data.pop("parameters", {})
@@ -60,6 +89,7 @@ class QueryViewSet(CreateModelMixin, UpdateModelMixin, RetrieveModelMixin, ListM
         query = UIQuery(**input_serializer.validated_data)
         record_serializer = RecordListParameterSerializer(data=request.GET)
         record_serializer.is_valid(raise_exception=True)
+        # TODO This should be removed
         records = query.get_records()
         records = paginator.paginate_queryset(
             queryset=self.get_queryset(),
