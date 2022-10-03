9827b7d246001fd9f6a7aa6357b488704fab5917
Sean Colsen <colsen.sean@gmail.com>
2022-10-03 09:19:57 -0400

    Make LinkedRecordInput UX match LinkedRecordCell

--- mathesar_ui/src/components/LinkedRecord.svelte
+++ mathesar_ui/src/components/LinkedRecord.svelte
@@ -1,33 +1,72 @@
 <script lang="ts">
+  import { Icon } from '@mathesar-component-library';
+  import { iconDeleteMinor } from '@mathesar/icons';
   import {
     renderTransitiveRecordSummary,
     type DataForRecordSummaryInFkCell,
   } from '@mathesar/stores/table-data/record-summaries/recordSummaryUtils';
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
 
   export let recordId: unknown;
   export let dataForRecordSummaryInFkCell:
     | DataForRecordSummaryInFkCell
     | undefined;
+  export let hasDeleteButton: boolean = false;
 
   $: recordSummary = dataForRecordSummaryInFkCell
     ? renderTransitiveRecordSummary(dataForRecordSummaryInFkCell)
     : String(recordId);
+
+  function handleDeleteButtonClick() {
+    dispatch('delete');
+  }
 </script>
 
-<span class="linked-record">
-  {recordSummary}
+<span class="linked-record" class:has-delete-button={hasDeleteButton}>
+  <span class="record-summary">{recordSummary}</span>
+  {#if hasDeleteButton}
+    <!--
+    Why is `.delete-button` not an actual `button` element? Because we need to
+    be able to nest it inside LinkedRecordInput which acts as an input (i.e. it
+    receives focus and gets nested within a label). There may be a better way to
+    do this from an a11y perspective.
+   -->
+    <span
+      class="delete-button"
+      on:click|stopPropagation={handleDeleteButtonClick}
+      role="button"
+      aria-label="Clear value"
+      title="Clear value"
+    >
+      <Icon {...iconDeleteMinor} />
+    </span>
+  {/if}
 </span>
 
 <style>
   .linked-record {
     background: var(--color-fk);
-    padding: 0.1rem 0.4rem;
     border-radius: 0.25rem;
-    display: block;
     max-width: max-content;
+    display: grid;
+    grid-template: auto / 1fr auto;
+  }
+  .record-summary {
+    padding: 0.1rem 0.4rem;
     overflow: hidden;
     white-space: nowrap;
     text-overflow: ellipsis;
-    box-sizing: content-box;
+  }
+  .delete-button {
+    opacity: 0.4;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    padding: 0 0.3rem;
+  }
+  .delete-button:hover {
+    opacity: 1;
   }
 </style>
--- mathesar_ui/src/components/cell-fabric/data-types/components/linked-record/LinkedRecordInput.svelte
+++ mathesar_ui/src/components/cell-fabric/data-types/components/linked-record/LinkedRecordInput.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import { createEventDispatcher } from 'svelte';
-  import LinkedRecord from '@mathesar/components/LinkedRecord.svelte';
+
   // TODO remove dependency cycle
   // eslint-disable-next-line import/no-cycle
   import { getRecordSelectorFromContext } from '@mathesar/systems/record-selector/RecordSelectorController';
+
+  import { Icon, iconExpandDown } from '@mathesar-component-library';
+  import LinkedRecord from '@mathesar/components/LinkedRecord.svelte';
   import type { LinkedRecordCellProps } from '@mathesar/components/cell-fabric/data-types/components/typeDefinitions';
-  import ClearCue from './ClearCue.svelte';
 
   type $$Props = LinkedRecordCellProps & {
     class?: string;
@@ -32,9 +34,9 @@
     value = undefined;
     dispatch('artificialChange', undefined);
     dispatch('artificialInput', undefined);
-    // If the value is cleared via the ClearCue, the focus will shift to the
-    // clear button in the ClearCue. We'd like to shift it back to the input
-    // element to that the user can press `Enter` to launch the record selector.
+    // If the value is cleared via a button, the focus may shift to that button.
+    // We'd like to shift it back to the input element to that the user can
+    // press `Enter` to launch the record selector.
     element.focus();
   }
 
@@ -80,37 +82,52 @@
   class:is-acquiring-input={isAcquiringInput}
   tabindex={isAcquiringInput ? undefined : 0}
   bind:this={element}
-  on:dblclick={launchRecordSelector}
+  on:click={launchRecordSelector}
   on:focus={handleFocus}
   on:focus
   on:blur={handleBlur}
   on:blur
 >
-  {#if hasValue}
-    <span class="content {classes}">
-      <LinkedRecord recordId={value} {dataForRecordSummaryInFkCell} />
-    </span>
-    <ClearCue on:click={clear} />
-  {:else if !isAcquiringInput}
-    <!-- TODO: add dropdown arrow -->
-  {/if}
+  <span class="content {classes}">
+    {#if hasValue}
+      <LinkedRecord
+        recordId={value}
+        {dataForRecordSummaryInFkCell}
+        hasDeleteButton
+        on:delete={clear}
+      />
+    {/if}
+  </span>
+  <!--
+    Why is `.dropdown-button` not an actual `button` element? Because we need to
+    be able to nest this entire LinkedRecordInput inside a label and we don't
+    want to semantically associate the button with the label. There may be a
+    better way to do this from an a11y perspective.
+   -->
+  <span
+    class="dropdown-button"
+    on:click={launchRecordSelector}
+    role="button"
+    aria-label="Pick a record"
+    title="Pick a record"
+  >
+    <Icon {...iconExpandDown} />
+  </span>
 </span>
 
 <style>
-  /* TODO resolve code duplication with `.input-element` */
+  /* TODO resolve some code duplication with `.input-element` */
   .linked-record-input {
-    /**
-     * This size is hard-coded because we need to leave room for it, but for
-     * z-layering reasons we don't want to include it within the content
-     */
-    --clear-button-width: 2.2em;
     width: 100%;
-    display: block;
+    display: grid;
+    grid-template: auto / 1fr auto;
     position: relative;
     border: 1px solid #dfdfdf;
     border-radius: 0.25rem;
     background: #fff;
     min-height: 2.25em;
+    --padding: 0.5rem;
+    cursor: default;
   }
 
   .linked-record-input:focus {
@@ -122,8 +139,17 @@
   .content {
     position: relative;
     display: block;
-    width: calc(100% - var(--clear-button-width));
-    padding: 6px;
+    padding: var(--padding);
     z-index: 2;
+  }
+  .dropdown-button {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    color: var(--color-gray-dark);
+    padding: var(--padding);
+  }
+  .dropdown-button:hover {
+    color: var(--color-text);
   }
 </style>
