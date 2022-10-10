afc802030b1234e69a253076e50c855de70e3c12
Kriti Godey <kriti@kritigodey.com>
2022-10-10 17:55:41 -0400

    Replace double quotes with single quote

--- mathesar/api/ui/permissions/users.py
+++ mathesar/api/ui/permissions/users.py
@@ -5,24 +5,24 @@ class UserAccessPolicy(AccessPolicy):
     statements = [
         # Anyone can read all users
         {
-            "action": ["list", "retrieve"],
-            "principal": "*",
-            "effect": "allow"
+            'action': ['list', 'retrieve'],
+            'principal': '*',
+            'effect': 'allow'
         },
         # Only superusers can create users
         {
-            "action": ["create"],
-            "principal": ["*"],
-            "effect": "allow",
-            "condition": "is_superuser"
+            'action': ['create'],
+            'principal': ['*'],
+            'effect': 'allow',
+            'condition': 'is_superuser'
         },
         # Users can edit and delete themselves
         # Superusers can also edit and delete users
         {
-            "action": ["destroy", "partial_update", "update"],
-            "principal": ["*"],
-            "effect": "allow",
-            "condition_expression": ["(is_superuser or is_self)"]
+            'action': ['destroy', 'partial_update', 'update'],
+            'principal': ['*'],
+            'effect': 'allow',
+            'condition_expression': ['(is_superuser or is_self)']
         },
     ]
 
