7f48b8eba8163b255846d3701bb3ad95a70e7f0e
Kriti Godey <kriti@kritigodey.com>
2022-10-10 17:25:53 -0400

    Move serializers into UI folder.

--- mathesar/api/ui/viewsets/users.py
+++ mathesar/api/ui/viewsets/users.py
@@ -2,7 +2,7 @@ from rest_access_policy import AccessViewSetMixin
 from rest_framework import viewsets
 from rest_framework.exceptions import MethodNotAllowed
 
-from mathesar.api.serializers.users import UserSerializer, DatabaseRoleSerializer, SchemaRoleSerializer
+from mathesar.api.ui.serializers.users import UserSerializer, DatabaseRoleSerializer, SchemaRoleSerializer
 from mathesar.api.pagination import DefaultLimitOffsetPagination
 from mathesar.api.ui.permissions.users import UserAccessPolicy
 from mathesar.models.users import User, DatabaseRole, SchemaRole
