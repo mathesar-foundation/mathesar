129260f057419942b145cade57a6da6c46506ecd
Rajat Vijay <rajatvijay5@gmail.com>
2022-10-12 15:50:07 +0530

    added some comments to explain the decisions

--- mathesar_ui/src/component-library/dropdown/Dropdown.svelte
+++ mathesar_ui/src/component-library/dropdown/Dropdown.svelte
@@ -40,6 +40,12 @@
   $: tgClasses = calculateTriggerClass(triggerClass, showArrow);
 
   function toggle(e: Event) {
+    /**
+     * To make it work when wrapped with an anchor tag.
+     * Since the event is already not bubbling up
+     * and this component will always necessarily handle the click event,
+     * preventing the default behaviour should be fine
+     */
     e.preventDefault();
     e.stopPropagation();
     isOpen = !isOpen;
