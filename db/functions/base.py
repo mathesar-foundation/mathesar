966a9fa0789938eee58c6cb04b1dc2e0c7110078
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2022-10-28 16:27:45 +0300

    Readability improvements

--- db/functions/base.py
+++ db/functions/base.py
@@ -386,9 +386,7 @@ class ArrayAgg(DBFunction):
 
     @staticmethod
     def to_sa_expression(column_expr):
-        column_expr_type = column_expr.type
-        if isinstance(column_expr_type, HasUnderlyingType):
-            column_expr = column_expr_type.cast_to_underlying_type(column_expr)
+        _maybe_downcast(column_expr)
         return array_agg(column_expr)
 
 
@@ -544,3 +542,14 @@ class ExtractEmailDomain(DBFunction):
     @staticmethod
     def to_sa_expression(email):
         return sa_call_sql_function(EMAIL_DOMAIN_NAME, email, return_type=PostgresType.TEXT)
+
+
+def _maybe_downcast(column_expr):
+    """
+    We'll want to downcast some types to a psycopg-compatible type sometimes.
+    See documentation in `HasUnderlyingType`.
+    """
+    column_expr_type = column_expr.type
+    if isinstance(column_expr_type, HasUnderlyingType):
+        column_expr = column_expr_type.downcast_to_underlying_type(column_expr)
+    return column_expr
--- db/types/custom/underlying_type.py
+++ db/types/custom/underlying_type.py
@@ -3,7 +3,7 @@ from sqlalchemy import cast
 
 class HasUnderlyingType:
     """
-    We apply this mixin and its `underlying_type` property to a custom SQLAlchemy type to designate
+    We apply this mixin and its `underlying_type` attribute to a custom SQLAlchemy type to designate
     what its underlying SQLAlchemy type is. We do this, because our custom types misbehave in some
     cases (e.g. array aggregation), and so, in those cases, we cast them to what we consider the
     underlying type. Can also be thought of as the fallback type. For example, for URIs we fallback
@@ -13,17 +13,12 @@ class HasUnderlyingType:
     possible, but seemed burdensome.
     """
 
-    @property
-    def underlying_type(self):
-        """
-        The property is expected to hold an SQLAlchemy type, like `sqlalchemy.NUMERIC`.
+    # This attribute is expected to hold an SQLAlchemy type, like `sqlalchemy.NUMERIC`.
+    # Would make sense for this to be a `db.types.base.DatabaseType`, but to get the SA type
+    # class mapped to its instance (via `get_sa_class`) regrettably requires an engine, which is
+    # inappropriate here. That's pretty much a bug, and can be fixed, but might not be worth the
+    # effort.
+    underlying_type = None
 
-        Would make sense for this to be a `db.types.base.DatabaseType`, but to get the SA type
-        class mapped to its instance (via `get_sa_class`) regrettably requires an engine, which is
-        inappropriate here. That's pretty much a bug, and can be fixed, but might not be worth the
-        effort.
-        """
-        pass
-
-    def cast_to_underlying_type(self, column_expr):
+    def downcast_to_underlying_type(self, column_expr):
         return cast(column_expr, self.underlying_type)
