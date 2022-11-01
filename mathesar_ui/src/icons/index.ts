6f372a9abea11c323f447d509c3afc377ddaa995
pavish <pavishkumar.r@gmail.com>
2022-11-01 23:42:56 +0530

    Add custom array icon

--- mathesar_ui/src/icons/index.ts
+++ mathesar_ui/src/icons/index.ts
@@ -48,11 +48,10 @@ import {
   faUpload,
   faUser,
   faSave,
-  faLayerGroup,
   faTimes,
 } from '@fortawesome/free-solid-svg-icons';
-
 import type { IconProps } from '@mathesar-component-library/types';
+import { arrayIcon } from './customIcons';
 
 /**
  * @file
@@ -136,4 +135,4 @@ export const iconUiTypeText: IconProps = { data: faAlignLeft };
 export const iconUiTypeTime: IconProps = { data: faClock };
 export const iconUiTypeUnknown: IconProps = { data: faQuestion };
 export const iconUiTypeUri: IconProps = { data: faGlobe };
-export const iconUiTypeArray: IconProps = { data: faLayerGroup };
+export const iconUiTypeArray: IconProps = { data: arrayIcon };
