fc82dcff37c1d9ad6db4028a047e59fcf14ad4ef
silentninja <mmukesh95@hotmail.com>
2022-11-05 02:09:36 +0400

    Restrict database roles creation access to database managers

---
+++ mathesar/api/db/permissions/database.py
@@ -0,0 +1,30 @@
+from django.db.models import Q
+from rest_access_policy import AccessPolicy
+
+from mathesar.models.users import Role
+
+
+class DatabaseAccessPolicy(AccessPolicy):
+    statements = [
+        # Anyone can read all schema
+        {
+            'action': ['list', 'retrieve'],
+            'principal': '*',
+            'effect': 'allow',
+        },
+        # Only superusers can create users
+        {
+            'action': ['create', 'destroy', 'partial_update', 'update'],
+            'principal': ['*'],
+            'effect': 'allow',
+        }
+    ]
+
+    @classmethod
+    def scope_queryset(cls, request, qs):
+        if not request.user.is_superuser:
+            allowed_roles = (Role.MANAGER.value)
+            if request.method.lower() != 'post':
+                allowed_roles = allowed_roles + (Role.EDITOR.value, Role.VIEWER.value)
+            qs = qs.filter(Q(databaserole__role__in=allowed_roles) & Q(databaserole__user=request.user))
+        return qs
--- mathesar/api/ui/permissions/database_role.py
+++ mathesar/api/ui/permissions/database_role.py
@@ -1,19 +1,42 @@
+from django.db.models import Q
 from rest_access_policy import AccessPolicy
+
+from mathesar.models.base import Database
+from mathesar.models.users import DatabaseRole, Role
 
 
 class DatabaseRoleAccessPolicy(AccessPolicy):
     statements = [
-        # Avoid showing other users roles if they are not a superuser
+        # Creating a database role is allowed for everyone
         {
-            'action': ['list', 'retrieve'],
+            'action': ['list', 'retrieve', 'create'],
             'principal': '*',
             'effect': 'allow',
         },
         # Only superuser can assign a database role
         {
-            'action': ['create', 'destroy'],
+            'action': ['destroy', 'partial_update', 'update'],
             'principal': ['*'],
             'effect': 'allow',
-            'condition': 'is_superuser'
+            'condition_expression': ['(is_superuser or is_db_manager)']
         },
     ]
+
+    @classmethod
+    def scope_queryset(cls, request, qs):
+        if not request.user.is_superuser:
+            # TODO Consider moving to more reusable place
+            allowed_roles = (Role.MANAGER.value, Role.EDITOR.value, Role.VIEWER.value)
+            databases_with_view_access = Database.objects.filter(
+                Q(databaserole__role__in=allowed_roles) & Q(databaserole__user=request.user)
+            )
+            qs = qs.filter(database__in=databases_with_view_access)
+        return qs
+
+    def is_db_manager(self, request, view, action):
+        database_role = view.get_object()
+        return DatabaseRole.objects.filter(
+            user=request.user,
+            database=database_role.database,
+            role=Role.MANAGER.value
+        ).exists()
