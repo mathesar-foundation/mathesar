4f602f1d5cb5bde3c6401bd045c07a6e13c879a2
silentninja <mmukesh95@hotmail.com>
2022-11-07 01:24:05 +0400

    Add `DeletedColumnAccessAPIException` class

--- mathesar/api/exceptions/query_exceptions/exceptions.py
+++ mathesar/api/exceptions/query_exceptions/exceptions.py
@@ -1,6 +1,34 @@
+from rest_framework import status
+
+from mathesar.api.exceptions.error_codes import ErrorCodes
+from mathesar.api.exceptions.generic_exceptions.base_exceptions import MathesarAPIException
+
+
 class DeletedColumnAccess(Exception):
     def __init__(
             self,
             column_id
     ):
         self.column_id = column_id
+
+
+class DeletedColumnAccessAPIException(MathesarAPIException):
+    """
+
+    """
+    error_code = ErrorCodes.DeletedColumnAccess.value
+
+    def __init__(
+            self,
+            exception,
+            query,
+            message="Query contains an deleted column",
+            field=None,
+            status_code=status.HTTP_400_BAD_REQUEST,
+    ):
+
+        details = {
+            'query': query,
+            'column_id': exception.column_id
+        }
+        super().__init__(exception, self.error_code, message, field, details, status_code)
