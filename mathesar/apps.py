a939df3bbfb0609e2d74eeed3abe8d7fb77e13c5
Brent Moran <brentmoran@gmail.com>
2022-11-07 21:26:11 +0800

    reorganize DB model init code so first run doesn't fail

--- mathesar/apps.py
+++ mathesar/apps.py
@@ -1,5 +1,18 @@
 from django.apps import AppConfig
 from django.conf import settings
+from django.db.models.signals import post_migrate
+
+
+def _prepare_database_model(sender, **kwargs):
+    from mathesar.models.base import Database  # noqa
+    from mathesar.state.django import sync_databases_status  # noqa
+    dbs_in_settings = set(settings.DATABASES)
+    # We only want to track non-django dbs
+    dbs_in_settings.remove('default')
+    for db_name in dbs_in_settings:
+        Database.current_objects.get_or_create(name=db_name)
+    if not settings.TEST:
+        sync_databases_status()
 
 
 class MathesarConfig(AppConfig):
@@ -7,18 +20,7 @@ class MathesarConfig(AppConfig):
 
     name = "mathesar"
 
-    def _prepare_database_model(self):
-        from mathesar.models.base import Database  # noqa
-        from mathesar.state.django import sync_databases_status  # noqa
-        dbs_in_settings = set(settings.DATABASES)
-        # We only want to track non-django dbs
-        dbs_in_settings.remove('default')
-        for db_name in dbs_in_settings:
-            Database.current_objects.get_or_create(name=db_name)
-        if not settings.TEST:
-            sync_databases_status()
-
     def ready(self):
         """Perform initialization tasks."""
         import mathesar.signals  # noqa
-        self._prepare_database_model()
+        post_migrate.connect(_prepare_database_model, sender=self)
