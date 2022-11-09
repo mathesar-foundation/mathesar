2fa4c4a868f0957fdf848f17e9239fd89eef63bb
pavish <pavishkumar.r@gmail.com>
2022-11-10 00:55:19 +0530

    Allow custom trigger slot in Collapsible component

--- mathesar_ui/src/component-library/collapsible/Collapsible.svelte
+++ mathesar_ui/src/component-library/collapsible/Collapsible.svelte
@@ -4,20 +4,25 @@
   import { iconExpandDown } from '@mathesar-component-library-dir/common/icons';
 
   export let isOpen = false;
+
+  function toggle() {
+    isOpen = !isOpen;
+  }
 </script>
 
 <div class="collapsible">
-  <Button
-    class="collapsible-header"
-    on:click={() => {
-      isOpen = !isOpen;
-    }}
-  >
-    <div class="collapsible-header-title">
-      <slot name="header" />
-    </div>
-    <Icon {...iconExpandDown} size="0.7rem" rotate={isOpen ? 180 : undefined} />
-  </Button>
+  <slot name="trigger" {toggle} {isOpen}>
+    <Button class="collapsible-header" on:click={toggle}>
+      <div class="collapsible-header-title">
+        <slot name="header" />
+      </div>
+      <Icon
+        {...iconExpandDown}
+        size="0.7rem"
+        rotate={isOpen ? 180 : undefined}
+      />
+    </Button>
+  </slot>
   {#if isOpen}
     <div class="collapsible-content">
       <slot name="content" />
