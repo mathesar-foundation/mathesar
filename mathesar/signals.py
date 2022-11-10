b026fff41baef1cf05509cf7a53ea0334cda4543
silentninja <mmukesh95@hotmail.com>
2022-11-10 22:07:52 +0400

    Fix `give_manager_access_to_public_schema` signal to use reflection queryset

--- mathesar/signals.py
+++ mathesar/signals.py
@@ -34,5 +34,5 @@ def compute_preview_column_settings(**kwargs):
 def give_manager_access_to_public_schema(**kwargs):
     if kwargs['created']:
         instance = kwargs['instance']
-        public_schema = Schema.current_objects.get(oid=2200, database=instance.database)
+        public_schema = Schema.objects.get(oid=2200, database=instance.database)
         SchemaRole.objects.create(schema=public_schema, user=instance.user, role=Role.MANAGER.value)
