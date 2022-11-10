6839c02452b1e11a401f057a2e26734f1f9e0c62
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2022-11-10 18:46:51 +0200

    Revert "Remove unused query records and columns endpoints"
    This reverts commit c71b3362e61d87c8cf40ca3484b17c0f96eeb6a1.

--- mathesar/api/db/viewsets/queries.py
+++ mathesar/api/db/viewsets/queries.py
@@ -34,6 +34,30 @@ class QueryViewSet(
         return queryset.order_by('-created_at')
 
     @action(methods=['get'], detail=True)
+    def records(self, request, pk=None):
+        paginator = TableLimitOffsetPagination()
+        query = self.get_object()
+        serializer = RecordListParameterSerializer(data=request.GET)
+        serializer.is_valid(raise_exception=True)
+        records = paginator.paginate_queryset(
+            queryset=self.get_queryset(),
+            request=request,
+            table=query,
+            filters=serializer.validated_data['filter'],
+            order_by=serializer.validated_data['order_by'],
+            grouping=serializer.validated_data['grouping'],
+            search=serializer.validated_data['search_fuzzy'],
+            duplicate_only=serializer.validated_data['duplicate_only'],
+        )
+        return paginator.get_paginated_response(records)
+
+    @action(methods=['get'], detail=True)
+    def columns(self, request, pk=None):
+        query = self.get_object()
+        output_col_desc = query.output_columns_described
+        return Response(output_col_desc)
+
+    @action(methods=['get'], detail=True)
     def results(self, request, pk=None):
         paginator = TableLimitOffsetPagination()
         query = self.get_object()
