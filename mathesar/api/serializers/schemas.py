fef735fa4fd72fc4ca273cd6e81dfbe7a1f465e7
silentninja <mmukesh95@hotmail.com>
2022-11-14 23:36:55 +0400

    Remove unnecessary custom DRF field `ModelNameField`

--- mathesar/api/serializers/schemas.py
+++ mathesar/api/serializers/schemas.py
@@ -5,17 +5,6 @@ from mathesar.api.exceptions.mixins import MathesarErrorMessageMixin
 from mathesar.models.base import Database, Schema
 
 
-class ModelNameField(serializers.CharField):
-    """
-    De-serializes the request field as a string, but serializes the response field as
-    `model.name`. Required to support passing and returing a model name from the
-    endpoint, while also storing the model as a related field.
-    """
-
-    def to_representation(self, value):
-        return value.name
-
-
 class SchemaSerializer(MathesarErrorMessageMixin, serializers.HyperlinkedModelSerializer):
     name = serializers.CharField()
     database = SlugRelatedField(slug_field='name', queryset=Database.current_objects.all())
