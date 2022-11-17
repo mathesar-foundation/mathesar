7e0ba5fddde27af2fdea69726589b67765b1e6c0
Alek Lefebvre <info@aleklefebvre.ca>
2022-11-16 22:57:28 -0500

    await promise

--- mathesar_ui/src/stores/table-data/tabularData.ts
+++ mathesar_ui/src/stores/table-data/tabularData.ts
@@ -145,7 +145,7 @@ export class TabularData {
       this.meta.sorting.update((s) => s.without(columnId));
       this.meta.grouping.update((g) => g.withoutColumn(columnId));
       this.meta.filtering.update((f) => f.withoutColumn(columnId));
-      this.constraintsDataStore.fetch();
+      await this.constraintsDataStore.fetch();
     });
     this.columnsDataStore.on('columnPatched', async () => {
       await this.recordsData.fetch();
