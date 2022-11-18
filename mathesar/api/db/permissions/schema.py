b2f89b14a7e9e58ef7d7a2648376f440b1a4763c
silentninja <mmukesh95@hotmail.com>
2022-11-18 20:41:58 +0400

    Rename `schemaroles` to `schema_roles` for related fields

--- mathesar/api/db/permissions/schema.py
+++ mathesar/api/db/permissions/schema.py
@@ -14,8 +14,8 @@ class SchemaAccessPolicy(AccessPolicy):
             if request.method.lower() == 'get':
                 allowed_roles = allowed_roles + (Role.EDITOR.value, Role.VIEWER.value)
             permissible_database_role_filter = (
-                Q(database__databaserole__role__in=allowed_roles) & Q(database__databaserole__user=request.user)
+                Q(database__database_role__role__in=allowed_roles) & Q(database__database_role__user=request.user)
             )
-            permissible_schema_roles_filter = (Q(schemarole__role__in=allowed_roles) & Q(schemarole__user=request.user))
+            permissible_schema_roles_filter = (Q(schema_role__role__in=allowed_roles) & Q(schema_role__user=request.user))
             qs = qs.filter(permissible_database_role_filter | permissible_schema_roles_filter)
         return qs
