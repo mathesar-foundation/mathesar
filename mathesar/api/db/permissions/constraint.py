1de383ca6db5c96da4c755ff6249ce0a25a8da9e
silentninja <mmukesh95@hotmail.com>
2022-12-05 18:26:06 +0400

    Fix docstrings for Constraint permissions

--- mathesar/api/db/permissions/constraint.py
+++ mathesar/api/db/permissions/constraint.py
@@ -6,19 +6,20 @@ from mathesar.models.users import DatabaseRole, Role, SchemaRole
 
 
 class ConstraintAccessPolicy(AccessPolicy):
-    # Anyone can view a Constraint as long as they have
-    # at least a Viewer access to the schema or its database
-    # Create Access is restricted to superusers or managers of the schema or the database the constraint is part of.
+    """
+    Anyone can view Constraint as long as they have
+    at least a Viewer access to the schema or its database
+    Only superuser or schema/database manager can create/delete/modify/update the Constraint
+    """
     statements = [
         {
             'action': ['list', 'retrieve'],
             'principal': '*',
             'effect': 'allow',
         },
-        # Only superuser or schema/database manager can delete the role
         {
             'action': ['create', 'destroy', 'update', 'partial_update'],
-            'principal': ['*'],
+            'principal': '*',
             'effect': 'allow',
             'condition_expression': ['(is_superuser or is_table_manager)']
         },
@@ -29,9 +30,13 @@ class ConstraintAccessPolicy(AccessPolicy):
         if not request.user.is_superuser:
             allowed_roles = (Role.MANAGER.value, Role.EDITOR.value, Role.VIEWER.value)
             permissible_database_role_filter = (
-                Q(table__schema__database__database_role__role__in=allowed_roles) & Q(table__schema__database__database_role__user=request.user)
+                Q(table__schema__database__database_role__role__in=allowed_roles)
+                & Q(table__schema__database__database_role__user=request.user)
             )
-            permissible_schema_roles_filter = (Q(table__schema__schema_role__role__in=allowed_roles) & Q(table__schema__schema_role__user=request.user))
+            permissible_schema_roles_filter = (
+                Q(table__schema__schema_role__role__in=allowed_roles)
+                & Q(table__schema__schema_role__user=request.user)
+            )
             qs = qs.filter(permissible_database_role_filter | permissible_schema_roles_filter)
 
         return qs
