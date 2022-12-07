d5892e650467874dee5ea0e25464dba57acfd9a1
Sean Colsen <colsen.sean@gmail.com>
2022-12-07 15:38:28 -0500

    Better handle long table names in table widgets

--- mathesar_ui/src/pages/record/TableWidget.svelte
+++ mathesar_ui/src/pages/record/TableWidget.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { Truncate } from '@mathesar-component-library';
   import type { TableEntry } from '@mathesar/api/types/tables';
   import type { Column } from '@mathesar/api/types/tables/columns';
   import TableName from '@mathesar/components/TableName.svelte';
@@ -37,44 +38,26 @@
 
 <div class="table-widget">
   <div class="top">
-    <div class="left">
-      <h3 class="passthrough">
-        <strong><TableName {table} /></strong>
-      </h3>
-    </div>
-    <div class="right">
-      <MiniActionsPane />
-    </div>
+    <Truncate><strong><TableName {table} /></strong></Truncate>
+    <MiniActionsPane />
   </div>
 
   <div class="results">
-    <TableView />
+    <TableView context="widget" />
   </div>
 </div>
 
 <style>
   .top {
-    --spacing-x: 1rem;
-    --spacing-y: 0.3rem;
-    display: flex;
+    display: grid;
+    grid-template: auto / 1fr auto;
+    gap: 0.5rem;
     justify-content: space-between;
     align-items: center;
-    margin: calc(-1 * var(--spacing-y)) calc(-1 * var(--spacing-x));
-    padding-bottom: 1rem;
-    flex-wrap: wrap;
+    padding-bottom: 0.5rem;
+    overflow: hidden;
   }
   .top > :global(*) {
-    margin: var(--spacing-y) var(--spacing-x);
-  }
-  .left {
-    display: grid;
-    grid-auto-flow: column;
-    gap: 0.5rem;
-    align-items: center;
-  }
-  .right {
-    flex: 1 0 auto;
-    display: flex;
-    justify-content: flex-end;
+    overflow: hidden;
   }
 </style>
