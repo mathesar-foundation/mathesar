583f5398eebd68778937c7d42c1d2a9ef7780157
silentninja <mmukesh95@hotmail.com>
2022-12-08 21:13:39 +0400

    Update RecordAccessPolicy docstrings to include a link to the Spec

--- mathesar/api/db/permissions/records.py
+++ mathesar/api/db/permissions/records.py
@@ -10,7 +10,7 @@ class RecordAccessPolicy(AccessPolicy):
     at least a Viewer access to the Schema or the Database of the Table.
     The permissions trickle down, so if someone has a Viewer Access for a Database
     They automatically become a Schema Viewer
-    https://wiki.mathesar.org/en/product/specs/users-permissions#database-permissions
+    Refer https://wiki.mathesar.org/en/product/specs/users-permissions#database-permissions
     Only superuser or schema/database Manager/Editor can delete/modify/create a Record
     """
     statements = [
