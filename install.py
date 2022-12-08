1f9a82698780a9dd5933ddc0fe1e7cad1a0a8870
silentninja <mmukesh95@hotmail.com>
2022-12-08 16:16:53 +0400

    Add Django config check and createsuperuser step to install script

--- install.py
+++ install.py
@@ -2,10 +2,17 @@
 This script installs functions and types for Mathesar onto the configured DB.
 """
 import getopt
+import os
 import sys
+
+import getpass
+
+import django
+from django.contrib.auth import get_user_model
 
 from config.settings import DATABASES
 from db import install
+import secrets
 
 
 def main():
@@ -14,67 +21,60 @@ def main():
     for (opt, value) in opts:
         if (opt == "-s") or (opt == "--skip-confirm"):
             skip_confirm = True
-
+    check_missing_dj_config()
+    django.setup()
+    if not superuser_exists():
+        create_superuser()
     for database_key in [key for key in DATABASES if key != "default"]:
         install_on_db_with_key(database_key, skip_confirm)
 
 
 def superuser_exists():
-    pass
-
-
-def secret_key_exists():
-    pass
+    return get_user_model().objects.filter(is_superuser=True).exists()
 
 
 def create_superuser():
-    pass
+    username = input("Username: ")
+    email = input("Email: ")
+    password = getpass.getpass('Password:')
+    get_user_model().objects.create_superuser(username, email, password)
 
 
 def generate_secretkey():
-    pass
+    secret_key = secrets.token_urlsafe()
+    # TODO Add documentation link
+    documentation_link = ""
+    print(f"Please follow the instructions in {documentation_link} to add the below secret key to the application")
+    print(f"Secret Key: {secret_key}")
+
+
+def check_missing_dj_config():
+    try:
+        os.environ.get('SECRET_KEY', None)
+    except KeyError:
+        generate_secretkey()
+    # TODO Add documentation link
+    documentation_link = ""
+    try:
+        os.environ['DJANGO_DATABASE_KEY']
+        os.environ['DJANGO_SETTINGS_MODULE']
+        os.environ['DJANGO_DATABASE_URL']
+        os.environ['MATHESAR_DATABASES']
+    except KeyError as e:
+        missing_config_key = e.args[0]
+        raise Exception(f"{missing_config_key} is missing from the config."
+                        f" Please follow the documentation {documentation_link} add the missing config")
 
 
 def install_on_db_with_key(database_key, skip_confirm):
-    if DATABASES[database_key]["HOST"] == "mathesar_db":
-        # if we're going to install on the docker-created Postgres, we'll
-        # create the DB
-        print("Creating Mathesar DB on docker-created PostgreSQL instance")
-        install.create_mathesar_database(
-            user_database=DATABASES[database_key]["NAME"],
-            username=DATABASES["default"]["USER"],
-            password=DATABASES["default"]["PASSWORD"],
-            hostname=DATABASES["default"]["HOST"],
-            root_database=DATABASES["default"]["NAME"],
-            port=DATABASES["default"]["PORT"],
-        )
-    else:
-        # if we're installing anywhere else, we require the DB to exist in
-        # advance.
-        username = DATABASES[database_key]["USER"]
-        password = DATABASES[database_key]["PASSWORD"]
-        host = DATABASES[database_key]["HOST"]
-        db_name = DATABASES[database_key]["NAME"]
-        port = DATABASES[database_key]["PORT"]
-        print(f"Installing Mathesar DB {db_name} on preexisting PostgreSQL instance at host {host}...")
-        if skip_confirm is True:
-            confirmation = "y"
-        else:
-            confirmation = input(
-                f"Mathesar will be installed on DB {db_name} at host {host}."
-                "Confirm? (y/n) >  "
-            )
-        if confirmation.lower() in ["y", "yes"]:
-            print("Installing...")
-            install.install_mathesar_on_preexisting_database(
-                username,
-                password,
-                host,
-                db_name,
-                port,
-            )
-        else:
-            print("Skipping DB with key {database_key}.")
+    install.install_mathesar(
+        user_database=DATABASES[database_key]["NAME"],
+        username=DATABASES["default"]["USER"],
+        password=DATABASES["default"]["PASSWORD"],
+        hostname=DATABASES["default"]["HOST"],
+        port=DATABASES["default"]["PORT"],
+        skip_confirm=skip_confirm
+    )
 
 
 if __name__ == "__main__":
