7525443e32ce6ea047000c13fed1d83d2d94427e
Rajat Vijay <rajatvijay5@gmail.com>
2022-12-14 20:36:52 +0000

    drop constraint

--- mathesar_ui/src/systems/table-view/constraints/ConstraintCollapseHeader.svelte
+++ mathesar_ui/src/systems/table-view/constraints/ConstraintCollapseHeader.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import type { Constraint } from '@mathesar/api/types/tables/constraints';
+  import { Button, Icon } from '@mathesar/component-library';
   import ColumnName from '@mathesar/components/column/ColumnName.svelte';
+  import { iconDeleteMajor } from '@mathesar/icons';
+  import { confirmDelete } from '@mathesar/stores/confirmation';
   import { getTabularDataStoreFromContext } from '@mathesar/stores/table-data';
 
   export let constraint: Constraint;
@@ -8,20 +11,44 @@
   const tabularData = getTabularDataStoreFromContext();
 
   $: columnsInTable = $tabularData.columnsDataStore.columns;
+  $: constraintsDataStore = $tabularData.constraintsDataStore;
   $: columnsInConstraint = $columnsInTable.filter((c) =>
     constraint.columns.includes(c.id),
   );
+
+  function handleDrop() {
+    void confirmDelete({
+      identifierType: 'Constraint',
+      identifierName: constraint.name,
+      body: ['Are you sure you want to proceed?'],
+      onProceed: () => {
+        return constraintsDataStore.remove(constraint.id);
+      },
+    });
+  }
 </script>
 
-<div class="column-names">
-  {#each columnsInConstraint as column (column.id)}
-    <span class="column-name-container">
-      <ColumnName {column} />
-    </span>
-  {/each}
+<div class="column-names-header">
+  <div class="column-names">
+    {#each columnsInConstraint as column (column.id)}
+      <span class="column-name-container">
+        <ColumnName {column} />
+      </span>
+    {/each}
+  </div>
+  <Button on:click={handleDrop} size="small" appearance="plain"
+    ><Icon {...iconDeleteMajor} /></Button
+  >
 </div>
 
 <style lang="scss">
+  .column-names-header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
   .column-names {
     display: flex;
     flex-direction: row;
@@ -29,12 +56,12 @@
     > :global(* + *) {
       margin-left: 0.25rem;
     }
+  }
 
-    .column-name-container {
-      font-size: var(--text-size-small);
-      background-color: var(--slate-200);
-      border-radius: var(--border-radius-xl);
-      padding: 4px 6px;
-    }
+  .column-name-container {
+    font-size: var(--text-size-small);
+    background-color: var(--slate-200);
+    border-radius: var(--border-radius-xl);
+    padding: 4px 6px;
   }
 </style>
