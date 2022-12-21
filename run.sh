83feeaff510cdd58ce17c770d9e0d82e37a993f6
silentninja <mmukesh95@hotmail.com>
2022-12-21 20:14:43 +0400

    Add profiles to docker compose

--- run.sh
+++ run.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
 
-# This script checks if Mathesar is running in dev mode
-# or prod. If dev mode, it starts the vite dev server,
-# else it runs a client build.
-
-# This script is only supposed to be used either directly
-# or with docker-compose. It rebuilds the client everytime
-# Mathesar starts.
-
-# For deployments, the DockerFile is configured to build the
-# client. Hence, instead of using this script, the web server
-# can be directly started.
-
+# Start the Django server on port 8000.
 python manage.py runserver 0.0.0.0:8000 && fg
