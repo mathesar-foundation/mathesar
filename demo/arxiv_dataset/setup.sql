0e2371c8affabc559a49678a1a1553db921fb7a2
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2023-01-09 14:56:59 +0200

    Simplify model

--- demo/arxiv_dataset/setup.sql
+++ demo/arxiv_dataset/setup.sql
@@ -1,22 +1,19 @@
 -- Authors
 
 CREATE TABLE "Authors" (
-    id SERIAL PRIMARY KEY,
-    "Name" text
+    "Name" text PRIMARY KEY
 );
 
 -- Categories
 
 CREATE TABLE "Categories" (
-    id SERIAL PRIMARY KEY,
-    "Name" text
+    "Name" text PRIMARY KEY
 );
 
 -- Links
 
 CREATE TABLE "Links" (
-    id SERIAL PRIMARY KEY,
-    "URL" mathesar_types.uri
+    "URL" mathesar_types.uri PRIMARY KEY
 );
 
 -- Papers
@@ -30,35 +27,35 @@ CREATE TABLE "Papers" (
     "Comment" text,
     "Journal reference" text,
     "DOI" text,
-    "Primary category" integer
+    "Primary category" text references Categories(Name)
 );
 
 -- Paper-Author bridge table
 
 CREATE TABLE "Paper-Author" (
-  paper_id integer,
-  author_id integer,
+  paper_id text,
+  author_id text,
   PRIMARY KEY (paper_id, author_id),
   CONSTRAINT fk_paper FOREIGN KEY(paper_id) REFERENCES Papers(id),
-  CONSTRAINT fk_author FOREIGN KEY(author_id) REFERENCES Authors(id)
+  CONSTRAINT fk_author FOREIGN KEY(author_id) REFERENCES Authors(Name)
 );
 
 -- Paper-Category bridge table
 
 CREATE TABLE "Paper-Category" (
-  paper_id integer,
-  category_id integer,
+  paper_id text,
+  category_id text,
   PRIMARY KEY (paper_id, category_id),
   CONSTRAINT fk_paper FOREIGN KEY(paper_id) REFERENCES Papers(id),
-  CONSTRAINT fk_category FOREIGN KEY(category_id) REFERENCES Categories(id)
+  CONSTRAINT fk_category FOREIGN KEY(category_id) REFERENCES Categories(Name)
 );
 
 -- Paper-Link bridge table
 
 CREATE TABLE "Paper-Link" (
-  paper_id integer,
-  link_id integer,
+  paper_id text,
+  link_id text,
   PRIMARY KEY (paper_id, link_id),
   CONSTRAINT fk_paper FOREIGN KEY(paper_id) REFERENCES Papers(id),
-  CONSTRAINT fk_link FOREIGN KEY(link_id) REFERENCES Links(id)
+  CONSTRAINT fk_link FOREIGN KEY(link_id) REFERENCES Links(URL)
 );
