1089993e854b8aa310b5622b22e3e1f31204229a
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2023-01-09 15:38:08 +0200

    Finish writing SQL queries

--- demo/arxiv_dataset/script.py
+++ demo/arxiv_dataset/script.py
@@ -2,6 +2,8 @@ import arxiv
 import json
 from sqlalchemy import text
 
+from mathesar.database.base import create_mathesar_engine
+
 from demo.arxiv_dataset.base import get_arxiv_db_and_schema_log_path
 
 
@@ -9,11 +11,12 @@ def update_our_arxiv_dbs():
     papers = _download_arxiv_papers()
     db_schema_pairs = _get_logged_db_schema_pairs()
     for db_name, schema_name in db_schema_pairs:
-        engine = _get_engine(db_name)
+        engine = create_mathesar_engine(db_name)
         with engine.begin() as conn:
             _set_search_path(conn, schema_name)
             for paper in papers:
-                persist_paper(conn, schema_name, paper)
+                persist_paper(conn, paper)
+        engine.dispose()
 
 
 def _download_arxiv_papers():
@@ -29,55 +32,106 @@ def _set_search_path(conn, schema_name):
     conn.execute(set_search_path)
 
 
-def _get_engine(db_name):
-    # TODO
-    pass
-
-
-def persist_paper(conn, schema_name, paper):
-    # TODO maybe run this only once?
-    authors = None
-    categories = None
-    links = None
-    map_of_author_to_id = _persist_authors(conn, authors)
-    map_of_category_to_id = _persist_categories(conn, categories)
-    map_of_link_to_id = _persist_links(conn, links)
-    # TODO finish insert_query
-    insert_query = text(
-        f"""
-            INSERT INTO Authors (Name)
-            VALUES ()
-            RETURNING id
-        """
-    )
+def persist_paper(conn, paper):
+    authors = paper.authors
+    categories = paper.categories
+    links = paper.links
+    for x in [*authors, *categories, *links]:
+        #TODO remove before merge
+        assert type(x) is str
+    _persist_authors(conn, authors)
+    _persist_categories(conn, categories)
+    _persist_links(conn, links)
+    insert_query = _get_persist_paper_insert_query(paper)
     [paper_id] = conn.execute(insert_query)
-    # TODO insert into Paper-Author, Paper-Category, Paper-Link bridge tables
+    _persist_paper_authors(conn, paper_id, authors)
+    _persist_paper_categories(conn, paper_id, categories)
+    _persist_paper_links(conn, paper_id, links)
+
+
+def _get_persist_paper_insert_query(paper):
+    id = paper.entry_id
+    updated = paper.updated
+    published = paper.published
+    title = paper.title
+    summary = paper.summary
+    comment = paper.comment
+    journal_reference = paper.journal_ref
+    doi = paper.doi
+    primary_category = paper.primary_category
+    return text(
+        f"""
+                INSERT INTO Papers (id, Updated, Published, Title, Summary, Comment, "Journal reference", DOI, "Primary category")
+                VALUES ({id, updated, published, title, summary, comment, journal_reference, doi, primary_category})
+                RETURNING id
+            """
+    )
 
 
 def _persist_authors(conn, author_names):
-    map_of_author_to_id = {}
     for author_name in author_names:
         insert_query = text(
             f"""
                 INSERT INTO Authors (Name)
                 VALUES ({author_name})
-                RETURNING id
             """
         )
-        result = conn.execute(insert_query)
-        [author_id] = result.fetchone()
-        map_of_author_to_id[author_name] = author_id
-    return map_of_author_to_id
+        conn.execute(insert_query)
 
 
-def _persist_categories(conn, category_names):
-    # TODO
-    pass
+def _persist_categories(conn, categories):
+    for category in categories:
+        insert_query = text(
+            f"""
+                INSERT INTO Categories (Name)
+                VALUES ({category})
+            """
+        )
+        conn.execute(insert_query)
 
 
-def _persist_links(conn, link_urls):
-    # TODO
-    pass
+def _persist_links(conn, links):
+    for link in links:
+        insert_query = text(
+            f"""
+                INSERT INTO Links (URL)
+                VALUES ({link})
+            """
+        )
+        conn.execute(insert_query)
+
+
+def _persist_paper_authors(conn, paper_id, author_ids):
+    for author_id in author_ids:
+        insert_query = text(
+            f"""
+                INSERT INTO Paper-Author (paper_id, author_id)
+                VALUES ({paper_id, author_id})
+            """
+        )
+        conn.execute(insert_query)
+
+
+def _persist_paper_categories(conn, paper_id, category_ids):
+    for category_id in category_ids:
+        insert_query = text(
+            f"""
+                INSERT INTO Paper-Category (paper_id, category_id)
+                VALUES ({paper_id, category_id})
+            """
+        )
+        conn.execute(insert_query)
+
+
+def _persist_paper_links(conn, paper_id, link_ids):
+    for link_id in link_ids:
+        insert_query = text(
+            f"""
+                INSERT INTO Paper-Link (paper_id, link_id)
+                VALUES ({paper_id, link_id})
+            """
+        )
+        conn.execute(insert_query)
 
 
 def _get_logged_db_schema_pairs():
