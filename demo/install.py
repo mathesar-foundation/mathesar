a652728accfef470abdb825b503e96cc80c06ef6
silentninja <mmukesh95@hotmail.com>
2023-01-11 20:19:25 +0400

    Add request parameters to Stale database API

--- demo/install.py
+++ demo/install.py
@@ -111,37 +111,50 @@ def _get_dj_column_by_name(table, name):
             return c
 
 
-def drop_all_stale_databases():
-    excluded_databases = ['mathesar', 'postgres', 'template0', 'template1']
-    databases = Database.objects.filter(created_at__gt=now() - timedelta(days=5))
-    for database in databases:
+def drop_all_stale_databases(force=False, max_days=3):
+    excluded_databases = [
+        settings.DATABASES["default"]["NAME"],
+        # Exclude Postgres default databases
+        'postgres',
+        'template0',
+        'template1'
+    ]
+    stale_databases = Database.objects.filter(created_at__lt=now() - timedelta(days=max_days))
+    deleted_databases = []
+    for database in stale_databases:
         if database.name not in excluded_databases and database.deleted is False:
-            drop_mathesar_database(
-                database,
+            dropped = drop_mathesar_database(
+                database.name,
                 username=settings.DATABASES["default"]["USER"],
                 password=settings.DATABASES["default"]["PASSWORD"],
                 hostname=settings.DATABASES["default"]["HOST"],
-                port=settings.DATABASES["default"]["PORT"]
+                root_database=settings.DATABASES["default"]["NAME"],
+                port=settings.DATABASES["default"]["PORT"],
+                force=force
             )
-            reflect_db_objects(get_empty_metadata())
+            if dropped:
+                deleted_databases.append(database.name)
+                database.delete()
     reflect_db_objects(get_empty_metadata())
+    return deleted_databases
 
 
 def drop_mathesar_database(
-        user_database, username, password, hostname, port,
+        user_database, username, password, hostname, root_database, port, force=False
 ):
-    user_db_engine = engine.create_future_engine(
-        username, password, hostname, user_database.name, port
-    )
     try:
-        user_db_engine.connect()
-        with user_db_engine.connect() as conn:
+        root_db_engine = engine.create_future_engine(
+            username, password, hostname, root_database, port,
+        )
+        with root_db_engine.connect() as conn:
             conn.execution_options(isolation_level="AUTOCOMMIT")
-            conn.execute(text(f"DROP DATABASE {user_database.name}"))
+            delete_stmt = f"DROP DATABASE {user_database} {'WITH (FORCE)' if force else ''}"
+            conn.execute(text(delete_stmt))
+            # This database is not created using a config file,
+            # so their objects can be safety delete
+            # as won't be created again during reflection or when running install script
+            return True
     except OperationalError:
-        user_db_engine.dispose()
-    finally:
-        # This database is not created using a config file,
-        # so their objects can be safety delete
-        # as won't be created again during reflection or when running install script
-        user_database.delete()
+        # Database is in use
+        pass
+    return False
