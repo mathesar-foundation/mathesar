a16bc955b42407e82b41a7fee522c3605f04c28d
Brent Moran <brentmoran@gmail.com>
2023-01-12 14:56:18 +0800

    set up template DB when service (re)starts

--- demo/settings.py
+++ demo/settings.py
@@ -1,6 +1,9 @@
 from config.settings import *  # noqa
 from decouple import config as decouple_config
 
+INSTALLED_APPS += [  # noqa
+    "demo"
+]
 
 MIDDLEWARE += [  # noqa
     "demo.middleware.LiveDemoModeMiddleware",
@@ -9,3 +12,5 @@ MIDDLEWARE += [  # noqa
 MATHESAR_LIVE_DEMO = True
 MATHESAR_LIVE_DEMO_USERNAME = decouple_config('MATHESAR_LIVE_DEMO_USERNAME', default=None)
 MATHESAR_LIVE_DEMO_PASSWORD = decouple_config('MATHESAR_LIVE_DEMO_PASSWORD', default=None)
+
+MATHESAR_DEMO_TEMPLATE = 'mathesar_demo_template'
