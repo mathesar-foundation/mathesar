061fa39569d8c3ada627b03f8a44160bae23484a
Brent Moran <brentmoran@gmail.com>
2023-01-12 14:57:32 +0800

    use template rather than fresh data load for user DB

--- demo/middleware.py
+++ demo/middleware.py
@@ -3,9 +3,8 @@ import logging
 
 from django.conf import settings
 
-from demo.install import load_datasets, customize_settings
+from demo.install import load_datasets, customize_settings, create_demo_database
 from demo.db_namer import get_name
-from db.install import create_mathesar_database
 from mathesar.database.base import create_mathesar_engine
 from mathesar.models.base import Database
 from mathesar.state import reset_reflection
@@ -20,21 +19,20 @@ class LiveDemoModeMiddleware:
 
     def __call__(self, request):
         sessionid = request.COOKIES.get('sessionid', None)
-        # every 4th character obfuscates sessionid (a bit)
         db_name = get_name(str(sessionid))
         database, created = Database.current_objects.get_or_create(name=db_name)
         if created:
-            create_mathesar_database(
+            create_demo_database(
                 db_name,
-                username=settings.DATABASES["default"]["USER"],
-                password=settings.DATABASES["default"]["PASSWORD"],
-                hostname=settings.DATABASES["default"]["HOST"],
-                root_database=settings.DATABASES["default"]["NAME"],
-                port=settings.DATABASES["default"]["PORT"],
+                settings.DATABASES["default"]["USER"],
+                settings.DATABASES["default"]["PASSWORD"],
+                settings.DATABASES["default"]["HOST"],
+                settings.DATABASES["default"]["NAME"],
+                settings.DATABASES["default"]["PORT"],
+                settings.MATHESAR_DEMO_TEMPLATE
             )
-            engine = create_mathesar_engine(db_name)
-            load_datasets(engine)
             reset_reflection()
+            engine = create_mathesar_engine(db_name)
             customize_settings(engine)
 
         logger.debug(f"Using database {db_name} for sessionid {sessionid}")
