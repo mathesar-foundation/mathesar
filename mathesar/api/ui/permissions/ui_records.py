8077853971ecaede696b13dff424ef14b68a8019
aagman <aagmanbhatt945@gmail.com>
2023-01-27 17:18:56 +0530

    Add ui record access policy

---
+++ mathesar/api/ui/permissions/ui_records.py
@@ -0,0 +1,65 @@
+from rest_access_policy import AccessPolicy
+
+from mathesar.api.utils import get_table_or_404
+from mathesar.models.users import DatabaseRole, Role, SchemaRole
+
+
+class RecordAccessPolicy(AccessPolicy):
+    """
+    Anyone can view a Record as long as they are a superuser or have
+    at least a Viewer access to the Schema or the Database of the Table.
+    The permissions trickle down, so if someone has a Viewer Access for a Database
+    They automatically become a Schema Viewer
+    Refer https://wiki.mathesar.org/en/product/specs/users-permissions#database-permissions
+    Only superuser or schema/database Manager/Editor can delete/modify/create a Record
+    """
+    statements = [
+        {
+            'action': ['list', 'retrieve'],
+            'principal': '*',
+            'effect': 'allow',
+            'condition_expression': ['(is_superuser or is_table_viewer)']
+        },
+        {
+            'action': [ 'update', 'partial_update', 'create', 'delete'],
+            'principal': '*',
+            'effect': 'allow',
+            'condition_expression': ['(is_superuser or is_table_editor)']
+        },
+    ]
+
+    def is_table_viewer(self, request, view, action):
+        # Record access control is based on Schema and Database Roles as of now
+        # TODO Include Table Role based access when Table Roles are introduced
+        table = get_table_or_404(view.kwargs['table_pk'])
+        allowed_roles = (Role.MANAGER.value, Role.EDITOR.value, Role.VIEWER.value)
+
+        is_schema_viewer = SchemaRole.objects.filter(
+            user=request.user,
+            schema=table.schema,
+            role__in=allowed_roles
+        ).exists()
+        is_db_viewer = DatabaseRole.objects.filter(
+            user=request.user,
+            database=table.schema.database,
+            role__in=allowed_roles
+        ).exists()
+        return is_db_viewer or is_schema_viewer
+
+    def is_table_editor(self, request, view, action):
+        # Record access control is based on Schema and Database Roles as of now
+        # TODO Include Table Role based access when Table Roles are introduced
+        table = get_table_or_404(view.kwargs['table_pk'])
+        allowed_roles = (Role.MANAGER.value, Role.EDITOR.value)
+
+        is_schema_manager = SchemaRole.objects.filter(
+            user=request.user,
+            schema=table.schema,
+            role__in=allowed_roles
+        ).exists()
+        is_db_manager = DatabaseRole.objects.filter(
+            user=request.user,
+            database=table.schema.database,
+            role__in=allowed_roles
+        ).exists()
+        return is_db_manager or is_schema_manager
