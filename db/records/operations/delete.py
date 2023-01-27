a6a10cd43a8f03d3ece287e8784d1701f5ac85d3
aagman <aagmanbhatt945@gmail.com>
2023-01-27 17:17:21 +0530

    added bulk_delete_records operation

--- db/records/operations/delete.py
+++ db/records/operations/delete.py
@@ -8,3 +8,9 @@ def delete_record(table, engine, id_value):
     query = delete(table).where(primary_key_column == id_value)
     with engine.begin() as conn:
         return conn.execute(query)
+
+def bulk_delete_records(table, engine, id_values):
+    primary_key_column = get_primary_key_column(table)
+    query = delete(table).where(primary_key_column.in_(id_values))
+    with engine.begin() as conn:
+        return conn.execute(query)
