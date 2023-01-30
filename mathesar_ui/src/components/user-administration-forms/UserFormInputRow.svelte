c027485d909c89dfad10739d30f8b0423d020b4d
pavish <pavishkumar.r@gmail.com>
2023-01-31 03:14:18 +0530

    Create UserFormInputRow component

---
+++ mathesar_ui/src/components/user-administration-forms/UserFormInputRow.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let bypass = false;
+</script>
+
+{#if bypass}
+  <slot />
+{:else}
+  <div class="input-row" style="display:contents;">
+    <slot />
+  </div>
+{/if}
+
+<style lang="scss">
+  .input-row + :global(.input-row .cell) {
+    margin-top: var(--size-large);
+  }
+</style>
