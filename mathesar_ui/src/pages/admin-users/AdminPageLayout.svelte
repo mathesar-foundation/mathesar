b16980454b572182751343134153782fe5d95e78
Rajat Vijay <rajatvijay5@gmail.com>
2023-02-08 11:24:19 +0000

    mobile responsive layout fixes

--- mathesar_ui/src/pages/admin-users/AdminPageLayout.svelte
+++ mathesar_ui/src/pages/admin-users/AdminPageLayout.svelte
@@ -10,39 +10,42 @@
     : undefined;
 </script>
 
-<div class="admin-layout" {style}>
-  <aside>
-    <slot name="sidebar" />
-  </aside>
-  <main>
-    <slot />
-  </main>
+<div class="admin-layout-container" {style}>
+  <div class="admin-layout">
+    <aside>
+      <slot name="sidebar" />
+    </aside>
+    <main>
+      <slot />
+    </main>
+  </div>
 </div>
 
 <style lang="scss">
-  .admin-layout {
-    display: flex;
-    flex-wrap: wrap;
+  .admin-layout-container {
     max-width: var(--max-layout-width, 85rem);
     margin-left: auto;
     margin-right: auto;
     padding: 1rem 0;
+  }
 
-    > :global(* + *) {
-      margin-left: 1rem;
+  .admin-layout {
+    --gap: 1rem;
+    display: flex;
+    flex-wrap: wrap;
+    margin-left: calc(-1 * var(--gap));
+
+    aside {
+      flex-grow: 1;
+      flex-basis: 18rem;
+      margin-left: var(--gap);
     }
-  }
 
-  aside {
-    flex-grow: 1;
-    flex-basis: 18rem;
-
-    border-right: 1px solid var(--slate-200);
-  }
-
-  main {
-    flex-basis: 0;
-    flex-grow: 999;
-    min-inline-size: 50%;
+    main {
+      flex-basis: 0;
+      flex-grow: 999;
+      min-inline-size: 50%;
+      margin-left: var(--gap);
+    }
   }
 </style>
