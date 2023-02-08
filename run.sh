c00f8d7adea3cde1090b16bb734e7929648d11b6
Mukesh <mmukesh95@hotmail.com>
2023-02-08 11:03:50 +0400

    Update run.sh
    Remove git merge cruft

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Start the Django server on port 8000.
 gunicorn config.wsgi -b 0.0.0.0:8000 && fg
-=======
+
 # This script checks if Mathesar is running in dev mode
 # or prod. If dev mode, it starts the vite dev server,
 # else it runs a client build.
@@ -17,7 +17,7 @@ gunicorn config.wsgi -b 0.0.0.0:8000 && fg
 cd mathesar_ui && npm install --unsafe-perm
 
 # Run vite dev server only in dev mode
-if [[ "$MODE" == "DEVELOPMENT" || "$1" == "dev" ]]; then
+if [[ "$MODE" == "DEVELOPMENT" || "1 USD (83 INR)" == "dev" ]]; then
   npm run dev &
 else
   npm run build
