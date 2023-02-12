2cba81b1234f07ba50eb90f213c0b1b60aae0def
silentninja <mmukesh95@hotmail.com>
2023-02-13 03:06:08 +0400

    Convert user database correctly when running install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,7 @@ secret_key=$(tr -dc 'a-z0-9!@#$%^&*(-_=+)' < /dev/urandom | head -c50)
 echo "Secret key generated successfully"
 echo "Mathesar needs a Postgres database to store the metadata. We will be creating a database now"
 read -r -p "Please enter the username to use for the database: " dj_db_username
-read -r -p "Please enter the password to use for the database: " dj_db_password
-read -r -p "Please enter the name of the metadata database: " dj_db_name
+read -r -s -p "Please enter the password to use for the database: " dj_db_password
 read -r -p "Please enter the port the database should listen on: " db_host
 read -r -p "Enter the domain name under which Mathesar will be hosted. Skip if you are planning to host it locally: " allowed_hosts
 read -r -p "Enter superuser username: " superuser_username
@@ -23,30 +22,27 @@ do
     if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
     then
       read -r -p "Please enter the username to use for the database: " db_username
-      read -r -p "Please enter the password to use for the database: " db_password
-      read -r -p "Please enter the name of the metadata database: " db_name
-      user_database_url="(mathesar_tables|postgresql://$db_username:$db_password@mathesar_db:5432/$db_name)"
+      read -r -s -p "Please enter the password to use for the database: " db_password
+      read -r -p "Please enter the name of the database: " db_name
+      user_database_urls_lst+=("($db_name|postgresql://$db_username:$db_password@mathesar_db:5432/$db_name)")
     else
-        exit 0
+        break
     fi
 done
-IFS=\| eval 'user_database_urls="${user_database_urls[*]}"'
-
-
-read -r -p "Enter the database url you would like Mathesar to manage: " user_database_urls
+IFS=\| eval 'user_database_urls="${user_database_urls_lst[*]}"'
 read -r -p "Enter the http port for the Mathesar webserver to user [Defaults to port 80]: " http_port
 read -r -p "Enter the https port for the Mathesar webserver to user [Defaults to port 443: " https_port
 allowed_hosts=${allowed_hosts:-"*"}
 http_port=${http_port:-80}
 https_port=${https_port:-443}
 tee .env <<EOF >/dev/null
-POSTGRES_USER=$db_username
-POSTGRES_PASSWORD=$db_password
+POSTGRES_USER=$dj_db_username
+POSTGRES_PASSWORD=$dj_db_password
 POSTGRES_HOST=$db_host
 ALLOWED_HOSTS=[$allowed_hosts]
 SECRET_KEY=$secret_key
 DJANGO_DATABASE_KEY=default
-DJANGO_DATABASE_URL=postgres://$db_username:$db_password@mathesar_db:5432/mathesar_django
+DJANGO_DATABASE_URL=postgres://$dj_db_username:$dj_db_password@mathesar_db:5432/mathesar_django
 MATHESAR_DATABASES=$user_database_urls
 DJANGO_SUPERUSER_PASSWORD=$superuser_password
 HTTP_PORT=$http_port
