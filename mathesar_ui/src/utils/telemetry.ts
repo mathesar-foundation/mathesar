58674e8fb5094a0454f700cad36bc173745a97eb
silentninja <mmukesh95@hotmail.com>
2023-02-18 03:56:28 +0400

    Add function to dispatch telemetry events

---
+++ mathesar_ui/src/utils/telemetry.ts
@@ -0,0 +1,8 @@
+export function logEvent(eventName: string, metadata: Record<string, string | number | boolean | Date>): void {
+    window.dispatchEvent(new CustomEvent('event', {
+        detail: {
+            eventName,
+            metadata
+        },
+    }));
+}
