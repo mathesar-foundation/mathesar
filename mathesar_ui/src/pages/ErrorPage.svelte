42b1e3b60b9255adae55c526ac40097d2a1e460b
Rajat Vijay <rajatvijay5@gmail.com>
2023-02-21 18:30:29 +0530

    consistent 404 pages across client and server

--- mathesar_ui/src/pages/ErrorPage.svelte
+++ mathesar_ui/src/pages/ErrorPage.svelte
@@ -3,6 +3,14 @@
 </script>
 
 <LayoutWithHeader>
-  <h1>Error</h1>
-  <slot />
+  <div class="error-page-container">
+    <h1>Error</h1>
+    <slot />
+  </div>
 </LayoutWithHeader>
+
+<style>
+  .error-page-container {
+    margin-top: var(--size-x-large);
+  }
+</style>
