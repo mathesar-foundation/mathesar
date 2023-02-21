dcd2b5d087c17d5e97a06ff332ccc29c06b6702b
Sean Colsen <colsen.sean@gmail.com>
2023-02-21 10:35:13 -0500

    Change upgrade API request method from POST to GET

--- mathesar_ui/src/api/upgrade.ts
+++ mathesar_ui/src/api/upgrade.ts
@@ -2,7 +2,8 @@ export const upgradeApi = {
   /** @throws Error for some very limited failure scenarios. */
   async upgrade(): Promise<void> {
     const response = await fetch('/api/ui/v0/upgrade/', {
-      method: 'POST',
+      // TODO: this should be POST, but the backend doesn't support it yet.
+      method: 'GET',
     });
     if (!response.ok) {
       throw new Error('Unable to upgrade.');
