b0e5e43359efc1fdea846dddfc121f95e6f1a142
Sean Colsen <colsen.sean@gmail.com>
2023-02-21 10:42:53 -0500

    Revert "Change upgrade API request method from POST to GET"
    This reverts commit dcd2b5d087c17d5e97a06ff332ccc29c06b6702b.

--- mathesar_ui/src/api/upgrade.ts
+++ mathesar_ui/src/api/upgrade.ts
@@ -2,8 +2,7 @@ export const upgradeApi = {
   /** @throws Error for some very limited failure scenarios. */
   async upgrade(): Promise<void> {
     const response = await fetch('/api/ui/v0/upgrade/', {
-      // TODO: this should be POST, but the backend doesn't support it yet.
-      method: 'GET',
+      method: 'POST',
     });
     if (!response.ok) {
       throw new Error('Unable to upgrade.');
