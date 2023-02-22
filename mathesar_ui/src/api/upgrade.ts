b13a9e316cb5a3185de43e25a821b6538f22ded0
Sean Colsen <colsen.sean@gmail.com>
2023-02-22 11:30:49 -0500

    Use common postAPI function within upgradeApi

--- mathesar_ui/src/api/upgrade.ts
+++ mathesar_ui/src/api/upgrade.ts
@@ -1,11 +1,8 @@
+import { postAPI } from './utils/requestUtils';
+
 export const upgradeApi = {
   /** @throws Error for some very limited failure scenarios. */
   async upgrade(): Promise<void> {
-    const response = await fetch('/api/ui/v0/upgrade/', {
-      method: 'POST',
-    });
-    if (!response.ok) {
-      throw new Error('Unable to upgrade.');
-    }
+    await postAPI('/api/ui/v0/upgrade/', {});
   },
 };
