19093340335a4ff1f7515d737da04214fb9b7936
Rajat Vijay <rajatvijay5@gmail.com>
2023-02-22 21:05:10 +0530

    added home button on error page

--- mathesar_ui/src/pages/ErrorPage.svelte
+++ mathesar_ui/src/pages/ErrorPage.svelte
@@ -1,16 +1,31 @@
 <script lang="ts">
+  import { AnchorButton } from '@mathesar/component-library';
   import LayoutWithHeader from '@mathesar/layouts/LayoutWithHeader.svelte';
+
+  export let showGoToRoot = true;
 </script>
 
 <LayoutWithHeader>
   <div class="error-page-container">
     <h1>Error</h1>
-    <slot />
+    <div>
+      <slot />
+    </div>
+    {#if showGoToRoot}
+      <AnchorButton appearance="primary" href="/">Go to home</AnchorButton>
+    {/if}
   </div>
 </LayoutWithHeader>
 
-<style>
+<style lang="scss">
   .error-page-container {
     margin-top: var(--size-x-large);
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    > :global(* + *) {
+      margin-top: var(--size-small);
+    }
   }
 </style>
