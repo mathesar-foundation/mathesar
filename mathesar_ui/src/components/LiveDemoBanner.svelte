e676521e276922e088bde551c9faaa2f9f4fc23b
Sean Colsen <colsen.sean@gmail.com>
2023-02-23 12:18:58 -0500

    Improve live demo banner

--- mathesar_ui/src/components/LiveDemoBanner.svelte
+++ mathesar_ui/src/components/LiveDemoBanner.svelte
@@ -1,25 +1,38 @@
 <script lang="ts">
-  import { Icon, iconWarning } from '@mathesar-component-library';
+  import { Help, Icon } from '@mathesar-component-library';
+  import { iconJoinEmailList, iconGoToMarketingSite } from '@mathesar/icons';
   import { preloadCommonData } from '@mathesar/utils/preloadData';
 
   const commonData = preloadCommonData();
+
+  $: liveDemo = !!commonData?.live_demo_mode;
 </script>
 
-{#if commonData?.live_demo_mode}
-  <div>
-    <Icon {...iconWarning} />
-    <strong>Demo Mode</strong>
-    <span>
-      Mathesar is in live demo mode. Each session has its own copy of demo data.
-      Data will be deleted regularly.
-    </span>
+{#if liveDemo}
+  <div class="demo-banner">
+    <div class="side left">
+      <i>Mathesar is in live demo mode.</i>
+      <Help>
+        Each session has its own copy of demo data. Data will be deleted
+        regularly.
+      </Help>
+    </div>
+    <div class="side right">
+      <a href="https://mathesar.org/mailing-list.html" target="_blank">
+        <Icon {...iconJoinEmailList} />
+        Join Email List
+      </a>
+      <a href="http://mathesar.org" target="_blank">
+        <Icon {...iconGoToMarketingSite} />
+        Mathesar.org
+      </a>
+    </div>
   </div>
 {/if}
 
-<style lang="scss">
-  div {
-    flex: 0 0;
-    padding: var(--size-xx-small);
+<style>
+  .demo-banner {
+    padding: 0 0.5rem;
     color: var(--yellow-200);
     /**
      * TODO: Have a separate variable for this color
@@ -27,10 +40,22 @@
      */
     background: #2d2508;
     display: flex;
+    flex-wrap: wrap;
     align-items: center;
-
-    > :global(* + *) {
-      margin-left: var(--size-xx-small);
-    }
+  }
+  .side {
+    margin: 0.5rem;
+  }
+  .right {
+    flex: 1 0 auto;
+    display: flex;
+    justify-content: flex-end;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  a + a {
+    margin-left: 1rem;
   }
 </style>
