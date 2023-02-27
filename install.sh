75628bb0b8f0c453104a5c91a39d7e177dee1230
Brent Moran <brentmoran@gmail.com>
2023-02-27 17:45:07 +0800

    remove ignoring dupe admin user loop

--- install.sh
+++ install.sh
@@ -326,12 +326,7 @@ printf "
 Service is ready and healthy!
 Adding admin user to Django webservice now.
 "
-while !  sudo docker exec mathesar_service python manage.py createsuperuser --no-input --username "$superuser_username" --email "$superuser_email"
-do
-  read -r -p "Choose a different admin username [mathesar]: " superuser_username
-  superuser_username=${superuser_username:-mathesar}
-  superuser_email=$superuser_username@example.com
-done
+sudo docker exec mathesar_service python manage.py createsuperuser --no-input --username "$superuser_username" --email "$superuser_email"
 read -r -p "Press ENTER to continue. "
 printf "\n"
 clear -x
