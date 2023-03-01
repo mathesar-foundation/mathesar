cea458f954ee3512d56c9994604b395a40d976a1
Ryanwalker277 <andianurag277@gmail.com>
2023-03-01 11:16:40 +0530

    Added resize functionality to the side inspector view

--- mathesar_ui/src/systems/table-view/table-inspector/TableInspector.svelte
+++ mathesar_ui/src/systems/table-view/table-inspector/TableInspector.svelte
@@ -44,25 +44,124 @@
       [, , activeTab] = tabs;
     }
   }
+
+  function move(element) {
+    return {
+      destroy() {},
+    };
+  }
+
+  function resize(element) {
+    const left = document.createElement('div');
+    left.direction = 'west';
+    left.classList.add('grabber');
+    left.classList.add('left');
+
+    const grabbers = [left];
+
+    let active = null,
+        initialRect = null,
+        initialPos = null;
+
+    grabbers.forEach((grabber) => {
+      element.appendChild(grabber);
+      grabber.addEventListener('mousedown', onMousedown);
+    });
+
+    function onMousedown(event) {
+      active = event.target;
+      const rect = element.getBoundingClientRect();
+      const parent = element.parentElement.getBoundingClientRect();
+
+      console.log({ rect, parent });
+
+      initialRect = {
+        width: rect.width,
+        height: rect.height,
+        left: rect.left - parent.left,
+        right: parent.right - rect.right,
+        top: rect.top - parent.top,
+        bottom: parent.bottom - rect.bottom,
+      };
+      initialPos = { x: event.pageX, y: event.pageY };
+      active.classList.add('selected');
+    }
+
+    function onMouseup(event) {
+      if (!active) return;
+
+      active.classList.remove('selected');
+      active = null;
+      initialRect = null;
+      initialPos = null;
+    }
+
+    function onMove(event) {
+      if (!active) return;
+
+      const direction = active.direction;
+      let delta;
+
+      if (direction.match('east')) {
+        delta = event.pageX - initialPos.x;
+        element.style.width = `${initialRect.width + delta}px`;
+      }
+
+      if (direction.match('west')) {
+        delta = initialPos.x - event.pageX;
+        element.style.left = `${initialRect.left - delta}px`;
+        element.style.width = `${initialRect.width + delta}px`;
+      }
+
+      if (direction.match('north')) {
+        delta = initialPos.y - event.pageY;
+        element.style.top = `${initialRect.top - delta}px`;
+        element.style.height = `${initialRect.height + delta}px`;
+      }
+
+      if (direction.match('south')) {
+        delta = event.pageY - initialPos.y;
+        element.style.height = `${initialRect.height + delta}px`;
+      }
+    }
+
+    window.addEventListener('mousemove', onMove);
+    window.addEventListener('mouseup', onMouseup);
+
+    return {
+      destroy() {
+        window.removeEventListener('mousemove', onMove);
+        window.removeEventListener('mousemove', onMousedown);
+
+        grabbers.forEach((grabber) => {
+          element.removeChild(grabber);
+        });
+      },
+    };
+  }
+
+  let grabber = true;
 </script>
 
-<div class="table-inspector-container">
-  <TabContainer
-    bind:activeTab
-    {tabs}
-    tabStyle="compact"
-    fillContainerHeight
-    fillTabWidth
-  >
-    <slot>
-      {#if activeTab}
-        <div class="tabs-container">
-          <svelte:component this={activeTab.component} />
-        </div>
-      {/if}
-    </slot>
-  </TabContainer>
-</div>
+<main class:hide-grabber={!grabber}>
+  <div class="table-inspector-container box" use:move use:resize>
+    <TabContainer
+      bind:activeTab
+      {tabs}
+      tabStyle="compact"
+      fillContainerHeight
+      fillTabWidth
+    >
+      <slot>
+        {#if activeTab}
+          <div class="tabs-container">
+            <svelte:component this={activeTab.component} />
+          </div>
+        {/if}
+      </slot>
+    </TabContainer>
+  </div>
+</main>
 
 <style lang="scss">
   .table-inspector-container {
@@ -84,5 +183,27 @@
         background-color: var(--sand-400);
       }
     }
+  }
+
+  .box {
+    height: 100%;
+    display: flex;
+    // justify-content: center;
+    position: relative;
+    // user-select: none;
+  }
+
+  :global(.grabber) {
+    position: absolute;
+    box-sizing: border-box;
+    background-color: var(--sand-100);
+  }
+
+  :global(.grabber.left) {
+    width: 10px;
+    height: 100%;
+    background: blue;
+    left: -5px;
+    cursor: col-resize;
   }
 </style>
