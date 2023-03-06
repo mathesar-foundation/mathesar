9a66df3a199cb33652e1e6dbb15832f6610b1b5b
Sean Colsen <colsen.sean@gmail.com>
2023-03-06 14:19:56 -0500

    Code cleanup

--- mathesar_ui/src/pages/database/SchemaRow.svelte
+++ mathesar_ui/src/pages/database/SchemaRow.svelte
@@ -29,11 +29,57 @@
   $: isLocked = schema.name === 'public';
 </script>
 
-<div class="schema-row-parent" class:hover={isHovered}>
+<div class="schema-row" class:hover={isHovered} class:is-locked={isLocked}>
+  <div class="title-and-meta">
+    <div class="name"><SchemaName {schema} iconHasBox /></div>
+
+    {#if isLocked}
+      <div class="lock"><Icon {...iconNotEditable} /></div>
+    {:else if canExecuteDDL}
+      <div class="menu-trigger">
+        <DropdownMenu
+          showArrow={false}
+          triggerAppearance="plain"
+          closeOnInnerClick={true}
+          label=""
+          icon={iconMoreActions}
+          menuStyle="--spacing-y:0.8em;"
+        >
+          <ButtonMenuItem on:click={() => dispatch('edit')} icon={iconEdit}>
+            Edit Schema
+          </ButtonMenuItem>
+          <MenuDivider />
+          <ButtonMenuItem
+            danger
+            on:click={() => dispatch('delete')}
+            icon={iconDeleteMajor}
+          >
+            Delete Schema
+          </ButtonMenuItem>
+        </DropdownMenu>
+      </div>
+    {/if}
+  </div>
+
+  {#if schema.description}
+    <p class="description" title={schema.description}>
+      {schema.description}
+    </p>
+  {/if}
+
+  <SchemaConstituentCounts {schema} />
+
+  {#if isDefault}
+    <InfoBox>
+      Every PostgreSQL database includes the "public" schema. This protected
+      schema can be read by anybody who accesses the database.
+    </InfoBox>
+  {/if}
+
   <!-- svelte-ignore a11y-missing-content -->
   <a
     {href}
-    class="schema-row-overlay"
+    class="hyperlink-overlay"
     aria-label={schema.name}
     on:mouseenter={() => {
       isHovered = true;
@@ -42,69 +88,35 @@
       isHovered = false;
     }}
   />
-  <div class="schema-details-link">
-    <div class="schema-row" class:is-locked={isLocked}>
-      <div class="title-and-meta">
-        <span class="name"><SchemaName {schema} iconHasBox /></span>
-
-        {#if isLocked}
-          <span class="lock"><Icon {...iconNotEditable} /></span>
-        {:else if canExecuteDDL}
-          <div class="menu-trigger">
-            <DropdownMenu
-              showArrow={false}
-              triggerAppearance="plain"
-              closeOnInnerClick={true}
-              label=""
-              icon={iconMoreActions}
-              menuStyle="--spacing-y:0.8em;"
-            >
-              <ButtonMenuItem on:click={() => dispatch('edit')} icon={iconEdit}
-                >Edit Schema</ButtonMenuItem
-              >
-              <MenuDivider />
-              <ButtonMenuItem
-                danger
-                on:click={() => dispatch('delete')}
-                icon={iconDeleteMajor}>Delete Schema</ButtonMenuItem
-              >
-            </DropdownMenu>
-          </div>
-        {/if}
-      </div>
-
-      {#if schema.description}
-        <p class="description" title={schema.description}>
-          {schema.description}
-        </p>
-      {/if}
-
-      <SchemaConstituentCounts {schema} />
-
-      {#if isDefault}
-        <InfoBox>
-          Every PostgreSQL database includes the "public" schema. This protected
-          schema can be read by anybody who accesses the database.
-        </InfoBox>
-      {/if}
-    </div>
-  </div>
 </div>
 
-<style lang="scss">
-  .schema-row-parent {
+<style>
+  .schema-row {
     position: relative;
     isolation: isolate;
     --z-index-hyperlink-overlay: 1;
     --z-index-menu-trigger: 2;
+    border-radius: var(--border-radius-l);
+    border: 1px solid var(--slate-300);
+    padding: 1.142em;
+    display: flex;
+    flex-direction: column;
   }
 
-  .schema-row-parent.hover .schema-row {
+  .schema-row > :global(* + *) {
+    margin-top: 0.75rem;
+  }
+
+  .schema-row.is-locked {
+    background-color: var(--slate-100);
+  }
+
+  .schema-row.hover {
     border: solid 1px var(--slate-500);
     box-shadow: 0 0.2rem 0.4rem 0 rgba(0, 0, 0, 0.1);
   }
 
-  .schema-row-overlay {
+  .hyperlink-overlay {
     position: absolute;
     height: 100%;
     width: 100%;
@@ -117,35 +129,15 @@
     z-index: var(--z-index-menu-trigger);
   }
 
-  .schema-details-link {
-    text-decoration: none;
-    color: inherit;
-  }
-
   .description {
     font-weight: 400;
     font-size: var(--text-size-large);
     color: var(--slate-700);
-    margin: 0;
+    margin-bottom: 0;
     display: -webkit-box;
     -webkit-line-clamp: 1;
     -webkit-box-orient: vertical;
     overflow: hidden;
-  }
-
-  .schema-row {
-    border-radius: var(--border-radius-l);
-    padding: 1.142em;
-    border: 1px solid var(--slate-300);
-    display: flex;
-    flex-direction: column;
-    > :global(* + *) {
-      margin-top: 0.75rem;
-    }
-  }
-
-  .schema-row.is-locked {
-    background-color: var(--slate-100);
   }
 
   .title-and-meta {
