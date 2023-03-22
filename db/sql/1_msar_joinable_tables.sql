1e9395b81506afea0470c0f38eff190fa60c83c7
Brent Moran <brentmoran@gmail.com>
2023-03-22 10:54:48 +0800

    reorganize and add documentation for joinable tables

--- db/sql/1_msar_joinable_tables.sql
+++ db/sql/1_msar_joinable_tables.sql
@@ -4,25 +4,48 @@ automatically.
 
 A table is 'joinable' to another in this context if it can be reached by following a sequence of
 foreign key links from the original table.
+
+This is NOT YET USED in the python layer.
+
+A Join Path is an array of arrays of arrays:
+
+[
+  [[L_oid0, L_attnum0], [R_oid0, R_attnum0]],
+  [[L_oid1, L_attnum1], [R_oid1, R_attnum1]],
+  [[L_oid2, L_attnum2], [R_oid2, R_attnum2]],
+  ...
+]
+
+Here, [L_oidN, L_attnumN] represents the left column of a join, and [R_oidN, R_attnumN] the right.
+
+A Foreign Key path gives the same information in a different form:
+[
+    [constraint_id0, reversed],
+    [constraint_id1, reversed],
+]
+
+In this form, `constraint_idN` is a foreign key constraint, and `reversed` is a boolean giving
+whether to travel from referrer to referant (when False) or from referant to referrer (when True).
 */
 
 
 CREATE TYPE mathesar_types.joinable_tables AS (
-  base integer,
-  target integer,
-  join_path jsonb,
+  base integer, -- The OID of the table from which the paths start
+  target integer, -- The OID of the table where the paths end
+  join_path jsonb, -- A JSONB array of arrays of arrays
   fkey_path jsonb,
-  depth integer
+  depth integer,
+  multiple_results boolean
 );
 
 
 CREATE OR REPLACE FUNCTION
 msar.get_joinable_tables(max_depth integer) RETURNS SETOF mathesar_types.joinable_tables AS $$/*
-This function returns a table of the form
+This function returns a table of mathesar_types.joinable_tables objects, giving paths to various
+joinable tables.
 
- base  | target |      path
--------+--------+---------------------------------
-<oid>  | <oid>  | json array of arrays of arrays
+Args:
+  max_depth: This controls how far to search for joinable tables.
 
 The base and target are OIDs of a base table, and a target table that can be
 joined by some combination of joins along single-column foreign key column
