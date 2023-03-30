122f7ccd22ba071d7188c1c0342b1040eafdba53
Shailesh Rawat <shailesh18h@gmail.com>
2023-03-30 05:31:36 +0530

    fix 1490

--- db/records/operations/sort.py
+++ db/records/operations/sort.py
@@ -73,7 +73,7 @@ def _is_col_orderable(col):
     data_type = col.type
     non_orderable_type = ['Binary', 'LargeBinary', 'PickleType', 'ARRAY', 'JSON', 'JSONB']
     if str(data_type) not in non_orderable_type:
-        e = "Column type in not orederable"
+        e = "Column type is not orederable"
         raise InvalidGroupType(e)
     else:
         return True
