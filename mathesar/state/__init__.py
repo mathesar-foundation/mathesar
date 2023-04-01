94fdb3c3fb46c88ae8b7757966f7e35f625fd85c
Aritra8438 <aritramajumder8438@gmail.com>
2023-04-01 12:28:18 +0530

    Import newly added function.

--- mathesar/state/__init__.py
+++ mathesar/state/__init__.py
@@ -1,2 +1,2 @@
-from mathesar.state.base import make_sure_initial_reflection_happened, reset_reflection, get_preview_columnId_for_tableId, set_preview_columnId_for_tableId  # noqa: F401
+from mathesar.state.base import make_sure_initial_reflection_happened, reset_reflection, get_preview_columnId_for_tableId, set_preview_columnId_for_tableId, remove_preview_columnId_from_cache  # noqa: F401
 from mathesar.state.metadata import get_cached_metadata  # noqa: F401
