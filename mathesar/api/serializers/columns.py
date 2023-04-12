7851c5634d38329464f3dc5714595e43146e427a
Aritra8438 <aritramajumder8438@gmail.com>
2023-04-13 04:08:57 +0530

    Move the validation inside Serializer Validation.

--- mathesar/api/serializers/columns.py
+++ mathesar/api/serializers/columns.py
@@ -4,7 +4,7 @@ from rest_framework.fields import empty, SerializerMethodField
 from rest_framework.settings import api_settings
 
 from db.identifiers import is_identifier_too_long
-from db.columns.exceptions import InvalidTypeError
+from db.columns.exceptions import InvalidTypeError, DynamicDefaultModificationError
 from db.columns.exceptions import InvalidTypeOptionError
 from db.types.base import PostgresType, MathesarCustomType
 from db.types.operations.convert import get_db_type_enum_from_id
@@ -17,6 +17,7 @@ from mathesar.api.serializers.shared_serializers import (
     DISPLAY_OPTIONS_SERIALIZER_MAPPING_KEY,
 )
 from mathesar.models.base import Column
+from mathesar.utils.columns import is_primary_column
 
 
 class InputValueField(serializers.CharField):
@@ -169,6 +170,14 @@ class ColumnSerializer(SimpleColumnSerializer):
     def validate(self, data):
         data = super().validate(data)
         # Reevaluate column display options based on the new column type.
+        if 'column_default_dict' in data:
+            column_has_primary_key_constraint = is_primary_column(column_id=self.instance.id, table_id=self.instance.table.id)
+            if column_has_primary_key_constraint:
+                raise database_api_exceptions.DynamicDefaultAPIException(
+                    DynamicDefaultModificationError,
+                    message="Dynamic Default can not be altered",
+                    status_code=status.HTTP_400_BAD_REQUEST
+                )
         if TYPE_KEY in data and self.instance:
             db_type = get_db_type_enum_from_id(data[TYPE_KEY].lower())
             target_types = self.instance.valid_target_types
