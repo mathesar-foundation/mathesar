602066a1e047ffc5ccc80e48e0db0de9873db3ea
Sean Colsen <colsen.sean@gmail.com>
2023-04-18 20:32:29 -0400

    Revert changes to SelectColumn.svelte

--- mathesar_ui/src/components/SelectColumn.svelte
+++ mathesar_ui/src/components/SelectColumn.svelte
@@ -1,16 +1,11 @@
 <script lang="ts">
   import { Select } from '@mathesar-component-library';
-  import { getTabularDataStoreFromContext } from '@mathesar/stores/table-data';
   import type { Column } from '@mathesar/api/types/tables/columns';
-  import { getColumnConstraintTypeByColumnId } from '@mathesar/utils/columnUtils';
   import ColumnName from './column/ColumnName.svelte';
 
   // Additional classes
   let classes = '';
   export { classes as class };
-
-  const tabularData = getTabularDataStoreFromContext();
-  $: ({ processedColumns } = $tabularData);
 
   export let columns: Column[];
   export let column: Column | undefined;
@@ -28,14 +23,6 @@
   let:option
 >
   {#if option}
-    <ColumnName
-      column={{
-        ...option,
-        constraintsType: getColumnConstraintTypeByColumnId(
-          option.id,
-          $processedColumns,
-        ),
-      }}
-    />
+    <ColumnName column={option} />
   {/if}
 </Select>
