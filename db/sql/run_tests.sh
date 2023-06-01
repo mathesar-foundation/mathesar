4958e79745560c1eeccef2688c566bd87f69c895
Brent Moran <brentmoran@gmail.com>
2023-06-01 13:49:30 +0800

    remove dependency on test-service for DB tests

--- db/sql/run_tests.sh
+++ db/sql/run_tests.sh
@@ -6,8 +6,8 @@ else
     sql=./
 fi
 
-psql -q -U mathesar -f "$sql"/test_startup.sql
+psql -q -U mathesar -d postgres -f "$sql"/test_startup.sql
 pg_prove --runtests -U mathesar -d mathesar_testing
 exit_code=$?
-psql -q -U mathesar -f "$sql"/test_shutdown.sql
+psql -q -U mathesar -d postgres -f "$sql"/test_shutdown.sql
 exit $exit_code
