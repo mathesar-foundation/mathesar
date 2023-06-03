29006da5b6f76a4307c0ecfe45e6b7d07062daf1
Aritra8438 <aritramajumder8438@gmail.com>
2023-06-03 19:31:06 +0530

    Add `max` aggregation class

--- mathesar/models/query.py
+++ mathesar/models/query.py
@@ -6,7 +6,7 @@ from db.queries.operations.process import get_transforms_with_summarizes_speced
 from db.transforms.operations.deserialize import deserialize_transformation
 from db.transforms.operations.serialize import serialize_transformation
 from db.transforms.base import Summarize
-from db.functions.base import Count, ArrayAgg
+from db.functions.base import Count, ArrayAgg, Max
 from db.functions.packed import DistinctArrayAgg
 
 from mathesar.api.exceptions.query_exceptions.exceptions import DeletedColumnAccess
@@ -419,6 +419,7 @@ def _get_default_display_name_for_agg_output_alias(
             DistinctArrayAgg.id: " distinct list",
             ArrayAgg.id: " list",
             Count.id: " count",
+            Max.id: " max",
         }
         suffix_to_add = map_of_agg_function_to_suffix.get(agg_function)
         if suffix_to_add:
