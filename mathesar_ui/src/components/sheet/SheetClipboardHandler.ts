909547534d25d40375e0268afcc531ee1dade966
Aritra8438 <aritramajumder8438@gmail.com>
2023-06-09 10:40:43 +0530

    Fix lint issues

--- mathesar_ui/src/components/sheet/SheetClipboardHandler.ts
+++ mathesar_ui/src/components/sheet/SheetClipboardHandler.ts
@@ -86,7 +86,8 @@ interface SheetClipboardHandlerDeps<
 export class SheetClipboardHandler<
   Row extends QueryRow | RecordRow,
   Column extends ProcessedQueryOutputColumn | ProcessedColumn,
-> implements ClipboardHandler {
+> implements ClipboardHandler
+{
   private readonly deps: SheetClipboardHandlerDeps<Row, Column>;
 
   constructor(deps: SheetClipboardHandlerDeps<Row, Column>) {
