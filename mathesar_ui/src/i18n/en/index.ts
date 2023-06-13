0ebae5cb7882cfe322ab215c221b786689e6bd7d
Rajat Vijay <rajatvijay5@gmail.com>
2023-06-13 19:54:35 +0530

    typesafe-i18n installation

---
+++ mathesar_ui/src/i18n/en/index.ts
@@ -0,0 +1,9 @@
+import type { BaseTranslation } from '../i18n-types';
+
+const en: BaseTranslation = {
+  // TODO: your translations go here
+  HI: 'Hi {name:string}! Please leave a star if you like this project: https://github.com/ivanhofer/typesafe-i18n',
+  allDatabases: 'All Databases',
+};
+
+export default en;
