dbf2877748fb1521f1c1739b9c1b8e0ff6ea2276
Anish <umaleanish120@gmail.com>
2023-07-11 01:12:47 +0530

    add docstring for many_to_one_links python func

--- db/links/operations/create.py
+++ db/links/operations/create.py
@@ -16,6 +16,21 @@ def create_foreign_key_link(
         referent_table_oid,
         unique_link=False
 ):
+    """
+    Creates a Many-to-One or One-to-One link.
+
+    Args:
+        engine: SQLAlchemy engine object for connecting.
+        referrer_column_name: Name of the new column to be created
+                              in the referrer table.
+        referrer_table_oid: The OID of the referrer table.
+        referent_table_oid: The OID of the referent table.
+        unique_link: Whether to make the link one-to-one 
+                     instead of many-to-one.
+
+    Returns:
+        Returns a string giving the command that was run.
+    """
     return execute_msar_func_with_engine(
         engine,
         'add_many_to_one_link',
