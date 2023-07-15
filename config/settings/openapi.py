551dc5fea36a012f4107dd3fc6d3c6856bf2b442
Varsha-dr <114489823+varshadr@users.noreply.github.com>
2023-07-15 23:39:14 +0530

    Implemented  preprocessing hook to extract OpenAPI spec for just /datafiles/ endpoint

---
+++ config/settings/openapi.py
@@ -0,0 +1,8 @@
+def custom_preprocessing_hook(endpoints):
+    filtered = []
+    for (path, path_regex, method, callback) in endpoints:
+        # Remove all but DRF API endpoints
+        if path.startswith("/api/db/v0/data_files/"):
+            filtered.append((path, path_regex, method, callback))
+    return filtered
+
