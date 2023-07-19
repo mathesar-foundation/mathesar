b69f67a6469ebe0c7396fa056ccd21ab05f986c0
Varsha-dr <114489823+varshadr@users.noreply.github.com>
2023-07-20 01:21:09 +0530

    Implemented  preprocessing hook to extract OpenAPI spec for just /schemas/ endpoint

---
+++ config/settings/openapi.py
@@ -0,0 +1,7 @@
+def custom_preprocessing_hook(endpoints):
+    filtered = []
+    for (path, path_regex, method, callback) in endpoints:
+        # Remove all but DRF API endpoints
+        if path.startswith("/api/db/v0/schemas/"):
+            filtered.append((path, path_regex, method, callback))
+    return filtered
