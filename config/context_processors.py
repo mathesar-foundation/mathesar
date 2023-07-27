b1ceaa9ea4c07584e373e4bf394b1888ad9a0cc9
Rajat Vijay <rajatvijay5@gmail.com>
2023-07-27 09:17:21 +0000

    BE - save users lang in the db

--- config/context_processors.py
+++ config/context_processors.py
@@ -4,12 +4,7 @@ from mathesar.utils.frontend import get_manifest_data
 
 
 def frontend_settings(request):
-    """
-    Hard coding this for now
-    but will be taken from users model
-    and cookies later on
-    """
-    preferred_language = 'en'
+    preferred_language = get_user_preferred_language_from_request(request)
     frontend_settings = {
         'development_mode': settings.MATHESAR_MODE == 'DEVELOPMENT',
         'manifest_data': get_manifest_data(preferred_language),
@@ -22,3 +17,14 @@ def frontend_settings(request):
     if frontend_settings['development_mode'] is True:
         frontend_settings['client_dev_url'] = settings.MATHESAR_CLIENT_DEV_URL
     return frontend_settings
+
+
+def get_user_preferred_language_from_request(request):
+    # https://docs.djangoproject.com/en/4.2/topics/i18n/translation/#how-django-discovers-language-preference
+    # This automatically fallbacks to en because of https://docs.djangoproject.com/en/4.2/ref/settings/#std-setting-LANGUAGE_CODE
+    lang_from_locale_middleware = request.LANGUAGE_CODE
+
+    if request.user.is_authenticated:
+        return request.user.preferred_language or lang_from_locale_middleware
+    else:
+        return lang_from_locale_middleware
