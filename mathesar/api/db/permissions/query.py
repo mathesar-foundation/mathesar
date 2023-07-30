49202ec563dc0649187790c98eb0a600350304e9
pavish <pavishkumar.r@gmail.com>
2023-07-31 00:29:51 +0530

    Update QueryAccessPolicy to account for shared queries

--- mathesar/api/db/permissions/query.py
+++ mathesar/api/db/permissions/query.py
@@ -1,6 +1,8 @@
 from django.db.models import Q
 from rest_access_policy import AccessPolicy
 
+from mathesar.api.utils import SHARED_LINK_UUID_QUERY_PARAM
+from mathesar.api.permission_utils import QueryAccessInspector
 from mathesar.models.users import Role
 
 
@@ -8,18 +10,25 @@ class QueryAccessPolicy(AccessPolicy):
 
     statements = [
         {
+            'action': [
+                'retrieve',
+                'results',
+            ],
+            'principal': '*',
+            'effect': 'allow',
+            'condition_expression': 'is_atleast_query_viewer'
+        },
+        {
             # Restrictions for the create method is done by the Serializers when creating the query,
             # As the permissions depend on the base_table object and not on the query object.
             'action': [
                 'list',
-                'retrieve',
                 'destroy',
                 'create',
                 'run',
                 'update',
                 'partial_update',
                 'columns',
-                'results',
                 'records',
 
             ],
@@ -43,3 +52,11 @@ class QueryAccessPolicy(AccessPolicy):
             qs = qs.filter(permissible_database_role_filter | permissible_schema_roles_filter)
 
         return qs
+
+    def is_atleast_query_viewer(self, request, view, action):
+        query = view.get_object()
+        return QueryAccessInspector(
+            request.user,
+            query,
+            token=request.query_params.get(SHARED_LINK_UUID_QUERY_PARAM)
+        ).is_atleast_viewer()
