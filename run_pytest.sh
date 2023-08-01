23b5f3046711890d9638ddc85f5cc02e84f24755
pavish <pavishkumar.r@gmail.com>
2023-08-01 16:43:43 +0530

    Ignore exit code 5 for pytest runs

--- run_pytest.sh
+++ run_pytest.sh
@@ -5,3 +5,17 @@ pytest --last-failed --last-failed-no-failures none
 pytest --last-failed --last-failed-no-failures none
 pytest --last-failed --last-failed-no-failures none
 pytest --last-failed --last-failed-no-failures none
+
+exitCode=$?
+
+if [ "$exitCode" -eq "5" ]; then
+  # `pytest --last-failed --last-failed-no-failures none` returns 5
+  # if pytest did not run any tests. This happens when all tests have
+  # already been passed in previous run.
+  # Since pipelines consider any non-zero status codes as failures,
+  # we are returning 0 for this particular case.
+  # Refer: https://docs.pytest.org/en/7.1.x/reference/exit-codes.html
+  exit 0
+else
+  exit $exitCode
+fi
