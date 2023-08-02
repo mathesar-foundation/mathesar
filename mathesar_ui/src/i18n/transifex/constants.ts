1af127003b3f495e7f533ef071c206bfe67408c4
Rajat Vijay <rajatvijay5@gmail.com>
2023-08-02 03:03:21 +0000

    export and upload FE source string

---
+++ mathesar_ui/src/i18n/transifex/constants.ts
@@ -0,0 +1,3 @@
+export const TRANSIFEX_ORG_SLUG = 'mathesar';
+export const TRANSIFEX_PROJECT_SLUG = 'mathesar';
+export const TRANSIFEX_FE_RESOURCE_SLUG = 'mathesar_ui';
---
+++ mathesar_ui/src/i18n/transifex/upload.ts
@@ -0,0 +1,73 @@
+import { readTranslationFromDisk } from 'typesafe-i18n/exporter';
+import { transifexApi } from '@transifex/api';
+import type { BaseTranslation } from 'typesafe-i18n';
+import * as readline from 'readline';
+import {
+  TRANSIFEX_ORG_SLUG,
+  TRANSIFEX_PROJECT_SLUG,
+  TRANSIFEX_FE_RESOURCE_SLUG,
+} from './constants';
+
+const logger = console;
+
+async function promptTransifexToken(): Promise<string> {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  return new Promise<string>((resolve) => {
+    rl.question(
+      "API token not found. Please provide it. If you don't have an API token, you can generate one in https://app.transifex.com/user/settings/api/",
+      resolve,
+    );
+  }).finally(() => {
+    rl.close();
+  });
+}
+
+async function sendSourceContentToTransifex(
+  sourceContent: BaseTranslation | BaseTranslation[],
+) {
+  const TRANSIFEX_TOKEN =
+    process.env.TRANSIFEX_TOKEN ?? (await promptTransifexToken());
+
+  transifexApi.setup({
+    auth: TRANSIFEX_TOKEN,
+    host: 'https://rest.api.transifex.com',
+  });
+
+  /**
+   * Following lines that requires interaction with the Transifex SDK
+   * requires a ton of eslint-disables and @ts-expect-errors
+   * due to incorrect typings provided by the package
+   */
+  /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
+  const organization = await transifexApi.Organization.get({
+    slug: TRANSIFEX_ORG_SLUG,
+  });
+  // @ts-expect-error Incorrect typings from the package
+  const projects = await organization.fetch('projects');
+  // @ts-expect-error Incorrect typings from the package
+  const project = await projects.get({ slug: TRANSIFEX_PROJECT_SLUG });
+  const resources = await project.fetch('resources');
+  const resource = await resources.get({ slug: TRANSIFEX_FE_RESOURCE_SLUG });
+  const content = JSON.stringify(sourceContent);
+
+  logger.info(`Uploading source content of length ${content.length}`);
+  // @ts-expect-error Incorrect typings from the package
+  await transifexApi.ResourceStringsAsyncUpload.upload({
+    resource,
+    content,
+  });
+  /* eslint-enable */
+
+  logger.info('Source content uploaded');
+}
+
+async function exportAndUploadSourceContent() {
+  const sourceLocale = 'en';
+  const mapping = await readTranslationFromDisk(sourceLocale);
+  await sendSourceContentToTransifex(mapping.translations);
+}
+
+void exportAndUploadSourceContent();
