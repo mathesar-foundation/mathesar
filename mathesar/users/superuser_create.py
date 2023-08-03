3f808e1c71ed29d7fadb071d0e2fde141217a14c
silentninja <mmukesh95@hotmail.com>
2023-08-04 01:50:10 +0400

    Login in the superuser automatically upon creation.

--- mathesar/users/superuser_create.py
+++ mathesar/users/superuser_create.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 
@@ -12,7 +12,8 @@ class SuperuserForm(UserCreationForm):
 
     def save(self, commit=True):
         user = super(SuperuserForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
+        password = self.cleaned_data["password1"]
+        user.set_password(password)
         user.is_superuser = True
         if commit:
             user.save()
@@ -23,3 +24,9 @@ class SuperuserFormView(CreateView):
     template_name = "registration/superuser_create.html"
     form_class = SuperuserForm
     success_url = "/auth/login"
+
+    def form_valid(self, form):
+        success = super().form_valid(form)
+        # Fetch the user object and login
+        login(self.request, self.object)
+        return success
