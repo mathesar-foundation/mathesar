b0310ea2fdd0707fe455b2772739d326d7c08880
silentninja <mmukesh95@hotmail.com>
2023-08-07 14:25:56 +0400

    Move correct directories when building debian package

--- release-scripts/build-debian.sh
+++ release-scripts/build-debian.sh
@@ -8,27 +8,13 @@ set -ex
 DIST='ubuntu:jammy'
 
 # we get a read-only copy of the source: make a writeable copy
-cp -aT ../mathesar build
-cp -aT ../config build/
-cp -aT ../db build/
-cp -aT ../media build/
-cp -aT ../install.py build/
-cp -aT ../manage.py build/
-cp -aT debian build/
+cp -aT ../mathesar build/mathesar/
+cp -aT ../config build/config/
+cp -aT ../db build/db
+cp -aT ../media build/media
+cp -aT ../install.py build/install.py
+cp -aT ../manage.py build/manage.py
+cp -aT debian build/debian
 cd build
 
-
-dch -M -l "+$DIST" "build for $DIST"
-dch -M -r "" --force-distribution --distribution "$DIST"
-
 dpkg-buildpackage -us -uc
-
-ls -l ..
-
-# copy the build results out, setting perms if necessary
-shopt -s nullglob
-for i in ../*.deb ../*.dsc ../*.tar.xz ../*.changes ../*.buildinfo; do
-    [ -z "$TARGET_USERID" ] || chown "$TARGET_USERID" "$i"
-    [ -z "$TARGET_GROUPID" ] || chgrp "$TARGET_GROUPID" "$i"
-    mv "$i" /debs
-done
