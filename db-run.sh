6aa8a47e5fcccfa4c855ed20a2b0bf8eb9832249
silentninja <mmukesh95@hotmail.com>
2023-08-17 16:15:17 +0400

    Use pg_ctlcluster for starting the database.

--- db-run.sh
+++ db-run.sh
@@ -15,19 +15,10 @@ docker_setup_env
 # only run initialization on an empty data directory
 if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
   pg_createcluster -d "$PGDATA" -p 5432 -u "postgres" 13 mathesar
-  cp -p -f -r /etc/postgresql/13/mathesar/* "$PGDATA"
-  sed -i "s|^hba_file =.*|hba_file = '$PGDATA/pg_hba.conf'|" "$PGDATA/postgresql.conf"
-  sed -i "s|^ident_file =.*|ident_file = '$PGDATA/pg_ident.conf'|" "$PGDATA/postgresql.conf"
-  sed -i 's/^host\s\+all\s\+all\s\+::1\/128\s\+md5$/# &/' "$PGDATA/pg_hba.conf"
-
   # Create a temporary postgres server for setting password to the postgres user and for creating the default database
   pg_ctlcluster 13 mathesar start
   sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'mathesar';"
   sudo -u postgres psql -c "CREATE DATABASE mathesar_django;"
   pg_ctlcluster 13 mathesar stop
 fi
-		if [ "$(id -u)" = '0' ]; then
-			# then restart script as postgres user
-			exec gosu postgres "$BASH_SOURCE" "$@"
-		fi
- pg_ctl -D "$PGDATA" start
+pg_ctlcluster 13 mathesar start
