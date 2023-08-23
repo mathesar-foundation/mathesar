2cf97b6ffb74e0fafd910529de0562eaaf04657b
Varsha-dr <114489823+varshadr@users.noreply.github.com>
2023-08-23 15:45:19 +0530

    added support for multipart form data in the request bodies for all endpoints, by adding a parser class to the DEFAULT PARSER CLASSES

--- config/settings/common_settings.py
+++ config/settings/common_settings.py
@@ -180,6 +180,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.MultiPartParser',
     ],
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
