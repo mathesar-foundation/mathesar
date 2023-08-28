9b4f6c8064ecfaaf73e012000809c9ed9539f6c9
Anish <umaleanish120@gmail.com>
2023-08-28 17:34:26 +0530

    some cleanups for create_mathesar_engine

--- mathesar/database/base.py
+++ mathesar/database/base.py
@@ -1,5 +1,3 @@
-from demo.utils import get_is_live_demo_mode
-
 from db import engine
 DEFAULT_DB = 'default'
 
@@ -9,27 +7,11 @@ def create_mathesar_engine(db_model):
     import logging
     logger = logging.getLogger('create_mathesar_engine')
     logger.debug('enter')
-    try:
-        credentials = _get_credentials_for_db_name_in_settings(db_model)
-    except KeyError:
-        if get_is_live_demo_mode():
-            credentials = _get_credentials_for_db_name_not_in_settings(db_model)
-        else:
-            raise
+    credentials = _get_credentials_for_db_model(db_model)
     return engine.create_future_engine_with_custom_types(**credentials)
 
 
-def _get_credentials_for_db_name_in_settings(db_model):
-    return dict(
-        username=db_model.db_username,
-        password=db_model.db_password,
-        hostname=db_model.db_host,
-        database=db_model.name,
-        port=db_model.db_port,
-    )
-
-
-def _get_credentials_for_db_name_not_in_settings(db_model):
+def _get_credentials_for_db_model(db_model):
     return dict(
         username=db_model.db_username,
         password=db_model.db_password,
