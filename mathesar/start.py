ec8eb58b4815e7d1fe95d9d19c790ce2a98dc4d0
silentninja <mmukesh95@hotmail.com>
2023-09-07 17:29:06 +0530

    Avoid running collectstatic when building debian package

--- mathesar/start.py
+++ mathesar/start.py
@@ -4,7 +4,5 @@ import os
 from mathesar.install import main as run_install
 # This is just a simple way to supply args to gunicorn
 sys.argv = [".", "config.wsgi", "--bind=0.0.0.0:8000"]
-# Hack to prevent collect static when install.main() is called. We will be removing this when install.py is reworked
-os.environ.setdefault("DEBUG", "true")
-run_install()
+run_install(skip_static_collection=True)
 wsgi.run()
