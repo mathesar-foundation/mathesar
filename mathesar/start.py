abd055e43144d0ae1e6ce6445fc6ddf710e160e2
silentninja <mmukesh95@hotmail.com>
2023-09-08 14:36:53 +0530

    Remove unused imports

--- mathesar/start.py
+++ mathesar/start.py
@@ -1,6 +1,5 @@
 import sys
 import gunicorn.app.wsgiapp as wsgi
-import os
 from mathesar.install import main as run_install
 # This is just a simple way to supply args to gunicorn
 sys.argv = [".", "config.wsgi", "--bind=0.0.0.0:8000"]
