816772150fa55258318e134ea4244789d635db6f
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2023-10-21 00:07:21 +0000

    Revert "Remove leftover fixture"
    This reverts commit db8140d66a57fd555ecd0d1dffb22bbc1a20fd3e.
    
    Revert "Revert change to how port is defined in testing"
    
    This reverts commit f62977c95785040ae213530cb4a0aa35efb63a70.
    
    Revert "Redo how default db credentials are obtained"
    
    This reverts commit 667d9dcab68413d321e663603d43acf1abead545.
    
    Revert "Ignore unused parameter"
    
    This reverts commit efcd9a721c33299982e61c136b6edea6c8375e84.
    
    Revert "Linter fix"
    
    This reverts commit cf0653adb013270513493f064fc231924d711850.
    
    Revert "Add test for db.install.install_mathesar"
    
    This reverts commit 15182119654953781c406e895795d56dc3ac0e6c.

--- db/tests/test_utils.py
+++ db/tests/test_utils.py
@@ -1,11 +1,7 @@
-import pytest
 from sqlalchemy import MetaData, Column, String, Table
 
 from db.columns.utils import get_enriched_column_table
 from db.metadata import get_empty_metadata
-from db.install import install_mathesar
-from conftest import _drop_database
-from db.credentials import DbCredentials
 
 
 def test_get_enriched_column_table(engine):
@@ -20,25 +16,3 @@ def test_get_enriched_column_table_no_engine():
     table = Table("testtable", MetaData(), Column(abc, String), Column('def', String))
     enriched_table = get_enriched_column_table(table, metadata=get_empty_metadata())
     assert enriched_table.columns[abc].engine is None
-
-
-@pytest.fixture
-def install_fixture(FUN_engine_cache, SES_engine_cache, get_uid):
-    db_name = get_uid()
-    engine = FUN_engine_cache(db_name)
-    credentials = DbCredentials.from_engine(engine)
-    # Will create a db
-    # Note, does not use cached engines, may leak
-    install_mathesar(credentials)
-    yield
-    _drop_database(engine, SES_engine_cache)
-
-
-def test_install(install_fixture):  # noqa: F841
-    """
-    Tests that the instralling fixture (db.install.install_mathesar) doesn't
-    panic. As of time of writing, our tests use a different set of routines for
-    setting up a database (would be nice to fix that), so need this test to
-    have full(-er?) coverage.
-    """
-    pass
