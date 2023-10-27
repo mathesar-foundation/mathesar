b3bda1ee3c14a794de85d55ddb1837669ff19f66
Varsha-dr <114489823+varshadr@users.noreply.github.com>
2023-10-27 16:45:15 +0530

    Add users endpoint to the preprocessing hook

--- config/settings/openapi.py
+++ config/settings/openapi.py
@@ -6,7 +6,8 @@ def custom_preprocessing_hook(endpoints):
         "/api/db/v0/tables/",
         "/api/db/v0/links/",
         "/api/db/v0/queries/",
-        "/api/ui/v0/databases/"
+        "/api/ui/v0/databases/",
+        "/api/ui/v0/users/"
     ]
     filtered = [(path, path_regex, method, callback) for path, path_regex, method, callback in endpoints if any(path.startswith(prefix) for prefix in prefixes)]
     return filtered
