6030a024df1103c5a8351321c92091ec0e79f2ff
Varsha-dr <114489823+varshadr@users.noreply.github.com>
2023-10-28 20:14:22 +0530

    Update TEMPLATES setting to add DIRS: [BASE_DIR / 'templates'] to ensure that Django can find the templates directory

--- config/settings/common_settings.py
+++ config/settings/common_settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
