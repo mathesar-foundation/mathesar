8b85b31ac9b26514489e95fd5d27ef3b4e7764ab
Anish <umaleanish120@gmail.com>
2023-11-22 00:26:11 +0530

    figured out caddy, volumes, also added prereqs

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,8 +4,6 @@ from config.settings.common_settings import * # noqa
 DEBUG = False
 MATHESAR_MODE = 'PRODUCTION'
 # Use a local.py module for settings that shouldn't be version tracked
-
-print(DATABASES['default'])
 try:
     from .local import * # noqa 
 except ImportError:
