bf82d3943e4a26a04e80428c6298e5dcfe834312
pavish <pavishkumar.r@gmail.com>
2023-11-30 19:03:55 +0530

    Add update method in connections api file

--- mathesar_ui/src/api/connections.ts
+++ mathesar_ui/src/api/connections.ts
@@ -1,12 +1,29 @@
 import type { Database } from '@mathesar/AppTypes';
-import { getAPI, type PaginatedResponse } from './utils/requestUtils';
+import { getAPI, patchAPI, type PaginatedResponse } from './utils/requestUtils';
+
+export type Connection = Database;
+
+interface ConnectionWithPassword extends Connection {
+  password: string;
+}
 
 function list() {
-  return getAPI<PaginatedResponse<Database>>(
+  return getAPI<PaginatedResponse<Connection>>(
     '/api/db/v0/connections/?limit=500',
+  );
+}
+
+function update(
+  connectionId: Connection['id'],
+  properties: Partial<Omit<ConnectionWithPassword, 'id' | 'nickname'>>,
+) {
+  return patchAPI<Connection>(
+    `/api/db/v0/connections/${connectionId}/`,
+    properties,
   );
 }
 
 export default {
   list,
+  update,
 };
