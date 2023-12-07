3a9a189db40698c0cdcb5e65e4c2c20bfed9d2af
pavish <pavishkumar.r@gmail.com>
2023-12-07 19:02:11 +0530

    Move blocks that require translation into function call/class constructor instead of top level constants

--- mathesar_ui/src/systems/data-explorer/QueryInspector.ts
+++ mathesar_ui/src/systems/data-explorer/QueryInspector.ts
@@ -9,21 +9,21 @@ import { _ } from 'svelte-i18n';
 import type { Tab } from '@mathesar-component-library/types';
 import type QueryModel from './QueryModel';
 
-const generalTabs: Tab[] = [
-  { id: 'inspect-column', label: get(_)('column') },
-  { id: 'inspect-cell', label: get(_)('cell') },
-];
-const tabsWhenQueryIsSaved: Tab[] = [
-  { id: 'inspect-exploration', label: get(_)('exploration') },
-  ...generalTabs,
-];
-
 export default class QueryInspector {
   tabs: Readable<Tab[]>;
 
   activeTab: Writable<Tab | undefined>;
 
   constructor(query: Writable<QueryModel>) {
+    const generalTabs: Tab[] = [
+      { id: 'inspect-column', label: get(_)('column') },
+      { id: 'inspect-cell', label: get(_)('cell') },
+    ];
+    const tabsWhenQueryIsSaved: Tab[] = [
+      { id: 'inspect-exploration', label: get(_)('exploration') },
+      ...generalTabs,
+    ];
+
     this.tabs = derived(query, ($query) => {
       if ($query.isSaved()) {
         return tabsWhenQueryIsSaved;
