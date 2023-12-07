394a3b2588dea03203f5d464b72634366540e920
pavish <pavishkumar.r@gmail.com>
2023-12-07 19:13:56 +0530

    Translate Record Selector

--- mathesar_ui/src/utils/languageUtils.ts
+++ mathesar_ui/src/utils/languageUtils.ts
@@ -1,11 +1,5 @@
 import { assertExhaustive } from './typeUtils';
 
-const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
-
-export function getArticleForWord(word: string): string {
-  return vowels.has(word[0]?.toLowerCase()) ? 'an' : 'a';
-}
-
 export function makeSingular(word: string): string {
   return word.length > 1 ? word.replace(/s$/i, '') : word;
 }
