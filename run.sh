9bd39c71ce4a2d993404e1154bb012cc3f15f862
Brent Moran <brentmoran@gmail.com>
2023-12-19 17:33:13 +0800

    add other default variables in entrypoint script

--- run.sh
+++ run.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-# don't know if this is necessary, I was facing some issues when I was mounting the recent develop code.
-pip install -r requirements.txt
+# Start the database
+#
+if [ -z "${POSTGRES_USER}" ] || [ -z "${POSTGRES_PASSWORD}" ] \
+  || [ -z "${POSTGRES_HOST}" ] || [ -z "${POSTGRES_PORT}" ] \
+  || [ -z "${POSTGRES_DB}" ]
+then
+  echo "Starting inbuilt database"
+  ./db-run.sh
+  export POSTGRES_USER=postgres
+  export POSTGRES_PASSWORD=mathesar
+  export POSTGRES_HOST=localhost
+  export POSTGRES_PORT=5432
+  export POSTGRES_DB=mathesar_django
+  if [[ -z "${MATHESAR_DATABASES}" ]]; then
+    export MATHESAR_DATABASES='(mathesar_tables|postgresql://postgres:mathesar@localhost:5432/mathesar)'
+  fi
+fi
 
 python -m mathesar.install --skip-confirm
 # Start the Django server on port 8000.
