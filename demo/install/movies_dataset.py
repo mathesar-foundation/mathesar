3a049b2c42ae2d9173086bfd02674db3af36ec92
Anish Umale <umaleanish120@gmail.com>
2024-02-25 19:18:39 +0530

    some cleanups

--- demo/install/movies_dataset.py
+++ demo/install/movies_dataset.py
@@ -1,5 +1,4 @@
 """This module contains functions to load the Movie Collection dataset."""
-import bz2
 import os
 from sqlalchemy import text
 
@@ -16,19 +15,15 @@ def load_movies_dataset(engine, safe_mode=False):
                    schema already exists instead of dropping it.
     """
     drop_schema_query = text(f"""DROP SCHEMA IF EXISTS "{MOVIE_COLLECTION}" CASCADE;""")
-    # create_schema_query = text(f"""CREATE SCHEMA "{MOVIE_COLLECTION}";""")
-    # set_search_path = text(f"""SET search_path="{MOVIE_COLLECTION}";""")
     with engine.begin() as conn, open(MOVIES_SQL) as f, open(MOVIES_SQL2) as f2:
         if safe_mode is False:
             conn.execute(drop_schema_query)
-        # conn.execute(create_schema_query)
-        # conn.execute(set_search_path)
         conn.execute(text(f.read()))
         for file in os.scandir(MOVIES_CSV):
             table_name = file.name.split('.csv')[0]
-            with open(file, 'r') as csv:
+            with open(file, 'r') as csv_file:
                 conn.connection.cursor().copy_expert(
                 f"""COPY "{MOVIE_COLLECTION}"."{table_name}" FROM STDIN DELIMITER ',' CSV HEADER""",
-                csv
+                csv_file
                 )
         conn.execute(text(f2.read()))
