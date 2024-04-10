2dff73f8ca7f47b7b6f5ce54b729b5d1e362a7b7
Sean Colsen <colsen.sean@gmail.com>
2024-04-10 15:07:59 -0400

    Improve parsing/serialization of cellId values

--- mathesar_ui/src/components/sheet/cellIds.ts
+++ mathesar_ui/src/components/sheet/cellIds.ts
@@ -2,29 +2,23 @@ import { map } from 'iter-tools';
 
 import { cartesianProduct } from '@mathesar/utils/iterUtils';
 
-const CELL_ID_DELIMITER = '-';
-
-/**
- * We can serialize a cell id this way only because we're confident that the
- * rowId will never contain the delimiter. Some columnIds _do_ contain
- * delimiters (e.g. in the Data Explorer), but that's okay because we can still
- * separate the values based on the first delimiter.
- */
 export function makeCellId(rowId: string, columnId: string): string {
-  return `${rowId}${CELL_ID_DELIMITER}${columnId}`;
+  return JSON.stringify([rowId, columnId]);
 }
 
 export function parseCellId(cellId: string): {
   rowId: string;
   columnId: string;
 } {
-  const delimiterIndex = cellId.indexOf(CELL_ID_DELIMITER);
-  if (delimiterIndex === -1) {
-    throw new Error(`Unable to parse cell id without a delimiter: ${cellId}.`);
+  try {
+    const [rowId, columnId] = JSON.parse(cellId) as unknown[];
+    if (typeof rowId !== 'string' || typeof columnId !== 'string') {
+      throw new Error();
+    }
+    return { rowId, columnId };
+  } catch {
+    throw new Error(`Unable to parse cell id: ${cellId}.`);
   }
-  const rowId = cellId.slice(0, delimiterIndex);
-  const columnId = cellId.slice(delimiterIndex + 1);
-  return { rowId, columnId };
 }
 
 export function makeCells(
