adf17b1766ca352c4d65a568ea51e8d63c506345
Sean Colsen <colsen.sean@gmail.com>
2024-04-27 20:16:45 -0400

    Reorganize some code

--- mathesar_ui/src/components/sheet/selection/SheetSelection.ts
+++ mathesar_ui/src/components/sheet/selection/SheetSelection.ts
@@ -7,96 +7,18 @@ import { makeCellId, makeCells, parseCellId } from '../cellIds';
 import { Direction, getColumnOffset } from './Direction';
 import Plane from './Plane';
 import {
+  basisFromDataCells,
+  basisFromEmptyColumns,
+  basisFromOneDataCell,
+  basisFromPlaceholderCell,
+  basisFromZeroEmptyColumns,
+  emptyBasis,
+  type Basis,
+} from './basis';
+import {
   fitSelectedValuesToSeriesTransformation,
   type SheetCellDetails,
 } from './selectionUtils';
-
-/**
- * - `'dataCells'` means that the selection contains data cells. This is by far
- *   the most common type of selection basis.
- *
- * - `'emptyColumns'` is used when the sheet has no rows. In this case we still
- *   want to allow the user to select columns, so we use this basis.
- *
- * - `'placeholderCell'` is used when the user is selecting a cell in the
- *   placeholder row. This is a special case because we don't want to allow the
- *   user to select multiple cells in the placeholder row, and we also don't
- *   want to allow selections that include cells in data rows _and_ the
- *   placeholder row.
- *
- * - `'empty'` is used when no cells are selected. We try to avoid this state,
- *   but we also allow for it because it makes it easier to construct selection
- *   instances if we don't already have the full plane data.
- */
-type BasisType = 'dataCells' | 'emptyColumns' | 'placeholderCell' | 'empty';
-
-/**
- * This type stores data about "which cells are selected", with some redundancy
- * for efficient and consistent lookup across different kinds of selections.
- *
- * Due to the redundant nature of some properties on this type, you should be
- * sure to only instantiate Basis using the utility functions below. This will
- * ensure that the data is always valid.
- */
-interface Basis {
-  readonly type: BasisType;
-  readonly activeCellId: string | undefined;
-  readonly cellIds: ImmutableSet<string>;
-  readonly rowIds: ImmutableSet<string>;
-  readonly columnIds: ImmutableSet<string>;
-}
-
-function basisFromDataCells(
-  cellIds: Iterable<string>,
-  activeCellId?: string,
-): Basis {
-  const parsedCells = [...cellIds].map(parseCellId);
-  return {
-    type: 'dataCells',
-    activeCellId: activeCellId ?? first(cellIds),
-    cellIds: new ImmutableSet(cellIds),
-    columnIds: new ImmutableSet(parsedCells.map((cellId) => cellId.columnId)),
-    rowIds: new ImmutableSet(parsedCells.map((cellId) => cellId.rowId)),
-  };
-}
-
-function basisFromOneDataCell(cellId: string): Basis {
-  return basisFromDataCells([cellId], cellId);
-}
-
-function basisFromEmptyColumns(columnIds: Iterable<string>): Basis {
-  return {
-    type: 'emptyColumns',
-    activeCellId: undefined,
-    cellIds: new ImmutableSet(),
-    columnIds: new ImmutableSet(columnIds),
-    rowIds: new ImmutableSet(),
-  };
-}
-
-function basisFromZeroEmptyColumns(): Basis {
-  return basisFromEmptyColumns([]);
-}
-
-function basisFromPlaceholderCell(activeCellId: string): Basis {
-  return {
-    type: 'placeholderCell',
-    activeCellId,
-    cellIds: new ImmutableSet([activeCellId]),
-    columnIds: new ImmutableSet([parseCellId(activeCellId).columnId]),
-    rowIds: new ImmutableSet(),
-  };
-}
-
-function emptyBasis(): Basis {
-  return {
-    type: 'empty',
-    activeCellId: undefined,
-    cellIds: new ImmutableSet(),
-    columnIds: new ImmutableSet(),
-    rowIds: new ImmutableSet(),
-  };
-}
 
 function getFullySelectedColumnIds(
   plane: Plane,
---
+++ mathesar_ui/src/components/sheet/selection/basis.ts
@@ -0,0 +1,95 @@
+import { first } from 'iter-tools';
+
+import { ImmutableSet } from '@mathesar/component-library';
+import { parseCellId } from '../cellIds';
+
+/**
+ * - `'dataCells'` means that the selection contains data cells. This is by far
+ *   the most common type of selection basis.
+ *
+ * - `'emptyColumns'` is used when the sheet has no rows. In this case we still
+ *   want to allow the user to select columns, so we use this basis.
+ *
+ * - `'placeholderCell'` is used when the user is selecting a cell in the
+ *   placeholder row. This is a special case because we don't want to allow the
+ *   user to select multiple cells in the placeholder row, and we also don't
+ *   want to allow selections that include cells in data rows _and_ the
+ *   placeholder row.
+ *
+ * - `'empty'` is used when no cells are selected. We try to avoid this state,
+ *   but we also allow for it because it makes it easier to construct selection
+ *   instances if we don't already have the full plane data.
+ */
+export type BasisType =
+  | 'dataCells'
+  | 'emptyColumns'
+  | 'placeholderCell'
+  | 'empty';
+
+/**
+ * This type stores data about "which cells are selected", with some redundancy
+ * for efficient and consistent lookup across different kinds of selections.
+ *
+ * Due to the redundant nature of some properties on this type, you should be
+ * sure to only instantiate Basis using the utility functions below. This will
+ * ensure that the data is always valid.
+ */
+export interface Basis {
+  readonly type: BasisType;
+  readonly activeCellId: string | undefined;
+  readonly cellIds: ImmutableSet<string>;
+  readonly rowIds: ImmutableSet<string>;
+  readonly columnIds: ImmutableSet<string>;
+}
+
+export function basisFromDataCells(
+  cellIds: Iterable<string>,
+  activeCellId?: string,
+): Basis {
+  const parsedCells = [...cellIds].map(parseCellId);
+  return {
+    type: 'dataCells',
+    activeCellId: activeCellId ?? first(cellIds),
+    cellIds: new ImmutableSet(cellIds),
+    columnIds: new ImmutableSet(parsedCells.map((cellId) => cellId.columnId)),
+    rowIds: new ImmutableSet(parsedCells.map((cellId) => cellId.rowId)),
+  };
+}
+
+export function basisFromOneDataCell(cellId: string): Basis {
+  return basisFromDataCells([cellId], cellId);
+}
+
+export function basisFromEmptyColumns(columnIds: Iterable<string>): Basis {
+  return {
+    type: 'emptyColumns',
+    activeCellId: undefined,
+    cellIds: new ImmutableSet(),
+    columnIds: new ImmutableSet(columnIds),
+    rowIds: new ImmutableSet(),
+  };
+}
+
+export function basisFromZeroEmptyColumns(): Basis {
+  return basisFromEmptyColumns([]);
+}
+
+export function basisFromPlaceholderCell(activeCellId: string): Basis {
+  return {
+    type: 'placeholderCell',
+    activeCellId,
+    cellIds: new ImmutableSet([activeCellId]),
+    columnIds: new ImmutableSet([parseCellId(activeCellId).columnId]),
+    rowIds: new ImmutableSet(),
+  };
+}
+
+export function emptyBasis(): Basis {
+  return {
+    type: 'empty',
+    activeCellId: undefined,
+    cellIds: new ImmutableSet(),
+    columnIds: new ImmutableSet(),
+    rowIds: new ImmutableSet(),
+  };
+}
