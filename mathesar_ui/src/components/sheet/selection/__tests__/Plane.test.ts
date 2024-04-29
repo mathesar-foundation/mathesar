274b5056add4f68f395f3d80d2c3249b0a6216ec
Sean Colsen <colsen.sean@gmail.com>
2024-04-29 11:18:29 -0400

    Fix failing tests

--- mathesar_ui/src/components/sheet/selection/__tests__/Plane.test.ts
+++ mathesar_ui/src/components/sheet/selection/__tests__/Plane.test.ts
@@ -6,53 +6,55 @@ test('Plane with placeholder row', () => {
   const p = new Plane(
     new Series(['r1', 'r2', 'r3', 'r4']),
     new Series(['c1', 'c2', 'c3', 'c4']),
-    'PL',
+    'PH',
   );
 
   expect([...p.allDataCells()]).toEqual([
-    'r1-c1',
-    'r1-c2',
-    'r1-c3',
-    'r1-c4',
-    'r2-c1',
-    'r2-c2',
-    'r2-c3',
-    'r2-c4',
-    'r3-c1',
-    'r3-c2',
-    'r3-c3',
-    'r3-c4',
-    'r4-c1',
-    'r4-c2',
-    'r4-c3',
-    'r4-c4',
+    '["r1","c1"]',
+    '["r1","c2"]',
+    '["r1","c3"]',
+    '["r1","c4"]',
+    '["r2","c1"]',
+    '["r2","c2"]',
+    '["r2","c3"]',
+    '["r2","c4"]',
+    '["r3","c1"]',
+    '["r3","c2"]',
+    '["r3","c3"]',
+    '["r3","c4"]',
+    '["r4","c1"]',
+    '["r4","c2"]',
+    '["r4","c3"]',
+    '["r4","c4"]',
   ]);
 
   expect([...p.dataCellsInFlexibleRowRange('r1', 'r2')].length).toBe(8);
   expect([...p.dataCellsInFlexibleRowRange('r1', 'r3')].length).toBe(12);
   expect([...p.dataCellsInFlexibleRowRange('r3', 'r4')].length).toBe(8);
-  expect([...p.dataCellsInFlexibleRowRange('r3', 'PL')].length).toBe(8);
-  expect([...p.dataCellsInFlexibleRowRange('PL', 'PL')]).toEqual([
-    'r4-c1',
-    'r4-c2',
-    'r4-c3',
-    'r4-c4',
+  expect([...p.dataCellsInFlexibleRowRange('r3', 'PH')].length).toBe(8);
+  expect([...p.dataCellsInFlexibleRowRange('PH', 'PH')]).toEqual([
+    '["r4","c1"]',
+    '["r4","c2"]',
+    '["r4","c3"]',
+    '["r4","c4"]',
   ]);
 
   expect([...p.dataCellsInColumnRange('c1', 'c2')].length).toBe(8);
   expect([...p.dataCellsInColumnRange('c1', 'c3')].length).toBe(12);
 
-  expect(p.getAdjacentCell('r4-c4', Direction.Up)).toEqual({
+  expect(p.getAdjacentCell('["r4","c4"]', Direction.Up)).toEqual({
     type: 'dataCell',
-    cellId: 'r3-c4',
+    cellId: '["r3","c4"]',
   });
-  expect(p.getAdjacentCell('r4-c4', Direction.Right)).toEqual({ type: 'none' });
-  expect(p.getAdjacentCell('r4-c4', Direction.Down)).toEqual({
+  expect(p.getAdjacentCell('["r4","c4"]', Direction.Right)).toEqual({
+    type: 'none',
+  });
+  expect(p.getAdjacentCell('["r4","c4"]', Direction.Down)).toEqual({
     type: 'placeholderCell',
-    cellId: 'PL-c4',
+    cellId: '["PH","c4"]',
   });
-  expect(p.getAdjacentCell('r4-c4', Direction.Left)).toEqual({
+  expect(p.getAdjacentCell('["r4","c4"]', Direction.Left)).toEqual({
     type: 'dataCell',
-    cellId: 'r4-c3',
+    cellId: '["r4","c3"]',
   });
 });
