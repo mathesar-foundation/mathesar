31829ae3991842874d0969bd2b6ca398e6ea0c80
Brent Moran <brentmoran@gmail.com>
2024-05-02 16:13:33 +0800

    add column info getter in python layer

--- db/columns/operations/select.py
+++ db/columns/operations/select.py
@@ -3,9 +3,46 @@ import warnings
 from sqlalchemy import and_, asc, cast, select, text, exists, Identity
 
 from db.columns.exceptions import DynamicDefaultWarning
-from db.connection import execute_msar_func_with_engine
+from db.connection import execute_msar_func_with_engine, exec_msar_func
 from db.tables.operations.select import reflect_table_from_oid
 from db.utils import execute_statement, get_pg_catalog_table
+
+
+def get_column_info_for_table(table, conn):
+    """
+    Return a list of dictionaries desrcibing the columns of the table.
+
+    The `table` can be given as either a "qualified name", or an OID.
+    The OID is the preferred identifier, since it's much more robust.
+
+    The returned list contains dictionaries of the following form:
+
+        {
+            "id": <int>,
+            "name": <str>,
+            "type": <str>,
+            "type_options": {
+                "precision": <int>,
+                "scale": <int>,
+                "fields": <str>,
+                "length": <int>,
+                "item_type": <str>,
+            },
+            "nullable": <bool>,
+            "primary_key": <bool>,
+            "valid_target_types": [<str>, <str>, ..., <str>]
+            "default": {"value": <str>, "is_dynamic": <bool>},
+            "has_dependents": <bool>,
+            "description": <str>
+        }
+
+    The fields of the "type_options" dictionary are all optional,
+    depending on the "type" value.
+
+    Args:
+        table: The table for which we want column info.
+    """
+    return exec_msar_func(conn, 'get_column_info', table).fetchone()[0]
 
 
 def get_column_description(oid, attnum, engine):
