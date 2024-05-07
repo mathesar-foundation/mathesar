b7c91d136316a08c342c97b85c19aa52e1ec6581
Brent Moran <brentmoran@gmail.com>
2024-05-07 14:00:36 +0800

    add test for column lister RPC function

---
+++ mathesar/tests/rpc/test_columns.py
@@ -0,0 +1,155 @@
+from contextlib import contextmanager
+
+from mathesar.rpc import columns
+from mathesar.models.users import User
+
+
+def test_columns_list(rf, monkeypatch):
+    request = rf.post('/api/rpc/v0/', data={})
+    request.user = User(username='alice', password='pass1234')
+    table_oid = 23457
+    database_id = 2
+
+    @contextmanager
+    def mock_connect(_database_id, user):
+        if _database_id == 2 and user.username == 'alice':
+            try:
+                yield True
+            finally:
+                pass
+        else:
+            raise AssertionError('incorrect parameters passed')
+
+    def mock_column_info(_table_oid, conn):
+        if _table_oid != table_oid:
+            raise AssertionError('incorrect parameters passed')
+        return [
+            {
+                'id': 1, 'name': 'id', 'type': 'integer',
+                'default': {'value': 'identity', 'is_dynamic': True},
+                'nullable': False, 'description': None, 'primary_key': True,
+                'type_options': None,
+                'has_dependents': True
+            }, {
+                'id': 2, 'name': 'numcol', 'type': 'numeric',
+                'default': {'value': "'8'::numeric", 'is_dynamic': False},
+                'nullable': True, 'description': None, 'primary_key': False,
+                'type_options': {'scale': None, 'precision': None},
+                'has_dependents': False
+            }, {
+                'id': 4, 'name': 'numcolmod', 'type': 'numeric',
+                'default': None,
+                'nullable': True, 'description': None, 'primary_key': False,
+                'type_options': {'scale': 3, 'precision': 5},
+                'has_dependents': False
+            }, {
+                'id': 6, 'name': 'numarr', 'type': '_array',
+                'default': None,
+                'nullable': True,
+                'description': 'a numeric array column',
+                'primary_key': False,
+                'type_options': {'scale': 2, 'item_type': 'numeric', 'precision': 8},
+                'has_dependents': False
+            }, {
+                'id': 8, 'name': 'ivlcolmod', 'type': 'interval',
+                'default': None,
+                'nullable': True, 'description': None, 'primary_key': False,
+                'type_options': {'fields': 'day to second', 'precision': 3},
+                'has_dependents': False
+            }, {
+                'id': 10, 'name': 'arrcol', 'type': '_array',
+                'default': None,
+                'nullable': True, 'description': None, 'primary_key': False,
+                'type_options': {'item_type': 'text'},
+                'has_dependents': False
+            }, {
+                'id': 12, 'name': 'vccol', 'type': 'character varying',
+                'default': None,
+                'nullable': True, 'description': None, 'primary_key': False,
+                'type_options': {'length': 123},
+                'has_dependents': False
+            },
+        ]
+
+    def mock_display_options(_database_id, _table_oid, attnums, user):
+        if (
+                database_id != 2
+                or table_oid != 23457
+                or attnums != (1, 2, 4, 6, 8, 10, 12)
+                or user.username != 'alice'
+        ):
+            raise AssertionError("incorrect parameters passed")
+        return [
+            {
+                'id': 4,
+                'use_grouping': 'true',
+                'number_format': 'english',
+                'show_as_percentage': False,
+                'maximum_fraction_digits': 2,
+                'minimum_fraction_digits': 2
+            }
+        ]
+    monkeypatch.setattr(columns, 'connect', mock_connect)
+    monkeypatch.setattr(columns, 'get_column_info_for_table', mock_column_info)
+    monkeypatch.setattr(columns, 'get_raw_display_options', mock_display_options)
+    expect_col_list = {
+        'column_info': (
+            {
+                'id': 1, 'name': 'id', 'type': 'integer',
+                'default': {'value': 'identity', 'is_dynamic': True},
+                'nullable': False, 'description': None, 'primary_key': True,
+                'type_options': None,
+                'has_dependents': True
+            }, {
+                'id': 2, 'name': 'numcol', 'type': 'numeric',
+                'default': {'value': "'8'::numeric", 'is_dynamic': False},
+                'nullable': True, 'description': None, 'primary_key': False,
+                'type_options': None,
+                'has_dependents': False
+            }, {
+                'id': 4, 'name': 'numcolmod', 'type': 'numeric',
+                'default': None,
+                'nullable': True, 'description': None, 'primary_key': False,
+                'type_options': {'scale': 3, 'precision': 5},
+                'has_dependents': False
+            }, {
+                'id': 6, 'name': 'numarr', 'type': '_array',
+                'default': None,
+                'nullable': True,
+                'description': 'a numeric array column',
+                'primary_key': False,
+                'type_options': {'scale': 2, 'item_type': 'numeric', 'precision': 8},
+                'has_dependents': False
+            }, {
+                'id': 8, 'name': 'ivlcolmod', 'type': 'interval',
+                'default': None,
+                'nullable': True, 'description': None, 'primary_key': False,
+                'type_options': {'fields': 'day to second', 'precision': 3},
+                'has_dependents': False
+            }, {
+                'id': 10, 'name': 'arrcol', 'type': '_array',
+                'default': None,
+                'nullable': True, 'description': None, 'primary_key': False,
+                'type_options': {'item_type': 'text'},
+                'has_dependents': False
+            }, {
+                'id': 12, 'name': 'vccol', 'type': 'character varying',
+                'default': None,
+                'nullable': True, 'description': None, 'primary_key': False,
+                'type_options': {'length': 123},
+                'has_dependents': False
+            }
+        ),
+        'display_options': [
+            {
+                'id': 4,
+                'use_grouping': 'true',
+                'number_format': 'english',
+                'show_as_percentage': False,
+                'maximum_fraction_digits': 2,
+                'minimum_fraction_digits': 2
+            }
+        ]
+    }
+    actual_col_list = columns.list_(table_oid=23457, database_id=2, request=request)
+    assert actual_col_list == expect_col_list
