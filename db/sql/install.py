f1b074478cc59f64d9a120a1f7531393d6b5394c
Sean Colsen <colsen.sean@gmail.com>
2024-05-08 11:15:51 -0400

    Refactor DB install script
    Make it easier to add new SQL files to the installation process.

--- db/sql/install.py
+++ db/sql/install.py
@@ -2,13 +2,17 @@ import os
 from db.connection import load_file_with_engine
 
 FILE_DIR = os.path.abspath(os.path.dirname(__file__))
-MSAR_SQL = os.path.join(FILE_DIR, '0_msar.sql')
-MSAR_AGGREGATE_SQL = os.path.join(FILE_DIR, '3_msar_custom_aggregates.sql')
 
+SQL_FILES = [
+    '0_msar.sql',
+    '3_msar_custom_aggregates.sql',
+]
+
+def _install_sql_file(engine, file_name):
+    with open(os.path.join(FILE_DIR, file_name)) as file_handle:
+        load_file_with_engine(engine, file_handle)
 
 def install(engine):
     """Install SQL pieces using the given engine."""
-    with open(MSAR_SQL) as file_handle:
-        load_file_with_engine(engine, file_handle)
-    with open(MSAR_AGGREGATE_SQL) as custom_aggregates:
-        load_file_with_engine(engine, custom_aggregates)
+    for file_name in SQL_FILES:
+        _install_sql_file(engine, file_name)
