641244ea805f5c9b8bc58fcc4aaa31f9694e24ee
Sean Colsen <colsen.sean@gmail.com>
2024-05-08 22:15:37 -0400

    Fix python linting errors

--- db/sql/install.py
+++ db/sql/install.py
@@ -8,9 +8,11 @@ SQL_FILES = [
     '3_msar_custom_aggregates.sql',
 ]
 
+
 def _install_sql_file(engine, file_name):
     with open(os.path.join(FILE_DIR, file_name)) as file_handle:
         load_file_with_engine(engine, file_handle)
+
 
 def install(engine):
     """Install SQL pieces using the given engine."""
