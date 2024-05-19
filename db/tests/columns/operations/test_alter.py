d781d7f8df73d1c4ac474cfdd68a00d0db33c250
Brent Moran <brentmoran@gmail.com>
2024-05-19 14:12:18 +0800

    update basic column alterer test with accurate name and assertion

--- db/tests/columns/operations/test_alter.py
+++ db/tests/columns/operations/test_alter.py
@@ -21,7 +21,7 @@ from db.metadata import get_empty_metadata
 from db.schemas.utils import get_schema_oid_from_name
 
 
-def test_alter_columns_in_table_smoke():
+def test_alter_columns_in_table_basic():
     with patch.object(col_alt.db_conn, 'exec_msar_func') as mock_exec:
         col_alt.alter_columns_in_table(
             123,
@@ -31,8 +31,7 @@ def test_alter_columns_in_table_smoke():
                     "type_options": {"precision": 8}, "nullable": True,
                     "default": {"value": 8, "is_dynamic": False},
                     "description": "third column"
-                },
-                {
+                }, {
                     "id": 6, "name": "colname6", "type": "character varying",
                     "type_options": {"length": 32}, "nullable": True,
                     "default": {"value": "blahblah", "is_dynamic": False},
@@ -41,34 +40,23 @@ def test_alter_columns_in_table_smoke():
             ],
             'conn'
         )
-        mock_exec.assert_called_once_with(
-            'conn', 'alter_columns', 123,
-            json.dumps(
-                [
-                    {
-                        "attnum": 3,
-                        "name": "colname3",
-                        "type": {
-                            "name": "numeric", "options": {"precision": 8},
-                        },
-                        "not_null": False,
-                        "name": "colname3",
-                        "default": 8,
-                        "description": "third column",
-                    },
-                    {
-                        "attnum": 6,
-                        "type": {
-                            "name": "character varying", "options": {"length": 32},
-                        },
-                        "not_null": False,
-                        "name": "colname6",
-                        "default": "blahblah",
-                        "description": "textual column"
-                    }
-                ],
-            )
-        )
+        expect_json_arg = [
+            {
+                "attnum": 3, "name": "colname3",
+                "type": {"name": "numeric", "options": {"precision": 8}},
+                "not_null": False, "default": 8, "description": "third column",
+            }, {
+                "attnum": 6, "name": "colname6",
+                "type": {
+                    "name": "character varying", "options": {"length": 32},
+                },
+                "not_null": False, "default": "blahblah",
+                "description": "textual column"
+            }
+        ]
+        assert mock_exec.call_args.args[:3] == ('conn', 'alter_columns', 123)
+        # Necessary since `json.dumps` mangles dict ordering, but we don't care.
+        assert json.loads(mock_exec.call_args.args[3]) == expect_json_arg
 
 
 def _rename_column_and_assert(table, old_col_name, new_col_name, engine):
