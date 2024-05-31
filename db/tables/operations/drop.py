553aceee20347193479c7c87028cae4848ab3fb1
Anish Umale <umaleanish120@gmail.com>
2024-05-31 17:21:33 +0530

    refactor funtion name, and remove if_exists from docstring

--- db/tables/operations/drop.py
+++ db/tables/operations/drop.py
@@ -5,15 +5,13 @@ def drop_table(name, schema, engine, cascade=False, if_exists=False):
     execute_msar_func_with_engine(engine, 'drop_table', schema, name, cascade, if_exists)
 
 
-def drop_table_from_schema(table_oid, conn, cascade=False):
+def drop_table_from_database(table_oid, conn, cascade=False):
     """
     Drop a table.
 
     Args:
         table_oid: OID of the table to drop.
         cascade: Whether to drop the dependent objects.
-        if_exists: Whether to ignore an error if the schema doesn't
-                   exist.
 
     Returns:
         Returns the fully qualified name of the dropped table.
