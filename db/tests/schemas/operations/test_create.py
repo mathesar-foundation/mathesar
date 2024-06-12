05eb1769f6a997bd0f060fd11907fb7818891a7d
Sean Colsen <colsen.sean@gmail.com>
2024-06-12 14:30:03 -0400

    Fix failing test

--- db/tests/schemas/operations/test_create.py
+++ db/tests/schemas/operations/test_create.py
@@ -1,16 +1,16 @@
 from unittest.mock import patch
-from db.schemas.operations.create import create_schema_via_sql_alchemy
+import db.schemas.operations.create as sch_create
 
 
 def test_create_schema_via_sql_alchemy(engine_with_schema):
     engine = engine_with_schema
-    with patch.object(create_schema_via_sql_alchemy, 'execute_msar_func_with_engine') as mock_exec:
-        create_schema_via_sql_alchemy(
+    with patch.object(sch_create, 'execute_msar_func_with_engine') as mock_exec:
+        sch_create.create_schema_via_sql_alchemy(
             schema_name='new_schema',
             engine=engine,
-            comment=None,
+            description=None,
         )
     call_args = mock_exec.call_args_list[0][0]
     assert call_args[0] == engine
-    assert call_args[1] == "create_schema_via_sql_alchemy"
+    assert call_args[1] == "create_schema"
     assert call_args[2] == "new_schema"
