000f3cd1606ef6f346533f866e00d47fc36c3a93
Brent Moran <brentmoran@gmail.com>
2024-06-18 16:17:44 +0800

    use new models to get connections for db access

--- mathesar/rpc/utils.py
+++ mathesar/rpc/utils.py
@@ -1,14 +1,14 @@
-from mathesar.database.base import get_psycopg_connection
-from mathesar.models.deprecated import Connection
+from mathesar.models.base import UserDatabaseRoleMap
 
 
-def connect(conn_id, user):
+def connect(database_id, user):
     """
     Return a psycopg connection, given a Connection model id.
 
     Args:
         conn_id: The Django id corresponding to the Connection.
     """
-    print("User is: ", user)
-    conn_model = Connection.current_objects.get(id=conn_id)
-    return get_psycopg_connection(conn_model)
+    user_database_role = UserDatabaseRoleMap.objects.get(
+        user=user, database__id=database_id
+    )
+    return user_database_role.connection
