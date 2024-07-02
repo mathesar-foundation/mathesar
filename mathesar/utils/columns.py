f59cb471e33399260b9794708324d59551fb591d
Brent Moran <brentmoran@gmail.com>
2024-07-02 17:55:49 +0800

    add metadata patch utility function

--- mathesar/utils/columns.py
+++ mathesar/utils/columns.py
@@ -1,5 +1,19 @@
-from mathesar.models.base import ColumnMetaData
+from mathesar.models.base import ColumnMetaData, Database
 
 
 def get_columns_meta_data(table_oid, database_id):
-    return ColumnMetaData.filter(database__id=database_id, table_oid=table_oid)
+    return ColumnMetaData.objects.filter(
+        database__id=database_id, table_oid=table_oid
+    )
+
+
+def patch_column_meta_data(column_meta_data_list, table_oid, database_id):
+    db_model = Database.objects.get(id=database_id)
+    for meta_data_dict in column_meta_data_list:
+        ColumnMetaData.objects.update_or_create(
+            database=db_model,
+            table_oid=table_oid,
+            attnum=meta_data_dict["attnum"],
+            defaults=meta_data_dict
+        )
+    return get_columns_meta_data(table_oid, database_id)
