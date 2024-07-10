243823f8ce7be40199b4d137ad6b4268da89ec10
Sean Colsen <colsen.sean@gmail.com>
2024-07-10 15:08:25 -0400

    Remove test

--- mathesar/tests/rpc/tables/test_t_metadata.py
+++ mathesar/tests/rpc/tables/test_t_metadata.py
@@ -42,25 +42,3 @@ def test_tables_meta_data_list(monkeypatch):
     ]
     actual_metadata_list = metadata.list_(database_id=database_id)
     assert actual_metadata_list == expect_metadata_list
-
-
-def test_tables_meta_data_set(monkeypatch):
-    database_id = 2
-    metadata_values = {'import_verified': True, 'column_order': [1, 4, 12]}
-
-    def mock_set_tables_meta_data(table_oid, metadata, _database_id):
-        server_model = Server(id=2, host='example.com', port=5432)
-        db_model = Database(id=_database_id, name='mymathesardb', server=server_model)
-        return TableMetaData(
-            id=1, database=db_model, table_oid=1234,
-            import_verified=True, column_order=[1, 4, 12], record_summary_customized=False,
-            record_summary_template="{5555}"
-        )
-    monkeypatch.setattr(metadata, "set_table_meta_data", mock_set_tables_meta_data)
-
-    expect_metadata_object = metadata.TableMetaDataRecord(
-        id=1, database_id=database_id, table_oid=1234,
-        import_verified=True, column_order=[1, 4, 12], record_summary_customized=False,
-        record_summary_template="{5555}"
-    )
-    metadata.set_(table_oid=1234, metadata=metadata_values, database_id=2)
