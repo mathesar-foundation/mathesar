7e36a9ff251b1dc0641c60471154b7568cb8c5cc
Anish Umale <umaleanish120@gmail.com>
2024-07-17 17:49:50 +0530

    add tests

--- mathesar/tests/rpc/test_endpoints.py
+++ mathesar/tests/rpc/test_endpoints.py
@@ -13,6 +13,7 @@ from mathesar.rpc import connections
 from mathesar.rpc import constraints
 from mathesar.rpc import database_setup
 from mathesar.rpc import databases
+from mathesar.rpc import explorations
 from mathesar.rpc import roles
 from mathesar.rpc import schemas
 from mathesar.rpc import tables
@@ -92,6 +93,16 @@ METHODS = [
     (
         databases.list_,
         "databases.list",
+        [user_is_authenticated]
+    ),
+    (
+        explorations.list_,
+        "explorations.list",
+        [user_is_authenticated]
+    ),
+    (
+        explorations.delete,
+        "explorations.delete",
         [user_is_authenticated]
     ),
     (
