96630236482464017cacdf216eb89af51f7b28e6
Sean Colsen <colsen.sean@gmail.com>
2024-07-19 14:16:00 -0400

    Add TupleMap utility

---
+++ mathesar_ui/src/packages/tuple-map/TupleMap.ts
@@ -0,0 +1,53 @@
+import { map } from 'iter-tools';
+
+function parse(key: string) {
+  return JSON.parse(key);
+}
+
+function serialize(keys: unknown[]) {
+  return JSON.stringify(keys);
+}
+
+export class TupleMap<K extends unknown[], V> {
+  private map = new Map<string, V>();
+
+  set(key: K, value: V) {
+    this.map.set(serialize(key), value);
+  }
+
+  get(key: K): V | undefined {
+    return this.map.get(serialize(key));
+  }
+
+  has(key: K): boolean {
+    return this.map.has(serialize(key));
+  }
+
+  delete(key: K): boolean {
+    return this.map.delete(serialize(key));
+  }
+
+  clear() {
+    this.map.clear();
+  }
+
+  forEach(fn: (value: V, key: K, map: TupleMap<K, V>) => void) {
+    this.map.forEach((value, key) => fn(value, parse(key), this));
+  }
+
+  get size() {
+    return this.map.size;
+  }
+
+  get keys(): IterableIterator<K> {
+    return map(parse, this.map.keys());
+  }
+
+  get values(): IterableIterator<V> {
+    return this.map.values();
+  }
+
+  get entries() {
+    return map(([key, value]) => [parse(key), value], this.map.entries());
+  }
+}
