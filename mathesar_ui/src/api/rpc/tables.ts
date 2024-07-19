2b4fe810720ec9bbfdd0502ae55af8d022c4ddab
Sean Colsen <colsen.sean@gmail.com>
2024-07-19 14:16:00 -0400

    Remove old properties from Table type

--- mathesar_ui/src/api/rpc/tables.ts
+++ mathesar_ui/src/api/rpc/tables.ts
@@ -1,26 +1,9 @@
 import { rpcMethodTypeContainer } from '@mathesar/packages/json-rpc-client-builder';
 
-import type { Column } from '../rest/types/tables/columns';
-
 export interface Table {
   oid: number;
   name: string;
   schema: number;
-  import_verified: boolean | null;
-  data_files?: number[];
-  columns: Pick<
-    Column,
-    'id' | 'name' | 'type' | 'type_options' | 'display_options'
-  >[];
-  settings: {
-    /** This is the settings id, not the table id */
-    id: number;
-    preview_settings: {
-      customized: boolean;
-      template: string;
-    };
-    column_order: number[];
-  };
   description: string | null;
 }
 
