5aae3f74501bf7c2d5beb44d8470c34248f8547a
Brent Moran <brentmoran@gmail.com>
2024-07-25 14:28:12 +0800

    add RPC test for record search

--- mathesar/tests/rpc/test_records.py
+++ mathesar/tests/rpc/test_records.py
@@ -59,3 +59,50 @@ def test_records_list(rf, monkeypatch):
         table_oid=table_oid, database_id=database_id, request=request
     )
     assert actual_records_list == expect_records_list
+
+
+def test_records_search(rf, monkeypatch):
+    username = 'alice'
+    password = 'pass1234'
+    table_oid = 23457
+    database_id = 2
+    request = rf.post('/api/rpc/v0/', data={})
+    request.user = User(username=username, password=password)
+
+    @contextmanager
+    def mock_connect(_database_id, user):
+        if _database_id == database_id and user.username == username:
+            try:
+                yield True
+            finally:
+                pass
+        else:
+            raise AssertionError('incorrect parameters passed')
+
+    def mock_search_records(
+            conn,
+            _table_oid,
+            search=[],
+            limit=10,
+    ):
+        if _table_oid != table_oid:
+            raise AssertionError('incorrect parameters passed')
+        return {
+            "count": 50123,
+            "results": [{"1": "abcde", "2": 12345}, {"1": "fghij", "2": 67890}],
+            "query": 'SELECT mycol AS "1", anothercol AS "2" FROM mytable LIMIT 2',
+        }
+
+    monkeypatch.setattr(records, 'connect', mock_connect)
+    monkeypatch.setattr(records.record_select, 'search_records_from_table', mock_search_records)
+    expect_records_list = {
+        "count": 50123,
+        "results": [{"1": "abcde", "2": 12345}, {"1": "fghij", "2": 67890}],
+        "group": None,
+        "preview_data": [],
+        "query": 'SELECT mycol AS "1", anothercol AS "2" FROM mytable LIMIT 2',
+    }
+    actual_records_list = records.search(
+        table_oid=table_oid, database_id=database_id, request=request
+    )
+    assert actual_records_list == expect_records_list
