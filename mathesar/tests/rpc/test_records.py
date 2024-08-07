47ac0968dc9f120b09dca9ddebefbca3a83ca4d9
Brent Moran <brentmoran@gmail.com>
2024-08-07 15:41:16 +0800

    add wiring test for record getter

--- mathesar/tests/rpc/test_records.py
+++ mathesar/tests/rpc/test_records.py
@@ -72,6 +72,54 @@ def test_records_list(rf, monkeypatch):
     assert actual_records_list == expect_records_list
 
 
+def test_records_get(rf, monkeypatch):
+    username = 'alice'
+    password = 'pass1234'
+    table_oid = 23457
+    database_id = 2
+    record_id = 2342
+    request = rf.post('/api/rpc/v0/', data={})
+    request.user = User(username=username, password=password)
+
+    @contextmanager
+    def mock_connect(_database_id, user):
+        if _database_id == database_id and user.username == username:
+            try:
+                yield True
+            finally:
+                pass
+        else:
+            raise AssertionError('incorrect parameters passed')
+
+    def mock_get_record(
+            conn,
+            _record_id,
+            _table_oid,
+    ):
+        if _table_oid != table_oid or _record_id != record_id:
+            raise AssertionError('incorrect parameters passed')
+        return {
+            "count": 1,
+            "results": [{"1": "abcde", "2": 12345}, {"1": "fghij", "2": 67890}],
+            "query": 'SELECT mycol AS "1", anothercol AS "2" FROM mytable LIMIT 2',
+            "grouping": None,
+        }
+
+    monkeypatch.setattr(records, 'connect', mock_connect)
+    monkeypatch.setattr(records.record_select, 'get_record_from_table', mock_get_record)
+    expect_record = {
+        "count": 1,
+        "results": [{"1": "abcde", "2": 12345}, {"1": "fghij", "2": 67890}],
+        "grouping": None,
+        "preview_data": [],
+        "query": 'SELECT mycol AS "1", anothercol AS "2" FROM mytable LIMIT 2',
+    }
+    actual_record = records.get(
+        record_id=record_id, table_oid=table_oid, database_id=database_id, request=request
+    )
+    assert actual_record == expect_record
+
+
 def test_records_search(rf, monkeypatch):
     username = 'alice'
     password = 'pass1234'
