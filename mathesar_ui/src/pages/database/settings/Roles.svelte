3509aabe50966aec5810c9341cb6a5ceff0af3ad
pavish <pavishkumar.r@gmail.com>
2024-08-16 23:32:09 +0530

    Implement basic Roles page

--- mathesar_ui/src/pages/database/settings/Roles.svelte
+++ mathesar_ui/src/pages/database/settings/Roles.svelte
@@ -1,5 +1,67 @@
 <script lang="ts">
   import { _ } from 'svelte-i18n';
+
+  import GridTable from '@mathesar/components/grid-table/GridTable.svelte';
+  import GridTableCell from '@mathesar/components/grid-table/GridTableCell.svelte';
+  import { Button, Spinner } from '@mathesar-component-library';
+
+  import { getDatabaseSettingsContext } from './databaseSettingsUtils';
+  import SettingsContentLayout from './SettingsContentLayout.svelte';
+
+  const databaseContext = getDatabaseSettingsContext();
+  $: ({ database, roles } = $databaseContext);
+
+  $: void roles.runIfNotInitialized({ database_id: database.id });
+  $: roleList = [...($roles.resolvedValue?.values() ?? [])];
 </script>
 
-{$_('roles')}
+<SettingsContentLayout>
+  <svelte:fragment slot="title">
+    {$_('roles')}
+  </svelte:fragment>
+  <svelte:fragment slot="actions">
+    <Button appearance="primary">
+      {$_('create_role')}
+    </Button>
+  </svelte:fragment>
+  {#if $roles.isLoading}
+    <Spinner />
+  {:else if $roles.isOk}
+    <div class="roles-table">
+      <GridTable>
+        <GridTableCell header>{$_('role')}</GridTableCell>
+        <!--eslint-disable-next-line @intlify/svelte/no-raw-text -->
+        <GridTableCell header>LOGIN</GridTableCell>
+        <GridTableCell header>
+          {$_('child_roles')}
+        </GridTableCell>
+        <GridTableCell header>{$_('actions')}</GridTableCell>
+        {#each roleList as role (role.name)}
+          <GridTableCell>{role.name}</GridTableCell>
+          <GridTableCell>
+            {role.login ? $_('yes') : $_('no')}
+          </GridTableCell>
+          <GridTableCell>
+            {#each role.members ?? [] as member (member.oid)}
+              {member.oid}
+            {/each}
+          </GridTableCell>
+          <GridTableCell>
+            <Button appearance="outline-primary">
+              {$_('drop_role')}
+            </Button>
+          </GridTableCell>
+        {/each}
+      </GridTable>
+    </div>
+  {:else if $roles.error}
+    {$roles.error}
+  {/if}
+</SettingsContentLayout>
+
+<style lang="scss">
+  .roles-table {
+    --Grid-table__template-columns: 3fr 2fr 3fr 2fr;
+    background: var(--white);
+  }
+</style>
