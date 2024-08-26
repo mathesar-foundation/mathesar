6a7bc0b14103fa8519841ee73762524cccb9da8c
Brent Moran <brentmoran@gmail.com>
2024-08-26 17:45:00 +0800

    add python wrapper for schema privilege lister

--- db/roles/operations/select.py
+++ db/roles/operations/select.py
@@ -9,5 +9,9 @@ def list_db_priv(db_name, conn):
     return exec_msar_func(conn, 'list_db_priv', db_name).fetchone()[0]
 
 
+def list_schema_privileges(schema_oid, conn):
+    return exec_msar_func(conn, 'list_schema_privileges', schema_oid).fetchone()[0]
+
+
 def get_curr_role_db_priv(db_name, conn):
     return exec_msar_func(conn, 'get_owner_oid_and_curr_role_db_priv', db_name).fetchone()[0]
