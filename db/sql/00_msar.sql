402baeb13cc390e4aab3edc28c005ca2ca608b4f
Brent Moran <brentmoran@gmail.com>
2024-08-28 13:32:51 +0800

    tidy and extract privilege listing logic

--- db/sql/00_msar.sql
+++ db/sql/00_msar.sql
@@ -835,6 +835,20 @@ SELECT EXISTS (SELECT 1 FROM pg_attribute WHERE attrelid=tab_id AND attname=col_
 $$ LANGUAGE SQL RETURNS NULL ON NULL INPUT;
 
 
+CREATE OR REPLACE FUNCTION
+msar.list_table_privileges_for_current_role(tab_id regclass) RETURNS jsonb AS $$/*
+Return a JSONB array of all privileges current_user holds on the passed table.
+*/
+SELECT coalesce(jsonb_agg(privilege), '[]'::jsonb)
+FROM
+  unnest(
+    ARRAY['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'TRUNCATE', 'REFERENCES', 'TRIGGER']
+  ) AS x(privilege),
+  has_table_privilege(tab_id, privilege) as has_privilege
+WHERE has_privilege;
+$$ LANGUAGE SQL STABLE RETURNS NULL ON NULL INPUT;
+
+
 CREATE OR REPLACE FUNCTION msar.get_table(tab_id regclass) RETURNS jsonb AS $$/*
 Given a table identifier, return a JSON object describing the table.
 
@@ -858,18 +872,7 @@ SELECT jsonb_build_object(
   'schema', relnamespace::bigint,
   'description', msar.obj_description(oid, 'pg_class'),
   'owner_oid', relowner,
-  'current_role_priv', array_remove(
-    ARRAY[
-      CASE WHEN pg_catalog.has_table_privilege(oid, 'SELECT') THEN 'SELECT' END,
-      CASE WHEN pg_catalog.has_table_privilege(oid, 'INSERT') THEN 'INSERT' END,
-      CASE WHEN pg_catalog.has_table_privilege(oid, 'UPDATE') THEN 'UPDATE' END,
-      CASE WHEN pg_catalog.has_table_privilege(oid, 'DELETE') THEN 'DELETE' END,
-      CASE WHEN pg_catalog.has_table_privilege(oid, 'TRUNCATE') THEN 'TRUNCATE' END,
-      CASE WHEN pg_catalog.has_table_privilege(oid, 'REFERENCES') THEN 'REFERENCES' END,
-      CASE WHEN pg_catalog.has_table_privilege(oid, 'TRIGGER') THEN 'TRIGGER' END
-    ],
-    NULL
-  ),
+  'current_role_priv', msar.list_table_privileges_for_current_role(tab_id),
   'current_role_owns', pg_catalog.pg_has_role(relowner, 'USAGE')
 ) FROM pg_catalog.pg_class WHERE oid = tab_id;
 $$ LANGUAGE SQL RETURNS NULL ON NULL INPUT;
@@ -900,18 +903,7 @@ SELECT coalesce(
       'schema', pgc.relnamespace::bigint,
       'description', msar.obj_description(pgc.oid, 'pg_class'),
       'owner_oid', pgc.relowner,
-      'current_role_priv', array_remove(
-        ARRAY[
-          CASE WHEN pg_catalog.has_table_privilege(pgc.oid, 'SELECT') THEN 'SELECT' END,
-          CASE WHEN pg_catalog.has_table_privilege(pgc.oid, 'INSERT') THEN 'INSERT' END,
-          CASE WHEN pg_catalog.has_table_privilege(pgc.oid, 'UPDATE') THEN 'UPDATE' END,
-          CASE WHEN pg_catalog.has_table_privilege(pgc.oid, 'DELETE') THEN 'DELETE' END,
-          CASE WHEN pg_catalog.has_table_privilege(pgc.oid, 'TRUNCATE') THEN 'TRUNCATE' END,
-          CASE WHEN pg_catalog.has_table_privilege(pgc.oid, 'REFERENCES') THEN 'REFERENCES' END,
-          CASE WHEN pg_catalog.has_table_privilege(pgc.oid, 'TRIGGER') THEN 'TRIGGER' END
-        ],
-        NULL
-      ),
+      'current_role_priv', msar.list_table_privileges_for_current_role(pgc.oid),
       'current_role_owns', pg_catalog.pg_has_role(pgc.relowner, 'USAGE')
     )
   ),
