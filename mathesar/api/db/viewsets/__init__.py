e84f1973dc6b71c27388b9dbb82fd3015f004274
Brent Moran <brentmoran@gmail.com>
2024-10-09 00:17:46 +0800

    remove links REST API code

--- mathesar/api/db/viewsets/__init__.py
+++ mathesar/api/db/viewsets/__init__.py
@@ -2,4 +2,3 @@ from mathesar.api.db.viewsets.data_files import DataFileViewSet # noqa
 from mathesar.api.db.viewsets.schemas import SchemaViewSet # noqa
 from mathesar.api.db.viewsets.table_settings import TableSettingsViewSet # noqa
 from mathesar.api.db.viewsets.tables import TableViewSet # noqa
-from mathesar.api.db.viewsets.links import LinkViewSet # noqa
