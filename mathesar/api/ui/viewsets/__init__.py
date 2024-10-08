c7523dc4466f49c4de7bd8a2b15e484fd82bd50d
Brent Moran <brentmoran@gmail.com>
2024-10-08 21:48:06 +0800

    remove failing versionviewset import

--- mathesar/api/ui/viewsets/__init__.py
+++ mathesar/api/ui/viewsets/__init__.py
@@ -1,3 +1,2 @@
 from mathesar.api.ui.viewsets.users import * # noqa
-from mathesar.api.ui.viewsets.version import VersionViewSet # noqa
 from mathesar.api.ui.viewsets.records import RecordViewSet # noqa
