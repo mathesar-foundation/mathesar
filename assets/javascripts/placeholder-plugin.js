8e9bd6143b8c1d468cdff4f0da6504f11738fb3e
Sean Colsen <colsen.sean@gmail.com>
2024-10-10 15:13:45 -0500

    Removed everything with mike 2.1.3

--- assets/javascripts/placeholder-plugin.js
+++
@@ -1,5 +0,0 @@
-// if this script is loaded first, placeholder.min.js will find window.PlaceholderPluginConfigJson
-window.PlaceholderPluginConfigJson = {"placeholder_list": [{"name": "MATHESAR_INSTALLATION_DIR", "description": "", "read_only": false, "allow_inner_html": false, "type": "textbox", "allow_recursive": true, "validators": [], "default_value": "/etc/mathesar"}, {"name": "MATHESAR_PG_DIR", "description": "", "read_only": false, "allow_inner_html": false, "type": "textbox", "allow_recursive": true, "validators": [], "default_value": "/var/lib/docker/volumes/mathesar_postgresql_data/_data"}, {"name": "DOMAIN_NAME", "description": "", "read_only": false, "allow_inner_html": false, "type": "textbox", "allow_recursive": true, "validators": [], "default_value": "localhost"}], "settings": {"debug": false, "delay_millis": 0}, "validators": []};
-
-// this will initialize the plugin if it was loaded before this file
-document.dispatchEvent(new Event("PlaceholderPluginConfigJson"));
