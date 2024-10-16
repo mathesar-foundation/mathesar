d8f83a470aac89e6c8bb2cc7623ec71cf8447ab6
Brent Moran <brentmoran@gmail.com>
2024-10-16 15:21:08 +0800

    remove some schema utils

--- db/schemas/utils.py
+++ db/schemas/utils.py
@@ -1,6 +1,4 @@
-from sqlalchemy import inspect
-
-from db.schemas.operations.select import reflect_schema, get_mathesar_schemas_with_oids
+from db.schemas.operations.select import reflect_schema
 
 
 def get_schema_name_from_oid(oid, engine, metadata=None):
@@ -13,15 +11,3 @@ def get_schema_oid_from_name(name, engine):
     schema_info = reflect_schema(engine, name=name)
     if schema_info:
         return schema_info["oid"]
-
-
-def get_mathesar_schemas(engine):
-    return [schema for schema, _ in get_mathesar_schemas_with_oids(engine)]
-
-
-def get_all_schemas(engine):
-    inspector = inspect(engine)
-    # We don't need to exclude system schemas (i.e., starting with "pg_")
-    # since Inspector.get_schema_names already excludes them.  Thus, this
-    # function actually gets all non-pg-reserved schemas.
-    return inspector.get_schema_names()
