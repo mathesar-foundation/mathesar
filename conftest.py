c10c8ce5cbccb26269bc881d02060794108f653f
Brent Moran <brentmoran@gmail.com>
2024-10-18 17:15:04 +0800

    remove unneeded connection functions

--- conftest.py
+++ conftest.py
@@ -11,7 +11,6 @@ from sqlalchemy import MetaData, text, Table, select, or_
 from sqlalchemy.exc import OperationalError
 from sqlalchemy_utils import database_exists, create_database, drop_database
 
-from db.connection import execute_msar_func_with_engine
 from db.engine import add_custom_types_to_ischema_names, create_engine as sa_create_engine
 from db.sql import install as sql_install
 from db.utils import get_pg_catalog_table
@@ -211,9 +210,28 @@ def create_db_schema(SES_engine_cache):
 
 
 def _create_schema_if_not_exists_via_sql_alchemy(schema_name, engine):
-    return execute_msar_func_with_engine(
+    return _execute_msar_func_with_engine(
         engine, 'create_schema_if_not_exists', schema_name
     ).fetchone()[0]
+
+
+def _execute_msar_func_with_engine(engine, func_name, *args):
+    """
+    Execute an msar function using an SQLAlchemy engine.
+
+    This is temporary scaffolding.
+
+    Args:
+        engine: an SQLAlchemy engine for connecting to a DB
+        func_name: The unqualified msar function name (danger; not sanitized)
+        *args: The list of parameters to pass
+    """
+    conn_str = str(engine.url)
+    with psycopg.connect(conn_str) as conn:
+        return conn.execute(
+            f"SELECT msar.{func_name}({','.join(['%s'] * len(args))})",
+            args
+        )
 
 
 def _get_schema_name_from_oid(oid, engine, metadata=None):
@@ -247,7 +265,7 @@ def _reflect_schema(engine, name=None, oid=None, metadata=None):
 
 
 def _drop_schema_via_name(engine, name, cascade=False):
-    execute_msar_func_with_engine(engine, 'drop_schema', name, cascade).fetchone()
+    _execute_msar_func_with_engine(engine, 'drop_schema', name, cascade).fetchone()
 
 
 # Seems to be roughly equivalent to mathesar/database/base.py::create_mathesar_engine
--- db/connection.py
+++ db/connection.py
@@ -3,44 +3,6 @@ import psycopg
 from psycopg.rows import dict_row
 
 
-def execute_msar_func_with_engine(engine, func_name, *args):
-    """
-    Execute an msar function using an SQLAlchemy engine.
-
-    This is temporary scaffolding.
-
-    Args:
-        engine: an SQLAlchemy engine for connecting to a DB
-        func_name: The unqualified msar function name (danger; not sanitized)
-        *args: The list of parameters to pass
-    """
-    conn_str = str(engine.url)
-    with psycopg.connect(conn_str) as conn:
-        # Returns a cursor
-        return conn.execute(
-            f"SELECT msar.{func_name}({','.join(['%s'] * len(args))})",
-            args
-        )
-
-
-def execute_msar_func_with_psycopg2_conn(conn, func_name, *args):
-    """
-    Execute an msar function using an SQLAlchemy engine.
-
-    This is *extremely* temporary scaffolding.
-
-    Args:
-        conn: a psycopg2 connection (from an SQLAlchemy engine)
-        func_name: The unqualified msar function name (danger; not sanitized)
-        *args: The list of parameters to pass
-    """
-    args_str = ", ".join([str(arg) for arg in args])
-    args_str = f"{args_str}"
-    stmt = text(f"SELECT msar.{func_name}({args_str})")
-    # Returns a cursor
-    return conn.execute(stmt)
-
-
 def exec_msar_func(conn, func_name, *args):
     """
     Execute an msar function using a psycopg (3) connection.
