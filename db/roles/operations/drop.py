d5d4b31a34dfea8ebd3f5a96c24b1feb55c0a05a
Brent Moran <brentmoran@gmail.com>
2024-10-23 16:37:56 +0800

    consolidate db.roles module

--- db/roles/operations/drop.py
+++
@@ -1,5 +0,0 @@
-from db.connection import exec_msar_func
-
-
-def drop_role(role_oid, conn):
-    exec_msar_func(conn, 'drop_role', role_oid)
--- db/roles/operations/membership.py
+++
@@ -1,5 +0,0 @@
-from db.connection import exec_msar_func
-
-
-def set_members_to_role(parent_role_oid, members, conn):
-    return exec_msar_func(conn, 'set_members_to_role', parent_role_oid, members).fetchone()[0]
