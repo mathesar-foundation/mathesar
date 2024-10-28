4eba0ebac114060f5e256a65dc65e1d8adccd787
Anish Umale <umaleanish120@gmail.com>
2024-10-28 18:06:07 +0530

    add a condition for running node during testing

--- dev-run.sh
+++ dev-run.sh
@@ -3,13 +3,15 @@
 # For deployments, the DockerFile is configured to build the
 # client. Hence, instead of using this script, the web server
 # can be directly started.
+if [ -z "$NODE_MAJOR" ]; then
+    cd mathesar_ui
 
-cd mathesar_ui
+    # Run vite dev server only in dev mode
+    npm run dev &
 
-# Run vite dev server only in dev mode
-npm run dev &
+    cd ..
+fi
 
-cd ..
 python -m mathesar.install --skip-confirm
 python manage.py createsuperuser --no-input --username admin --email admin@example.com
 python manage.py runserver 0.0.0.0:8000 && fg
