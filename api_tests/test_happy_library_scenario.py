f73f3cf3379e81f76f3a51c87c1f669a956f80b9
Brent Moran <brentmoran@gmail.com>
2024-11-11 13:29:05 +0800

    add framework and basic test for API scenario

---
+++ api_tests/test_happy_library_scenario.py
@@ -0,0 +1,46 @@
+import pytest
+import requests
+from urllib import parse as urlparse
+
+SERVICE_HOST = 'http://mathesar-api-test-service:8000'
+RPC_ENDPOINT = f'{SERVICE_HOST}/api/rpc/v0/'
+MATHESAR_DB = 'mathesar'
+
+@pytest.fixture(scope="session")
+def admin_session():
+    login_payload = {'username': 'admin', 'password': 'password'}
+    s = requests.Session()
+    s.get(f'{SERVICE_HOST}/auth/login/')
+    s.headers['X-CSRFToken'] = s.cookies['csrftoken']
+    s.post(f'{SERVICE_HOST}/auth/login/', data=login_payload)
+    s.headers['X-CSRFToken'] = s.cookies['csrftoken']
+    return s
+
+
+def test_empty_db_list(admin_session):
+    response = admin_session.post(
+        RPC_ENDPOINT,
+        json={"jsonrpc": "2.0", "method": "databases.configured.list", "id": 0}
+    )
+    print(response.json())
+    assert response.json()['result'] == []
+
+
+def test_create_mathesar_db(admin_session):
+    response = admin_session.post(
+        RPC_ENDPOINT,
+        json={
+            "jsonrpc": "2.0",
+            "method": "databases.setup.create_new",
+            "id": 0,
+            "params": {
+                "database": "mathesar",
+                "sample_data": ["library_management"],
+            }
+        }
+    )
+    print(response.json())
+    result = response.json()['result']
+    assert set(result.keys()) == set(['configured_role', 'database', 'server'])
+    assert result['database']['name'] == 'mathesar'
+    assert result['database']['needs_upgrade_attention'] is False
