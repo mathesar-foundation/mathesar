245b11ee0bd6d5697bd39d5eb5e4f73348bb389b
Brent Moran <brentmoran@gmail.com>
2024-11-12 17:50:27 +0800

    use globals for response state

--- api_tests/test_happy_db_setups.py
+++ api_tests/test_happy_db_setups.py
@@ -40,17 +40,20 @@ def test_empty_db_list(admin_rpc_call):
 
 
 def test_create_mathesar_db_internal(admin_rpc_call):
+    global internal_db
     result = admin_rpc_call(
         'databases.setup.create_new',
         database='mathesar',
         sample_data=['library_management']
     )
     assert set(result.keys()) == set(['configured_role', 'database', 'server'])
-    assert result['database']['name'] == 'mathesar'
-    assert result['database']['needs_upgrade_attention'] is False
+    internal_db = result['database']
+    assert internal_db['name'] == 'mathesar'
+    assert internal_db['needs_upgrade_attention'] is False
 
 
 def test_connect_mathesar_db_external(admin_rpc_call):
+    global external_db
     result = admin_rpc_call(
         'databases.setup.connect_existing',
         host='mathesar-test-user-db',
@@ -60,8 +63,9 @@ def test_connect_mathesar_db_external(admin_rpc_call):
         password='data1234',
     )
     assert set(result.keys()) == set(['configured_role', 'database', 'server'])
-    assert result['database']['name'] == 'my_data'
-    assert result['database']['needs_upgrade_attention'] is False
+    external_db = result['database']
+    assert external_db['name'] == 'my_data'
+    assert external_db['needs_upgrade_attention'] is False
     assert result['configured_role']['name'] == 'data_admin'
 
 
@@ -70,16 +74,8 @@ def test_list_databases_has_upgrade_status(admin_rpc_call):
     assert all(d['needs_upgrade_attention'] is False for d in result)
 
 
-def test_batch_sql_update_no_error(admin_rpc_call, admin_session):
-    servers_list = admin_rpc_call('servers.configured.list')
-    internal_server = [s for s in servers_list if s['host'] == INTERNAL_HOST][0]
-    external_server = [s for s in servers_list if s['host'] == EXTERNAL_HOST][0]
-    internal_db = admin_rpc_call(
-        'databases.configured.list', server_id=internal_server['id']
-    )[0]
-    external_db = admin_rpc_call(
-        'databases.configured.list', server_id=external_server['id']
-    )[0]
+def test_batch_sql_update_no_error(admin_session):
+    # globals: internal_db, external_db
     admin_session.post(
         RPC_ENDPOINT,
         json=[
