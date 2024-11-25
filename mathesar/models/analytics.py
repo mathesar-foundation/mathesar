1a5b45e6a335c6ff5a3ea0957f2c757d6911cd5a
Brent Moran <brentmoran@gmail.com>
2024-11-25 12:39:54 +0800

    add models to hold analytics statistics

---
+++ mathesar/models/analytics.py
@@ -0,0 +1,25 @@
+from django.db import models
+from mathesar.models.base import BaseModel
+
+
+class InstallationID(BaseModel):
+    # We shouldn't increment this, since only one row is allowed.
+    id = models.IntegerField(primary_key=True, default=1)
+    value = models.UUIDField()
+
+
+class AnalyticsReport(BaseModel):
+    installation_id = models.ForeignKey(
+        'InstallationID', default=1, on_delete=models.CASCADE
+    )
+    mathesar_version = models.CharField()
+    user_count = models.PositiveIntegerField(null=True, blank=True)
+    active_user_count = models.PositiveIntegerField(null=True, blank=True)
+    configured_role_count = models.PositiveIntegerField(null=True, blank=True)
+    connected_database_count = models.PositiveIntegerField(null=True, blank=True)
+    connected_database_schema_count = models.PositiveIntegerField(null=True, blank=True)
+    connected_database_table_count = models.PositiveIntegerField(null=True, blank=True)
+    exploration_count = models.PositiveIntegerField(null=True, blank=True)
+    recent_dql_query_count = models.PositiveIntegerField(null=True, blank=True)
+    recent_dml_query_count = models.PositiveIntegerField(null=True, blank=True)
+    recent_ddl_query_count = models.PositiveIntegerField(null=True, blank=True)
