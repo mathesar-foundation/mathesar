4401a54667d85bce001fb91692260195a029e5a3
Sean Colsen <colsen.sean@gmail.com>
2024-12-12 19:44:28 -0500

    Remove logic to auto-insert schema name in page title

--- mathesar_ui/src/pages/pageTitleUtils.ts
+++ mathesar_ui/src/pages/pageTitleUtils.ts
@@ -1,17 +1,10 @@
 import { get } from 'svelte/store';
 import { _ } from 'svelte-i18n';
 
-import { currentSchema } from '@mathesar/stores/schemas';
-
 const SEPARATOR = ' | ';
 
 function makePageTitle(parts: string[]): string {
-  const schema = get(currentSchema);
   const allParts = [...parts];
-  if (schema) {
-    // TODO_BETA: Make this reactive
-    allParts.push(get(schema.name));
-  }
   allParts.push(get(_)('mathesar'));
   return allParts.join(SEPARATOR);
 }
