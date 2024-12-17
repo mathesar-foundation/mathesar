9398a90ce5dbf0bcefe24ce13fb48a739c6a678f
Sean Colsen <colsen.sean@gmail.com>
2024-12-17 14:07:33 -0500

    Create "New Item Highlighter" tool

---
+++ mathesar_ui/src/packages/new-item-highlighter/highlight.ts
@@ -0,0 +1,74 @@
+import { HIGHLIGHT_TRANSITION_MS, HINT_EXPIRATION_END_MS } from './constants';
+import { displayHint } from './hint';
+import { getRectCssGeometry, onElementRemoved } from './utils';
+
+function makeHighlighterElement(): HTMLElement {
+  const effect = document.createElement('div');
+  effect.className = 'effect';
+
+  const highlight = document.createElement('div');
+  highlight.className = 'new-item-highlighter';
+  highlight.appendChild(effect);
+
+  return highlight;
+}
+
+function displayHighlight(target: HTMLElement): void {
+  const highlight = makeHighlighterElement();
+  highlight.style.setProperty('--duration', `${HIGHLIGHT_TRANSITION_MS}ms`);
+  document.body.appendChild(highlight);
+
+  function trackPosition() {
+    if (!target.isConnected) return;
+    const rect = target.getBoundingClientRect();
+    Object.assign(highlight.style, getRectCssGeometry(rect));
+    requestAnimationFrame(trackPosition);
+  }
+  trackPosition();
+
+  function cleanup() {
+    highlight.remove();
+  }
+
+  onElementRemoved(target, cleanup);
+  setTimeout(cleanup, HIGHLIGHT_TRANSITION_MS);
+}
+
+export function setupHighlighter(
+  target: HTMLElement,
+  options: {
+    scrollHint?: string;
+  },
+): () => void {
+  let cleanupHint: (() => void) | undefined;
+
+  const intersectionObserver = new IntersectionObserver(
+    (entries) => {
+      const entry = entries[0];
+      if (entry.isIntersecting && entry.intersectionRatio >= 0.5) {
+        displayHighlight(target);
+        // eslint-disable-next-line @typescript-eslint/no-use-before-define
+        cleanup();
+      } else if (options.scrollHint && !cleanupHint) {
+        cleanupHint = displayHint(target, options.scrollHint);
+      }
+    },
+    { threshold: [0.5] },
+  );
+
+  function cleanup() {
+    intersectionObserver.disconnect();
+    cleanupHint?.();
+  }
+
+  intersectionObserver.observe(target);
+
+  onElementRemoved(target, cleanup);
+
+  // If the user still hasn't seen the hint or the highlight (i.e. if it's
+  // scrolled out of view), then we give up and remove them. This means
+  // `displayHighlight` will never be called.
+  setTimeout(cleanup, HINT_EXPIRATION_END_MS);
+
+  return cleanup;
+}
---
+++ mathesar_ui/src/packages/new-item-highlighter/highlightNewItems.ts
@@ -0,0 +1,54 @@
+import type { ActionReturn } from 'svelte/action';
+
+import { setupHighlighter } from './highlight';
+import { getNewlyAddedItemsFromMutations } from './utils';
+
+export function highlightNewItems(
+  container: HTMLElement,
+  options: {
+    /**
+     * The number of milliseconds to wait before setting up highlighting.
+     *
+     * This defaults to 2000 (i.e. 2 seconds) to give children time for the
+     * initial load if necessary, and because in most of the contexts where we
+     * want to use this, we don't expect the user to be able to perform data
+     * entry in under 2 seconds.
+     *
+     * Set this to 0 to start highlighting immediately. If the children are
+     * rendered synchronously, then highlighting will still be deferred to new
+     * items.
+     */
+    wait?: number;
+    /**
+     * Pass a string to display a hint to the user when the new item is
+     * scrolled out of view.
+     */
+    scrollHint?: string;
+  } = {},
+): ActionReturn {
+  const wait = options.wait ?? 2000;
+
+  const cleanupFns: (() => void)[] = [];
+
+  function init() {
+    const mutationObserver = new MutationObserver((mutations) => {
+      for (const item of getNewlyAddedItemsFromMutations(mutations)) {
+        cleanupFns.push(setupHighlighter(item, options));
+      }
+    });
+    cleanupFns.push(() => mutationObserver.disconnect());
+    mutationObserver.observe(container, { childList: true });
+  }
+
+  if (wait) {
+    setTimeout(init, wait);
+  } else {
+    init();
+  }
+
+  return {
+    destroy() {
+      cleanupFns.forEach((fn) => fn());
+    },
+  };
+}
