36d75eec11799526e6e9c321e42836aa25e394ff
Sean Colsen <colsen.sean@gmail.com>
2025-01-02 21:02:21 -0500

    Re-apply "new DB" highlighting after git merge
    I had to resolve some conflicts in HomePage.svelte that wiped out
    previous work to highlight newly-added databases. This commit adds those
    changes back in — now in DatabaseList.svelte instead of HomePage.svelte.

--- mathesar_ui/src/pages/home/DatabasesList.svelte
+++ mathesar_ui/src/pages/home/DatabasesList.svelte
@@ -5,6 +5,7 @@
   import { RichText } from '@mathesar/components/rich-text';
   import { iconConnection } from '@mathesar/icons';
   import type { Database } from '@mathesar/models/Database';
+  import { highlightNewItems } from '@mathesar/packages/new-item-highlighter';
   import { databasesStore } from '@mathesar/stores/databases';
   import { modal } from '@mathesar/stores/modal';
   import { getUserProfileStoreFromContext } from '@mathesar/stores/userProfile';
@@ -78,7 +79,12 @@
 
       <svelte:fragment slot="content">
         {#if filteredDatabases.length}
-          <div data-identifier="databases-list-grid">
+          <div
+            data-identifier="databases-list-grid"
+            use:highlightNewItems={{
+              scrollHint: $_('database_new_items_scroll_hint'),
+            }}
+          >
             {#each filteredDatabases as database (database.id)}
               <DatabaseRow {database} />
             {/each}
