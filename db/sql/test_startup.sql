b2b941a3bf8bba0f2aca4f5007a5567aac4b13f3
Brent Moran <brentmoran@gmail.com>
2025-01-09 23:09:01 +0800

    update SQL tests for new casting domain

--- db/sql/test_startup.sql
+++ db/sql/test_startup.sql
@@ -8,5 +8,7 @@ $$ LANGUAGE plpgsql;
 CALL raise_notice('Creating testing DB');
 CREATE DATABASE mathesar_testing;
 \c mathesar_testing
+\ir 01_msar_types.sql
 \ir 05_msar.sql
+\ir 45_msar_type_casting.sql
 \ir test_sql_functions.sql
