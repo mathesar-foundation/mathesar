8d196da56b4e1b53a46943df12c7bf41cb0ec438
pavish <pavishkumar.r@gmail.com>
2025-01-22 20:51:56 +0400

    Add script to check for unused strings

---
+++ mathesar_ui/scripts/i18n/check_usused_strings.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import re
+import os
+import subprocess
+from pathlib import Path
+import json
+
+MATHESAR_UI_DIR = Path(__file__).resolve().parent.parent.parent
+
+EN_DICT_FILE = os.path.join(MATHESAR_UI_DIR, 'src/i18n/languages/en/dict.json')
+
+def grep_i18n_string(string):
+    try:
+        escaped_string = re.escape(string)
+        pattern = rf"\$_\('{escaped_string}'|get\(_\)\('{escaped_string}'"
+        subprocess.run(
+            [
+                'grep',
+                '-qrE',
+                pattern,
+                '--include=**.svelte',
+                '--include=**.ts',
+                '--exclude=\*i18n\*',
+                os.path.join(MATHESAR_UI_DIR, 'src')
+            ],
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+        return True
+    except subprocess.CalledProcessError as e:
+        return False
+    except Exception:
+        raise
+
+
+with open(EN_DICT_FILE, 'r') as en_dict_file:
+    en_dict_json = json.load(en_dict_file)
+    not_found_count = 0
+    for key in en_dict_json:
+        if not grep_i18n_string(key):
+            print("NOT FOUND: " + key)
+            not_found_count += 1
+    print("Number of strings not found: " + str(not_found_count));
