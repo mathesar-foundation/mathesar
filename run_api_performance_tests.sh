4cfca66e9e27ec64c6d7b5aca5aa085b64d2e6b1
Brent Moran <brentmoran@gmail.com>
2025-01-24 07:57:15 +0800

    add initial performance tests

---
+++ run_api_performance_tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+EXIT_CODE=0
+docker compose -f docker-compose.test.yml up --build test-db -d
+docker exec mathesar-test-db tc qdisc add dev eth0 root netem delay 50ms
+docker compose -f docker-compose.test.yml run --rm test-runner pytest -svv test_slow_db_setups.py
+# Needed once (if) we have multiple scenarios to test. This accumulates the max exit code.
+EXIT_CODE=$(( EXIT_CODE > $? ? EXIT_CODE : $? ))
+docker compose -f docker-compose.test.yml down -v -t 1
+exit $EXIT_CODE
