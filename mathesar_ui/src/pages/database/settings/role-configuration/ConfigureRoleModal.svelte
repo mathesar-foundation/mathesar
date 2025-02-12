3455fe6f57b119eb96bd31236fc61294285ea3c8
Brent Moran <brentmoran@gmail.com>
2025-02-12 16:26:31 +0800

    allow configuring role without password

--- mathesar_ui/src/pages/database/settings/role-configuration/ConfigureRoleModal.svelte
+++ mathesar_ui/src/pages/database/settings/role-configuration/ConfigureRoleModal.svelte
@@ -5,7 +5,7 @@
     FieldLayout,
     FormSubmit,
     makeForm,
-    requiredField,
+    optionalField,
   } from '@mathesar/components/form';
   import Field from '@mathesar/components/form/Field.svelte';
   import Identifier from '@mathesar/components/Identifier.svelte';
@@ -29,7 +29,7 @@
   export let controller: ModalController;
   export let combinedLoginRole: CombinedLoginRole;
 
-  const password = requiredField('');
+  const password = optionalField('');
   const form = makeForm({ password });
 
   async function configureRole() {
