3e99a00d97ee18169452907b6b2931b8c07bceec
Brent Moran <brentmoran@gmail.com>
2025-02-12 16:24:12 +0800

    allow connecting to preexisting DB without password

--- mathesar_ui/src/systems/databases/create-database/ConnectExistingDatabase.svelte
+++ mathesar_ui/src/systems/databases/create-database/ConnectExistingDatabase.svelte
@@ -7,6 +7,7 @@
     FieldLayout,
     FormSubmit,
     makeForm,
+    optionalField,
     requiredField,
   } from '@mathesar/components/form';
   import Field from '@mathesar/components/form/Field.svelte';
@@ -32,7 +33,7 @@
   const host = requiredField('localhost');
   const port = requiredField(5432);
   const role = requiredField('');
-  const password = requiredField('');
+  const password = optionalField('');
   const installationSchemas = requiredField<InstallationSchema[]>(['internal']);
   const form = makeForm({
     databaseName,
