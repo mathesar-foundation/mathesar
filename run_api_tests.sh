6f490acad2f11cec8d9a37ab877275b0dc589bbd
Anish Umale <umaleanish120@gmail.com>
2025-02-14 19:45:21 +0530

    revert run_api_tests.sh changes

--- run_api_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-# EXIT_CODE=0
-docker compose -f docker-compose.test.yml run --rm test-runner pytest -svv test_happy_db_setups.py
-# Needed once (if) we have multiple scenarios to test. This accumulates the max exit code.
-# EXIT_CODE=$(( EXIT_CODE > $? ? EXIT_CODE : $? ))
-# docker compose -f docker-compose.test.yml down -v -t 1
-# exit $EXIT_CODE
