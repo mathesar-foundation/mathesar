b09e6902e0acb9150b38c4156b6e9fa8ac010706
Brent Moran <brentmoran@gmail.com>
2025-02-17 22:52:07 +0800

    remove useless --skip-confirm flag

--- run.sh
+++ run.sh
@@ -16,6 +16,6 @@ then
   export POSTGRES_DB=mathesar_django
 fi
 
-python -m mathesar.install --skip-confirm
+python -m mathesar.install
 # Start the Django server on port 8000. Add debug log level to gunicorn when appropriate.
 gunicorn config.wsgi -b 0.0.0.0:8000 $([ "$DEBUG" = "true" ] && echo -n "--log-level=debug") && fg
