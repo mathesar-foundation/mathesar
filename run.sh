9e8d0c6a9a3265fec497bd9f20b46f1100c2666a
Brent Moran <brentmoran@gmail.com>
2025-02-17 16:15:05 +0800

    add debug log-level to gunicorn when DEBUG=true

--- run.sh
+++ run.sh
@@ -17,5 +17,5 @@ then
 fi
 
 python -m mathesar.install --skip-confirm
-# Start the Django server on port 8000.
-gunicorn config.wsgi -b 0.0.0.0:8000 && fg
+# Start the Django server on port 8000. Add debug log level to gunicorn when appropriate.
+gunicorn config.wsgi -b 0.0.0.0:8000 $([ "$DEBUG" = "true" ] && echo -n "--log-level=debug") && fg
