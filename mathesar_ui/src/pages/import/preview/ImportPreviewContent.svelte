4ae7d31dd282429aac3292ffe0dc6c75bd8644ca
pavish <pavishkumar.r@gmail.com>
2025-03-19 21:27:03 +0530

    Make recovery easier during failed import, show proper errors for header change failures

--- mathesar_ui/src/pages/import/preview/ImportPreviewContent.svelte
+++ mathesar_ui/src/pages/import/preview/ImportPreviewContent.svelte
@@ -94,7 +94,11 @@
   $: columnsFetch = new AsyncStore(runner(api.columns.list_with_metadata));
 
   $: records = $previewRequest.resolvedValue ?? getSkeletonRecords();
-  $: formInputsAreDisabled = !$previewRequest.isOk;
+  $: formInputsAreDisabled =
+    $columnsFetch.isLoading ||
+    $headerUpdateRequest.isLoading ||
+    $typeSuggestionsRequest.isLoading ||
+    $previewRequest.isLoading;
   $: canProceed = $previewRequest.isOk && $form.canSubmit;
   $: processedColumns = processColumns(columns);
 
@@ -246,17 +250,17 @@
           on:retry={init}
           on:delete={cancel}
         />
+      {:else if $headerUpdateRequest.error}
+        <ErrorInfo
+          error={$headerUpdateRequest.error}
+          on:retry={init}
+          on:delete={cancel}
+        />
       {:else if !$previewRequest.hasSettled}
         <div class="loading"><Spinner /></div>
       {:else if $previewRequest.error}
         <ErrorInfo
           error={$previewRequest.error}
-          on:retry={init}
-          on:delete={cancel}
-        />
-      {:else if $headerUpdateRequest.error}
-        <ErrorInfo
-          error={$headerUpdateRequest.error}
           on:retry={init}
           on:delete={cancel}
         />
