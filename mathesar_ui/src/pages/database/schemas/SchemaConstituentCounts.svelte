727e3c27db82d9da26fa246abb00fb5f394dc7d9
Sean Colsen <colsen.sean@gmail.com>
2025-06-09 18:53:14 -0400

    Translate table count text

--- mathesar_ui/src/pages/database/schemas/SchemaConstituentCounts.svelte
+++ mathesar_ui/src/pages/database/schemas/SchemaConstituentCounts.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
+  import { _ } from 'svelte-i18n';
+
   import type { Schema } from '@mathesar/models/Schema';
 
   export let schema: Schema;
 
   $: ({ tableCount } = schema);
-  $: actualTableCount = $tableCount;
-  $: tableText = actualTableCount === 1 ? 'table' : 'tables';
 </script>
 
 <div class="container">
   <p class="table-count">
-    {actualTableCount}
-    {tableText}
+    {$_('count_tables', { values: { count: $tableCount } })}
   </p>
 </div>
 
