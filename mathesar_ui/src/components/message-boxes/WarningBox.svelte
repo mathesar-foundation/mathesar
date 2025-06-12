ca347ddccbae62d99447847b33ac8511a842719a
Sean Colsen <colsen.sean@gmail.com>
2025-06-12 14:14:24 -0400

    Clean up and fix status box colors

--- mathesar_ui/src/components/message-boxes/WarningBox.svelte
+++ mathesar_ui/src/components/message-boxes/WarningBox.svelte
@@ -17,6 +17,6 @@
     --MessageBox__background: var(--warning-background-color);
     --MessageBox__border: 4px solid var(--warning-border-color);
     --MessageBox__icon-color: var(--warning-color);
-    --MessageBox__text-color: var(--text-color);
+    --MessageBox__text-color: var(--warning-color);
   }
 </style>
