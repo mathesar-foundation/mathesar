2d41a551afc092d07daeeee616f4cc86d688c7c5
Anish Umale <umaleanish120@gmail.com>
2025-06-14 20:51:56 +0530

    add docstrings

--- mathesar/rpc/forms.py
+++ mathesar/rpc/forms.py
@@ -1,7 +1,7 @@
 """
 Classes and functions exposed to the RPC endpoint for managing forms.
 """
-from typing import Optional, TypedDict
+from typing import Optional, TypedDict, Literal
 
 from modernrpc.core import REQUEST_KEY
 
@@ -10,12 +10,32 @@ from mathesar.utils.forms import create_form
 
 
 class FieldInfo(TypedDict):
+    """
+    Information about a form field.
+
+    Attributes:
+        id: The Django id of the Field on the database.
+        key: A unique string identifier for the field within a form.
+        attnum: The attnum of column to be selected as a field.
+        form_id: The Django id of the Form on the database.
+        index: The order in which the field should be displayed.
+        kind: Type of the selected column(scalar_column, foreign_key, reverse_foreign_key)
+        label: ???
+        help: ???
+        readonly: Specifies whether the selected field is readonly.
+        styling: ???
+        is_required: Specifies whether a value for the field is mandatory.
+        parent_field_id: The Django id of the Field set as parent for related fields.
+        target_table_oid: The OID of the related table.
+        allow_create: Specifies whether adding new records is allowed within related fields.
+        create_label: ???
+    """
     id: int
     key: str
     attnum: int
     form_id: int
     index: int
-    kind: str
+    kind: Literal["scalar_column", "foreign_key", "reverse_foreign_key"]
     label: Optional[str]
     help: Optional[str]
     readonly: bool
@@ -28,6 +48,29 @@ class FieldInfo(TypedDict):
 
 
 class FormInfo(TypedDict):
+    """
+    Information about a form.
+
+    Atrributes:
+        id: The Django id of the Form on the database.
+        created_at: The time at which the form model got created.
+        updated_at: The time at which the form model was last updated.
+        token: A UUIDv4 object used to identify a form uniquely.
+        name: The name of the form.
+        description: The desciription of the form.
+        version: The version of the form.
+        database_id: The Django id of the database containing the Form.
+        schema_oid: The OID of the schema where within which form exists.
+        base_table_oid: The table OID based on which a form will be created.
+        is_public: Specifies wheather the form is publically accessible.
+        header_title: The Django id of the configured role to be used while submitting a form.
+        header_subtitle: ???
+        submit_role_id: ???
+        submit_message: Message to be displayed upon submission.
+        redirect_url: Redirect path after submission.
+        submit_label: Text to be displayed on the submit button.
+        fields: Definition of Fields within the form.
+    """
     id: int
     created_at: str
     updated_at: str
@@ -55,10 +98,28 @@ class FormInfo(TypedDict):
 
 
 class FieldDef(TypedDict):
+    """
+    Definition needed to add/modify a form field.
+
+    Attributes:
+        key: A unique string identifier for the field within a form.
+        attnum: The attnum of column to be selected as a field.
+        index: The order in which the field should be displayed.
+        kind: Type of the selected column(scalar_column, foreign_key, reverse_foreign_key)
+        label: ???
+        help: ???
+        readonly: Specifies whether the selected field is readonly.
+        styling: ???
+        is_required: Specifies whether a value for the field is mandatory.
+        parent_field_key: Field key to specify parent field for foreign_key or reverse_foreign_key.
+        target_table_oid: The OID of the related table.
+        allow_create: Specifies whether adding new records is allowed within related fields.
+        create_label: ???
+    """
     key: str
     attnum: int
     index: int
-    kind: str
+    kind: Literal["scalar_column", "foreign_key", "reverse_foreign_key"]
     label: Optional[str]
     help: Optional[str]
     readonly: Optional[bool]
@@ -71,7 +132,27 @@ class FieldDef(TypedDict):
 
 
 class FormDef(TypedDict):
-    token: str
+    """
+    Definition needed to add/modify a form.
+
+    Attributes:
+        token: A UUIDv4 object used to identify a form uniquely.
+        name: The name of the form.
+        description: The desciription of the form.
+        version: The version of the form.
+        database_id: The Django id of the database containing the Form.
+        schema_oid: The OID of the schema where within which form exists.
+        base_table_oid: The table OID based on which a form will be created.
+        is_public: Specifies wheather the form is publically accessible.
+        header_title: ???
+        header_subtitle: ???
+        submit_role_id: The Django id of the configured role to be used while submitting a form.
+        submit_message: Message to be displayed upon submission.
+        redirect_url: Redirect path after submission.
+        submit_label: Text to be displayed on the submit button.
+        fields: Definition of Fields within the form.
+    """
+    token: Optional[str]
     name: str
     description: Optional[str]
     version: int
