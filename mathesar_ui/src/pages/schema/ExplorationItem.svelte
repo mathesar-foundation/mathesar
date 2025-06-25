5a554d4fcb414627a4c9234fa8879d7c84de85fb
Sean Colsen <colsen.sean@gmail.com>
2025-06-25 12:11:00 -0400

    Implement edit/delete exploration from schema page

--- mathesar_ui/src/pages/schema/ExplorationItem.svelte
+++ mathesar_ui/src/pages/schema/ExplorationItem.svelte
@@ -9,15 +9,26 @@
   import type { Database } from '@mathesar/models/Database';
   import type { Schema } from '@mathesar/models/Schema';
   import { getExplorationPageUrl } from '@mathesar/routes/urls';
+  import { confirmDelete } from '@mathesar/stores/confirmation';
+  import { deleteExploration } from '@mathesar/stores/queries';
   import { currentTablesData as tablesStore } from '@mathesar/stores/tables';
   import { ButtonMenuItem } from '@mathesar-component-library';
 
   export let exploration: SavedExploration;
   export let database: Database;
   export let schema: Schema;
+  export let openEditExplorationModal: (e: SavedExploration) => void;
 
   $: baseTable = $tablesStore.tablesMap.get(exploration.base_table_oid);
   $: href = getExplorationPageUrl(database.id, schema.oid, exploration.id);
+
+  function handleDelete() {
+    void confirmDelete({
+      identifierType: 'Exploration',
+      identifierName: exploration.name,
+      onProceed: () => deleteExploration(exploration.id),
+    });
+  }
 </script>
 
 <EntityListItem
@@ -36,10 +47,13 @@
     {/if}
   </svelte:fragment>
   <svelte:fragment slot="menu">
-    <ButtonMenuItem icon={iconEdit}>
+    <ButtonMenuItem
+      on:click={() => openEditExplorationModal(exploration)}
+      icon={iconEdit}
+    >
       {$_('edit_exploration')}
     </ButtonMenuItem>
-    <ButtonMenuItem danger icon={iconDeleteMajor}>
+    <ButtonMenuItem on:click={handleDelete} danger icon={iconDeleteMajor}>
       {$_('delete_exploration')}
     </ButtonMenuItem>
   </svelte:fragment>
