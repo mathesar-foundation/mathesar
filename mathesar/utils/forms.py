f6d85be52028eec4b2823d9c818a568691a189ec
pavish <pavishkumar.r@gmail.com>
2025-06-30 17:54:21 +0530

    Update rpc functions and utils based on changes to models, make child_fields request/reponse nested

--- mathesar/utils/forms.py
+++ mathesar/utils/forms.py
@@ -11,21 +11,21 @@ from mathesar.models.base import (
 from mathesar.rpc.columns.metadata import ColumnMetaDataBlob
 
 
-def get_submit_role(user, database_id, submit_role_id=None):
+def validate_and_get_associated_role(user, database_id, associated_role_id=None):
     user_dbrm = UserDatabaseRoleMap.objects.get(user=user, database__id=database_id)
     with user_dbrm.connection as conn:
-        submit_role = (
-            ConfiguredRole.objects.get(id=submit_role_id) if submit_role_id else None
+        associated_role = (
+            ConfiguredRole.objects.get(id=associated_role_id) if associated_role_id else None
             or user_dbrm.configured_role
         )
         current_role = get_current_role_from_db(conn)
         parent_names = {pr["name"] for pr in current_role["parent_roles"]}
         assert (
-            user_dbrm.configured_role == submit_role
+            user_dbrm.configured_role == associated_role
             or user_dbrm.user.is_superuser
-            or submit_role.name in parent_names
-        ), "Insufficient privileges for selected submit_role"
-    return submit_role
+            or associated_role.name in parent_names
+        ), "Insufficient privileges for selected associated_role"
+    return associated_role
 
 
 def get_oid_attnums_map(form_model):
@@ -60,10 +60,38 @@ def get_field_col_info_map(form_model):
     return fcim
 
 
+def iterate_field_defs(field_defs, parent_field_defn=None):
+    """
+    Depth-first generator that iterates through the field definitions
+    """
+    for fdef in field_defs:
+        yield fdef, parent_field_defn
+        yield from iterate_field_defs(
+            fdef.get("child_fields", []),
+            fdef
+        )
+
+
+def construct_form_field_model_from_defn(field_def, form_model, parent_field=None):
+    return FormField(
+        key=field_def["key"],
+        form=form_model,
+        index=field_def["index"],
+        label=field_def.get("label"),
+        help=field_def.get("help"),
+        kind=field_def["kind"],
+        column_attnum=field_def["column_attnum"],
+        constraint_oid=field_def["constraint_oid"],
+        parent_field=parent_field,
+        styling=field_def.get("styling"),
+        is_required=field_def.get("is_required", False),
+    )
+
+
 @transaction.atomic
 def create_form(form_def, user):
     database = Database.objects.get(id=form_def["database_id"])
-    submit_role = get_submit_role(user, database.id, submit_role_id=form_def.get("submit_role_id"))
+    associated_role = validate_and_get_associated_role(user, database.id, associated_role_id=form_def.get("associated_role_id"))
     form_model = Form.objects.create(
         token=form_def.get("token", uuid4()),
         name=form_def["name"],
@@ -73,47 +101,36 @@ def create_form(form_def, user):
         server=database.server,
         schema_oid=form_def["schema_oid"],
         base_table_oid=form_def["base_table_oid"],
-        is_public=form_def.get("is_public", False),
+        associated_role=associated_role,
         header_title=form_def["header_title"],
         header_subtitle=form_def.get("header_subtitle"),
-        submit_role=submit_role,
         submit_message=form_def.get("submit_message"),
-        redirect_url=form_def.get("redirect_url"),
-        submit_label=form_def.get("submit_label")
+        submit_redirect_url=form_def.get("submit_redirect_url"),
+        submit_button_label=form_def.get("submit_button_label")
     )
     field_instances = [
-        FormField(
-            key=field["key"],
-            attnum=field["attnum"],
-            form=form_model,
-            index=field["index"],
-            kind=field["kind"],
-            label=field.get("label"),
-            help=field.get("help"),
-            readonly=field.get("readonly", False),
-            styling=field.get("styling"),
-            is_required=field.get("is_required", False),
-            parent_field=None,
-            target_table_oid=field.get("target_table_oid"),
-            allow_create=field.get("allow_create", False),
-            create_label=field.get("create_label")
-        ) for field in form_def["fields"]
+        construct_form_field_model_from_defn(
+            field_def,
+            form_model,
+            None
+        ) for field_def in iterate_field_defs(form_def["fields"])
     ]
     created_fields = FormField.objects.bulk_create(field_instances)
-    fields_map = {field.key: field for field in created_fields}
+    created_fields_map = {field.key: field for field in created_fields}
     update_field_instances = []
-    for field in form_def["fields"]:
-        if field.get("parent_field_key"):
-            fields_map[field["key"]].parent_field = fields_map[field["parent_field_key"]]
-            update_field_instances.append(fields_map[field["key"]])
+    for field_def, parent_field_def in iterate_field_defs(form_def["fields"]):
+        if parent_field_def:
+            created_field = created_fields_map[field_def["key"]]
+            created_field.parent_field = created_fields_map[parent_field_def["key"]]
+            update_field_instances.append(created_field)
     if update_field_instances:
         FormField.objects.bulk_update(update_field_instances, ["parent_field"])
     field_col_info_map = get_field_col_info_map(form_model)
     return form_model, field_col_info_map
 
 
-def get_form(form_id, is_public=False):
-    form_model = Form.objects.get(id=form_id, is_public=True) if is_public else Form.objects.get(id=form_id)
+def get_form(form_id):
+    form_model = Form.objects.get(id=form_id)
     field_col_info_map = get_field_col_info_map(form_model)
     return form_model, field_col_info_map
 
