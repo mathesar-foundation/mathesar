d0e7a6b35e43f8f1d11bb1c75e59b8321e92cf48
Anish Umale <umaleanish120@gmail.com>
2025-07-04 03:16:01 +0530

    make comment shorter

--- db/forms.py
+++ db/forms.py
@@ -4,40 +4,31 @@ from db import connection as db_conn
 
 def get_info_for_table_col_cons_map(table_col_cons_map, conn):
     """
-    Returns table_info, column_info, and constraints_info for a given table_col_cons_map.
+      Returns table_info, column_info, and constraints_info for a given tab_col_con_map.
 
-    table_col_cons_map should have the following form:
-    {
-      "tables": {
-        "table_oid_1": [col_attnum_1, col_attnum_2, col_attnum_3],
-        "table_oid_2": [col_attnum_4, col_attnum_5]
-      },
-      "constraints": [cons_oid_1, cons_oid_2]
-    }
+      tab_col_con_map should have the following form:
+      {
+        "tables": {
+          "table_oid_1": [col_attnum_1, col_attnum_2, col_attnum_3],
+          "table_oid_2": [col_attnum_4, col_attnum_5]
+        },
+        "constraints": [cons_oid_1, cons_oid_2]
+      }
 
-    Returns:
-    {
-      "tables": {
-        "table_oid_1": {
-          "table_info": table_info(),
-          "columns": {
-            "col_attnum_1": col_info(),
-            "col_attnum_2": col_info(),
-            "col_attnum_3": col_info()
+      Returns:
+      {
+        "tables": {
+          "table_oid_1": {
+            "table_info": table_info(),
+            "columns": {"col_attnum_1": col_info(), "col_attnum_2": col_info(), "col_attnum_3": col_info()
+            }
+          },
+          "table_oid2": {
+            "table_info": table_info(),
+            "columns": {"col_attnum_4": col_info(), "col_attnum_5": col_info()}
           }
         },
-        "table_oid2": {
-          "table_info": table_info(),
-          "columns": {
-            "col_attnum_4": col_info(),
-            "col_attnum_5": col_info()
-          }
-        }
-      },
-      "constraints": {
-        "cons_oid1": cons_info(),
-        "cons_oid2": cons_info()
+        "constraints": {"cons_oid1": cons_info(), "cons_oid2": cons_info()}
       }
-    }
     """
     return db_conn.exec_msar_func(conn, 'get_info_for_table_col_cons_map', json.dumps(table_col_cons_map)).fetchone()[0]
