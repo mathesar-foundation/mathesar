a80c7671f1d794497276e3c12c4194e536e821b5
Anish Umale <umaleanish120@gmail.com>
2025-07-04 19:17:50 +0530

    minor comment fixup

--- db/forms.py
+++ db/forms.py
@@ -4,31 +4,31 @@ from db import connection as db_conn
 
 def get_tab_col_con_info_map(tab_col_con_map, conn):
     """
-      Returns table_info, column_info, and constraints_info for a given tab_col_con_map.
+    Returns table_info, column_info, and constraints_info for a given tab_col_con_map.
 
-      tab_col_con_map should have the following form:
-      {
-        "tables": {
-          "table_oid_1": [col_attnum_1, col_attnum_2, col_attnum_3],
-          "table_oid_2": [col_attnum_4, col_attnum_5]
-        },
-        "constraints": [cons_oid_1, cons_oid_2]
-      }
+    tab_col_con_map should have the following form:
+    {
+      "tables": {
+        "table_oid_1": [col_attnum_1, col_attnum_2, col_attnum_3],
+        "table_oid_2": [col_attnum_4, col_attnum_5]
+      },
+      "constraints": [cons_oid_1, cons_oid_2]
+    }
 
-      Returns:
-      {
-        "tables": {
-          "table_oid_1": {
-            "table_info": table_info(),
-            "columns": {"col_attnum_1": col_info(), "col_attnum_2": col_info(), "col_attnum_3": col_info()
-            }
-          },
-          "table_oid2": {
-            "table_info": table_info(),
-            "columns": {"col_attnum_4": col_info(), "col_attnum_5": col_info()}
+    Returns:
+    {
+      "tables": {
+        "table_oid_1": {
+          "table_info": table_info(),
+          "columns": {"col_attnum_1": col_info(), "col_attnum_2": col_info(), "col_attnum_3": col_info()
           }
         },
-        "constraints": {"cons_oid1": cons_info(), "cons_oid2": cons_info()}
-      }
+        "table_oid2": {
+          "table_info": table_info(),
+          "columns": {"col_attnum_4": col_info(), "col_attnum_5": col_info()}
+        }
+      },
+      "constraints": {"cons_oid1": cons_info(), "cons_oid2": cons_info()}
+    }
     """
     return db_conn.exec_msar_func(conn, 'get_tab_col_con_info_map', json.dumps(tab_col_con_map)).fetchone()[0]
