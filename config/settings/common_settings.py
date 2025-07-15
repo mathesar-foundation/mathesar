13bf71a05865cc3cb3b51743576d344f83e9157d
Anish Umale <umaleanish120@gmail.com>
2025-07-16 02:42:53 +0530

    add accidently deleted \n again

--- config/settings/common_settings.py
+++ config/settings/common_settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+
 import os
 import traceback
 from pathlib import Path
