669946ee8bc1627fb746fb3c4ef2be9e8e07c2b2
Anish Umale <umaleanish120@gmail.com>
2025-07-15 18:42:33 +0530

    add error message for failed user_dbms creation

--- mathesar/oidc.py
+++ mathesar/oidc.py
@@ -23,26 +23,30 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
 
     @transaction.atomic
     def save_user(self, request, sociallogin, form=None):
-        role_config_list = settings.OIDC_DEFAULT_PG_ROLE_MAP.get(str(sociallogin.provider))
         saved_user = super().save_user(request, sociallogin, form)
-        if role_config_list != []:
-            for role_config in role_config_list:
-                database_name = role_config["db_name"]
-                host = role_config["host"]
-                port = role_config["port"]
-                role_name = role_config["role_name"]
-                server = Server.objects.get(host=host, port=port)
-                database = Database.objects.get(
-                    name=database_name, server=server
-                )
-                configured_role = ConfiguredRole.objects.get(
-                    name=role_name,
-                    server=server
-                )
-                UserDatabaseRoleMap.objects.get_or_create(
-                    user=saved_user,
-                    database=database,
-                    configured_role=configured_role,
-                    server=server
-                )
+        try:
+            role_config_list = settings.OIDC_DEFAULT_PG_ROLE_MAP.get(str(sociallogin.provider))
+            if role_config_list != []:
+                for role_config in role_config_list:
+                    database_name = role_config["db_name"]
+                    host = role_config["host"]
+                    port = role_config["port"]
+                    role_name = role_config["role_name"]
+                    server = Server.objects.get(host=host, port=port)
+                    database = Database.objects.get(
+                        name=database_name, server=server
+                    )
+                    configured_role = ConfiguredRole.objects.get(
+                        name=role_name,
+                        server=server
+                    )
+                    UserDatabaseRoleMap.objects.get_or_create(
+                        user=saved_user,
+                        database=database,
+                        configured_role=configured_role,
+                        server=server
+                    )
+        except Exception:
+            messages.error(request, "Failed to automatically provision default postgres roles.")
+            raise ImmediateHttpResponse(redirect("account_login"))
         return saved_user
