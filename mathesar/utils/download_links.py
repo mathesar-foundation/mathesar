bed7d9128b1d2080c49394306b39edbaee0b033e
Brent Moran <brentmoran@gmail.com>
2025-08-12 14:27:26 +0800

    add thumbnail functionality to the API

--- mathesar/utils/download_links.py
+++ mathesar/utils/download_links.py
@@ -1,6 +1,9 @@
+import base64
+import io
 import mimetypes
 from django.shortcuts import get_object_or_404
 import fsspec
+from PIL import Image
 from mathesar.models import DownloadLink
 
 
@@ -12,10 +15,37 @@ def get_link_contents(request, download_link_id):
     )
     content_type = mimetypes.guess_type(link.uri)[0]
     of = fsspec.open(link.uri, "rb")
+    filename = of.path
 
-    def stream_file(of):
+    def stream_file():
         with of as f:
             while scoop := f.read(512):
                 yield scoop
 
-    return stream_file, of, content_type
+    return stream_file, filename, content_type
+
+
+def get_link_thumbnail(request, download_link_id):
+    link = get_object_or_404(
+        DownloadLink,
+        id=download_link_id,
+        session=request.session.session_key,
+    )
+    content_type = "image/jpeg"
+    size = int(request.GET.get("width", 500)), int(request.GET.get("height", 500))
+    key = f"{size[0]}x{size[1]}"
+
+    if (thumb_64 := link.thumbnail.get(key)) is None:
+        of = fsspec.open(link.uri, "rb")
+        img_byte_arr = io.BytesIO()
+        with of as f:
+            img = Image.open(f)
+            img.thumbnail(size)
+            img.save(img_byte_arr, format="JPEG")
+        thumbnail = img_byte_arr.getvalue()
+        link.thumbnail[key] = base64.b64encode(thumbnail).decode("utf-8")
+        link.save()
+    else:
+        thumbnail = base64.b64decode(bytes(thumb_64, "utf-8"))
+
+    return thumbnail, content_type
--- mathesar/views/download_link.py
+++ mathesar/views/download_link.py
@@ -1,21 +1,28 @@
 from django.contrib.auth.decorators import login_required
-from django.http import StreamingHttpResponse
+from django.http import HttpResponse, StreamingHttpResponse
 from django.views.decorators.http import require_http_methods
 
-from mathesar.utils.download_links import get_link_contents
+from mathesar.utils.download_links import get_link_contents, get_link_thumbnail
 
 
 @login_required
 @require_http_methods(["GET"])
 def download_file(request, download_link_id):
-    stream_file, of, content_type = get_link_contents(request, download_link_id)
-    response = StreamingHttpResponse(stream_file(of), content_type=content_type)
-    response['Content-Disposition'] = f'attachment; filename={of.path}'
+    stream_file, filename, content_type = get_link_contents(request, download_link_id)
+    response = StreamingHttpResponse(stream_file(), content_type=content_type)
+    response['Content-Disposition'] = f'attachment; filename={filename}'
     return response
 
 
 @login_required
 @require_http_methods(["GET"])
 def load_file(request, download_link_id):
-    stream_file, of, content_type = get_link_contents(request, download_link_id)
-    return StreamingHttpResponse(stream_file(of), content_type=content_type)
+    stream_file, _, content_type = get_link_contents(request, download_link_id)
+    return StreamingHttpResponse(stream_file(), content_type=content_type)
+
+
+@login_required
+@require_http_methods(["GET"])
+def load_file_thumbnail(request, download_link_id):
+    thumbnail, content_type = get_link_thumbnail(request, download_link_id)
+    return HttpResponse(thumbnail, content_type=content_type)
