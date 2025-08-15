4850e4959fbe3bc2e88786df7f70e817be0c8403
Sean Colsen <colsen.sean@gmail.com>
2025-08-15 07:28:13 -0400

    Improve responsive behavior of shared form page

--- mathesar_ui/src/pages/shared-data-form/SharedDataFormFillPage.svelte
+++ mathesar_ui/src/pages/shared-data-form/SharedDataFormFillPage.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
   import type { RawDataForm, RawDataFormSource } from '@mathesar/api/rpc/forms';
   import Errors from '@mathesar/components/errors/Errors.svelte';
-  import LayoutWithHeader from '@mathesar/layouts/LayoutWithHeader.svelte';
   import type { RpcError } from '@mathesar/packages/json-rpc-client-builder';
   import { makeSimplePageTitle } from '@mathesar/pages/pageTitleUtils';
   import type { AsyncStoreValue } from '@mathesar/stores/AsyncStore';
   import {
-    DataFormCanvas,
     DataFormStructure,
     FormSource,
     ReadonlyDataFormManager,
   } from '@mathesar/systems/data-forms/form-maker';
+  import DataFormBranding from '@mathesar/systems/data-forms/form-maker/DataFormBranding.svelte';
+  import DataFormFieldsContainer from '@mathesar/systems/data-forms/form-maker/elements/DataFormFieldsContainer.svelte';
+  import DataFormFooter from '@mathesar/systems/data-forms/form-maker/elements/DataFormFooter.svelte';
+  import DataFormHeader from '@mathesar/systems/data-forms/form-maker/elements/DataFormHeader.svelte';
 
   export let rawDataForm: RawDataForm;
   export let formSourceInfo: AsyncStoreValue<RawDataFormSource, RpcError>;
@@ -30,19 +32,40 @@
   <title>{makeSimplePageTitle(pageTitle)}</title>
 </svelte:head>
 
-<LayoutWithHeader fitViewport>
-  {#if dataFormManager}
-    <div class="data-form-filler">
-      <DataFormCanvas {dataFormManager} />
+{#if dataFormManager}
+  {@const { fields } = dataFormManager.dataFormStructure}
+  <div class="data-form-filler">
+    <div class="form">
+      <DataFormHeader {dataFormManager} />
+      <DataFormFieldsContainer {fields} {dataFormManager} />
+      <DataFormFooter {dataFormManager} />
+      <div class="branding">
+        <DataFormBranding />
+      </div>
     </div>
-  {:else if formSourceInfo.error}
-    <Errors errors={[formSourceInfo.error]} />
-  {/if}
-</LayoutWithHeader>
+  </div>
+{:else if formSourceInfo.error}
+  <Errors errors={[formSourceInfo.error]} />
+{/if}
 
 <style lang="scss">
   .data-form-filler {
+    overflow: auto;
+    position: relative;
     height: 100%;
-    overflow: hidden;
+    background: var(--elevated-background);
+    --data_forms__label-input-gap: var(--sm5);
+    --data_forms__selectable-element-padding: var(--sm5) 0 var(--sm5) 1rem;
+
+    .form {
+      margin: 0 auto;
+      max-width: 40rem;
+      padding-right: 1rem;
+
+      .branding {
+        border-top: 1px solid var(--border-color);
+        margin-top: var(--lg2);
+      }
+    }
   }
 </style>
