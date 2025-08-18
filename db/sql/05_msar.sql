00323c11d5cba94eaa7da188b6542cf0d72d6b84
Anish Umale <umaleanish120@gmail.com>
2025-08-19 01:17:21 +0530

    handle submit behavior when inserting into a column with foreign key constraints referencing multiple columns

--- db/sql/05_msar.sql
+++ db/sql/05_msar.sql
@@ -5740,6 +5740,16 @@ Returns:
 $$ LANGUAGE SQL RETURNS NULL ON NULL INPUT;
 
 
+CREATE OR REPLACE FUNCTION msar.raise_exception(err_msg text)
+RETURNS void AS $$/* 
+Utility function to raise an exceptions with an error message.
+*/
+BEGIN
+  RAISE EXCEPTION '%', err_msg;
+END;
+$$ LANGUAGE plpgsql RETURNS NULL ON NULL INPUT;
+
+
 CREATE OR REPLACE FUNCTION msar.build_insert_lookup_table(field_info_list jsonb, values_ jsonb) RETURNS TABLE
 (
   table_name text,
@@ -5768,8 +5778,6 @@ WITH k1_cte AS (
 */
 WITH cte AS (
   SELECT
-    fields.key::text AS key,
-    fields.column_attnum::smallint AS column_attnum,
     pga.attname::name AS column_name,
     fields.table_oid::bigint AS table_oid,
     fields.depth::integer AS depth,
@@ -5802,15 +5810,22 @@ WITH cte AS (
   LEFT JOIN unnest(pgc.conkey) WITH ORDINALITY AS ck(attnum, ord) ON ck.attnum = fields.column_attnum
   LEFT JOIN unnest(pgc.confkey) WITH ORDINALITY AS fk(attnum, ord) ON fk.ord = ck.ord
   LEFT JOIN pg_attribute ref_attr ON ref_attr.attrelid = pgc.confrelid AND ref_attr.attnum = fk.attnum
+), multi_fks_cte AS (
+  SELECT msar.raise_exception(
+    'Inserting into a column with foreign key constraints referencing multiple columns is currently unsupported.'
+  )
+  FROM cte GROUP BY cte.column_name, cte.from_cte_name HAVING count(*) > 1
 )
-SELECT 
-  __msar.get_qualified_relation_name(table_oid) AS table_name,
-  string_agg(quote_ident(column_name), ', ') AS column_names,
-  string_agg(value, ', ') AS values_,
-  cte_name,
-  string_agg(from_cte_name, ', ') AS from_cte_name
-FROM cte GROUP BY table_oid, cte_name, depth ORDER BY depth DESC;
-$$ LANGUAGE SQL STABLE;
+SELECT
+  __msar.get_qualified_relation_name(cte.table_oid) AS table_name,
+  string_agg(quote_ident(cte.column_name), ', ') AS column_names,
+  string_agg(cte.value, ', ') AS values_,
+  cte.cte_name,
+  string_agg(cte.from_cte_name, ', ') AS from_cte_name
+FROM cte
+CROSS JOIN (SELECT COUNT(*) FROM multi_fks_cte) AS force_multi_fks_cte_execution
+GROUP BY cte.table_oid, cte.cte_name, cte.depth ORDER BY cte.depth DESC;
+$$ LANGUAGE SQL STABLE RETURNS NULL ON NULL INPUT;
 
 
 CREATE OR REPLACE FUNCTION
