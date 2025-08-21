895314b4ec7ebcc16e6c83b8f38e6e1a2c6af5b2
Brent Moran <brentmoran@gmail.com>
2025-08-21 22:29:48 +0800

    add utility function for building DownloadLink objects

--- mathesar/models/base.py
+++ mathesar/models/base.py
@@ -375,7 +375,7 @@ class DataFile(BaseModel):
 class DownloadLink(BaseModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     sessions = models.ManyToManyField(Session)
-    uri = models.CharField()
+    uri = models.CharField()  # should not contain sensitive info
     thumbnail = models.JSONField(blank=True, default=dict)
     fsspec_kwargs = EncryptedJSONField(blank=True, default=dict)
 
--- mathesar/utils/download_links.py
+++ mathesar/utils/download_links.py
@@ -1,11 +1,18 @@
 import base64
+import hashlib
 import io
+import itertools
+import json
 import mimetypes
+import posixpath
+from django.conf import settings
 from django.shortcuts import get_object_or_404
 import fsspec
 from PIL import Image
+import yaml
 from mathesar.models import DownloadLink
 
+BACKEND_CONF_YAML = settings.BASE_DIR.joinpath('file_storage.yml')
 
 def get_link_contents(request, download_link_id):
     link = get_object_or_404(
@@ -15,7 +22,7 @@ def get_link_contents(request, download_link_id):
     )
     content_type = mimetypes.guess_type(link.uri)[0]
     of = fsspec.open(link.uri, "rb", **link.fsspec_kwargs)
-    filename = of.path
+    filename = posixpath.split(of.path)[-1]
 
     def stream_file():
         with of as f:
@@ -49,3 +56,28 @@ def get_link_thumbnail(request, download_link_id):
         thumbnail = base64.b64decode(bytes(thumb_64, "utf-8"))
 
     return thumbnail, content_type
+
+
+def create_mash_for_uri(uri, backend_key):
+    return hashlib.md5(
+        settings.SECRET_KEY.encode('utf-8')
+        + backend_key.encode('utf-8')
+        + uri.encode('utf-8')
+    ).hexdigest()
+
+
+def build_links_from_json(jsons):
+    """
+    Takes an iterable of JSON strings having "uri" and "mash" keys, and creates
+    DownloadLinks from them.
+    - matches each JSON URI and mash pair to the correct backend key for the
+      mash.
+    - Creates download links for each.
+    """
+    with open(BACKEND_CONF_YAML, 'r') as f:
+        backends = yaml.full_load(f)
+    return [
+        DownloadLink(uri=v.get("uri"), fsspec_kwargs=backends[b]["kwargs"])
+        for v, b in itertools.product((json.loads(p) for p in json_strs), backends)
+        if v["mash"] == create_mash_for_uri(v.get("uri", ""), b)
+    ]
