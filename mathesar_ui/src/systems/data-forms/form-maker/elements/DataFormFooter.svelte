518534571584d6b1fa6a67c58ebd8a2c358e6af5
pavish <pavishkumar.r@gmail.com>
2025-08-21 18:45:35 +0400

    Show generic error and hide actual message within a collapsible

--- mathesar_ui/src/systems/data-forms/form-maker/elements/DataFormFooter.svelte
+++ mathesar_ui/src/systems/data-forms/form-maker/elements/DataFormFooter.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import { _ } from 'svelte-i18n';
 
+  import ErrorList from '@mathesar/components/errors/ErrorList.svelte';
   import { FormSubmit } from '@mathesar/components/form';
+  import ErrorBox from '@mathesar/components/message-boxes/ErrorBox.svelte';
+  import { Collapsible } from '@mathesar-component-library';
 
   import {
     DataFormFillOutManager,
@@ -31,7 +34,25 @@
       icon: undefined,
     }}
     cancelButton={{ label: $_('clear_form') }}
-  />
+  >
+    <div class="error-container" slot="errors" let:errors>
+      <ErrorBox fullWidth>
+        <div>
+          {$_('error_submitting_form')}
+        </div>
+        <div class="error-detail">
+          <Collapsible triggerAppearance="ghost">
+            <span slot="header">
+              {$_('view_details')}
+            </span>
+            <div slot="content">
+              <ErrorList errorStrings={errors} />
+            </div>
+          </Collapsible>
+        </div>
+      </ErrorBox>
+    </div>
+  </FormSubmit>
 </div>
 
 <style lang="scss">
@@ -40,5 +61,9 @@
       var(--df__internal_element-right-padding)
       var(--df__internal__element-spacing)
       var(--df__internal_element-left-padding);
+  }
+  .error-detail {
+    --Collapsible_trigger-padding: 0;
+    margin-top: var(--sm5);
   }
 </style>
