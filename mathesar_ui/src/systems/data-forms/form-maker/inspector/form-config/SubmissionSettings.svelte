0ebcd3cd2e56ec781ac581812bcc1855ef9fec33
pavish <pavishkumar.r@gmail.com>
2025-08-25 13:03:57 +0400

    Make submission settings section more clear

--- mathesar_ui/src/systems/data-forms/form-maker/inspector/form-config/SubmissionSettings.svelte
+++ mathesar_ui/src/systems/data-forms/form-maker/inspector/form-config/SubmissionSettings.svelte
@@ -3,6 +3,7 @@
 
   import {
     Checkbox,
+    Fieldset,
     LabeledInput,
     TextArea,
     TextInput,
@@ -18,8 +19,37 @@
     dataFormStructure);
 </script>
 
-<div>
-  <div>
+<Fieldset boxed>
+  <span class="header" slot="label">
+    {$_('appearance')}
+  </span>
+  <section>
+    <LabeledInput layout="stacked" label={$_('button_label')}>
+      <TextInput
+        placeholder={$_('submit')}
+        value={$submitButtonLabel}
+        on:input={(e) =>
+          dataFormStructure.setSubmissionButtonLabel(
+            getStringValueFromEvent(e),
+          )}
+      />
+    </LabeledInput>
+  </section>
+</Fieldset>
+
+<Fieldset boxed>
+  <span class="header" slot="label">
+    {$_('after_submission')}
+  </span>
+  <section>
+    <LabeledInput layout="stacked" label={$_('confirmation_message')}>
+      <TextArea
+        placeholder={$_('thank_you_for_submitting_form')}
+        value={$submitMessage?.text}
+        on:input={(e) =>
+          dataFormStructure.setSubmissionMessage(getStringValueFromEvent(e))}
+      />
+    </LabeledInput>
     <LabeledInput layout="inline-input-first" label={$_('redirect_to_url')}>
       <Checkbox
         checked={isDefinedNonNullable($submitRedirectUrl)}
@@ -28,36 +58,25 @@
       />
     </LabeledInput>
     {#if isDefinedNonNullable($submitRedirectUrl)}
-      <div>
-        <TextInput
-          value={$submitRedirectUrl}
-          on:input={(e) => {
-            dataFormStructure.setSubmissionRedirectUrl(
-              getStringValueFromEvent(e),
-            );
-          }}
-        />
-      </div>
-    {/if}
-  </div>
-  <div>
-    <LabeledInput layout="stacked" label={$_('post_submission_message')}>
-      <TextArea
-        value={$submitMessage?.text}
-        on:input={(e) =>
-          dataFormStructure.setSubmissionMessage(getStringValueFromEvent(e))}
-      />
-    </LabeledInput>
-  </div>
-  <div>
-    <LabeledInput layout="stacked" label={$_('button_label')}>
       <TextInput
-        value={$submitButtonLabel}
-        on:input={(e) =>
-          dataFormStructure.setSubmissionButtonLabel(
+        value={$submitRedirectUrl}
+        on:input={(e) => {
+          dataFormStructure.setSubmissionRedirectUrl(
             getStringValueFromEvent(e),
-          )}
+          );
+        }}
       />
-    </LabeledInput>
-  </div>
-</div>
+    {/if}
+  </section>
+</Fieldset>
+
+<style lang="scss">
+  .header {
+    font-weight: var(--font-weight-medium);
+  }
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: var(--sm2);
+  }
+</style>
