1c2ddace740642ba1e6a20cecb2ce51706e75280
pavish <pavishkumar.r@gmail.com>
2025-09-09 13:16:42 +0400

    Fix surface raised-1 usage

--- mathesar_ui/src/components/InsetPageSection.svelte
+++ mathesar_ui/src/components/InsetPageSection.svelte
@@ -7,9 +7,9 @@
 
 <style>
   .section {
-    background: var(--surface-raised-1);
+    background: var(--color-surface-raised-1);
     padding: var(--inset-page-section-padding, 2rem 1.5rem);
-    border: solid 1px var(--surface-raised-1-border);
+    border: solid 1px var(--color-surface-raised-1-border);
     border-radius: 0.3rem;
   }
 </style>
