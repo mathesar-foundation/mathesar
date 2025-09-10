aa49955a40ccd3422e3e7feeae8df045ca80e3ec
Brent Moran <brentmoran@gmail.com>
2025-09-10 16:03:24 +0800

    add caching and security headers to responses

--- mathesar/views/download_link.py
+++ mathesar/views/download_link.py
@@ -5,14 +5,23 @@ from django.views.decorators.http import require_http_methods
 from mathesar.utils.download_links import get_link_contents, get_link_thumbnail
 
 
+CACHE_HEADER = {"Cache-Control": "public, max-age=31536000, immutable"}
+
+
 @login_required
 @require_http_methods(["GET"])
 def download_file(request, download_link_mash):
     stream_file, filename, content_type = get_link_contents(
         request.session.session_key, download_link_mash
     )
-    response = StreamingHttpResponse(stream_file(), content_type=content_type)
-    response['Content-Disposition'] = f'attachment; filename={filename}'
+    disposition_header = {
+        "Content-Disposition": f"attachment; filename={filename}"
+    }
+    response = StreamingHttpResponse(
+        stream_file(),
+        content_type=content_type,
+        headers=CACHE_HEADER | disposition_header,
+    )
     return response
 
 
@@ -22,7 +31,12 @@ def load_file(request, download_link_mash):
     stream_file, _, content_type = get_link_contents(
         request.session.session_key, download_link_mash
     )
-    return StreamingHttpResponse(stream_file(), content_type=content_type)
+    security_header = {"Content-Security-Policy": "default-src 'none'"}
+    return StreamingHttpResponse(
+        stream_file(),
+        content_type=content_type,
+        headers=CACHE_HEADER | security_header,
+    )
 
 
 @login_required
@@ -34,4 +48,6 @@ def load_file_thumbnail(request, download_link_mash):
         width=int(request.GET.get("width", 500)),
         height=int(request.GET.get("height", 500))
     )
-    return HttpResponse(thumbnail, content_type=content_type)
+    return HttpResponse(
+        thumbnail, content_type=content_type, headers=CACHE_HEADER
+    )
