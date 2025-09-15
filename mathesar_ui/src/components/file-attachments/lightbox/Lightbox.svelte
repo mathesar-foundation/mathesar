3ef751881f751cf40131a8b4415f010917294d4a
Sean Colsen <colsen.sean@gmail.com>
2025-09-15 11:49:36 -0400

    Make lightbox layout work w/ new img load pattern

--- mathesar_ui/src/components/file-attachments/lightbox/Lightbox.svelte
+++ mathesar_ui/src/components/file-attachments/lightbox/Lightbox.svelte
@@ -3,17 +3,15 @@
   import { _ } from 'svelte-i18n';
 
   import type { FileManifest } from '@mathesar/api/rpc/records';
-  import { Icon, iconClose } from '@mathesar/component-library';
+  import {
+    Icon,
+    iconClose,
+    makeStyleStringFromCssVariables,
+  } from '@mathesar/component-library';
   import Button from '@mathesar/component-library/button/Button.svelte';
   import focusTrap from '@mathesar/component-library/common/actions/focusTrap';
   import { iconDeleteMajor, iconDownload } from '@mathesar/icons';
-  import {
-    makeDimensionsStore,
-    resizeObserver,
-  } from '@mathesar/utils/resizeObserver';
   import { Dropdown, iconInfo, portal } from '@mathesar-component-library';
-
-  const imageDimensions = makeDimensionsStore();
 
   export let imageElement: HTMLImageElement;
   export let thumbnailElement: HTMLImageElement | undefined = undefined;
@@ -21,21 +19,12 @@
   export let close: () => void = () => {};
   export let removeFile: () => void = () => {};
 
-  let imageContainer: HTMLDivElement;
+  let imageHolder: HTMLDivElement;
+  let imageDisplayWidth: number;
 
   $: ({ uri, attachment: downloadUrl, mimetype } = fileManifest);
   $: ({ naturalHeight, naturalWidth } = imageElement);
-
-  /**
-   * I implemented this layout using a resize observer to monitor the image
-   * width. I tried HARD to get this layout to work without any JS, but I
-   * eventually gave up and just slapped some JS in here for the sake of time.
-   * Still, I feel it's a bit cringe. If you're reading this and you can figure
-   * out how to refactor this JS out of here and just use plain CSS, then please
-   * do so! It sure seems like it ought to be possible!
-   */
-  $: imageWidth = $imageDimensions.width;
-  $: showButtonLabels = imageWidth > 500;
+  $: showButtonLabels = imageDisplayWidth > 500;
 
   function handleKeydown(e: KeyboardEvent) {
     switch (e.key) {
@@ -48,7 +37,7 @@
   }
 
   onMount(() => {
-    imageContainer.appendChild(imageElement);
+    imageHolder.replaceChildren(imageElement);
     window.addEventListener('keydown', handleKeydown);
     return () => {
       window.removeEventListener('keydown', handleKeydown);
@@ -64,71 +53,80 @@
 </script>
 
 <div
-  class="image-file-viewer"
+  class="lightbox"
   use:portal
   use:focusTrap
-  style={`--image-width: ${imageWidth}px;`}
+  style={makeStyleStringFromCssVariables({
+    '--img-natural-height-raw': `${naturalHeight}`,
+    '--img-natural-width-raw': `${naturalWidth}`,
+    '--img-natural-height-px': `${naturalHeight}px`,
+    '--img-natural-width-px': `${naturalWidth}px`,
+  })}
 >
   <div class="overlay"></div>
-  <div class="content">
-    <div class="top">
-      <Button on:click={close}>
-        <Icon {...iconClose} />
-      </Button>
-    </div>
-    <!-- <img alt={uri} src={direct} use:resizeObserver={imageDimensions} /> -->
+  <div class="img-viewport-boundary">
+    <div
+      class="img-area"
+      bind:clientWidth={imageDisplayWidth}
+      class:show-button-labels={showButtonLabels}
+    >
+      <div class="top">
+        <Button on:click={close}>
+          <Icon {...iconClose} />
+        </Button>
+      </div>
 
-    <div class="image" bind:this={imageContainer}></div>
+      <div class="img-holder" bind:this={imageHolder} />
 
-    <div class="bottom">
-      <Button on:click={removeFile} aria-label={$_('remove')}>
-        <Icon {...iconDeleteMajor} />
-        {#if showButtonLabels}
-          <span>{$_('remove')}</span>
-        {/if}
-      </Button>
-
-      <Dropdown
-        showArrow={false}
-        {...$$restProps}
-        ariaLabel={$_('filter')}
-        placements={['bottom', 'top']}
-        contentClass="info-dropdown-content"
-      >
-        <svelte:fragment slot="trigger">
-          <Icon {...iconInfo} />
-          {#if showButtonLabels}
-            <span>{$_('info')}</span>
-          {/if}
-        </svelte:fragment>
-        <div class="info" slot="content">
-          <table>
-            <tr><th>{$_('storage_uri')}</th><td>{uri}</td></tr>
-            <tr><th>{$_('mime_type')}</th><td>{mimetype}</td></tr>
-          </table>
-        </div>
-      </Dropdown>
-
-      <a class="btn btn-default" href={downloadUrl}>
-        <Icon {...iconDownload} />
-        {#if showButtonLabels}
-          <span>{$_('download')}</span>
-        {/if}
-      </a>
+      <div class="bottom">
+        <Button on:click={removeFile} aria-label={$_('remove')}>
+          <Icon {...iconDeleteMajor} />
+          <span class="button-label">{$_('remove')}</span>
+        </Button>
+        <Dropdown
+          showArrow={false}
+          {...$$restProps}
+          ariaLabel={$_('filter')}
+          placements={['bottom', 'top']}
+          contentClass="info-dropdown-content"
+        >
+          <svelte:fragment slot="trigger">
+            <Icon {...iconInfo} />
+            <span class="button-label">{$_('info')}</span>
+          </svelte:fragment>
+          <div class="info" slot="content">
+            <table>
+              <tr><th>{$_('storage_uri')}</th><td>{uri}</td></tr>
+              <tr><th>{$_('mime_type')}</th><td>{mimetype}</td></tr>
+            </table>
+          </div>
+        </Dropdown>
+        <a class="btn btn-default" href={downloadUrl}>
+          <Icon {...iconDownload} />
+          <span class="button-label">{$_('download')}</span>
+        </a>
+      </div>
     </div>
   </div>
 </div>
 
 <style lang="scss">
-  .image-file-viewer {
+  .lightbox {
     position: absolute;
-    overflow: hidden;
     inset: 0;
     z-index: var(--modal-z-index, auto);
     isolation: isolate;
     display: grid;
     align-items: center;
     justify-content: center;
+    --viewport-padding-block: 1.5rem;
+    --viewport-padding-inline: 1rem;
+    --img-viewport-boundary-height: calc(
+      100dvh - 2 * var(--viewport-padding-block)
+    );
+    --img-viewport-boundary-width: calc(
+      100dvw - 2 * var(--viewport-padding-inline)
+    );
   }
   .overlay {
     background-color: var(--color-modal-overlay);
@@ -137,38 +135,67 @@
     z-index: 1;
   }
 
-  .content {
+  .img-viewport-boundary {
     z-index: 2;
-    max-width: 100%;
-    max-height: 100%;
-    padding: 1rem;
-    display: flex;
-    flex-direction: column;
+    height: var(--img-viewport-boundary-height);
+    width: var(--img-viewport-boundary-width);
+    display: grid;
     align-items: center;
-    overflow: hidden;
-    .top {
-      display: flex;
-      justify-content: flex-end;
-      width: var(--image-width);
-      margin-bottom: -1rem;
-      margin-right: -1rem;
-    }
-    img {
-      width: fit-content;
-      max-width: 100%;
-      min-height: 0;
-      flex: 0 1 max-content;
+    align-content: center;
+    justify-items: center;
+    justify-content: center;
+  }
+
+  .img-area {
+    min-width: 0;
+    // Width is the smallest of:
+    //
+    // - The natural image width — to prevent upscaling
+    // - The container width to — prevent horizontal overflow
+    // - A computed value to — prevent vertical overflow
+    width: min(
+      var(--img-natural-width-px),
+      var(--img-viewport-boundary-width),
+      calc(
+        var(--img-viewport-boundary-height) * var(--img-natural-width-raw) /
+          var(--img-natural-height-raw)
+      )
+    );
+    aspect-ratio: var(--img-natural-width-raw) / var(--img-natural-height-raw);
+    height: auto;
+    position: relative;
+
+    .img-holder {
+      width: 100%;
+      height: 100%;
       border-radius: var(--sm3);
       box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.4);
+      overflow: hidden;
+      :global(img) {
+        width: 100%;
+        height: 100%;
+        display: block;
+      }
+    }
+
+    .top {
+      position: absolute;
+      top: -1rem;
+      right: -0.5rem;
     }
     .bottom {
-      margin-top: -1rem;
+      position: absolute;
+      bottom: -1rem;
+      left: 0;
       padding-inline: 1rem;
-      text-align: center;
-      width: var(--image-width);
+      width: 100%;
       display: flex;
       flex-wrap: wrap;
       justify-content: space-between;
+    }
+
+    &:not(.show-button-labels) .button-label {
+      display: none;
     }
   }
 
