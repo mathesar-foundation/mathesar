fdbd5ef1c01875195d3d815aab3ada946a273ec9
Sean Colsen <colsen.sean@gmail.com>
2025-10-03 15:03:28 -0400

    Don't pass click when closing context menu

--- mathesar_ui/src/component-library/context-menu/ContextMenu.svelte
+++ mathesar_ui/src/component-library/context-menu/ContextMenu.svelte
@@ -1,32 +1,25 @@
 <script lang="ts">
-  import { setContext } from 'svelte';
-
-  import clickOffBounds from '@mathesar-component-library-dir/common/actions/clickOffBounds';
   import popper, {
     getVirtualReferenceElement,
   } from '@mathesar-component-library-dir/common/actions/popper';
   import portal from '@mathesar-component-library-dir/common/actions/portal';
   import { PreparedMenu } from '@mathesar-component-library-dir/prepared-menu';
 
-  import { AccompanyingElements } from '../dropdown';
-
   import type { ContextMenuController } from './ContextMenuController';
-
-  const accompanyingElements = new AccompanyingElements();
-  setContext('dropdownAccompanyingElements', accompanyingElements);
 
   export let controller: ContextMenuController;
 
   $: props = $controller;
+
+  function handleClickBackdrop() {
+    controller.close();
+  }
 </script>
 
 {#if props}
+  <div class="context-menu-backdrop" on:click={handleClickBackdrop} />
   <div
     class="context-menu dropdown content"
-    use:clickOffBounds={{
-      callback: () => controller.close(),
-      references: accompanyingElements,
-    }}
     use:popper={{ reference: getVirtualReferenceElement(props.position) }}
     use:portal
     on:click={() => controller.close()}
@@ -44,3 +37,11 @@
     />
   </div>
 {/if}
+
+<style>
+  .context-menu-backdrop {
+    position: fixed;
+    inset: 0;
+    z-index: var(--dropdown-z-index);
+  }
+</style>
