ef0cf9e173ca3a1d8f94f38df4a0e096178915d0
pavish <pavishkumar.r@gmail.com>
2025-10-06 15:55:29 +0400

    Add size limit for anonymously uploaded files

--- mathesar/views/download_link.py
+++ mathesar/views/download_link.py
@@ -1,4 +1,7 @@
+import html
+from django.conf import settings
 from django.http import HttpResponse, StreamingHttpResponse, JsonResponse
+from django.template.defaultfilters import filesizeformat
 from django.views.decorators.http import require_http_methods
 from functools import wraps
 from mathesar.utils.forms import get_col_info_for_field
@@ -90,4 +93,24 @@ def load_file_thumbnail(request, download_link_mash):
 @require_log_in_or_access_via_shared_form(response_type="json")
 @require_http_methods(["POST"])
 def upload_file(request):
+    uploaded_file = request.FILES.get("file")
+    if not uploaded_file:
+        return JsonResponse({"detail": "No file uploaded"}, status=400)
+
+    user = getattr(request, "user", None)
+    is_authenticated_user = user and user.is_authenticated
+
+    if not is_authenticated_user and uploaded_file.size > settings.MATHESAR_PUBLIC_FILE_UPLOAD_MAX_SIZE:
+        human_readable_size = _get_human_readable_file_size(settings.MATHESAR_PUBLIC_FILE_UPLOAD_MAX_SIZE)
+        return JsonResponse(
+            {"detail": f"Max allowed file size is {human_readable_size}"},
+            status=400
+        )
+
     return JsonResponse(save_file(request.FILES["file"], request))
+
+
+def _get_human_readable_file_size(size):
+    # Django's default `filesizeformat` returns html (- the &nbsp; character)
+    human_readable_size = html.unescape(filesizeformat(size))
+    return human_readable_size.replace("\xa0", " ")
