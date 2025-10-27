884f6a7f91da62eea61cc4a16c78d23a27b9c781
Sean Colsen <colsen.sean@gmail.com>
2025-10-27 09:41:28 -0400

    Remove outdated comments in test

--- mathesar_ui/src/component-library/formatted-input/__tests__/formattedInputUtils.test.ts
+++ mathesar_ui/src/component-library/formatted-input/__tests__/formattedInputUtils.test.ts
@@ -2,13 +2,10 @@ import { getCursorPositionAfterReformat } from '../formattedInputUtils';
 
 test.each(
   // prettier-ignore
-  // // Same cases are commented out because they're not yet handled.
-  // // See https://github.com/centerofci/mathesar/issues/1284
   [ // oldText       oldCursorPosition  newText        newCursorPosition
     [  ''          , 0                , ''           , 0 ],
     [  '123'       , 1                , '123'        , 1 ],
     [  '1234'      , 4                , '1,234'      , 5 ],
-    // [  '1234'      , 1                , '1,234'      , 2 ],
     [  '.12,345'   , 1                , '0.12345'    , 2 ],
     [  '1,24'      , 3                , '124'        , 2 ],
     [  '1,23'      , 4                , '123'        , 3 ],
@@ -16,7 +13,6 @@ test.each(
     [  '1,000,00'  , 7                , '100,000'    , 6 ],
     [  '2a3'       , 2                , '23'         , 1 ],
     [  '2aaaa3'    , 5                , '23'         , 1 ],
-    // [  '23'        , 2                , '23'         , 1 ],
     [  '-23'       , 1                , '23'         , 0 ],
   ],
 )(
