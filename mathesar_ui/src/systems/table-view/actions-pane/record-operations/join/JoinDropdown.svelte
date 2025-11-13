b635d242d63c0f7f90458bc6612a1acc566aa911
Sean Colsen <colsen.sean@gmail.com>
2025-11-13 13:40:57 -0500

    Add Join button to table page actions pane
    Cursor prompts:
    
    The table view has an actions pane, implemented in @ActionsPane.svelte.
    It has dropdowns for "Filter", "Sort", and "Group". Add a new dropdown
    button, after "Group". The button label should be "Join" (and should be
    translated through the i18n system). The icon should be the same link
    icon used for a foreign key column. When the dropdown opens, it should
    display the text "TODO" (untranslated).
    
    ---
    
    Get rid of Join.svelte. Just inline this content for now.
    
    ---
    
    Add an entry for the string 'join' in the i18n system. Just add it to
    the English language file.

---
+++ mathesar_ui/src/systems/table-view/actions-pane/record-operations/join/JoinDropdown.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import type { ComponentProps } from 'svelte';
+  import { _ } from 'svelte-i18n';
+
+  import { iconTableLink } from '@mathesar/icons';
+  import { Dropdown, Icon } from '@mathesar-component-library';
+
+  type $$Props = ComponentProps<Dropdown>;
+</script>
+
+<Dropdown
+  showArrow={false}
+  triggerAppearance="secondary"
+  {...$$restProps}
+  ariaLabel={$_('join')}
+>
+  <svelte:fragment slot="trigger">
+    <Icon {...iconTableLink} />
+    <span class="responsive-button-label">
+      {$_('join')}
+    </span>
+  </svelte:fragment>
+  <div slot="content">TODO</div>
+</Dropdown>
+
+<style lang="scss">
+  .responsive-button-label {
+    display: inline-flex;
+    align-items: center;
+  }
+</style>
