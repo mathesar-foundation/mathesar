a22d7d232d913ac59c2833287408e0f8a5478cdd
Sean Colsen <colsen.sean@gmail.com>
2025-11-13 20:49:52 -0500

    Render table name with icon

--- mathesar_ui/src/systems/table-view/actions-pane/record-operations/join/JoinConfig.svelte
+++ mathesar_ui/src/systems/table-view/actions-pane/record-operations/join/JoinConfig.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-  import { CheckboxGroup } from '@mathesar-component-library';
+  import { CheckboxGroup, component } from '@mathesar-component-library';
 
   import type { SimpleManyToManyRelationship } from './joinConfigUtils';
+  import TableName from '@mathesar/components/TableName.svelte';
 
   export let simpleManyToManyRelationships: SimpleManyToManyRelationship[];
 </script>
 
 <CheckboxGroup
   options={simpleManyToManyRelationships}
-  getCheckboxLabel={(r) => r.targetTable.name}
+  getCheckboxLabel={(r) =>
+    component(TableName, { table: { name: r.targetTable.name } })}
 />
