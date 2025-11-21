ff0a19929ae6fb2755a4bb8f2165064515e4943e
tharik <mohammed.tharik.s.138@kalvium.community>
2025-11-21 22:37:51 +0530

    Updated button.scss to define separate control and input variants

--- mathesar_ui/src/component-library/select/Select.svelte
+++ mathesar_ui/src/component-library/select/Select.svelte
@@ -63,7 +63,7 @@
   /**
    * Appearance of the trigger button. One of: 'default', 'primary', 'secondary', 'plain', 'ghost'.
    */
-  export let triggerAppearance: DefinedProps['triggerAppearance'] = 'default';
+  export let triggerAppearance: DefinedProps['triggerAppearance'] = 'input';
 
   /**
    * The ARIA label for this select component.
