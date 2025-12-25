9dfd43a13b1217814f23364eb5906ef1cb39d092
Shaheem Niyaz <shaheembn@gmail.com>
2025-12-25 22:56:27 +0530

    Send null for empty strings in URI columns
    Introduces a new common formatter for "special strings" like URIs that
    replace empty strings with null values and trims the string for
    formatting.
    
    To make the existing UriCell component actually accept the formatter,
    the generic TextInput child component was replaced with the
    FormattedInput component, similar to the FormattedInputCell component.

--- mathesar_ui/src/components/cell-fabric/data-types/components/uri/UriCell.svelte
+++ mathesar_ui/src/components/cell-fabric/data-types/components/uri/UriCell.svelte
@@ -6,11 +6,12 @@
   } from '@mathesar-component-library';
 
   import SteppedInputCell from '../SteppedInputCell.svelte';
-  import type { CellTypeProps } from '../typeDefinitions';
+  import type { FormattedInputCellProps } from '../typeDefinitions';
 
   import UriCellContent from './UriCellContent.svelte';
+  import FormattedInput from '@mathesar/component-library/formatted-input/FormattedInput.svelte';
 
-  type $$Props = CellTypeProps<string>;
+  type $$Props = FormattedInputCellProps;
 
   export let isActive: $$Props['isActive'];
   export let value: $$Props['value'] = undefined;
@@ -19,6 +20,8 @@
   export let searchValue: $$Props['searchValue'] = undefined;
   export let isIndependentOfSheet: $$Props['isIndependentOfSheet'];
   export let showTruncationPopover: $$Props['showTruncationPopover'] = false;
+  export let formatter: $$Props['formatter'];
+  export let formatForDisplay: $$Props['formatForDisplay'];
 </script>
 
 <SteppedInputCell
@@ -31,6 +34,7 @@
   {showTruncationPopover}
   let:handleInputBlur
   let:setValueInEditMode
+  formatValue={formatForDisplay}
   on:movementKeyDown
   on:mouseenter
 >
@@ -45,11 +49,13 @@
       </UriCellContent>
     </CellValue>
   </span>
-  <TextInput
+  <FormattedInput
     focusOnMount={true}
+    {...$$restProps}
     {disabled}
     {value}
-    onValueChange={setValueInEditMode}
+    on:artificialInput={({ detail }) => setValueInEditMode(detail)}
+    {formatter}
     on:blur={handleInputBlur}
   />
 </SteppedInputCell>
--- mathesar_ui/src/components/cell-fabric/data-types/uri.ts
+++ mathesar_ui/src/components/cell-fabric/data-types/uri.ts
@@ -1,14 +1,38 @@
-import { TextInput } from '@mathesar-component-library';
+import {
+  FormattedInput,
+  isDefinedNonNullable,
+} from '@mathesar-component-library';
 import type { ComponentAndProps } from '@mathesar-component-library/types';
 
 import UriCell from './components/uri/UriCell.svelte';
 import type { CellComponentFactory } from './typeDefinitions';
+import { SpecialStringFormatter } from './utils';
+
+function getProps() {
+  const formatter = SpecialStringFormatter;
+  return {
+    formatter,
+    formatForDisplay: (
+      v: string | null | undefined,
+    ): string | null | undefined => {
+      if (!isDefinedNonNullable(v)) {
+        return v;
+      }
+      return formatter.format(v);
+    },
+  };
+}
 
 const uriType: CellComponentFactory = {
   initialInputValue: '',
-  get: (): ComponentAndProps => ({ component: UriCell }),
-  getInput: (): ComponentAndProps => ({ component: TextInput }),
-  getDisplayFormatter: () => String,
+  get: (): ComponentAndProps => ({ component: UriCell, props: getProps() }),
+  getInput: (): ComponentAndProps => ({
+    component: FormattedInput,
+    props: getProps(),
+  }),
+  getDisplayFormatter() {
+    return (v) => getProps().formatForDisplay(String(v));
+  },
 };
 
 export default uriType;
