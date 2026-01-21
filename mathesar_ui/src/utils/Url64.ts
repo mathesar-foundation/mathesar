e0ba2409ae77e3b8508b694696c8ba6348d2c1f9
Kamala Kiruthi <kamala.kiruthi.s.139@kalvium.community>
2026-01-21 11:48:41 +0530

    Replace code points with char codes

--- mathesar_ui/src/utils/Url64.ts
+++ mathesar_ui/src/utils/Url64.ts
@@ -9,7 +9,7 @@ export default class Url64 {
   static encode(s: string): string {
     const bytes = new TextEncoder().encode(s);
     const binString = Array.from(bytes, (byte) =>
-      String.fromCodePoint(byte),
+      String.fromCharCode(byte),
     ).join('');
     const base64 = btoa(binString);
 
@@ -20,7 +20,7 @@ export default class Url64 {
     const base64 = s.replace(/-/g, '+').replace(/_/g, '/');
 
     const binString = atob(base64);
-    const bytes = Uint8Array.from(binString, (m) => m.codePointAt(0));
+    const bytes = Uint8Array.from(binString, (m) => m.charCodeAt(0));
     return new TextDecoder().decode(bytes);
   }
 }
