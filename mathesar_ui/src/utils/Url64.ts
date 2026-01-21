9c2a1052395a88626d7fc7b024f673fb5c0c8fed
Shaheem Niyaz <shaheembn@gmail.com>
2026-01-21 11:00:32 +0530

    Replace deprecated functions with TextEncoder

--- mathesar_ui/src/utils/Url64.ts
+++ mathesar_ui/src/utils/Url64.ts
@@ -4,23 +4,23 @@
  * The resulting string won't have any characters which require URL encoding. We
  * change `+` to `-` and `/` to `_`. We remove the padding characters `=`
  * altogether because atob only needs them when inputs are concatenated.
- *
- * TODO:
- *
- * - Refactor this code to remove use of deprecated function `escape` and
- *   `unescape`.
- * - See: https://developer.mozilla.org/en-US/docs/Glossary/Base64
- * - Consider using a 3rd party library. Maybe `base64-js`, `base64url` or
- *   `utf8`.
  */
 export default class Url64 {
   static encode(s: string): string {
-    const base64 = btoa(unescape(encodeURIComponent(s)));
+    const bytes = new TextEncoder().encode(s);
+    const binString = Array.from(bytes, (byte) =>
+      String.fromCodePoint(byte),
+    ).join('');
+    const base64 = btoa(binString);
+
     return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
   }
 
   static decode(s: string): string {
     const base64 = s.replace(/-/g, '+').replace(/_/g, '/');
-    return decodeURIComponent(escape(atob(base64)));
+
+    const binString = atob(base64);
+    const bytes = Uint8Array.from(binString, (m) => m.codePointAt(0));
+    return new TextDecoder().decode(bytes);
   }
 }
