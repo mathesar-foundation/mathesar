df992808294209c7abd9306cd90ec03982f82a19
pavish <pavishkumar.r@gmail.com>
2026-05-14 22:05:30 +0400

    Remove superfluous simple input component

--- mathesar_ui/src/components/cell-fabric/data-types/user.ts
+++ mathesar_ui/src/components/cell-fabric/data-types/user.ts
@@ -3,7 +3,6 @@ import type { RawColumnWithMetadata } from '@mathesar/api/rpc/columns';
 import type { ComponentAndProps } from '@mathesar-component-library/types';
 
 import UserCell from './components/user/UserCell.svelte';
-import UserFilterInput from './components/user/UserFilterInput.svelte';
 import UserInput from './components/user/UserInput.svelte';
 import type { CellComponentFactory } from './typeDefinitions';
 
@@ -19,14 +18,6 @@ const userType: CellComponentFactory = {
   }),
   getInput: (column: RawColumnWithMetadata): ComponentAndProps => ({
     component: UserInput,
-    props: {
-      userDisplayField:
-        getMetadataValue(column.metadata ?? {}, 'user_display_field') ??
-        'username',
-    },
-  }),
-  getSimpleInput: (column: RawColumnWithMetadata): ComponentAndProps => ({
-    component: UserFilterInput,
     props: {
       userDisplayField:
         getMetadataValue(column.metadata ?? {}, 'user_display_field') ??
